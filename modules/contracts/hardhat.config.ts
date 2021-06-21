5ae7324c3e6a4241c1c09a18312e6340e0de40e0
LayneHaber <layne.haber@gmail.com>
2021-06-21 15:18:01 -0600

    Contracts compiling

--- modules/contracts/hardhat.config.ts
+++ modules/contracts/hardhat.config.ts
@@ -1,11 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-
 import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+
+import { HardhatUserConfig } from "hardhat/types";
 
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.4",
   },
+  paths: {
+    artifacts: "./artifacts",
+    sources: "./contracts",
+  },
+  defaultNetwork: "hardhat",
 };
 
 export default config;
