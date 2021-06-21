c594ec81c171e5e51899d7ca927b350a9cbf3f83
Rahul Sethuram <rahul@sethuram.com>
2021-06-21 22:40:22 +0300

    Add onready placeholder

--- modules/router/src/index.ts
+++ modules/router/src/index.ts
@@ -6,6 +6,10 @@ server.get("/ping", async () => {
   return "pong\n";
 });
 
+server.addHook("onReady", async function() {
+  // initialize all internal classes here
+});
+
 server.listen(8080, (err, address) => {
   if (err) {
     console.error(err);
