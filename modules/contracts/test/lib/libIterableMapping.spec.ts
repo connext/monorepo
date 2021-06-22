5f8c63fecdc0e6fd39c66cffae98d5f70c3dbc0f
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-22 21:00:57 +0530

    update: libIterableMapping tests

--- modules/contracts/test/lib/libIterableMapping.spec.ts
+++ modules/contracts/test/lib/libIterableMapping.spec.ts
@@ -5,12 +5,12 @@ import { solidity } from "ethereum-waffle";
 
 use(solidity);
 
-// import { AddressZero } from "@ethersproject/constants";
+import { formatBytes32String } from "@ethersproject/strings";
 
 // import types
 import { LibIterableMappingTest } from "../../typechain/LibIterableMappingTest";
 
-describe("LibErc20", function() {
+describe.only("libIterableMapping", function() {
   let libIterableMappingTest: LibIterableMappingTest;
 
   const fixture = async () => {
@@ -25,4 +25,53 @@ describe("LibErc20", function() {
     console.log("Address", libIterableMappingTest.address);
     expect(libIterableMappingTest.address).to.be.a("string");
   });
+
+  describe("#digestEqual", () => {
+    it.skip("should error if params are wrong", async () => {});
+    it.skip("should error if asset is not contract", async () => {});
+    it("happy case: should return false", async () => {
+      const res = await libIterableMappingTest.digestEqual(formatBytes32String("0"), formatBytes32String("1"));
+      console.log(res);
+    });
+
+    it("happy case: should return true", async () => {
+      const res = await libIterableMappingTest.digestEqual(formatBytes32String("0"), formatBytes32String("0"));
+      console.log(res);
+    });
+  });
+
+  describe("#isEmptyString", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  describe("#digestExists", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  describe("#length", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  describe("#getTransactionByDigest", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  describe("#getTransactionByIndex", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  describe("#getTransactions", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  
+  describe("#addTransaction", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  describe("#removeTransaction", () => {
+    it.skip("should do something right", async () => {});
+  });
+
+  
 });
