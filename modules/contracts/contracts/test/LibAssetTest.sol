358e5301c4021fca865b4b944f8e8f083acec853
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-22 16:14:00 +0530

    add: lib test contracts

---
+++ modules/contracts/contracts/test/LibAssetTest.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
+
+import "../lib/LibAsset.sol";
+
+/// @title LibAssetTest
+/// @author Connext
+/// @notice Used to easily test the internal methods of
+///         LibAsset.sol by aliasing them to public
+///         methods.
+
+contract LibAssetTest {
+  function isEther(address assetId) internal pure returns (bool) {
+    return LibAsset.isEther(assetId);
+  }
+
+  function getOwnBalance(address assetId) internal view returns (uint256) {
+    return LibAsset.getOwnBalance(assetId);
+  }
+
+  function transferEther(address payable recipient, uint256 amount) internal returns (bool) {
+    return LibAsset.transferEther(recipient, amount);
+  }
+
+  function transferERC20(
+    address assetId,
+    address recipient,
+    uint256 amount
+  ) internal returns (bool) {
+    return LibAsset.transferERC20(assetId, recipient, amount);
+  }
+
+  // This function is a wrapper for transfers of Ether or ERC20 tokens,
+  // both standard-compliant ones as well as tokens that exhibit the
+  // missing-return-value bug.
+  function transferAsset(
+    address assetId,
+    address payable recipient,
+    uint256 amount
+  ) internal returns (bool) {
+    return LibAsset.transferAsset(assetId, recipient, amount);
+  }
+}
