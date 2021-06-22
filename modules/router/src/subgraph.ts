50f7ba55f52187fc62ef0be740a5c8a77db25376
Rahul Sethuram <rahul@sethuram.com>
2021-06-22 18:55:54 +0300

    Fixes

--- modules/router/src/subgraph.ts
+++ modules/router/src/subgraph.ts
@@ -1,1 +1,16 @@
-export class Subgraph {}
+import { ApolloClient, InMemoryCache, gql, NormalizedCacheObject } from "@apollo/client";
+
+interface TransactionManager {
+  onSenderPrepare(handler: () => void): void;
+  onSenderPrepare(handler: () => void): void;
+}
+
+export class SubgraphTransactionManager implements TransactionManager {
+  private client: ApolloClient<NormalizedCacheObject>;
+  constructor(subgraphUrl: string) {
+    this.client = new ApolloClient({
+      uri: subgraphUrl,
+      cache: new InMemoryCache(),
+    });
+  }
+}
