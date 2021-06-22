b1479d705fc4761d2fe16562f92313010079e577
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-22 21:32:09 +0530

    update LibIterableMapping tests

--- modules/contracts/test/lib/libIterableMapping.spec.ts
+++ modules/contracts/test/lib/libIterableMapping.spec.ts
@@ -17,6 +17,15 @@ describe.only("libIterableMapping", function() {
     const libIterableMappingTestFactory = await ethers.getContractFactory("LibIterableMappingTest");
     return (await libIterableMappingTestFactory.deploy()) as LibIterableMappingTest;
   };
+
+  const createUnsignedTransactionDataMock = (params: { amount?: number; expiry?: number; digest?: string }) => {
+    return {
+      amount: params.amount ?? 1,
+      expiry: params.expiry ?? 10,
+      digest: params.digest ?? formatBytes32String("a"),
+    };
+  };
+
   beforeEach(async function() {
     libIterableMappingTest = await waffle.loadFixture(fixture);
   });
@@ -28,7 +37,6 @@ describe.only("libIterableMapping", function() {
 
   describe("#digestEqual", () => {
     it.skip("should error if params are wrong", async () => {});
-    it.skip("should error if asset is not contract", async () => {});
     it("happy case: should return false", async () => {
       const res = await libIterableMappingTest.digestEqual(formatBytes32String("0"), formatBytes32String("1"));
       console.log(res);
@@ -41,37 +49,102 @@ describe.only("libIterableMapping", function() {
   });
 
   describe("#isEmptyString", () => {
-    it.skip("should do something right", async () => {});
+    it("happy case: should return false", async () => {
+      const res = await libIterableMappingTest.isEmptyString(formatBytes32String("1"));
+      console.log(res);
+    });
+
+    it("happy case: should return true", async () => {
+      const res = await libIterableMappingTest.isEmptyString(formatBytes32String("0"));
+      console.log(res);
+    });
   });
 
   describe("#digestExists", () => {
-    it.skip("should do something right", async () => {});
+    it("happy case: should return false", async () => {
+      const res = await libIterableMappingTest.digestExists(formatBytes32String("1"));
+      console.log(res);
+    });
+
+    it("happy case: should return true", async () => {
+      const res = await libIterableMappingTest.digestExists(formatBytes32String("1"));
+      console.log(res);
+    });
   });
 
   describe("#length", () => {
-    it.skip("should do something right", async () => {});
+    it("happy case: length", async () => {
+      const res = await libIterableMappingTest.length();
+      console.log(res);
+    });
   });
 
   describe("#getTransactionByDigest", () => {
-    it.skip("should do something right", async () => {});
+    it.skip("happy case: getTransactionByDigest", async () => {
+      const res = await libIterableMappingTest.getTransactionByDigest(formatBytes32String("1"));
+      console.log(res);
+    });
   });
 
   describe("#getTransactionByIndex", () => {
-    it.skip("should do something right", async () => {});
+    it.skip("happy case: getTransactionByIndex", async () => {
+      const res = await libIterableMappingTest.getTransactionByIndex("1");
+      console.log(res);
+    });
   });
 
   describe("#getTransactions", () => {
-    it.skip("should do something right", async () => {});
+    it.skip("happy case: getTransactions", async () => {
+      const res = await libIterableMappingTest.getTransactions();
+      console.log(res);
+    });
   });
 
-  
   describe("#addTransaction", () => {
-    it.skip("should do something right", async () => {});
+    it("should revert if empty string", async () => {
+      const mockTestParam = createUnsignedTransactionDataMock({ digest: formatBytes32String("0") });
+      await expect(libIterableMappingTest.addTransaction(mockTestParam)).to.be.revertedWith(
+        "LibIterableMapping: EMPTY_DIGEST",
+      );
+    });
+
+    it("should revert if digest already exist", async () => {
+      const mockTestParam = createUnsignedTransactionDataMock({});
+      const res = await libIterableMappingTest.addTransaction(mockTestParam);
+      console.log(res);
+
+      await expect(libIterableMappingTest.addTransaction(mockTestParam)).to.be.revertedWith(
+        "LibIterableMapping: DIGEST_ALREADY_ADDED",
+      );
+    });
+
+    it("happy case: addTransaction", async () => {
+      const mockTestParam = createUnsignedTransactionDataMock({});
+      const res = await libIterableMappingTest.addTransaction(mockTestParam);
+      console.log(res);
+    });
   });
 
   describe("#removeTransaction", () => {
-    it.skip("should do something right", async () => {});
-  });
+    it("should revert if empty string", async () => {
+      await expect(libIterableMappingTest.removeTransaction(formatBytes32String("0"))).to.be.revertedWith(
+        "LibIterableMapping: EMPTY_DIGEST",
+      );
+    });
 
-  
+    it("should revert if digest don't exist string", async () => {
+      await expect(libIterableMappingTest.removeTransaction(formatBytes32String("1"))).to.be.revertedWith(
+        "LibIterableMapping: DIGEST_NOT_FOUND",
+      );
+    });
+
+    it("happy case: removeTransaction", async () => {
+      const mockTestParam = createUnsignedTransactionDataMock({});
+      const resAddTransaction = await libIterableMappingTest.addTransaction(mockTestParam);
+      console.log(resAddTransaction);
+
+      const res = await libIterableMappingTest.removeTransaction(formatBytes32String("a"));
+      console.log(res);
+    });
+  });
 });
