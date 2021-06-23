0724482eacbb1eb033dd4a3a39fb7ae9714a188e
Rahul Sethuram <rahul@sethuram.com>
2021-06-23 18:34:54 +0300

    Config fixes

--- modules/router/src/config.ts
+++ modules/router/src/config.ts
@@ -1,12 +1,10 @@
 ///NXTP Config Generator based on vector/modules/router/src/config.ts
-///Differences: removed the
 import { Type, Static } from "@sinclair/typebox";
 import { readFileSync } from "fs";
 import Ajv from "ajv";
 
 const ajv = new Ajv();
 
-//todo:why was I getting an error importing the same exact Types from @connext/vector-types?; possible sinclair version mismatch?
 // String pattern types
 export const TAddress = Type.RegEx(/^0x[a-fA-F0-9]{40}$/);
 export const TIntegerString = Type.RegEx(/^([0-9])*$/);
@@ -15,11 +13,15 @@ export const TUrl = Type.String({ format: "uri" });
 export const TChainId = Type.Number({ minimum: 1 });
 export const TDecimalString = Type.RegEx(/^[0-9]*\.?[0-9]*$/);
 
+export const TChainConfig = Type.Object({
+  provider: Type.Array(TUrl),
+  confirmations: Type.Number({ minimum: 1 }),
+  subgraph: TUrl,
+});
+
 const NxtpRouterConfigSchema = Type.Object({
   adminToken: Type.String(),
-  nodeUrl: TUrl,
-  chainProviders: Type.Dict(TUrl),
-  routerUrl: TUrl,
+  chainConfig: Type.Dict(TChainConfig),
   logLevel: Type.Optional(
     Type.Union([
       Type.Literal("fatal"),
@@ -31,11 +33,9 @@ const NxtpRouterConfigSchema = Type.Object({
       Type.Literal("silent"),
     ]),
   ),
-  messagingUrl: Type.Optional(TUrl),
-  natsUrl: Type.Optional(Type.String()),
-  authUrl: Type.Optional(TUrl),
-  // rebalanceProfiles: Type.Array(RebalanceProfileSchema),
-  mnemonic: Type.Optional(Type.String()),
+  natsUrl: Type.String(),
+  authUrl: TUrl,
+  mnemonic: Type.String(),
 });
 
 export type NxtpRouterConfig = Static<typeof NxtpRouterConfigSchema>;
@@ -73,17 +73,14 @@ export const getEnvConfig = (): NxtpRouterConfig => {
 
   const nxtpConfig: NxtpRouterConfig = {
     mnemonic: process.env.NXTP_MNEMONIC || configJson.mnemonic || configFile.mnemonic,
-    messagingUrl: process.env.NXTP_MESSAGING_URL || configJson.messagingUrl || configFile.messagingUrl,
     authUrl: process.env.NXTP_AUTH_URL || configJson.authUrl || configFile.authUrl,
     natsUrl: process.env.NXTP_NATS_URL || configJson.natsUrl || configFile.natsUrl,
     adminToken: process.env.NXTP_ADMIN_TOKEN || configJson.adminToken || configFile.adminToken,
-    chainProviders: process.env.NXTP_CHAIN_PROVIDERS
+    chainConfig: process.env.NXTP_CHAIN_PROVIDERS
       ? JSON.parse(process.env.NXTP_CHAIN_PROVIDERS)
-      : configJson.chainProviders
-      ? configJson.chainProviders
-      : configFile.chainProviders,
-    nodeUrl: process.env.NXTP_NODE_URL || configJson.nodeUrl || configFile.nodeUrl || "http://node:8000",
-    routerUrl: process.env.NXTP_ROUTER_URL || configJson.routerUrl || configFile.routerUrl || "http://router:8000",
+      : configJson.chainConfig
+      ? configJson.chainConfig
+      : configFile.chainConfig,
     logLevel: process.env.NXTP_FEE_LOG_LEVEL || configJson.logLevel || configFile.logLevel,
   };
   return nxtpConfig;
@@ -93,10 +90,6 @@ const nxtpConfig: NxtpRouterConfig = getEnvConfig();
 const validate = ajv.compile(NxtpRouterConfigSchema);
 
 const valid = validate(nxtpConfig);
-
-if (!nxtpConfig.adminToken && !nxtpConfig.messagingUrl && !nxtpConfig.natsUrl) {
-  nxtpConfig.messagingUrl = "http://messaging";
-}
 
 if (!valid) {
   console.error(`Invalid config: ${JSON.stringify(nxtpConfig, null, 2)}`);
