ab6fabf8926d4dfc02714ad8bb289d44bdc496e2
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-23 21:06:43 +0530

    move encode from router to utils

--- modules/router/src/handler.ts
+++ modules/router/src/handler.ts
@@ -1,8 +1,6 @@
-import { NxtpMessaging } from "@connext/nxtp-utils";
+import { NxtpMessaging, encodeTxData } from "@connext/nxtp-utils";
 import { Signer } from "ethers";
 import { BaseLogger } from "pino";
-import { Type, Static } from "@sinclair/typebox";
-import { defaultAbiCoder } from "ethers/lib/utils";
 
 import {
   ReceiverFulfillData,
@@ -11,45 +9,6 @@ import {
   SenderPrepareData,
   TransactionManagerListener,
 } from "./transactionManagerListener";
-
-export const tidy = (str: string): string => `${str.replace(/\n/g, "").replace(/ +/g, " ")}`;
-
-//todo: these types exist in config also need nxtp-types package
-export const TAddress = Type.RegEx(/^0x[a-fA-F0-9]{40}$/);
-
-export const TransactionDataParamsEncoding = tidy(`tuple(
-  address user,
-  address router,
-  address sendingAssetId,
-  address receivingAssetId,
-  address receivingAddress,
-  bytes callData,
-  bytes32 transactionId,
-  uint24 sendingChainId,
-  uint24 receivingChainId,
-  uint256 amount,
-  uint256 expiry,
-  uint256 blockNumber
-`);
-
-export interface TransactionDataParams {
-  user: Static<typeof TAddress>;
-  router: Static<typeof TAddress>;
-  sendingAssetId: Static<typeof TAddress>;
-  receivingAssetId: Static<typeof TAddress>;
-  receivingAddress: Static<typeof TAddress>;
-  callData: string;
-  transactionId: string;
-  sendingChainId: number;
-  receivingChainId: number;
-  amount: string;
-  expiry: number;
-  blockNumber: number;
-}
-
-const encodeTxData = (txDataParams: TransactionDataParams): string => {
-  return defaultAbiCoder.encode([TransactionDataParamsEncoding], [txDataParams]);
-};
 
 /*
     Handler.ts
---
+++ modules/utils/src/encode.ts
@@ -0,0 +1,42 @@
+import { Static, Type } from '@sinclair/typebox'
+import { defaultAbiCoder } from "ethers/lib/utils";
+import { BigNumberish } from "ethers";
+
+export const tidy = (str: string): string => `${str.replace(/\n/g, "").replace(/ +/g, " ")}`;
+
+//todo: these types exist in config also need nxtp-types package
+export const TAddress = Type.RegEx(/^0x[a-fA-F0-9]{40}$/);
+
+export const TransactionDataParamsEncoding = tidy(`tuple(
+  address user,
+  address router,
+  address sendingAssetId,
+  address receivingAssetId,
+  address receivingAddress,
+  bytes callData,
+  bytes32 transactionId,
+  uint24 sendingChainId,
+  uint24 receivingChainId,
+  uint256 amount,
+  uint256 expiry,
+  uint256 blockNumber
+`);
+
+export interface TransactionDataParams {
+  user: Static<typeof TAddress>;
+  router: Static<typeof TAddress>;
+  sendingAssetId: Static<typeof TAddress>;
+  receivingAssetId: Static<typeof TAddress>;
+  receivingAddress: Static<typeof TAddress>;
+  callData: string;
+  transactionId: string;
+  sendingChainId: BigNumberish;
+  receivingChainId: BigNumberish;
+  amount: BigNumberish;
+  expiry: BigNumberish;
+  blockNumber: BigNumberish;
+}
+
+export const encodeTxData = (txDataParams: TransactionDataParams): string => {
+  return defaultAbiCoder.encode([TransactionDataParamsEncoding], [txDataParams]);
+};
