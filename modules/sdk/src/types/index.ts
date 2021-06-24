c5cc6ac46c46b67f00d99f525bb139ec868c1d70
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-24 20:30:20 +0530

    new: types for crosschaintransfer

--- modules/sdk/src/types/index.ts
+++ modules/sdk/src/types/index.ts
@@ -1,7 +1,30 @@
 import { Web3Provider } from "@ethersproject/providers";
-import { BigNumberish } from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
 
 export type PrepareParamType = {
+  userWebProvider: Web3Provider;
+  routerAddress: string;
+  sendingChainId: number;
+  receivingChainId: number;
+  sendingAssetId: string;
+  receivingAssetId: string;
+  receivingAddress: string;
+  amount: BigNumberish;
+  expiry: string;
+  callData?: string;
+};
+
+export type listenRouterPrepareParamType = {
+  userWebProvider: Web3Provider;
+  receivingChainId: number;
+  relayerFee: BigNumberish;
+};
+
+export type listenRouterFulfillParamType = {
+  receivingChainId: number;
+};
+
+export type CrossChainParamType = {
   userWebProvider: Web3Provider;
   routerAddress: string;
   sendingChainId: number;
