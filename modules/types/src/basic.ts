27264e5bf57a3e0327cf48ee9513b6ea9bcc199f
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-24 15:06:21 +0530

    new: module types

--- modules/types/src/basic.ts
+++ modules/types/src/basic.ts
@@ -1,3 +1,13 @@
+import { Type } from "@sinclair/typebox";
+
 // strings aliases: these function more as documentation for devs than checked types
 export type Address = string; // aka HexString of length 42
 export type HexString = string; // eg "0xabc123" of arbitrary length
+
+// String pattern types
+export const TAddress = Type.RegEx(/^0x[a-fA-F0-9]{40}$/);
+export const TIntegerString = Type.RegEx(/^([0-9])*$/);
+export const TUrl = Type.String({ format: "uri" });
+// Convenience types
+export const TChainId = Type.Number({ minimum: 1 });
+export const TDecimalString = Type.RegEx(/^[0-9]*\.?[0-9]*$/);
--- modules/types/src/index.ts
+++ modules/types/src/index.ts
@@ -1,3 +1,2 @@
 export * from "./error";
-export * from "./chain";
 export * from "./basic";
