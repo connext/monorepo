8458c407e0ff782ba14cc56b5a0e3b3bd576d37b
Rahul Sethuram <rahul@sethuram.com>
2021-06-24 15:51:35 +0300

    Fianlly built

--- modules/router/src/index.ts
+++ modules/router/src/index.ts
@@ -1,13 +1,39 @@
+import { Wallet } from "ethers";
 import fastify from "fastify";
+import { NatsNxtpMessagingService } from "@connext/nxtp-utils";
+import TxService from "@connext/nxtp-txservice";
+import pino from "pino";
+
+import { getConfig } from "./config";
+import { Handler } from "./handler";
+import { SubgraphTransactionManagerListener } from "./transactionManagerListener";
+import { setupListeners } from "./listener";
 
 const server = fastify();
 
-server.get("/ping", async () => {
-  return "pong\n";
+const config = getConfig();
+const wallet = Wallet.fromMnemonic(config.mnemonic);
+const logger = pino({ name: wallet.address });
+const messaging = new NatsNxtpMessagingService({
+  signer: wallet,
+  authUrl: config.authUrl,
+  natsUrl: config.natsUrl,
 });
+const subgraphs: { [chainId: number]: string } = {};
+Object.entries(config.chainConfig).forEach(([chainId, config]) => {
+  // @ts-ignore
+  subgraphs[parseInt(chainId)] = config.subgraph;
+});
+const subgraph = new SubgraphTransactionManagerListener(subgraphs, wallet.address, logger);
+const txService = new TxService(logger, wallet);
+const handler = new Handler(messaging, subgraph, wallet, txService, logger);
 
 server.addHook("onReady", async function() {
-  // initialize all internal classes here
+  await setupListeners(messaging, subgraph, handler, logger);
+});
+
+server.get("/ping", async () => {
+  return "pong\n";
 });
 
 server.listen(8080, (err, address) => {
