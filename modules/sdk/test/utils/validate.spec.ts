2694d0b09053f666380fae9039dd4ae1ae96fbe3
Sanchay Mittal <sanchaymittal@gmail.com>
2021-06-25 13:13:11 +0530

    fix: validate test

--- modules/sdk/test/utils/validate.spec.ts
+++ modules/sdk/test/utils/validate.spec.ts
@@ -1,21 +1,24 @@
 import { validateAndParseAddress } from "../../src/utils";
+import { expect } from "chai";
 
 describe("#validateAndParseAddress", () => {
   it("returns same address if already checksummed", () => {
-    expect(validateAndParseAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")).toEqual(
+    expect(validateAndParseAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")).to.be.deep.eq(
       "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
     );
   });
 
   it("returns checksummed address if not checksummed", () => {
-    expect(validateAndParseAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f".toLowerCase())).toEqual(
+    expect(validateAndParseAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f".toLowerCase())).to.be.deep.eq(
       "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
     );
   });
 
   it("throws if not valid", () => {
-    expect(() => validateAndParseAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6")).toThrow(
-      "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6 is not a valid address.",
-    );
+    try {
+      validateAndParseAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6");
+    } catch (e) {
+      expect(e.message).to.be.deep.eq("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6 is not a valid address.");
+    }
   });
 });
