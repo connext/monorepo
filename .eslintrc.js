3d3bd0782e179882f45685c31539374e55307393
Rahul Sethuram <rahul@sethuram.com>
2021-06-30 16:10:32 +0300

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,6 +51,7 @@ module.exports = {
     "@typescript-eslint/no-unsafe-member-access": ["off"],
     "@typescript-eslint/no-unsafe-call": ["off"],
     "@typescript-eslint/no-unsafe-return": ["off"],
+    "@typescript-eslint/restrict-template-expressions": ["off"],
     "no-empty": ["off"],
     "require-await": "off",
     "@typescript-eslint/require-await": "off",
