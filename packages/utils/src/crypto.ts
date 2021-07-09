181967af88ee582987855cc2be5f25bb42e9a563
Sanchay Mittal <sanchaymittal@gmail.com>
2021-07-09 14:28:50 +0530

    export crypto func and cleanup

--- packages/utils/src/crypto.ts
+++ packages/utils/src/crypto.ts
@@ -8,29 +8,7 @@ import {
   utf8ToBuffer,
 } from "eccrypto-js";
 import { utils } from "ethers";
-
-////////////////////////////////////////
-// Validators
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getHexStringError = (value: any, length?: number): string | undefined => {
-  if (typeof value !== "string") {
-    return `Invalid hex string: ${value} is a ${typeof value}, expected a string`;
-  }
-  if (!value.startsWith("0x")) {
-    return `Invalid hex string: ${value} doesn't start with 0x`;
-  }
-  if (!utils.isHexString(value)) {
-    return `Invalid hex string: ${value}`;
-  }
-  if (length && utils.hexDataLength(value) !== length) {
-    return `Invalid hex string of length ${length}: ${value} is ${utils.hexDataLength(value)} bytes long`;
-  }
-  return undefined;
-};
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const isValidHexString = (value: any): boolean => !getHexStringError(value);
+import { isValidHexString } from "./hexStrings";
 
 export const bufferify = (input: Uint8Array | Buffer | string): Buffer =>
   typeof input === "string"
