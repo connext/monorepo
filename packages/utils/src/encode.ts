e382b84c5f180c7c1e77b4c695b331675c23fdf7
Rahul Sethuram <rahul@sethuram.com>
2021-07-16 18:21:48 +0300

    Lint

--- packages/utils/src/encode.ts
+++ packages/utils/src/encode.ts
@@ -1,6 +1,6 @@
 import { utils } from "ethers";
-import { AuctionBid } from "./messaging";
 
+import { AuctionBid } from "./messaging";
 import { InvariantTransactionData, VariantTransactionData } from "./transactionManager";
 
 export const tidy = (str: string): string => `${str.replace(/\n/g, "").replace(/ +/g, " ")}`;
