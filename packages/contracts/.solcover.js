fff63f397518866bd651a87f3de84b02bc32875c
LayneHaber <layne.haber@gmail.com>
2021-07-18 18:47:50 -0500

    Update coverage

--- packages/contracts/.solcover.js
+++ packages/contracts/.solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   skipFiles: [
     "./test/Counter.sol",
-    "./test/LibAssetTest.sol",
+    "./test/AssetTest.sol",
     "./test/TestERC20.sol",
     "./libraries/WadRayMath.sol",
     "./test/RevertableERC20.sol",
