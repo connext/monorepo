fba4b4bc6e62cf5f613635159c6a50de9841414f
LayneHaber <layne.haber@gmail.com>
2021-07-29 17:14:00 -0500

    Add local router

--- packages/contracts/deploy/deploy.ts
+++ packages/contracts/deploy/deploy.ts
@@ -5,6 +5,7 @@ const TEST_ROUTERS = [
   "0x9ADA6aa06eF36977569Dc5b38237809c7DF5082a", // live testnet router
   "0x0EC26F03e3dBA9bb5162D28fD5a3378A25f168d1", // rahul test router
   "0xDc150c5Db2cD1d1d8e505F824aBd90aEF887caC6", // ci/shared router
+  "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", // local router
 ];
 
 /**
