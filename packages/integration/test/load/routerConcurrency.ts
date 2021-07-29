a68074381d50b6b75a84330780de89a6b4fc0bda
LayneHaber <layne.haber@gmail.com>
2021-07-29 17:14:22 -0500

    Works with tokens

--- packages/integration/test/load/routerConcurrency.ts
+++ packages/integration/test/load/routerConcurrency.ts
@@ -3,6 +3,7 @@ import PriorityQueue from "p-queue";
 
 import { SdkManager } from "../utils/sdkManager";
 import { getConfig } from "../utils/config";
+import { getOnchainBalance } from "../utils/chain";
 
 const TIMEOUT = 15 * 60 * 100; // 15m in ms
 
@@ -15,15 +16,34 @@ const TIMEOUT = 15 * 60 * 100; // 15m in ms
 const routerConcurrencyTest = async (maxConcurrency: number, numberTransactions: number): Promise<void> => {
   const config = getConfig();
   const log = pino({ level: config.logLevel ?? "info" });
+
+  // Get transfer info from config
+  // TODO: randomize?
+  const sendingChainId = parseInt(Object.keys(config.chainConfig)[0]);
+  const receivingChainId = parseInt(Object.keys(config.chainConfig)[1]);
+  const swap = config.swapPools.find((swap) => {
+    // Must have sending and receiving chain
+    const chains = swap.assets.map((a) => a.chainId);
+    return chains.includes(sendingChainId) && chains.includes(receivingChainId);
+  });
+  if (!swap) {
+    throw new Error(`Could not find matching swap in config: ${config.swapPools}`);
+  }
+  const { assetId: sendingAssetId } = swap.assets.find((a) => a.chainId === sendingChainId)!;
+  const { assetId: receivingAssetId } = swap.assets.find((a) => a.chainId === receivingChainId)!;
+
   // Create manager
   const manager = await SdkManager.connect(
     config.chainConfig,
     config.mnemonic,
-    numberTransactions,
+    numberTransactions + 1,
     config.natsUrl,
     config.authUrl,
   );
   log.info({ agents: maxConcurrency }, "Created manager");
+
+  // Update with token
+  await manager.giftAgentsOnchain(sendingAssetId, sendingChainId);
 
   let concurrency = 0;
   let loopStats;
@@ -36,21 +56,6 @@ const routerConcurrencyTest = async (maxConcurrency: number, numberTransactions:
     // concurrency
     const queue = new PriorityQueue({ concurrency });
 
-    // Get transfer info from config
-    // TODO: randomize?
-    const sendingChainId = parseInt(Object.keys(config.chainConfig)[0]);
-    const receivingChainId = parseInt(Object.keys(config.chainConfig)[1]);
-    const swap = config.swapPools.find((swap) => {
-      // Must have sending and receiving chain
-      const chains = swap.assets.map((a) => a.chainId);
-      return chains.includes(sendingChainId) && chains.includes(receivingChainId);
-    });
-    if (!swap) {
-      throw new Error(`Could not find matching swap in config: ${config.swapPools}`);
-    }
-    const { assetId: sendingAssetId } = swap.assets.find((a) => a.chainId === sendingChainId)!;
-    const { assetId: receivingAssetId } = swap.assets.find((a) => a.chainId === receivingChainId)!;
-
     // Add payment tasks to the queue
     // Each payment should be made via some random agent
     // TODO: no single agent should be sending multiple txs
@@ -62,13 +67,23 @@ const routerConcurrencyTest = async (maxConcurrency: number, numberTransactions:
       .map((_) => {
         const task = async () => {
           const agent = manager.getRandomAgent();
+          const balance = await getOnchainBalance(
+            sendingAssetId,
+            agent.address,
+            config.chainConfig[sendingChainId].provider,
+          );
+          const amount = "100000";
+          if (balance.lt(amount)) {
+            throw new Error(`Agent has insufficient funds of ${sendingAssetId}`);
+          }
+          // TODO: has to work with tokens!
           const result = await manager.transfer(
             {
               sendingAssetId,
               sendingChainId,
               receivingAssetId,
               receivingChainId,
-              amount: "100000",
+              amount,
             },
             TIMEOUT,
             agent,
@@ -97,4 +112,4 @@ const routerConcurrencyTest = async (maxConcurrency: number, numberTransactions:
   process.exit(0);
 };
 
-routerConcurrencyTest(parseInt(process.env.CONCURRENCY ?? "15"), parseInt(process.env.NUM_TRANSACTIONS ?? "15"));
+routerConcurrencyTest(parseInt(process.env.CONCURRENCY ?? "10"), parseInt(process.env.NUM_TRANSACTIONS ?? "15"));
