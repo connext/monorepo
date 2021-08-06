2b8a49005400d2571699861267c8025125638dee
Sanchay Mittal <sanchaymittal@gmail.com>
2021-08-06 11:15:41 +0530

    fix: config test

--- packages/router/test/config.spec.ts
+++ packages/router/test/config.spec.ts
@@ -1,9 +1,21 @@
 import { expect } from "../../utils/dist";
+import { stub, restore, reset } from "sinon";
 import { getEnvConfig, getConfig } from "../src/config";
+import { fakeConfig } from "./utils";
 
 describe("Config", () => {
+  afterEach(() => {
+    restore();
+    reset();
+  });
+
   describe("getEnvConfig", () => {
     it("should read config from default filepath", () => {
+      stub(process, "env").value({
+        ...process.env,
+        NXTP_CONFIG: JSON.stringify(fakeConfig),
+      });
+
       let res;
       let error;
 
@@ -13,11 +25,23 @@ describe("Config", () => {
         error = e;
       }
 
+      console.log(error);
       expect(error).to.be.undefined;
     });
   });
   describe("getConfig", () => {
     it("should getEnvConfig", () => {
+      stub(process, "env").value({
+        ...process.env,
+        NXTP_AUTH_URL: fakeConfig.authUrl,
+        NXTP_NATS_URL: fakeConfig.natsUrl,
+        NXTP_MNEMONIC: fakeConfig.mnemonic,
+        NXTP_ADMIN_TOKEN: fakeConfig.adminToken,
+        NXTP_CHAIN_CONFIG: JSON.stringify(fakeConfig.chainConfig),
+        NXTP_SWAP_POOLS: JSON.stringify(fakeConfig.swapPools),
+        NXTP_LOG_LEVEL: fakeConfig.logLevel,
+      });
+
       let res;
       let error;
 
@@ -26,6 +50,8 @@ describe("Config", () => {
       } catch (e) {
         error = e;
       }
+
+      console.log(error);
 
       expect(error).to.be.undefined;
     });
