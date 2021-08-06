ac3c8d11c1e5a84990b20df9c4a9d8f7ec4eb1c5
z <z@zs-MacBook-Pro.local>
2021-08-06 10:22:35 -0500

    added token accounting logic for load router load tests

--- packages/integration/test/utils/accountManager.ts
+++ packages/integration/test/utils/accountManager.ts
@@ -9,6 +9,8 @@ const funderQueue = new PriorityQueue({ concurrency: 1 });
 
 export class OnchainAccountManager {
   USER_MIN_ETH = utils.parseEther("0.2");
+  USER_MIN_TOKEN = utils.parseEther("1000000");
+
 
   public readonly wallets: Wallet[] = [];
   walletsWSufficientBalance: number[] = [];
@@ -17,7 +19,7 @@ export class OnchainAccountManager {
 
   constructor(public readonly chainProviders: ChainConfig, mnemonic: string, public readonly num_users: number) {
     this.funder = Wallet.fromMnemonic(mnemonic);
-    for (let i = 1; i < num_users + 1; i++) {
+    for (let i = 0; i < num_users; i++) {
       const newWallet = Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i + 1}`);
       if (newWallet) {
         this.wallets.push(newWallet);
@@ -43,33 +45,47 @@ export class OnchainAccountManager {
     if (!provider) {
       throw new Error(`Provider not configured for ${chainId}`);
     }
-
-    const initial = await getOnchainBalance(assetId, account, provider);
-    if (initial.gte(this.USER_MIN_ETH)) {
-      console.log(`No need to top up balance of ${assetId}`);
-      return initial;
-    }
-
-    const remainder = this.USER_MIN_ETH.sub(initial);
-    const funderBalance = await getOnchainBalance(assetId, this.funder.address, provider);
-    if (funderBalance.lt(remainder)) {
-      throw new Error(
-        `${
-          this.funder.address
-        } has insufficient funds to top up. Has ${funderBalance.toString()}, needs ${remainder.toString()}`,
+    //not sending native token
+    if(assetId !== constants.AddressZero){
+      const initial = await getOnchainBalance(assetId, account, provider);
+      if (initial.gte(this.USER_MIN_TOKEN)) {
+        console.log(`No need to top up token balance of ${account}`);
+        return initial;
+      }
+      //no need to check if sugar daddy has balance we just mint directly to them
+      const receipt = await funderQueue.add(() =>
+          sendGift(assetId, this.USER_MIN_TOKEN.mul("2").toString(), account, this.funder.connect(provider)),
       );
+      console.log(`Sent token ${assetId} to topup: ${account},  txHash: ${receipt.transactionHash}`);
+      const final = await getOnchainBalance(assetId, account, provider);
+      return final;
+    }else{
+      const initial = await getOnchainBalance(assetId, account, provider);
+      if (initial.gte(this.USER_MIN_ETH)) {
+        console.log(`No need to top up eth balance of ${account}`);
+        return initial;
+      }
+
+      const remainder = this.USER_MIN_ETH.sub(initial);
+      const funderBalance = await getOnchainBalance(assetId, this.funder.address, provider);
+      if (funderBalance.lt(remainder)) {
+        throw new Error(
+            `${
+                this.funder.address
+            } has insufficient funds to top up. Has ${funderBalance.toString()}, needs ${remainder.toString()}`,
+        );
+      }
+
+      // fund with sugardaddy
+      const receipt = await funderQueue.add(() =>
+          sendGift(assetId, remainder.toString(), account, this.funder.connect(provider)),
+      );
+
+      console.log(`Sent ${assetId} to topup: ${account},  txHash: ${receipt.transactionHash}`);
+      // confirm balance
+      const final = await getOnchainBalance(assetId, account, provider);
+      return final;
     }
-
-    // fund with sugardaddy
-    const receipt = await funderQueue.add(() =>
-      sendGift(assetId, remainder.toString(), account, this.funder.connect(provider)),
-    );
-
-    console.log(`Sent ${assetId} to topup: ${account},  txHash: ${receipt.transactionHash}`);
-    // confirm balance
-    const final = await provider.getBalance(account);
-
-    return final;
   }
 
   getCanonicalWallets(num: number): Wallet[] {
@@ -80,23 +96,5 @@ export class OnchainAccountManager {
       }
     }
     return wallets;
-  }
-
-  /**
-   * Chooses a wallet at random (excluding the funder!).
-   *
-   * @param excluding - (optional) Agent to exclude from selection
-   * @returns Wallet
-   */
-  public getRandomWallet(excluding: Wallet[] = []): Wallet {
-    excluding.push(this.funder);
-    const filtered = this.wallets.filter((n) => {
-      const addrs = excluding.map((e) => e.address);
-      return !addrs.includes(n.address);
-    });
-    if (filtered.length === 0) {
-      throw new Error("Failed to get random wallet: none left after filtering.");
-    }
-    return filtered[Math.floor(Math.random() * filtered.length)];
   }
 }
