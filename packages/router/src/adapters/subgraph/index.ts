1f98b80078f10c33545df74e1a5e9215772ad012
LayneHaber <layne.haber@gmail.com>
2021-08-11 18:26:24 -0600

    Add getAssetDecimals to contract reader

--- packages/router/src/adapters/subgraph/index.ts
+++ packages/router/src/adapters/subgraph/index.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from "ethers/lib/ethers";
+import { BigNumber, constants, Contract, providers } from "ethers/lib/ethers";
 import { GraphQLClient } from "graphql-request";
 
 import { ActiveTransaction, SingleChainTransaction } from "../../lib/entities";
@@ -7,6 +7,17 @@ import { getContext } from "../../router";
 
 import { getSdk, Sdk } from "./graphqlsdk";
 import { getActiveTransactions, getAssetBalance, getTransactionForChain } from "./subgraph";
+
+const ERC20Abi = [
+  // Read-Only Functions
+  "function balanceOf(address owner) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function symbol() view returns (string)",
+
+  // Authenticated Functions
+  "function transfer(address to, uint amount) returns (boolean)",
+  "function mint(address account, uint256 amount)",
+];
 
 export type ContractReader = {
   getActiveTransactions: () => Promise<ActiveTransaction<any>[]>;
@@ -22,9 +33,18 @@ export type ContractReader = {
    *
    * @param assetId - The asset you want to determine router liquidity of
    * @param chainId - The chain you want to determine liquidity on
-   * @returns The available balance (or undefined)
+   * @returns The available balance
    */
   getAssetBalance: (assetId: string, chainId: number) => Promise<BigNumber>;
+
+  /**
+   * Returns decimals for given asset
+   *
+   * @param assetId - The asset you want to determine decimals of
+   * @param chainId - The chain asset exists on
+   * @returns The decimals of the asset
+   */
+  getAssetDecimals: (assetId: string, chainId: number) => Promise<number>;
 };
 
 const sdks: Record<number, Sdk> = {};
@@ -43,9 +63,30 @@ export const subgraphContractReader = (): ContractReader => {
     sdks[parseInt(chainId)] = getSdk(client);
   });
 
+  // TODO: can this be made into a subgraph function?
+  const getAssetDecimals = async (assetId: string, chainId: number): Promise<number> => {
+    if (assetId === constants.AddressZero) {
+      return 18;
+    }
+    // Get provider
+    const { providers: _providers } = config.chainConfig[chainId] ?? {};
+    if (!providers) {
+      throw new ContractReaderNotAvailableForChain(chainId, { available: Object.keys(config.chainConfig) });
+    }
+    const provider =
+      _providers.length === 1
+        ? new providers.JsonRpcProvider(p)
+        : new providers.FallbackProvider(
+            _providers.map((p) => new providers.JsonRpcProvider(p)),
+            1,
+          );
+    return (await new Contract(assetId, ERC20Abi, provider).decimals()).toNumber();
+  };
+
   return {
     getActiveTransactions,
     getTransactionForChain,
     getAssetBalance,
+    getAssetDecimals,
   };
 };
