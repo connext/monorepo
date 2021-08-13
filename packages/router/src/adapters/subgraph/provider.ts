ed520cdafaafbc144b42253865c9fe786521adda
LayneHaber <layne.haber@gmail.com>
2021-08-13 12:14:45 -0600

    Update structure

--- packages/router/src/adapters/subgraph/provider.ts
+++ packages/router/src/adapters/subgraph/provider.ts
@@ -1,4 +1,5 @@
-import { providers } from "ethers";
+import { providers, Contract, constants } from "ethers";
+import { ERC20Abi } from "@connext/nxtp-utils";
 
 import { ContractReaderNotAvailableForChain } from "../../lib/errors";
 import { getContext } from "../../router";
@@ -18,4 +19,25 @@ export const getBlockTime = async (chainId: number): Promise<number> => {
       : new providers.FallbackProvider(urls.map((url) => new providers.JsonRpcProvider(url), 1));
   const block = await provider.getBlock("latest");
   return block.timestamp;
+};
+
+export const getAssetDecimals = async (assetId: string, chainId: number): Promise<number> => {
+  if (assetId === constants.AddressZero) {
+    return 18;
+  }
+
+  const { config } = getContext();
+
+  if (!Object.keys(config.chainConfig).includes(chainId.toString())) {
+    throw new ContractReaderNotAvailableForChain(chainId, { available: Object.keys(chainId) });
+  }
+
+  // Get provider
+  const urls = config.chainConfig[chainId].providers;
+  const provider =
+    urls.length === 1
+      ? new providers.JsonRpcProvider(urls[0])
+      : new providers.FallbackProvider(urls.map((url) => new providers.JsonRpcProvider(url), 1));
+
+  return new Contract(assetId, ERC20Abi, provider).decimals();
 };
