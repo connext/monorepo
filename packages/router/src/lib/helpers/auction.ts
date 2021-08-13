379e87d200973c7cd7a5a3b759b67dd6d5710c6f
LayneHaber <layne.haber@gmail.com>
2021-08-13 12:11:14 -0600

    Add remarks on local time

--- packages/router/src/lib/helpers/auction.ts
+++ packages/router/src/lib/helpers/auction.ts
@@ -1,6 +1,8 @@
 /**
  * Gets the expiry on a given auction bid
  *
+ * @remarks Fine for bid expiry to use local clock, since only router will be validating the bid timestamp
+ *
  * @returns Expiry time of a given bid in s
  */
 export const getBidExpiry = () => Math.floor(Date.now() / 1000) + 60 * 5;
