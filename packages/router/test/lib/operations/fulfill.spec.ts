501cd3938857a4c067b94c173f05b0b0222ee749
Sanchay Mittal <sanchaymittal@gmail.com>
2021-08-16 20:06:07 +0530

    add: test for fulfill validation

--- packages/router/test/lib/operations/fulfill.spec.ts
+++ packages/router/test/lib/operations/fulfill.spec.ts
@@ -6,15 +6,65 @@ import { fulfillParamsMock, invariantDataMock, txReceiptMock } from "@connext/nx
 import { fulfill } from "../../../src/lib/operations";
 import { contractWriterMock } from "../../globalTestHook";
 import { fulfillInputMock } from "../../utils";
+import { receiverFulfilling } from "../../../src/lib/operations/fulfill";
 
 const requestContext = createRequestContext("TEST");
 
 describe("Fulfill Receiver Operation", () => {
   describe("#fulfillReceiver", () => {
+    it("should error if invariant data validation fails", async () => {
+      const _invariantDataMock = { ...invariantDataMock, user: "abc" };
+      await expect(fulfill(_invariantDataMock, fulfillInputMock, requestContext)).to.eventually.be.rejectedWith(
+        "Params invalid",
+      );
+    });
+
+    it("should error if prepare input validation fails", async () => {
+      const _fulfillInputMock = { ...fulfillInputMock, signature: "abc" };
+      await expect(fulfill(invariantDataMock, _fulfillInputMock, requestContext)).to.eventually.be.rejectedWith(
+        "Params invalid",
+      );
+    });
+
+    it("should error if prepare input validation fails for side", async () => {
+      const _fulfillInputMock = { ...fulfillInputMock, side: "buggy" };
+      await expect(fulfill(invariantDataMock, _fulfillInputMock, requestContext)).to.eventually.be.rejectedWith(
+        "Params invalid",
+      );
+    });
+
     it("should error if no config available for receiving chain", async () => {
       await expect(
         fulfill({ ...invariantDataMock, receivingChainId: 1234 }, fulfillInputMock, requestContext),
       ).to.eventually.be.rejectedWith("No chain config for chainId");
+    });
+
+    it("should not fulfill if already fulfilling", async () => {
+      receiverFulfilling.set(invariantDataMock.transactionId, true);
+      const receipt = await fulfill(invariantDataMock, fulfillInputMock, requestContext);
+      expect(receipt).to.be.undefined;
+      receiverFulfilling.set(invariantDataMock.transactionId, false);
+    });
+
+    it("happy: should fulfill on sender chain", async () => {
+      const receipt = await fulfill(invariantDataMock, { ...fulfillInputMock, side: "sender" }, requestContext);
+
+      expect(receipt).to.deep.eq(txReceiptMock);
+      expect(contractWriterMock.fulfill).to.be.calledOnceWith(
+        invariantDataMock.sendingChainId,
+        {
+          relayerFee: fulfillInputMock.relayerFee,
+          signature: fulfillInputMock.signature,
+          callData: fulfillInputMock.callData,
+          txData: {
+            ...invariantDataMock,
+            amount: fulfillInputMock.amount,
+            expiry: fulfillInputMock.expiry,
+            preparedBlockNumber: fulfillInputMock.preparedBlockNumber,
+          },
+        },
+        requestContext,
+      );
     });
 
     it("happy: should fulfill on receiver chain", async () => {
