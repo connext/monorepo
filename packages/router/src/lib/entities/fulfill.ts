99f5909dfb77cd15c8dcf6c47dd632545a336682
Sanchay Mittal <sanchaymittal@gmail.com>
2021-08-16 16:23:32 +0530

    update: fulfill types to sinclair

--- packages/router/src/lib/entities/fulfill.ts
+++ packages/router/src/lib/entities/fulfill.ts
@@ -1,9 +1,14 @@
-export type FulfillInput = {
-  amount: string;
-  expiry: number;
-  preparedBlockNumber: number;
-  signature: string;
-  relayerFee: string;
-  callData: string;
-  side: "sender" | "receiver";
-};
+import { Type, Static } from "@sinclair/typebox";
+import { TIntegerString } from "@connext/nxtp-utils";
+
+export const FulfillInputSchema = Type.Object({
+  amount: TIntegerString,
+  expiry: Type.Number(),
+  preparedBlockNumber: Type.Number(),
+  signature: Type.RegEx(/^0x[a-fA-F0-9]*$/),
+  relayerFee: TIntegerString,
+  callData: Type.RegEx(/^0x[a-fA-F0-9]*$/),
+  side: Type.RegEx(/sender|receiver/),
+});
+
+export type FulfillInput = Static<typeof FulfillInputSchema>;
