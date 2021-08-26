1ed76cfb9e3b529b141e0fff2c6529a69ebe4bf5
LayneHaber <layne.haber@gmail.com>
2021-08-26 13:40:44 -0600

    Add subgraph URLs

--- packages/utils/src/subgraph.ts
+++ packages/utils/src/subgraph.ts
@@ -17,10 +17,18 @@ export const getDeployedSubgraphUri = (chainId: number): string | undefined => {
       return "https://api.thegraph.com/subgraphs/name/connext/nxtp-goerli";
     case 42:
       return "https://api.thegraph.com/subgraphs/name/connext/nxtp-kovan";
+    case 56:
+      return "https://api.thegraph.com/subgraphs/name/connext/nxtp-bsc";
     case 69:
       return "https://api.thegraph.com/subgraphs/name/connext/nxtp-optimism-kovan";
     case 97:
       return "https://api.thegraph.com/subgraphs/name/connext/nxtp-chapel";
+    case 100:
+      return "https://api.thegraph.com/subgraphs/name/connext/nxtp-xdai";
+    case 137:
+      return "https://api.thegraph.com/subgraphs/name/connext/nxtp-matic";
+    case 250:
+      return "https://api.thegraph.com/subgraphs/name/connext/nxtp-ftm";
     case 80001:
       return "https://api.thegraph.com/subgraphs/name/connext/nxtp-mumbai";
     case 421611:
