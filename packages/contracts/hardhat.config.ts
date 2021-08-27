4b4c749d5355bd2f7758ba3be9e8de4707fa7324
Rahul Sethuram <rahul@sethuram.com>
2021-08-27 14:38:08 +0400

    Remove eth_call from mapping unless absolutely necessary

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -93,30 +93,40 @@ const config: HardhatUserConfig = {
       chainId: 42,
       url: urlOverride || process.env.KOVAN_ETH_PROVIDER_URL || "http://localhost:8545",
     },
+    bsc: {
+      accounts: { mnemonic },
+      chainId: 56,
+      url: urlOverride || process.env.BSC_PROVIDER_URL || "https://bsc-dataseed.binance.org/",
+    },
     chapel: {
       accounts: { mnemonic },
       chainId: 97,
       url: "https://data-seed-prebsc-2-s1.binance.org:8545/",
     },
-    bsc: {
+    xdai: {
       accounts: { mnemonic },
-      chainId: 56,
-      url: urlOverride || process.env.BSC_PROVIDER_URL || "https://bsc-dataseed.binance.org/",
+      chainId: 100,
+      url: urlOverride || process.env.XDAI_PROVIDER_URL || "https://rpc.xdaichain.com/",
     },
     matic: {
       accounts: { mnemonic },
       chainId: 137,
       url: urlOverride || process.env.MATIC_PROVIDER_URL || "http://localhost:8545",
     },
-    fuji: {
+    ftm: {
       accounts: { mnemonic },
-      chainId: 43113,
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 250,
+      url: urlOverride || process.env.FTM_PROVIDER_URL || "https://rpcapi.fantom.network/",
     },
     mbase: {
       accounts: { mnemonic },
       chainId: 1287,
       url: "https://moonbeam-alpha.api.onfinality.io/public",
+    },
+    fuji: {
+      accounts: { mnemonic },
+      chainId: 43113,
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
     },
     mumbai: {
       accounts: { mnemonic },
@@ -127,16 +137,6 @@ const config: HardhatUserConfig = {
       accounts: { mnemonic },
       chainId: 421611,
       url: urlOverride || process.env.ARB_RINK_ETH_PROVIDER_URL || "https://rinkeby.arbitrum.io/rpc",
-    },
-    ftm: {
-      accounts: { mnemonic },
-      chainId: 250,
-      url: urlOverride || process.env.FTM_PROVIDER_URL || "https://rpcapi.fantom.network/",
-    },
-    xdai: {
-      accounts: { mnemonic },
-      chainId: 100,
-      url: urlOverride || process.env.XDAI_PROVIDER_URL || "https://rpc.xdaichain.com/",
     },
   },
 };
