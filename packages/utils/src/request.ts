1700e387f75c59181e279d982c903fe01f553ee9
LayneHaber <layne.haber@gmail.com>
2021-08-31 21:14:31 -0600

    Use conditional types

--- packages/utils/src/request.ts
+++ packages/utils/src/request.ts
@@ -10,11 +10,18 @@ export const getUuid = () => hId();
 /**
  * Top-level identifier. I.e. API function that was called
  */
-export type RequestContext = {
+export type BaseRequestContext = {
   id: string;
   origin: string;
-  transactionId?: string;
 };
+
+export type RequestContextWithTransactionId = BaseRequestContext & {
+  transactionId: string;
+};
+
+export type RequestContext<T extends string | undefined = undefined> = T extends undefined
+  ? BaseRequestContext
+  : RequestContextWithTransactionId;
 
 /**
  * Low-level identifier. I.e. what function am i in now
@@ -29,10 +36,19 @@ export type MethodContext = {
  * @param origin The origin of the request
  * @returns
  */
-export const createRequestContext = (origin: string, transactionId?: string): RequestContext => {
+export function createRequestContext<T extends string | undefined = undefined>(
+  origin: string,
+  transactionId?: T,
+): RequestContext<T> {
   const id = getUuid();
-  return transactionId ? { id, origin, transactionId } : { id, origin };
-};
+  if (transactionId) {
+    return { id, origin, transactionId } as RequestContext<string>;
+  }
+  // FIXME: why will it not play nicely
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return { id, origin } as RequestContext<undefined>;
+}
 
 /**
  * Creates a MethodContext that logs within functions
@@ -43,9 +59,13 @@ export const createMethodContext = (name: string) => {
   return { id: getUuid(), name };
 };
 
-export const createLoggingContext = (methodName: string, inherited?: RequestContext, transactionId?: string) => {
+export const createLoggingContext = <T extends string | undefined = undefined>(
+  methodName: string,
+  inherited?: RequestContext<T>,
+  transactionId?: T,
+) => {
   return {
     methodContext: createMethodContext(methodName),
-    requestContext: inherited ?? createRequestContext(methodName, transactionId),
+    requestContext: inherited ? { transactionId, ...inherited } : createRequestContext<T>(methodName, transactionId),
   };
 };
