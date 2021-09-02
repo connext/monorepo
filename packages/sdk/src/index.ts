1d9307bea0f7729c1b973939007b707d36b776b8
Rahul Sethuram <rahul@sethuram.com>
2021-09-02 13:38:38 +0400

    Fix build

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,4 @@
 export { NxtpSdk } from "./sdk";
 export { getDeployedTransactionManagerContract } from "./transactionManager/transactionManager";
 export * from "./types";
+export { getMinExpiryBuffer } from "./utils";
