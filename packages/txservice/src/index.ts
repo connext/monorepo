1d303d53fdef4dab66809f4051ad24da23ca2b05
Jake <jcb.kdd@gmail.com>
2021-09-15 20:58:16 -0400

    fix export

--- packages/txservice/src/index.ts
+++ packages/txservice/src/index.ts
@@ -1,6 +1,6 @@
 // NOTE: We are NOT exporting provider, signer, or transaction here, as those classes should be used
 // internally to this package only.
-export * from "./chainservice";
+export { ChainService as TransactionService } from "./chainservice";
 export * from "./chainreader";
 export * from "./types";
 export * from "./config";
