8d4b638850539da63b924f995dd439175d035b12
Rahul Sethuram <rahul@sethuram.com>
2021-09-19 17:45:49 +0400

    Fix sig

--- packages/utils/src/signatures.ts
+++ packages/utils/src/signatures.ts
@@ -52,8 +52,8 @@ export const signFulfillTransactionPayload = async (
   // https://github.com/ethers-io/ethers.js/pull/1542
   let signature;
   if (signer.provider && signer.provider instanceof providers.Web3Provider) {
-    console.log("********* signer.provider: ", signer.provider);
-    signature = await signer.provider.send("personal_sign", [utils.arrayify(hash)]);
+    const signerAddress = await signer.getAddress();
+    signature = await signer.provider.send("personal_sign", [utils.arrayify(hash), signerAddress]);
   } else {
     signature = await signer.signMessage(utils.arrayify(hash));
   }
