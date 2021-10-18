a3e1e639611d4797dbca81f3791189278123b408
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-18 13:09:29 +0530

    add: staging config

---
+++ packages/subgraph/config/stagging.ts
@@ -0,0 +1,43 @@
+/// PROD NETWORK
+export const arbitrumOne = {
+  network: "arbitrum-one",
+  address: "0xF293D5d599c046681AB28c1Bc7927Fff859c67A7",
+  startBlock: 244433,
+};
+
+export const fantom = {
+  network: "fantom",
+  address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+  startBlock: 15584609,
+};
+
+export const matic = {
+  network: "matic",
+  address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+  startBlock: 18421338,
+};
+
+export const bsc = {
+  network: "bsc",
+  address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+  startBlock: 10370113,
+};
+
+export const xdai = {
+  network: "xdai",
+  address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+  startBlock: 17774514,
+};
+
+/// TEST NETWORK
+export const goerli = {
+  network: "goerli",
+  address: "0xD22dB9ad317A09384a86f310c973CDc350Bb510E",
+  startBlock: 5336026,
+};
+
+export const rinkeby = {
+  network: "rinkeby",
+  address: "0xe71678794fff8846bFF855f716b0Ce9d9a78E844",
+  startBlock: 8970584,
+};
