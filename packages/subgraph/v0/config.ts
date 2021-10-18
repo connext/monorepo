685b19e1e70e686669390062c705ef8eccecebec
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-18 18:03:11 +0530

    more restructure

---
+++ packages/subgraph/v0/config.ts
@@ -0,0 +1,33 @@
+/// PROD NETWORK
+export const Networks = [
+  {
+    subgraphName: "connext/nxtp-arbitrum-one-v0",
+    network: "arbitrum-one",
+    address: "0xF293D5d599c046681AB28c1Bc7927Fff859c67A7",
+    startBlock: 244433,
+  },
+  {
+    subgraphName: "connext/nxtp-fantom-v0",
+    network: "fantom",
+    address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+    startBlock: 15584609,
+  },
+  {
+    subgraphName: "connext/nxtp-matic-v0",
+    network: "matic",
+    address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+    startBlock: 18421338,
+  },
+  {
+    subgraphName: "connext/nxtp-bsc-v0",
+    network: "bsc",
+    address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+    startBlock: 10370113,
+  },
+  {
+    subgraphName: "connext/nxtp-xdai-v0",
+    network: "xdai",
+    address: "0x31eFc4AeAA7c39e54A33FDc3C46ee2Bd70ae0A09",
+    startBlock: 17774514,
+  },
+];
