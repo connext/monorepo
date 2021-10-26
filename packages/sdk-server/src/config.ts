0d511f29012e5e568337296500e323f7561926b3
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-26 15:11:31 +0530

    fix: config

--- packages/sdk-server/src/config.ts
+++ packages/sdk-server/src/config.ts
@@ -1,10 +1,21 @@
-import { SdkBaseChainConfigParams } from "@connext/nxtp-sdk";
+import { providers } from "ethers";
 
 export const getConfig = () => {
   const config = {
     network: undefined,
-    chainConfig: {} as SdkBaseChainConfigParams,
-    mnemonic: "",
+    chainConfig: {
+      "4": {
+        provider: new providers.FallbackProvider([
+          new providers.JsonRpcProvider("https://rinkeby.infura.io/v3/f7a467b4bd1045bf991c169d4fefe17b"),
+        ]),
+      },
+      "5": {
+        provider: new providers.FallbackProvider([
+          new providers.JsonRpcProvider("https://goerli.infura.io/v3/f7a467b4bd1045bf991c169d4fefe17b"),
+        ]),
+      },
+    },
+    mnemonic: "category matrix shine sense east large leave pony talk enrich orchard flip",
   };
   return config;
 };
