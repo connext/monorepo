0b5b22f9d1174454e2f17654bd08f106459de0cf
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-02 01:02:50 +0530

    update web3Signer class

--- packages/router/src/adapters/web3signer/index.ts
+++ packages/router/src/adapters/web3signer/index.ts
@@ -1,5 +1,6 @@
-import { Signer, providers, utils, Bytes } from "ethers";
-import { getAddressFromPublicKey } from "@connext/nxtp-utils";
+import { Signer, providers, utils, Bytes, Wallet } from "ethers";
+import { getAddressFromPublicKey, isValidHexString } from "@connext/nxtp-utils";
+import { arrayToBuffer, concatBuffers, hexToBuffer, utf8ToBuffer } from "eccrypto-js";
 
 import { signing, getPublicKey } from "./api";
 
@@ -26,7 +27,28 @@ export class Web3Signer extends Signer {
   }
 
   public async signMessage(message: Bytes | string): Promise<string> {
-    const response = await signing(this.web3SignerUrl, message);
+    const identifier = await getPublicKey(this.web3SignerUrl);
+
+    const prefix = "\x19Ethereum Signed Message:\n";
+
+    const digestBytes = utils.hexlify(
+      utils.keccak256(
+        concatBuffers(Buffer.from(prefix), Buffer.from(`${message.length}`), Buffer.from(message.toString())),
+      ),
+    );
+
+    // const digestBytes = utils.arrayify(utils.hashMessage(message));
+
+    console.log("digestBytes", digestBytes);
+
+    const response = await signing(this.web3SignerUrl, identifier, digestBytes);
+
+    const wallet = Wallet.fromMnemonic(
+      "luxury genuine glue face keep wasp victory clever solution drive venue convince",
+    );
+    const cRes = await wallet.signMessage(message);
+
+    console.log("compare", response, cRes);
     return response;
   }
 
@@ -35,7 +57,7 @@ export class Web3Signer extends Signer {
       ...transaction,
       nonce: Number(transaction.nonce),
     };
-    const data = utils.serializeTransaction(tx);
+    const data = utils.keccak256(utils.serializeTransaction(tx));
 
     const response = await this.signMessage(data);
     return response;
