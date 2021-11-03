d29876911e34bd327589f18bdcb9e7a4fd31eead
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-03 15:10:44 +0530

    fix: signMessage

--- packages/router/src/adapters/web3signer/index.ts
+++ packages/router/src/adapters/web3signer/index.ts
@@ -1,7 +1,16 @@
-import { Signer, providers, utils, Bytes } from "ethers";
+import { Signer, providers, utils, Bytes, Wallet } from "ethers";
 import { getAddressFromPublicKey } from "@connext/nxtp-utils";
 
 import { signing, getPublicKey } from "./api";
+
+export const messagePrefix = "\x19Ethereum Signed Message:\n";
+
+export function prepareEthereumSignedMessage(message: Bytes | string): Bytes {
+  if (typeof message === "string") {
+    message = utils.toUtf8Bytes(message);
+  }
+  return utils.concat([utils.toUtf8Bytes(messagePrefix), utils.toUtf8Bytes(String(message.length)), message]);
+}
 
 export class Web3Signer extends Signer {
   public address?: string;
@@ -27,7 +36,8 @@ export class Web3Signer extends Signer {
 
   public async signMessage(message: Bytes | string): Promise<string> {
     const identifier = await getPublicKey(this.web3SignerUrl);
-    const digestBytes = utils.hashMessage(message);
+    const data = prepareEthereumSignedMessage(message);
+    const digestBytes = utils.hexZeroPad(data, data.length);
 
     const response = await signing(this.web3SignerUrl, identifier, digestBytes);
 
@@ -39,9 +49,10 @@ export class Web3Signer extends Signer {
       ...transaction,
       nonce: Number(transaction.nonce),
     };
-    const data = utils.keccak256(utils.serializeTransaction(tx));
+    const identifier = await getPublicKey(this.web3SignerUrl);
+    const digestBytes = utils.serializeTransaction(tx);
 
-    const response = await this.signMessage(data);
+    const response = await signing(this.web3SignerUrl, identifier, digestBytes);
     return response;
   }
 }
