97326ad3ab1eeb0bf9232f8e393d5732c92b14d3
LayneHaber <layne.haber@gmail.com>
2021-11-03 19:41:21 -0700

    Implement tests

--- packages/contracts/test/interpreters/signatureInterpreter.spec.ts
+++ packages/contracts/test/interpreters/signatureInterpreter.spec.ts
@@ -1,12 +1,13 @@
-import { ethers, waffle } from "hardhat";
+import { waffle } from "hardhat";
 import { expect, use } from "chai";
 import { solidity } from "ethereum-waffle";
 import { Wallet } from "ethers";
 import { deployContract } from "../utils";
 import { SignatureInterpreter } from "../../typechain";
+import { signCancelTransactionPayload, signFulfillTransactionPayload, txDataMock } from "@connext/nxtp-utils";
 use(solidity);
 
-describe.skip("SignatureInterpreter.sol", () => {
+describe("SignatureInterpreter.sol", () => {
   const createFixtureLoader = waffle.createFixtureLoader;
   const [wallet, other] = waffle.provider.getWallets() as Wallet[];
 
@@ -26,14 +27,170 @@ describe.skip("SignatureInterpreter.sol", () => {
   });
 
   describe("shouldFulfill", () => {
-    it("should fail if the transaction manager id is not the sending or receiving chainId", async () => {});
-    it("should return true if user signed", async () => {});
-    it("should return false if user did not sign", async () => {});
+    it("should fail if the transaction manager id is not the sending or receiving chainId", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+        user: user.address,
+      };
+      const relayerFee = "0";
+      const signature = await signFulfillTransactionPayload(
+        txData.transactionId,
+        relayerFee,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      await expect(signatureInterpreter.shouldFulfill(txData, signature, relayerFee, 1)).revertedWith("#SI:012");
+    });
+
+    it("should return true if user signed", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+        user: user.address,
+      };
+      const relayerFee = "0";
+      const signature = await signFulfillTransactionPayload(
+        txData.transactionId,
+        relayerFee,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldFulfill(txData, signature, relayerFee, txData.receivingChainId);
+      expect(result).to.be.true;
+    });
+
+    it("should work on the sending chain if user signed", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+        user: user.address,
+      };
+      const relayerFee = "0";
+      const signature = await signFulfillTransactionPayload(
+        txData.transactionId,
+        relayerFee,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldFulfill(txData, signature, relayerFee, txData.sendingChainId);
+      expect(result).to.be.true;
+    });
+
+    it("should return false if user did not sign", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+      };
+      const relayerFee = "0";
+      const signature = await signFulfillTransactionPayload(
+        txData.transactionId,
+        relayerFee,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldFulfill(txData, signature, relayerFee, txData.receivingChainId);
+      expect(result).to.be.false;
+    });
+
+    it("should return false if user signed wrong payload", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+      };
+      const relayerFee = "0";
+      const signature = await signCancelTransactionPayload(
+        txData.transactionId,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldFulfill(txData, signature, relayerFee, txData.receivingChainId);
+      expect(result).to.be.false;
+    });
   });
 
   describe("shouldCancel", () => {
-    it("should fail if the transaction manager id is not the sending or receiving chainId", async () => {});
-    it("should return true if user signed", async () => {});
-    it("should return false if user did not sign", async () => {});
+    it("should fail if the transaction manager id is not the sending or receiving chainId", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+        user: user.address,
+      };
+      const signature = await signCancelTransactionPayload(
+        txData.transactionId,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      await expect(signatureInterpreter.shouldFulfill(txData, signature, "0", 1)).revertedWith("#SI:012");
+    });
+
+    it("should return true if user signed", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+        user: user.address,
+      };
+      const signature = await signCancelTransactionPayload(
+        txData.transactionId,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldCancel(txData, signature, txData.receivingChainId);
+      expect(result).to.be.true;
+    });
+
+    it("should return true if user signed and submitted on the sending chain", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+        user: user.address,
+      };
+      const signature = await signCancelTransactionPayload(
+        txData.transactionId,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldCancel(txData, signature, txData.sendingChainId);
+      expect(result).to.be.true;
+    });
+
+    it("should return false if user did not sign", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+      };
+      const signature = await signCancelTransactionPayload(
+        txData.transactionId,
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldCancel(txData, signature, txData.receivingChainId);
+      expect(result).to.be.false;
+    });
+
+    it("should return false if user signed the wrong payload", async () => {
+      const user = Wallet.createRandom();
+      const txData = {
+        ...txDataMock,
+      };
+      const signature = await signFulfillTransactionPayload(
+        txData.transactionId,
+        "0",
+        txData.receivingChainId,
+        txData.receivingChainTxManagerAddress,
+        user,
+      );
+      const result = await signatureInterpreter.shouldCancel(txData, signature, txData.receivingChainId);
+      expect(result).to.be.false;
+    });
   });
 });
