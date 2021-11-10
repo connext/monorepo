b65636c1bc04898f0f989a54bffa25bc1de9bd75
Rahul Sethuram <rahul@sethuram.com>
2021-11-10 14:21:34 +0400

    Bump gas estimate

--- packages/utils/src/gasEstimates.ts
+++ packages/utils/src/gasEstimates.ts
@@ -1,4 +1,4 @@
 export const GAS_ESTIMATES = {
   prepare: "86580",
-  fulfill: "103495",
+  fulfill: "126000", // https://bscscan.com/tx/0xcaba240ab17f006586086cd460fffab09a028905d7c497c31667c1d5eb58e153 example tx, add 5% buffer
 };
