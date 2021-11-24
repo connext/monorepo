f502ab54ee2f941de0fbe72ed831c83e3ef0b603
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-24 12:35:08 +0530

    add: new test for transferQuote chg: params to fix test

--- packages/sdk/test/unit/sdkBase.spec.ts
+++ packages/sdk/test/unit/sdkBase.spec.ts
@@ -7,6 +7,7 @@ import {
   VariantTransactionData,
   AuctionBid,
   Logger,
+  chainDataMock,
   GAS_ESTIMATES,
 } from "@connext/nxtp-utils";
 import { expect } from "chai";
@@ -34,8 +35,9 @@ import {
   InvalidCallTo,
   NoValidBids,
   RelayFailed,
+  NotEnoughAmount,
 } from "../../src/error";
-import { getAddress, keccak256 } from "ethers/lib/utils";
+import { getAddress, keccak256, parseEther } from "ethers/lib/utils";
 import { CrossChainParams, NxtpSdkEvents, HistoricalTransactionStatus } from "../../src";
 import { Subgraph } from "../../src/subgraph/subgraph";
 import { getMinExpiryBuffer, getMaxExpiryBuffer } from "../../src/utils";
@@ -57,6 +59,7 @@ describe("NxtpSdkBase", () => {
   let messaging: SinonStubbedInstance<UserNxtpNatsMessagingService>;
   let subgraph: SinonStubbedInstance<Subgraph>;
   let transactionManager: SinonStubbedInstance<TransactionManager>;
+  let chainData: SinonStub;
   let getDeployedChainIdsForGasFeeStub: SinonStub;
   let getDeployedPriceOracleContractStub: SinonStub;
   let provider1337: SinonStubbedInstance<providers.FallbackProvider>;
@@ -101,7 +104,8 @@ describe("NxtpSdkBase", () => {
     subgraph.getSyncStatus.returns({ latestBlock: 0, synced: true, syncedBlock: 0 });
     transactionManager = createStubInstance(TransactionManager);
 
-    stub(utils, "getDecimals").resolves(18);
+    stub(utils, "getChainData").resolves(chainDataMock);
+    stub(utils, "getDecimalsForAsset").resolves(18);
     stub(utils, "getTokenPrice").resolves(BigNumber.from(10).pow(21));
     stub(utils, "getTimestampInSeconds").resolves(Math.floor(Date.now() / 1000));
     provider1337.getGasPrice.resolves(BigNumber.from(10).pow(9));
@@ -214,7 +218,7 @@ describe("NxtpSdkBase", () => {
       receivingAssetId: mkAddress("0xb"),
       callTo: AddressZero,
       receivingAddress: mkAddress("0xa"),
-      amount: "1000000",
+      amount: parseEther("1").toString(),
       expiry: Math.floor(Date.now() / 1000) + 24 * 3600 * 3,
       transactionId,
       ...crossChainParamsOverrides,
@@ -226,7 +230,7 @@ describe("NxtpSdkBase", () => {
       initiator: user,
       sendingChainId,
       sendingAssetId: mkAddress("0xa"),
-      amount: "1000000",
+      amount: parseEther("1").toString(),
       receivingChainId,
       receivingAssetId: mkAddress("0xb"),
       amountReceived: "1000000",
@@ -443,6 +447,11 @@ describe("NxtpSdkBase", () => {
       await expect(sdk.getTransferQuote(crossChainParams)).to.eventually.be.rejectedWith(
         InvalidExpiry.getMessage(crossChainParams.expiry, getMinExpiryBuffer(), getMaxExpiryBuffer()),
       );
+    });
+
+    it("should error if receiverAmount is lower than gasFee", async () => {
+      const { crossChainParams } = getMock({ amount: "100000" });
+      await expect(sdk.getTransferQuote(crossChainParams)).to.eventually.be.rejectedWith(NotEnoughAmount.getMessage());
     });
 
     it("should not include improperly signed bids", async () => {
