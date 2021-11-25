f22b0497c16add83876ad4ed74e970b9a5e666a9
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-25 12:39:03 +0530

    move shared func from contract writer

--- packages/router/src/lib/helpers/shared.ts
+++ packages/router/src/lib/helpers/shared.ts
@@ -4,12 +4,36 @@ import {
   GAS_ESTIMATES,
   getChainData,
   createLoggingContext,
+  getInvariantTransactionDigest,
+  getVariantTransactionDigest,
+  TransactionData,
+  PrepareParams,
+  FulfillParams,
+  CancelParams,
 } from "@connext/nxtp-utils";
-import { BigNumber, constants, utils } from "ethers";
+import { BigNumber, constants, utils, Contract } from "ethers";
+import { Evt } from "evt";
 
-import { getOracleContractAddress, getPriceOracleInterface } from "../../adapters/contract/contract";
+import { Interface } from "ethers/lib/utils";
+import {
+  TransactionManager as TTransactionManager,
+  ConnextPriceOracle as TConnextPriceOracle,
+} from "@connext/nxtp-contracts/typechain";
+import { Router as TRouter } from "@connext/nxtp-contracts/typechain";
+
+import RouterArtifact from "@connext/nxtp-contracts/artifacts/contracts/Router.sol/Router.json";
+import TransactionManagerArtifact from "@connext/nxtp-contracts/artifacts/contracts/TransactionManager.sol/TransactionManager.json";
+import PriceOracleArtifact from "@connext/nxtp-contracts/artifacts/contracts/ConnextPriceOracle.sol/ConnextPriceOracle.json";
+
 import { getDeployedChainIdsForGasFee } from "../../config";
+
+import { NotExistPriceOracle } from "../../lib/errors/contracts";
 import { getContext } from "../../router";
+
+import { TransactionStatus } from "../../adapters/subgraph/graphqlsdk";
+import { SanitationCheckFailed } from "../../lib/errors/contracts";
+
+const { HashZero } = constants;
 
 const NO_ORACLE_CHAINS: number[] = [];
 
@@ -19,6 +43,31 @@ const NO_ORACLE_CHAINS: number[] = [];
 export const getNtpTimeSeconds = async () => {
   return await _getNtpTimeSeconds();
 };
+
+export const getContractAddress = (chainId: number): string => {
+  const { config } = getContext();
+  const nxtpContractAddress = config.chainConfig[chainId]?.transactionManagerAddress;
+  if (!nxtpContractAddress) {
+    throw new Error(`No contract exists for chain ${chainId}`);
+  }
+  return nxtpContractAddress;
+};
+
+export const getOracleContractAddress = (chainId: number, requestContext: RequestContext): string => {
+  const { config } = getContext();
+  const oracleContractAddress = config.chainConfig[chainId]?.priceOracleAddress;
+  if (!oracleContractAddress) {
+    throw new NotExistPriceOracle(chainId, requestContext);
+  }
+  return oracleContractAddress;
+};
+
+export const getRouterContractInterface = () => new Interface(RouterArtifact.abi) as TRouter["interface"];
+
+export const getTxManagerInterface = () =>
+  new Interface(TransactionManagerArtifact.abi) as TTransactionManager["interface"];
+
+export const getPriceOracleInterface = () => new Interface(PriceOracleArtifact.abi) as TConnextPriceOracle["interface"];
 
 /**
  * Returns the mainnet equivalent of the given asset on the given chain.
@@ -289,4 +338,162 @@ export const getGasPrice = async (chainId: number, requestContext: RequestContex
  */
 export const getChainIdForGasFee = (): number[] => {
   return getDeployedChainIdsForGasFee();
+};
+
+export const prepareEvt = new Evt<{ event: any; args: PrepareParams }>(); // TODO: fix types
+export const fulfillEvt = new Evt<{ event: any; args: FulfillParams }>();
+export const cancelEvt = new Evt<{ event: any; args: CancelParams }>();
+
+export const startContractListeners = (): void => {
+  const { config, txService } = getContext();
+  Object.entries(config.chainConfig).forEach(async ([_chainId, conf]) => {
+    const chainId = Number(_chainId);
+    if (conf.routerContractAddress) {
+      // needs event listeners for listening to relayed events
+      // TODO remove this when we can query gelato for tx receipts
+      // alternatively allow listening on the subgraph
+      const contract = new Contract(
+        conf.transactionManagerAddress,
+        TransactionManagerArtifact.abi,
+        txService.getProvider(chainId).provider,
+      ) as TTransactionManager;
+
+      contract.on("TransactionPrepared", (_user, _router, _transactionId, _txData, _caller, args, event) => {
+        prepareEvt.post({ event, args });
+      });
+
+      contract.on(
+        "TransactionFulfilled",
+        (_user, _router, _transactionId, args, _success, _isContract, _returnData, _caller, event) => {
+          fulfillEvt.post({ event, args });
+        },
+      );
+
+      contract.on("TransactionCancelled", (_user, _router, _transactionId, args, _caller, event) => {
+        cancelEvt.post({ event, args });
+      });
+    }
+  });
+};
+
+export const sanitationCheck = async (
+  chainId: number,
+  transactionData: TransactionData,
+  functionCall: "prepare" | "fulfill" | "cancel",
+  _requestContext?: RequestContext<string>,
+) => {
+  const { txService, contractReader } = getContext();
+
+  const { requestContext, methodContext } = createLoggingContext(
+    sanitationCheck.name,
+    _requestContext,
+    transactionData.transactionId,
+  );
+
+  const nxtpContractAddress = getContractAddress(chainId);
+
+  const invariantDigest = getInvariantTransactionDigest({
+    receivingChainTxManagerAddress: transactionData.receivingChainTxManagerAddress,
+    user: transactionData.user,
+    router: transactionData.router,
+    initiator: transactionData.initiator,
+    sendingAssetId: transactionData.sendingAssetId,
+    receivingAssetId: transactionData.receivingAssetId,
+    sendingChainFallback: transactionData.sendingChainFallback,
+    callTo: transactionData.callTo,
+    receivingAddress: transactionData.receivingAddress,
+    sendingChainId: transactionData.sendingChainId,
+    receivingChainId: transactionData.receivingChainId,
+    callDataHash: transactionData.callDataHash,
+    transactionId: transactionData.transactionId,
+  });
+
+  const encodeVariantTransactionData = getTxManagerInterface().encodeFunctionData("variantTransactionData", [
+    invariantDigest,
+  ]);
+
+  const variantTransactionDigest = await txService.readTx({
+    chainId,
+    to: nxtpContractAddress,
+    data: encodeVariantTransactionData,
+  });
+
+  if (functionCall === "prepare") {
+    // variantTransactionDigest exist then transaction is already prepared
+    if (variantTransactionDigest !== HashZero) {
+      throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
+        requestContext,
+        methodContext,
+        variantTransactionDigest,
+      });
+    }
+  } else {
+    const expectedVariantDigest = getVariantTransactionDigest({
+      amount: transactionData.amount,
+      expiry: transactionData.expiry,
+      preparedBlockNumber: transactionData.preparedBlockNumber,
+    });
+
+    if (expectedVariantDigest === variantTransactionDigest) {
+      // All is good, no issues
+      return;
+    }
+
+    // transaction should be prepared before fulfill
+    if (variantTransactionDigest === HashZero) {
+      throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
+        requestContext,
+        methodContext,
+        variantTransactionDigest,
+      });
+    }
+
+    // transaction is already fulfilled
+    // get expected fulfilled/cancelled variant hash
+    const fulfilledOrCancelledVariant = getVariantTransactionDigest({
+      amount: transactionData.amount,
+      expiry: transactionData.expiry,
+      preparedBlockNumber: 0,
+    });
+
+    if (variantTransactionDigest === fulfilledOrCancelledVariant) {
+      throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
+        requestContext,
+        methodContext,
+        variantTransactionDigest,
+        fulfilledOrCancelledVariant,
+      });
+    }
+
+    if (functionCall === "cancel" && chainId === transactionData.sendingChainId) {
+      const receivingChainNxtpContractAddress = getContractAddress(transactionData.receivingChainId);
+
+      const receivingChainVariantTransactionDigest = await txService.readTx({
+        chainId: transactionData.receivingChainId,
+        to: receivingChainNxtpContractAddress,
+        data: encodeVariantTransactionData,
+      });
+
+      if (receivingChainVariantTransactionDigest === HashZero) {
+        // cancel is allowed when no transaction is prepared
+        return;
+      } else {
+        const receivingChainTransaction = await contractReader.getTransactionForChain(
+          transactionData.transactionId,
+          transactionData.user,
+          transactionData.receivingChainId,
+        );
+
+        if (receivingChainTransaction?.status === TransactionStatus.Cancelled) {
+          // cancel is allowed when transaction is cancelled on receiving chain
+          return;
+        } else {
+          throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
+            requestContext,
+            methodContext,
+          });
+        }
+      }
+    }
+  }
 };
