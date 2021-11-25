e2a6131dce7d03a3aad47e44ce94c92718807488
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-25 12:40:57 +0530

    multiple changes & routerContract prepare gelato imp

--- packages/router/src/adapters/contract/contract.ts
+++ packages/router/src/adapters/contract/contract.ts
@@ -4,209 +4,21 @@ import {
   FulfillParams,
   PrepareParams,
   RequestContext,
-  getInvariantTransactionDigest,
-  getVariantTransactionDigest,
-  TransactionData,
   isChainSupportedByGelato,
   gelatoSend,
-  MetaTxTypes,
+  signRouterPrepareTransactionPayload,
+  NxtpError,
 } from "@connext/nxtp-utils";
-import { BigNumber, constants, Contract, providers } from "ethers/lib/ethers";
-import { Interface } from "ethers/lib/utils";
-import {
-  TransactionManager as TTransactionManager,
-  ConnextPriceOracle as TConnextPriceOracle,
-} from "@connext/nxtp-contracts/typechain";
-import TransactionManagerArtifact from "@connext/nxtp-contracts/artifacts/contracts/TransactionManager.sol/TransactionManager.json";
-import PriceOracleArtifact from "@connext/nxtp-contracts/artifacts/contracts/ConnextPriceOracle.sol/ConnextPriceOracle.json";
-import { Evt } from "evt";
+import { BigNumber, constants, providers } from "ethers/lib/ethers";
 
 import { getContext } from "../../router";
-import { TransactionStatus } from "../../adapters/subgraph/graphqlsdk";
-import { NotExistPriceOracle, SanitationCheckFailed } from "../../lib/errors/contracts";
-
-const { HashZero } = constants;
-
-const prepareEvt = new Evt<{ event: any; args: PrepareParams }>(); // TODO: fix types
-const fulfillEvt = new Evt<{ event: any; args: FulfillParams }>();
-const cancelEvt = new Evt<{ event: any; args: CancelParams }>();
-
-export const startContractListeners = (): void => {
-  const { config, txService } = getContext();
-  Object.entries(config.chainConfig).forEach(async ([_chainId, conf]) => {
-    const chainId = Number(_chainId);
-    if (conf.routerContractAddress) {
-      // needs event listeners for listening to relayed events
-      // TODO remove this when we can query gelato for tx receipts
-      // alternatively allow listening on the subgraph
-      const contract = new Contract(
-        conf.transactionManagerAddress,
-        TransactionManagerArtifact.abi,
-        txService.getProvider(chainId).provider,
-      ) as TTransactionManager;
-
-      contract.on("TransactionPrepared", (_user, _router, _transactionId, _txData, _caller, args, event) => {
-        prepareEvt.post({ event, args });
-      });
-
-      contract.on(
-        "TransactionFulfilled",
-        (_user, _router, _transactionId, args, _success, _isContract, _returnData, _caller, event) => {
-          fulfillEvt.post({ event, args });
-        },
-      );
-
-      contract.on("TransactionCancelled", (_user, _router, _transactionId, args, _caller, event) => {
-        cancelEvt.post({ event, args });
-      });
-    }
-  });
-};
-
-export const getContractAddress = (chainId: number): string => {
-  const { config } = getContext();
-  const nxtpContractAddress = config.chainConfig[chainId]?.transactionManagerAddress;
-  if (!nxtpContractAddress) {
-    throw new Error(`No contract exists for chain ${chainId}`);
-  }
-  return nxtpContractAddress;
-};
-
-export const getOracleContractAddress = (chainId: number, requestContext: RequestContext): string => {
-  const { config } = getContext();
-  const oracleContractAddress = config.chainConfig[chainId]?.priceOracleAddress;
-  if (!oracleContractAddress) {
-    throw new NotExistPriceOracle(chainId, requestContext);
-  }
-  return oracleContractAddress;
-};
-
-export const getTxManagerInterface = () =>
-  new Interface(TransactionManagerArtifact.abi) as TTransactionManager["interface"];
-
-export const getPriceOracleInterface = () => new Interface(PriceOracleArtifact.abi) as TConnextPriceOracle["interface"];
-
-export const sanitationCheck = async (
-  chainId: number,
-  transactionData: TransactionData,
-  functionCall: "prepare" | "fulfill" | "cancel",
-  _requestContext?: RequestContext<string>,
-) => {
-  const { txService, contractReader } = getContext();
-
-  const { requestContext, methodContext } = createLoggingContext(
-    sanitationCheck.name,
-    _requestContext,
-    transactionData.transactionId,
-  );
-
-  const nxtpContractAddress = getContractAddress(chainId);
-
-  const invariantDigest = getInvariantTransactionDigest({
-    receivingChainTxManagerAddress: transactionData.receivingChainTxManagerAddress,
-    user: transactionData.user,
-    router: transactionData.router,
-    initiator: transactionData.initiator,
-    sendingAssetId: transactionData.sendingAssetId,
-    receivingAssetId: transactionData.receivingAssetId,
-    sendingChainFallback: transactionData.sendingChainFallback,
-    callTo: transactionData.callTo,
-    receivingAddress: transactionData.receivingAddress,
-    sendingChainId: transactionData.sendingChainId,
-    receivingChainId: transactionData.receivingChainId,
-    callDataHash: transactionData.callDataHash,
-    transactionId: transactionData.transactionId,
-  });
-
-  const encodeVariantTransactionData = getTxManagerInterface().encodeFunctionData("variantTransactionData", [
-    invariantDigest,
-  ]);
-
-  const variantTransactionDigest = await txService.readTx({
-    chainId,
-    to: nxtpContractAddress,
-    data: encodeVariantTransactionData,
-  });
-
-  if (functionCall === "prepare") {
-    // variantTransactionDigest exist then transaction is already prepared
-    if (variantTransactionDigest !== HashZero) {
-      throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
-        requestContext,
-        methodContext,
-        variantTransactionDigest,
-      });
-    }
-  } else {
-    const expectedVariantDigest = getVariantTransactionDigest({
-      amount: transactionData.amount,
-      expiry: transactionData.expiry,
-      preparedBlockNumber: transactionData.preparedBlockNumber,
-    });
-
-    if (expectedVariantDigest === variantTransactionDigest) {
-      // All is good, no issues
-      return;
-    }
-
-    // transaction should be prepared before fulfill
-    if (variantTransactionDigest === HashZero) {
-      throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
-        requestContext,
-        methodContext,
-        variantTransactionDigest,
-      });
-    }
-
-    // transaction is already fulfilled
-    // get expected fulfilled/cancelled variant hash
-    const fulfilledOrCancelledVariant = getVariantTransactionDigest({
-      amount: transactionData.amount,
-      expiry: transactionData.expiry,
-      preparedBlockNumber: 0,
-    });
-
-    if (variantTransactionDigest === fulfilledOrCancelledVariant) {
-      throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
-        requestContext,
-        methodContext,
-        variantTransactionDigest,
-        fulfilledOrCancelledVariant,
-      });
-    }
-
-    if (functionCall === "cancel" && chainId === transactionData.sendingChainId) {
-      const receivingChainNxtpContractAddress = getContractAddress(transactionData.receivingChainId);
-
-      const receivingChainVariantTransactionDigest = await txService.readTx({
-        chainId: transactionData.receivingChainId,
-        to: receivingChainNxtpContractAddress,
-        data: encodeVariantTransactionData,
-      });
-
-      if (receivingChainVariantTransactionDigest === HashZero) {
-        // cancel is allowed when no transaction is prepared
-        return;
-      } else {
-        const receivingChainTransaction = await contractReader.getTransactionForChain(
-          transactionData.transactionId,
-          transactionData.user,
-          transactionData.receivingChainId,
-        );
-
-        if (receivingChainTransaction?.status === TransactionStatus.Cancelled) {
-          // cancel is allowed when transaction is cancelled on receiving chain
-          return;
-        } else {
-          throw new SanitationCheckFailed(functionCall, transactionData.transactionId, chainId, {
-            requestContext,
-            methodContext,
-          });
-        }
-      }
-    }
-  }
-};
+import {
+  getContractAddress,
+  getTxManagerInterface,
+  sanitationCheck,
+  getRouterContractInterface,
+  prepareEvt,
+} from "../../lib/helpers";
 
 /**
  * Method calls `prepare` on the `TransactionManager` on the given chain. Should be used to `prepare` the receiver-side transaction. Resolves when the transaction has been mined.
@@ -230,52 +42,50 @@ export const prepare = async (
 ): Promise<providers.TransactionReceipt> => {
   const { methodContext } = createLoggingContext(prepare.name);
 
-  const { logger, txService, wallet, config, messaging } = getContext();
+  const { logger, txService, wallet, config } = getContext();
   logger.info("Method start", requestContext, methodContext, { prepareParams });
 
   const { txData, amount, expiry, encodedBid, bidSignature, encryptedCallData } = prepareParams;
 
-  const nxtpContractAddress = getContractAddress(chainId);
-
-  const encodedData = getTxManagerInterface().encodeFunctionData("prepare", [
-    {
-      invariantData: txData,
+  // send to gelato service
+  const routerContractAddress = config.chainConfig[chainId]?.routerContractAddress;
+  if (
+    routerContractAddress &&
+    routerContractAddress === txData.router &&
+    isChainSupportedByGelato(chainId) &&
+    chainId === txData.receivingChainId
+  ) {
+    const signature = await signRouterPrepareTransactionPayload(
+      txData,
       amount,
       expiry,
       encryptedCallData,
       encodedBid,
       bidSignature,
-      encodedMeta: "0x",
-    },
-  ]);
+      "0x",
+      wallet,
+    );
+    const encodedData = getRouterContractInterface().encodeFunctionData("prepare", [
+      {
+        invariantData: txData,
+        amount,
+        expiry,
+        encryptedCallData,
+        encodedBid,
+        bidSignature,
+        encodedMeta: "0x",
+      },
+      signature,
+    ]);
 
-  if (config.chainConfig[chainId].routerContractAddress) {
-    if (isChainSupportedByGelato(chainId)) {
-      try {
-        await gelatoSend(
-          chainId,
-          nxtpContractAddress,
-          encodedData,
-          chainId === txData.sendingChainId ? txData.sendingAssetId : txData.receivingAssetId,
-          "0",
-        );
-      } catch (err) {}
-    } else {
-      await messaging.publishMetaTxRequest({
-        type: MetaTxTypes.Prepare,
-        relayerFee: "0",
-        to: nxtpContractAddress,
-        chainId,
-        data: {
-          txData,
-          amount,
-          expiry,
-          encryptedCallData,
-          encodedBid,
-          bidSignature,
-        },
-      });
+    try {
+      await gelatoSend(chainId, routerContractAddress, encodedData, txData.receivingAssetId, "0");
+    } catch (err) {
+      logger.error("gelato send failed", requestContext, methodContext, err as NxtpError, { prepareParams });
+
+      /// TODO fallback to metaTx routerContractPrepare
     }
+
     // listen for event on contract
     const { event } = await prepareEvt
       .pipe(({ args }) => args.txData.transactionId === txData.transactionId)
@@ -283,6 +93,19 @@ export const prepare = async (
     const receipt = await txService.getTransactionReceipt(chainId, event.transactionHash);
     return receipt;
   } else {
+    const nxtpContractAddress = getContractAddress(chainId);
+    const encodedData = getTxManagerInterface().encodeFunctionData("prepare", [
+      {
+        invariantData: txData,
+        amount,
+        expiry,
+        encryptedCallData,
+        encodedBid,
+        bidSignature,
+        encodedMeta: "0x",
+      },
+    ]);
+
     return await txService.sendTx(
       {
         to: nxtpContractAddress,
