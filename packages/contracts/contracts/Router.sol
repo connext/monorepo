a4c6c763e75e3817575a91e6636edaf503eb15b0
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-25 12:37:05 +0530

    add: move contracts on mono repo

---
+++ packages/contracts/contracts/Router.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "./interfaces/ITransactionManager.sol";
+import "./lib/LibAsset.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract Router is Ownable {
+
+  ITransactionManager public immutable transactionManager;
+
+  address public recipient;
+
+  address public signer;
+
+  uint256 private immutable chainId;
+
+  struct SignedRemoveLiquidityData {
+    uint256 amount;
+    address assetId;
+    uint256 chainId; // For domain separation
+    address signer; // For domain separation
+  }
+
+  constructor(address _transactionManager, address _signer, address _recipient, address _owner, uint256 _chainId) {
+    transactionManager = ITransactionManager(_transactionManager);
+    signer = _signer;
+    recipient = _recipient;
+    chainId = _chainId;
+    transferOwnership(_owner);
+  }
+
+  function setRecipient(address _recipient) external onlyOwner {
+    recipient = _recipient;
+  }
+
+  function setSigner(address _signer) external onlyOwner {
+    signer = _signer;
+  }
+
+  function removeLiquidity(uint256 amount, address assetId, bytes calldata signature) external {
+    if (msg.sender != signer) {
+      SignedRemoveLiquidityData memory payload = SignedRemoveLiquidityData({
+        amount: amount,
+        assetId: assetId,
+        chainId: chainId,
+        signer: signer
+      });
+      address recovered = recoverSignature(abi.encode(payload), signature);
+      require(recovered == signer, "Router signature is not valid");
+    }
+
+    return transactionManager.removeLiquidity(amount, assetId, payable(recipient));
+  }
+
+  function prepare(
+    ITransactionManager.PrepareArgs calldata args, 
+    bytes calldata signature
+  ) payable external returns (ITransactionManager.TransactionData memory) {
+    if (msg.sender != signer) {
+      address recovered = recoverSignature(abi.encode(args), signature);
+      require(recovered == signer, "Router signature is not valid");
+    }
+
+    return transactionManager.prepare{ value: LibAsset.isNativeAsset(args.invariantData.sendingAssetId) ? msg.value : 0 }(args);
+  }
+
+  function fulfill(
+    ITransactionManager.FulfillArgs calldata args, 
+    bytes calldata signature
+  ) external returns (ITransactionManager.TransactionData memory) {
+    if (msg.sender != signer) {
+      address recovered = recoverSignature(abi.encode(args), signature);
+      require(recovered == signer, "Router signature is not valid");
+    }
+
+    return transactionManager.fulfill(args);
+  }
+
+  function cancel(
+    ITransactionManager.CancelArgs calldata args, 
+    bytes calldata signature
+  ) external returns (ITransactionManager.TransactionData memory) {
+    if (msg.sender != signer) {
+      address recovered = recoverSignature(abi.encode(args), signature);
+      require(recovered == signer, "Router signature is not valid");
+    }
+
+    return transactionManager.cancel(args);
+  }
+
+  /**
+    * @notice Holds the logic to recover the signer from an encoded payload.
+    *         Will hash and convert to an eth signed message.
+    * @param encodedPayload The payload that was signed
+    * @param signature The signature you are recovering the signer from
+    */
+  function recoverSignature(bytes memory encodedPayload, bytes calldata  signature) internal pure returns (address) {
+    // Recover
+    return ECDSA.recover(
+      ECDSA.toEthSignedMessageHash(keccak256(encodedPayload)),
+      signature
+    );
+  }
+}
---
+++ packages/contracts/contracts/RouterFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "./interfaces/IRouterFactory.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ITransactionManager.sol";
+import "./Router.sol";
+
+contract RouterFactory is IRouterFactory, Ownable {
+
+  ITransactionManager public transactionManager;
+
+  constructor(address _transactionManager) {
+    transactionManager = ITransactionManager(_transactionManager);
+  }
+
+  function setTransactionManager(address _transactionManager) external onlyOwner {
+    transactionManager = ITransactionManager(_transactionManager);
+  }
+
+  function createRouter(address signer, address recipient, uint256 chainId) override external returns (address) {
+    Router router = new Router(address(transactionManager), signer, recipient, msg.sender, chainId);
+    emit RouterCreated(address(router));
+    return address(router);
+  }
+}
