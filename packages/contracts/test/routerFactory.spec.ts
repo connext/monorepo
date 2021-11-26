cf5bc7749cef49b128c244f2528ce6e5b6f13dfa
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-26 22:17:59 +0530

    update test

--- packages/contracts/test/routerFactory.spec.ts
+++ packages/contracts/test/routerFactory.spec.ts
@@ -1,12 +1,13 @@
-import { waffle } from "hardhat";
+import { ethers, waffle } from "hardhat";
 import { expect, use } from "chai";
 import { solidity } from "ethereum-waffle";
 
 use(solidity);
 
-import { Wallet, providers } from "ethers";
+import { Wallet, providers, Contract } from "ethers";
 import { TransactionManager } from "@connext/nxtp-contracts";
 import TransactionManagerArtifact from "@connext/nxtp-contracts/artifacts/contracts/TransactionManager.sol/TransactionManager.json";
+import RouterArtifact from "@connext/nxtp-contracts/artifacts/contracts/Router.sol/Router.json";
 
 import { deployContract, assertReceiptEvent } from "./utils";
 
@@ -14,7 +15,7 @@ import { deployContract, assertReceiptEvent } from "./utils";
 import { RouterFactory } from "../typechain";
 
 const createFixtureLoader = waffle.createFixtureLoader;
-describe("Router Contract", function () {
+describe.only("Router Contract", function () {
   const [wallet, routerSigner, recipient, other] = waffle.provider.getWallets() as Wallet[];
   let routerFactory: RouterFactory;
   let transactionManagerReceiverSide: TransactionManager;
@@ -77,9 +78,11 @@ describe("Router Contract", function () {
         router: computedRouterAddress,
         routerSigner: routerSigner.address,
         recipient: recipient.address,
-        owner: routerSigner.address,
         transactionManager: transactionManagerReceiverSide.address,
       });
+
+      const routerInstance = new Contract(computedRouterAddress, RouterArtifact.abi, ethers.provider);
+      expect(await routerInstance.routerFactory()).to.be.eq(routerFactory.address);
     });
   });
 });
