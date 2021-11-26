1e7156e3b78759b1a0c44943d2586c9cb80f8d2e
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-26 23:53:07 +0530

    update test

--- packages/contracts/test/routerFactory.spec.ts
+++ packages/contracts/test/routerFactory.spec.ts
@@ -15,23 +15,34 @@ import { deployContract, assertReceiptEvent } from "./utils";
 import { RouterFactory } from "../typechain";
 
 const createFixtureLoader = waffle.createFixtureLoader;
-describe.only("Router Contract", function () {
+describe("Router Contract", function () {
   const [wallet, routerSigner, recipient, other] = waffle.provider.getWallets() as Wallet[];
   let routerFactory: RouterFactory;
+  let routerFactorySec: RouterFactory;
+
+  let transactionManager: TransactionManager;
   let transactionManagerReceiverSide: TransactionManager;
+
+  const sendingChainId = 1338;
   const receivingChainId = 1338;
 
   const fixture = async () => {
+    transactionManager = await deployContract<TransactionManager>(TransactionManagerArtifact, receivingChainId);
+
     transactionManagerReceiverSide = await deployContract<TransactionManager>(
       TransactionManagerArtifact,
       receivingChainId,
     );
 
-    routerFactory = await deployContract<RouterFactory>(
-      "RouterFactory",
-      transactionManagerReceiverSide.address,
-      receivingChainId,
-    );
+    routerFactory = await deployContract<RouterFactory>("RouterFactory");
+
+    const initTx = await routerFactory.init(transactionManagerReceiverSide.address, receivingChainId);
+    await initTx.wait();
+
+    routerFactorySec = await deployContract<RouterFactory>("RouterFactory");
+
+    const initSecTx = await routerFactorySec.init(transactionManager.address, sendingChainId);
+    await initSecTx.wait();
 
     return {
       routerFactory,
@@ -67,11 +78,11 @@ describe.only("Router Contract", function () {
       const receipt = await tx.wait();
       expect(receipt.status).to.be.eq(1);
 
-      const computedRouterAddress = await routerFactory.getRouterAddress(
-        routerSigner.address,
-        recipient.address,
-        routerSigner.address,
-      );
+      const computedRouterAddress = await routerFactory.getRouterAddress(routerSigner.address);
+
+      const computedRouterAddressSec = await routerFactorySec.getRouterAddress(routerSigner.address);
+
+      console.log(computedRouterAddress, computedRouterAddressSec);
       expect(computedRouterAddress).to.be.a("string");
 
       await assertReceiptEvent(receipt, "RouterCreated", {
@@ -82,7 +93,10 @@ describe.only("Router Contract", function () {
       });
 
       const routerInstance = new Contract(computedRouterAddress, RouterArtifact.abi, ethers.provider);
-      expect(await routerInstance.routerFactory()).to.be.eq(routerFactory.address);
+
+      expect(await routerInstance.transactionManager()).to.be.eq(transactionManagerReceiverSide.address);
+      expect(await routerInstance.recipient()).to.be.eq(recipient.address);
+      expect(await routerInstance.routerSigner()).to.be.eq(routerSigner.address);
     });
   });
 });
