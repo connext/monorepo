59fcfbe1c1aaec2a1d07d3e28ee3b3beb17d516c
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-30 19:19:17 +0530

    add: relayer fee functionality

--- packages/contracts/contracts/Router.sol
+++ packages/contracts/contracts/Router.sol
@@ -7,6 +7,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Router is Ownable {
+  address public immutable routerFactory;
+
   ITransactionManager public transactionManager;
 
   uint256 private chainId;
@@ -22,7 +24,18 @@ contract Router is Ownable {
     address routerSigner; // For domain separation
   }
 
-  constructor() {}
+  event RelayerFeeAdded(address assetId, uint256 amount, address caller);
+  event RelayerFeeRemoved(address assetId, uint256 amount, address caller);
+
+  constructor(address _routerFactory) {
+    routerFactory = _routerFactory;
+  }
+
+  // Prevents from calling methods other than routerFactory contract
+  modifier onlyViaFactory() {
+    require(address(this) == routerFactory, "ONLY_VIA_FACTORY");
+    _;
+  }
 
   function init(
     address _transactionManager,
@@ -30,7 +43,7 @@ contract Router is Ownable {
     address _routerSigner,
     address _recipient,
     address _owner
-  ) external onlyOwner {
+  ) external onlyViaFactory {
     transactionManager = ITransactionManager(_transactionManager);
     chainId = _chainId;
     routerSigner = _routerSigner;
@@ -44,6 +57,34 @@ contract Router is Ownable {
 
   function setSigner(address _routerSigner) external onlyOwner {
     routerSigner = _routerSigner;
+  }
+
+  function addRelayerFee(uint256 amount, address assetId) external payable {
+    // Sanity check: nonzero amounts
+    require(amount > 0, "#ARF:002");
+
+    // Transfer funds to contract
+    // Validate correct amounts are transferred
+    if (LibAsset.isNativeAsset(assetId)) {
+      require(msg.value == amount, "#ARF:005");
+    } else {
+      require(msg.value == 0, "#ARF:006");
+      LibAsset.transferFromERC20(assetId, msg.sender, address(this), amount);
+    }
+
+    // Emit event
+    emit RelayerFeeAdded(assetId, amount, msg.sender);
+  }
+
+  function removeRelayerFee(uint256 amount, address assetId) external onlyOwner {
+    // Sanity check: nonzero amounts
+    require(amount > 0, "#RRF:002");
+
+    // Transfer funds to contract
+    LibAsset.transferAsset(assetId, payable(recipient), amount);
+
+    // Emit event
+    emit RelayerFeeRemoved(assetId, amount, msg.sender);
   }
 
   function removeLiquidity(
@@ -65,14 +106,20 @@ contract Router is Ownable {
     return transactionManager.removeLiquidity(amount, assetId, payable(recipient));
   }
 
-  function prepare(ITransactionManager.PrepareArgs calldata args, bytes calldata signature)
-    external
-    payable
-    returns (ITransactionManager.TransactionData memory)
-  {
+  function prepare(
+    ITransactionManager.PrepareArgs calldata args,
+    address relayerFeeAsset,
+    uint256 relayerFee,
+    bytes calldata signature
+  ) external payable returns (ITransactionManager.TransactionData memory) {
     if (msg.sender != routerSigner) {
       address recovered = recoverSignature(abi.encode(args), signature);
       require(recovered == routerSigner, "Router signature is not valid");
+
+      // Send the relayer the fee
+      if (relayerFee > 0) {
+        LibAsset.transferAsset(relayerFeeAsset, payable(msg.sender), relayerFee);
+      }
     }
 
     return
@@ -81,25 +128,39 @@ contract Router is Ownable {
       );
   }
 
-  function fulfill(ITransactionManager.FulfillArgs calldata args, bytes calldata signature)
-    external
-    returns (ITransactionManager.TransactionData memory)
-  {
+  function fulfill(
+    ITransactionManager.FulfillArgs calldata args,
+    address relayerFeeAsset,
+    uint256 relayerFee,
+    bytes calldata signature
+  ) external returns (ITransactionManager.TransactionData memory) {
     if (msg.sender != routerSigner) {
       address recovered = recoverSignature(abi.encode(args), signature);
       require(recovered == routerSigner, "Router signature is not valid");
+
+      // Send the relayer the fee
+      if (relayerFee > 0) {
+        LibAsset.transferAsset(relayerFeeAsset, payable(msg.sender), relayerFee);
+      }
     }
 
     return transactionManager.fulfill(args);
   }
 
-  function cancel(ITransactionManager.CancelArgs calldata args, bytes calldata signature)
-    external
-    returns (ITransactionManager.TransactionData memory)
-  {
+  function cancel(
+    ITransactionManager.CancelArgs calldata args,
+    address relayerFeeAsset,
+    uint256 relayerFee,
+    bytes calldata signature
+  ) external returns (ITransactionManager.TransactionData memory) {
     if (msg.sender != routerSigner) {
       address recovered = recoverSignature(abi.encode(args), signature);
       require(recovered == routerSigner, "Router signature is not valid");
+
+      // Send the relayer the fee
+      if (relayerFee > 0) {
+        LibAsset.transferAsset(relayerFeeAsset, payable(msg.sender), relayerFee);
+      }
     }
 
     return transactionManager.cancel(args);
