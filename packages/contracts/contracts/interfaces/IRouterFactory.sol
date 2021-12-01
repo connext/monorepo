b827edd9a1d67856a53799c37dc2528d90da82be
Sanchay Mittal <sanchaymittal@gmail.com>
2021-12-01 21:06:42 +0530

    update: interface

--- packages/contracts/contracts/interfaces/IRouterFactory.sol
+++ packages/contracts/contracts/interfaces/IRouterFactory.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.4;
 interface IRouterFactory {
   event RouterCreated(address router, address routerSigner, address recipient, address transactionManager);
 
+  function getRouterAddress(address routerSigner) external view returns (address);
+
   function createRouter(address router, address recipient) external returns (address);
 }
