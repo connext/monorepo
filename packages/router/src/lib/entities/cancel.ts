41b7f5cd626c7f0a1fd1abf52569e5386bb78a54
LayneHaber <layne.haber@gmail.com>
2021-12-03 11:42:38 -0700

    Add hashes to cancellation calls to handle arbitrum case

--- packages/router/src/lib/entities/cancel.ts
+++ packages/router/src/lib/entities/cancel.ts
@@ -1,10 +1,11 @@
 import { Type, Static } from "@sinclair/typebox";
-import { TIntegerString } from "@connext/nxtp-utils";
+import { TBytes32, TIntegerString } from "@connext/nxtp-utils";
 
 export const CancelInputSchema = Type.Object({
   amount: TIntegerString,
   expiry: Type.Number(),
   preparedBlockNumber: Type.Number(),
+  preparedTransactionHash: TBytes32,
   side: Type.RegEx(/sender|receiver/),
 });
 
