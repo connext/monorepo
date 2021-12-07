e2b85d514ef67fb468144046e648a159bef2848e
Rahul Sethuram <rahul@sethuram.com>
2021-12-07 17:32:57 +0100

    Moar test fixes

--- packages/router/test/lib/operations/fulfill.spec.ts
+++ packages/router/test/lib/operations/fulfill.spec.ts
@@ -24,23 +24,10 @@ describe("Fulfill Receiver Operation", () => {
       );
     });
 
-    it("should error if prepare input validation fails for side", async () => {
-      const _fulfillInputMock = { ...fulfillInputMock, side: "buggy" };
-      await expect(fulfill(invariantDataMock, _fulfillInputMock, requestContext)).to.eventually.be.rejectedWith(
-        "Params invalid",
-      );
-    });
-
-    it("should error if no config available for receiving chain", async () => {
+    it("should error if no config available for sending chain", async () => {
       await expect(
-        fulfill({ ...invariantDataMock, receivingChainId: 1234 }, fulfillInputMock, requestContext),
+        fulfill({ ...invariantDataMock, sendingChainId: 1234 }, fulfillInputMock, requestContext),
       ).to.eventually.be.rejectedWith("No chain config for chainId");
-    });
-
-    it("should error if relayer fee is lower than the lower bound", async () => {
-      await expect(
-        fulfill(invariantDataMock, { ...fulfillInputMock, relayerFee: "100000000000" }, requestContext),
-      ).to.eventually.be.rejectedWith("Not enough relayer fee");
     });
 
     it("happy: should fulfill on sender chain", async () => {
@@ -49,27 +36,6 @@ describe("Fulfill Receiver Operation", () => {
       expect(receipt).to.deep.eq(txReceiptMock);
       expect(contractWriterMock.fulfillTransactionManager).to.be.calledOnceWith(
         invariantDataMock.sendingChainId,
-        {
-          relayerFee: fulfillInputMock.relayerFee,
-          signature: fulfillInputMock.signature,
-          callData: fulfillInputMock.callData,
-          txData: {
-            ...invariantDataMock,
-            amount: fulfillInputMock.amount,
-            expiry: fulfillInputMock.expiry,
-            preparedBlockNumber: fulfillInputMock.preparedBlockNumber,
-          },
-        },
-        requestContext,
-      );
-    });
-
-    it("happy: should fulfill on receiver chain", async () => {
-      const receipt = await fulfill(invariantDataMock, fulfillInputMock, requestContext);
-
-      expect(receipt).to.deep.eq(txReceiptMock);
-      expect(contractWriterMock.fulfillTransactionManager).to.be.calledOnceWith(
-        invariantDataMock.receivingChainId,
         {
           relayerFee: fulfillInputMock.relayerFee,
           signature: fulfillInputMock.signature,
