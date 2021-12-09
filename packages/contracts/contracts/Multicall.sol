4b11d021ae871605288d3e66e1bfb2f9d7e1bd98
wanglonghong <wanglonghong4467@gmail.com>
2021-12-09 23:03:39 +0800

    test cases for multicall and price oralce

--- packages/contracts/contracts/Multicall.sol
+++ packages/contracts/contracts/Multicall.sol
@@ -20,32 +20,4 @@ contract Multicall {
         }
     }
 
-    // Helper functions
-    function getEthBalance(address addr) public view returns (uint256 balance) {
-        balance = addr.balance;
-    }
-
-    function getBlockHash(uint256 blockNumber) public view returns (bytes32 blockHash) {
-        blockHash = blockhash(blockNumber);
-    }
-
-    function getLastBlockHash() public view returns (bytes32 blockHash) {
-        blockHash = blockhash(block.number - 1);
-    }
-
-    function getCurrentBlockTimestamp() public view returns (uint256 timestamp) {
-        timestamp = block.timestamp;
-    }
-
-    function getCurrentBlockDifficulty() public view returns (uint256 difficulty) {
-        difficulty = block.difficulty;
-    }
-
-    function getCurrentBlockGasLimit() public view returns (uint256 gaslimit) {
-        gaslimit = block.gaslimit;
-    }
-
-    function getCurrentBlockCoinbase() public view returns (address coinbase) {
-        coinbase = block.coinbase;
-    }
 }
