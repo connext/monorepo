c0930d0d533f4af21673fe6b7216d50711f94c63
LayneHaber <layne.haber@gmail.com>
2021-12-17 11:35:28 -0700

    More detailed logs

--- packages/router/src/bindings/contractReader/index.ts
+++ packages/router/src/bindings/contractReader/index.ts
@@ -452,6 +452,10 @@ export const handleSingle = async (
         logger.info("Got fees in sending asset", requestContext, methodContext, {
           receivedAmount: _transaction.crosschainTx.receiving!.amount,
           sentAmount: _transaction.crosschainTx.sending.amount,
+          sendingAssetId: _transaction.crosschainTx.invariant.sendingAssetId,
+          sendingChainId: _transaction.crosschainTx.invariant.sendingChainId,
+          receivingAssetId: _transaction.crosschainTx.invariant.receivingAssetId,
+          receivingChainId: _transaction.crosschainTx.invariant.receivingChainId,
           fees,
         });
 
