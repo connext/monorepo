3b53d58b07cf381fdf2d69bc9a8008938136f925
wanglonghong <wanglonghong4467@gmail.com>
2021-12-22 20:38:18 +0800

    arbitrum gasLimits

--- packages/utils/src/gasEstimates.ts
+++ packages/utils/src/gasEstimates.ts
@@ -1,4 +1,6 @@
-export const GAS_ESTIMATES = {
+import { CHAIN_ID } from ".";
+
+export const DEFAULT_GAS_ESTIMATES = {
   prepare: "112000", // https://etherscan.io/tx/0x4e1be107ca80265b7ae65f77e00f14dd726d08dae763955fb1cf2a754d9cc1a8 example tx, add 5% buffer
   fulfill: "126000", // https://bscscan.com/tx/0xcaba240ab17f006586086cd460fffab09a028905d7c497c31667c1d5eb58e153 example tx, add 5% buffer
   cancel: "126000",
@@ -8,7 +10,39 @@ export const GAS_ESTIMATES = {
   prepareRouterContract: "121000",
   fulfillRouterContract: "160000",
   cancelRouterContract: "130000",
-  prepareRouterContractL1: "17300",
-  fulfillRouterContractL1: "11800",
-  cancelRouterContractL1: "11800",
+};
+
+export const GAS_LIMIT_MAP = {
+  [CHAIN_ID.ARBITRUM]: {
+    prepare: "2400000", // https://arbiscan.io/tx/0x381ec9af326241282a2d651799e7794dadfa21acd0bcec8c0898f829beb6508c
+    fulfill: "1935000", // https://arbiscan.io/tx/0x8d1dcf58d84aedce3d71d14697aefbf2173270c0e3ee7f6aacabc19a50127c29
+    cancel: "1935000",
+    prepareRouterContract: "2400000",
+    fulfillRouterContract: "1935000",
+    cancelRouterContract: "1935000",
+  },
+};
+
+export const getHardcodedGasLimits = (
+  chainId: number,
+): {
+  prepare: string;
+  fulfill: string;
+  cancel: string;
+  prepareRouterContract: string;
+  fulfillRouterContract: string;
+  cancelRouterContract: string;
+} => {
+  if (GAS_LIMIT_MAP[chainId]) {
+    return GAS_LIMIT_MAP[chainId];
+  } else {
+    return {
+      prepare: DEFAULT_GAS_ESTIMATES.prepare,
+      fulfill: DEFAULT_GAS_ESTIMATES.fulfill,
+      cancel: DEFAULT_GAS_ESTIMATES.cancel,
+      prepareRouterContract: DEFAULT_GAS_ESTIMATES.prepareRouterContract,
+      fulfillRouterContract: DEFAULT_GAS_ESTIMATES.fulfillRouterContract,
+      cancelRouterContract: DEFAULT_GAS_ESTIMATES.cancelRouterContract,
+    };
+  }
 };
