18c94adcbb873d36df6e174189096ce99d22a3a1
Liu <mailto123ads@gmail.com>
2021-12-23 22:23:02 +0800

    add unit tests for router/shared, status

--- packages/router/test/lib/operations/status.spec.ts
+++ packages/router/test/lib/operations/status.spec.ts
@@ -2,11 +2,18 @@ import { expect, createLoggingContext, mkBytes32 } from "@connext/nxtp-utils";
 
 import { getStatus } from "../../../src/lib/operations/status";
 
+import { ctxMock } from "../../globalTestHook";
+
 const { requestContext } = createLoggingContext("TEST", undefined, mkBytes32("0xabc"));
 
 describe("Status Operation", () => {
   it("should work", async () => {
+    const ctxMockAssets = ctxMock.config.swapPools[0].assets;
     const res = await getStatus(requestContext);
-    expect(res).to.be.ok;
+    const resSwapPools = res.swapPools;
+
+    ctxMockAssets.forEach(({ assetId, chainId }) => {
+      expect(resSwapPools.get(chainId).includes(assetId)).to.be.true;
+    });
   });
 });
