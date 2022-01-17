032660d9f9ad5ebea29dde83ee571ddfc085b388
Rahul Sethuram <rahul@sethuram.com>
2022-01-17 17:10:49 +0400

    Don't allow relayer for removeLiq

--- packages/router/src/bindings/fastify/index.ts
+++ packages/router/src/bindings/fastify/index.ts
@@ -73,36 +73,16 @@ export const bindFastify = () =>
         try {
           let result;
           if (isRouterContract) {
-            const routerRelayerFeeAsset = utils.getAddress(
-              config.chainConfig[chainId].routerContractRelayerAsset ?? constants.AddressZero,
-            );
-            const relayerFeeAssetDecimal = await txService.getDecimalsForAsset(chainId, routerRelayerFeeAsset);
-            const routerRelayerFee = await txService.calculateGasFee(
-              chainId,
-              routerRelayerFeeAsset,
-              relayerFeeAssetDecimal,
-              "removeLiquidity",
-              isRouterContract,
-              chainData,
-              requestContext,
-            );
-            const signature = await signRouterRemoveLiquidityTransactionPayload(
-              amount,
-              assetId,
-              routerRelayerFeeAsset,
-              routerRelayerFee.toString(),
-              chainId,
-              wallet,
-            );
+            // hardcode to only use router signer
             result = await contractWriter.removeLiquidityRouterContract(
               chainId,
               amount,
               assetId,
               routerAddress,
-              signature,
-              routerRelayerFeeAsset,
-              routerRelayerFee.toString(),
-              true,
+              "0x",
+              constants.AddressZero,
+              "0",
+              false,
               requestContext,
             );
           } else {
