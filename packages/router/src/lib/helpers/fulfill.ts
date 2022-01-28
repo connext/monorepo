1b043e47f7b7edf756d559cc4abf469903322e9f
Liu <mailto123ads@gmail.com>
2022-01-28 09:39:19 -0500

    add WATCH_TOWER_BUFFER

--- packages/router/src/lib/helpers/fulfill.ts
+++ packages/router/src/lib/helpers/fulfill.ts
@@ -1,3 +1,5 @@
 import { signRouterFulfillTransactionPayload as _signRouterFulfillTransactionPayload } from "@connext/nxtp-utils";
 
 export const signRouterFulfillTransactionPayload = _signRouterFulfillTransactionPayload;
+
+export const WATCH_TOWER_BUFFER = 2 * 3600; // time buffer if watchtower can handle fulfill
