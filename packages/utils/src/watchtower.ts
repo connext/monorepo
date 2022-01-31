c982d0ab0d8f2f642076ca826e35a77c57d2100b
Jake Kidd <jcb.kdd@gmail.com>
2022-01-31 15:46:35 -0800

    add batch status api method

--- packages/utils/src/watchtower.ts
+++ packages/utils/src/watchtower.ts
@@ -3,7 +3,16 @@ import axios from "axios";
 // TODO: add url
 const WATCHTOWER_URL = "placeholder";
 
-type TransactionStatusResponse = {
+export type BatchStatusResponse = {
+  id: string;
+  size: number; // Total number of transactions in the same batch.
+
+  // These values will be defined IFF there is an automated job set up to send this batch.
+  ttl?: number; // Time left until the batch gets submitted.
+  timestamp?: number; // Unix timestamp of the batch creation.
+};
+
+export type TransactionStatusResponse = {
   transaction: {
     // Sanity check portion.
     transactionId: string;
@@ -11,18 +20,17 @@ type TransactionStatusResponse = {
     expiry: number;
     chain: number;
   };
-  batch: {
-    id: string;
-    size: number; // Total number of transactions in the same batch.
-
-    // These values will be defined IFF there is an automated job set up to send this batch.
-    ttl?: number; // Time left until the batch gets submitted.
-    timestamp?: number; // Unix timestamp of the batch creation.
-  };
+  batch: BatchStatusResponse;
 };
 
 export const getBatchedTransactionStatus = async (chainId: number, transactionId: number) => {
   const formattedUrl = `${WATCHTOWER_URL}/chain/${chainId}/transaction/${transactionId}`;
   const response = await axios.get<TransactionStatusResponse>(formattedUrl);
+  return response.data;
+};
+
+export const getBatchStatus = async (chainId: number, transactionType: "senderfulfill" | "receiverfulfill") => {
+  const formattedUrl = `${WATCHTOWER_URL}/chain/${chainId}/type/${transactionType}`;
+  const response = await axios.get<BatchStatusResponse>(formattedUrl);
   return response.data;
 };
