feeb2c4dc478420c2cdb01cdf905d9851c21bba9
Jake Kidd <jcb.kdd@gmail.com>
2022-02-03 15:58:06 -0800

    fix hardhat tasks; check for router ownership renounced

--- packages/contracts/src/tasks/addLiquidity.ts
+++ packages/contracts/src/tasks/addLiquidity.ts
@@ -2,30 +2,30 @@ import { task } from "hardhat/config";
 
 export default task("add-liquidity", "Add liquidity for a router")
   .addParam("router", "Router address")
-  .addParam("assetId", "Token address")
+  .addParam("asset", "Token address")
   .addParam("amount", "Amount (real units)")
-  .addOptionalParam("txManagerAddress", "Override tx manager address")
+  .addOptionalParam("txmanageraddress", "Override tx manager address")
   .setAction(
     async (
-      { assetId, router, txManagerAddress: _txManagerAddress, amount },
+      { asset, router, txmanageraddress: _txmanageraddress, amount },
       { deployments, getNamedAccounts, ethers },
     ) => {
       const namedAccounts = await getNamedAccounts();
 
       console.log("router: ", router);
-      console.log("assetId: ", assetId);
+      console.log("asset: ", asset);
       console.log("namedAccounts: ", namedAccounts);
 
-      let txManagerAddress = _txManagerAddress;
-      if (!txManagerAddress) {
+      let txmanageraddress = _txmanageraddress;
+      if (!txmanageraddress) {
         const txManagerDeployment = await deployments.get("TransactionManager");
-        txManagerAddress = txManagerDeployment.address;
+        txmanageraddress = txManagerDeployment.address;
       }
-      console.log("txManagerAddress: ", txManagerAddress);
+      console.log("txmanageraddress: ", txmanageraddress);
 
-      const txManager = await ethers.getContractAt("TransactionManager", txManagerAddress);
-      if (assetId !== ethers.constants.AddressZero) {
-        const erc20 = await ethers.getContractAt("TestERC20", assetId);
+      const txManager = await ethers.getContractAt("TransactionManager", txmanageraddress);
+      if (asset !== ethers.constants.AddressZero) {
+        const erc20 = await ethers.getContractAt("TestERC20", asset);
         const balance = await erc20.balanceOf(namedAccounts.deployer);
         console.log("balance: ", balance.toString());
         if (balance.lt(amount)) {
@@ -42,26 +42,29 @@ export default task("add-liquidity", "Add liquidity for a router")
         }
       }
 
-      const approvedRouter = await txManager.approvedRouters(router);
-      console.log("approvedRouter: ", approvedRouter);
-      if (!approvedRouter) {
-        throw new Error("Router not approved");
+      const ownershipRenounced = await txManager.isRouterOwnershipRenounced();
+      if (!ownershipRenounced) {
+        const approvedRouter = await txManager.approvedRouters(router);
+        console.log("approvedRouter: ", approvedRouter);
+        if (!approvedRouter) {
+          throw new Error("Router not approved");
+        }
       }
 
-      const approvedAsset = await txManager.approvedAssets(assetId);
+      const approvedAsset = await txManager.approvedAssets(asset);
       console.log("approvedAsset: ", approvedAsset);
       if (!approvedAsset) {
         throw new Error("Asset not approved");
       }
 
-      const tx = await txManager.addLiquidityFor(amount, assetId, router, {
+      const tx = await txManager.addLiquidityFor(amount, asset, router, {
         from: namedAccounts.deployer,
-        value: assetId === ethers.constants.AddressZero ? amount : 0,
+        value: asset === ethers.constants.AddressZero ? amount : 0,
       });
       console.log("addLiquidityFor tx: ", tx);
       const receipt = await tx.wait();
       console.log("addLiquidityFor tx mined: ", receipt.transactionHash);
-      const liquidity = await txManager.routerBalances(router, assetId);
+      const liquidity = await txManager.routerBalances(router, asset);
       console.log("liquidity: ", liquidity.toString());
     },
   );
--- packages/contracts/src/tasks/addRouter.ts
+++ packages/contracts/src/tasks/addRouter.ts
@@ -2,21 +2,28 @@ import { task } from "hardhat/config";
 
 export default task("add-router", "Add a router")
   .addParam("router", "The router's address to add")
-  .addOptionalParam("txManagerAddress", "Override tx manager address")
-  .setAction(async ({ router, txManagerAddress: _txManagerAddress }, { deployments, getNamedAccounts, ethers }) => {
+  .addOptionalParam("txmanageraddress", "Override tx manager address")
+  .setAction(async ({ router, txmanageraddress: _txmanageraddress }, { deployments, getNamedAccounts, ethers }) => {
     const namedAccounts = await getNamedAccounts();
 
     console.log("router: ", router);
     console.log("namedAccounts: ", namedAccounts);
 
-    let txManagerAddress = _txManagerAddress;
-    if (!txManagerAddress) {
+    let txmanageraddress = _txmanageraddress;
+    if (!txmanageraddress) {
       const txManagerDeployment = await deployments.get("TransactionManager");
-      txManagerAddress = txManagerDeployment.address;
+      txmanageraddress = txManagerDeployment.address;
     }
-    console.log("txManagerAddress: ", txManagerAddress);
+    console.log("txmanageraddress: ", txmanageraddress);
 
-    const txManager = await ethers.getContractAt("TransactionManager", txManagerAddress);
+    const txManager = await ethers.getContractAt("TransactionManager", txmanageraddress);
+
+    const ownershipRenounced = await txManager.isRouterOwnershipRenounced();
+    if (ownershipRenounced) {
+      console.log("Ownership has been renounced already, no need for approval");
+      return;
+    }
+
     const approved = await txManager.approvedRouters(router);
     if (approved) {
       console.log("approved, no need to add");
