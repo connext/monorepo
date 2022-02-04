931749c2f9a62e6d3c73745ad150e87789e00d8f
Daniel Kmak <neverkuzi@gmail.com>
2022-02-04 11:27:23 +0000

    chore: bump hardhat godwoken plugin

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -178,7 +178,8 @@ const config: HardhatUserConfig = {
         privateKey: "0xd9066ff9f753a1898709b568119055660a77d9aae4d7a4ad677b8fb3d2a571e5",
         rollupTypeHash: "0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a",
         ethAccountLockCodeHash: "0xdeec13a7b8e100579541384ccaf4b5223733e4a5483c3aec95ddc4c1d5ea5b22",
-      },
+        delayAfterDeploy: 10000
+      }
     },
   },
 };
