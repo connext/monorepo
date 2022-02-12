9714a16286d712b8ecff567d2b851896cd6df53d
Daniel Cabrera <daniel@gelato.digital>
2022-02-12 14:30:47 +0100

    chore: Passed gelato relay tests

--- packages/utils/test/gelato.spec.ts
+++ packages/utils/test/gelato.spec.ts
@@ -1,7 +1,18 @@
+import axios from "axios";
+import { SinonStub, stub } from "sinon";
 import { expect } from "../src/expect";
 import { isChainSupportedByGelato } from "../src/gelato";
-
+let axiosGetStub: SinonStub;
+const mockGelatoGetSupportedRelays = {
+    relays: ["1"],
+};
 describe("isChainSupportedByGelato", () => {
+  beforeEach(async () => {
+    axiosGetStub = stub(axios, "get");
+    axiosGetStub.resetHistory();
+    axiosGetStub.resolves({ data: mockGelatoGetSupportedRelays });
+  });
+
   it("should work if a chain is supported by gelato", async () => {
     expect(await isChainSupportedByGelato(1)).to.be.true;
   });
@@ -9,4 +20,8 @@ describe("isChainSupportedByGelato", () => {
   it("should work if a chain is not supported by gelato", async () => {
     expect(await isChainSupportedByGelato(12345)).to.be.false;
   });
+
+  afterEach(async () => {
+    axiosGetStub.restore();
+  })
 });
