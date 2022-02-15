963e9dacfcfde2c69114a4238a95160af08a4dd9
@base0010 <marcus@gatekeeper.network>
2022-02-15 14:58:02 -0700

    bringing over redis from watchtower, added docker redis yml

---
+++ packages/auctioneer/src/adapters/RedisStoreManager.ts
@@ -0,0 +1,63 @@
+
+import { jsonifyError, Logger } from "@connext/nxtp-utils";
+import Redis from "ioredis";
+
+import { Batch } from "../../lib";
+
+type StoreManagerParams = { redisUrl: string; logger: Logger; redis?: Redis.Redis };
+
+export class StoreManager {
+  private static instance: StoreManager | undefined;
+
+  private readonly redis: Redis.Redis;
+
+  private readonly logger: Logger;
+
+  private constructor({ redisUrl, logger, redis }: StoreManagerParams) {
+    if (redis) {
+      this.redis = redis;
+    } else {
+      this.redis = new Redis(redisUrl);
+    }
+    this.logger = logger;
+  }
+
+  public static getInstance({ redisUrl, logger, redis }: StoreManagerParams): StoreManager {
+    if (StoreManager.instance) {
+      return StoreManager.instance;
+    } else {
+      const store = new StoreManager({ redis, redisUrl, logger });
+      StoreManager.instance = store;
+      return store;
+    }
+  }
+
+  private getKey(chain: number, id: string) {
+    return `${chain}-${id}`;
+  }
+
+  public async getBatch(chain: number, id: string): Promise<Batch> {
+    const _stored = await this.redis.get(this.getKey(chain, id));
+    let stored: Batch | undefined;
+    try {
+      stored = JSON.parse(_stored as string);
+    } catch (error: any) {
+      this.logger.error("Error parsing JSON", undefined, undefined, jsonifyError(error));
+    }
+    if (!stored) {
+      // If entry doesn't exist, create it.
+      stored = {
+        id,
+        chain,
+        transactions: {},
+        history: [],
+      };
+      await this.save(stored);
+    }
+    return stored;
+  }
+
+  public async save(batch: Batch): Promise<void> {
+    await this.redis.set(this.getKey(batch.chain, batch.id), JSON.stringify(batch));
+  }
+}
--- packages/auctioneer/src/createAuctioneer.ts
+++ packages/auctioneer/src/createAuctioneer.ts
@@ -2,29 +2,58 @@ import { fastify, FastifyInstance } from 'fastify';
 import { gelatoSend, gelatoFulfill, isChainSupportedByGelato } from '@connext/nxtp-utils';
 import bidRoute from './handlers/bid';
 import pino from 'pino';
+import { StoreManager } from './adapters/RedisStoreManager';
+import Redis from 'ioredis';
+
+import { Logger } from "@connext/nxtp-utils";
 
 
-const REDIS_URL = process.env.REDIS_URL || 'http://localhost:6379';
+// const REDIS_URL = process.env.REDIS_URL || 'http://localhost:6379';
 const LISTEN_PORT = process.env.PORT || 1234;
 const LOG_LEVEL = process.env.loglevel || 'debug';
 
-const pino_logger = pino({ level: LOG_LEVEL });
 
-export async function fastifyStart(): Promise<FastifyInstance> {
-  const server = fastify({ logger: pino_logger });
-  try {
+export class Auctioneer {
+  store: StoreManager;
+  server: FastifyInstance;
+  logger: Logger;
 
-    //register bid routes;
-    server.register(bidRoute);
-    await server.listen(LISTEN_PORT);
-
-    server.log.info({},
-      `Auctioneer Listening @ ${LISTEN_PORT}`
-    );
-  }
-  catch (e) {
-    server.log.error(e);
+  constructor() {
+    
   }
 
-  return server;
+  async fastifyStart(): Promise<FastifyInstance> {
+    //nxtp logger
+    this.logger = new Logger({ level: 'debug' });
+
+    //fastify logger
+    const pino_logger = pino({ level: LOG_LEVEL });
+    this.server = fastify({ logger: pino_logger });
+
+    try {
+  
+      //register bid routes;
+      this.server.register(bidRoute);
+      await this.server.listen(LISTEN_PORT);
+  
+      this.server.log.info({},
+        `Auctioneer Listening @ ${LISTEN_PORT}`
+      );
+    }
+    catch (e) {
+      this.server.log.error(e);
+    }
+  
+    return this.server;
+   }
+  
+  async redisStart() {
+    const redis = new Redis();
+    let sManager: StoreManager;
+    sManager = StoreManager.getInstance({ redisUrl: "", logger: this.logger, redis });
+    
+    if (sManager) {
+      this.store = sManager;
+    }
+  }
 }
