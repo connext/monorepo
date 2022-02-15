f365f83b52244741b2de6846af98fd30068623fd
Jake Kidd <jcb.kdd@gmail.com>
2022-02-15 14:54:41 -0800

    add auctioneer stub

--- packages/router/src/adapters/auctioneer/index.ts
+++ packages/router/src/adapters/auctioneer/index.ts
@@ -9,7 +9,13 @@ export class Auctioneer {
 
   // TODO: MetaTx type
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async sendBid(metaTx: any) {
+  public async sendBid(metaTx: any) {
+    throw new Error("Not implemented");
+  }
+
+  public async getAuctionCycleTime(): Promise<number> {
+    // TODO: Should be cached locally, this value will reflect the period for which a
+    // transaction's auction will last, i.e. 30s.
     throw new Error("Not implemented");
   }
 }
