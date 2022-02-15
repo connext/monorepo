d6c5bd3fc4e61bf148fe847a7980af33e0cae2e3
Rahul Sethuram <rahul@sethuram.com>
2022-02-15 16:48:05 -0700

    Subgraph stuffs

--- packages/read-subgraph/src/index.ts
+++ packages/read-subgraph/src/index.ts
@@ -3,3 +3,5 @@ import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./runtime/graphqlsdk";
 
 export const getRuntimeSdk = (url: string) => getSdk(new GraphQLClient(url));
+
+export { Sdk, GetPreparedTransactionsQuery } from "./runtime/graphqlsdk";
