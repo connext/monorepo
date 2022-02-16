37375f3cc4f3c0e402f18d48605a83337e55e2bf
Sanchay Mittal <sanchaymittal@gmail.com>
2022-02-16 17:18:06 +0530

    new: entities and helper

---
+++ packages/read-subgraph/src/lib/entities/index.ts
@@ -0,0 +1,28 @@
+import { Type, Static } from "@sinclair/typebox";
+import { FallbackSubgraph, SubgraphDomain } from "@connext/nxtp-utils";
+import { Sdk, GetPreparedTransactionsQuery } from "../../adapters/runtime/graphqlsdk";
+import { ajv, ChainData, TAddress, TIntegerString } from "@connext/nxtp-utils";
+
+const MIN_SUBGRAPH_MAX_LAG = 25;
+
+export const TChainConfig = Type.Object({
+  subgraph: Type.Object({
+    analytics: Type.Array(Type.String()), // Analytics subgraph uri(s).
+    runtime: Type.Array(Type.String()), // Runtime subgraph uri(s).
+    maxLag: Type.Integer({ minimum: MIN_SUBGRAPH_MAX_LAG }), // If subgraph is out of sync by this number, will not process actions.
+  }),
+});
+
+export type ChainConfig = Static<typeof TChainConfig>;
+
+export const ReadSubgraphConfigSchema = Type.Object({
+  chains: Type.Record(TIntegerString, TChainConfig),
+});
+
+export type ReadSubgraphConfig = Static<typeof ReadSubgraphConfigSchema>;
+
+export type ChainSubgraphs = {
+  runtime: FallbackSubgraph<Sdk>;
+};
+
+export type SubgraphMap = Map<number, ChainSubgraphs>;
