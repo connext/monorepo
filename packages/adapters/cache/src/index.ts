a749851ff93851d7bcd5e0bfbcbd7e99f6ed052a
@base0010 <marcus@gatekeeper.network>
2022-02-16 21:25:04 -0700

    stubbing out cache adapter interface/class

--- packages/adapters/cache/src/index.ts
+++ packages/adapters/cache/src/index.ts
@@ -1,7 +1,8 @@
 import { BigNumber } from "ethers";
 import { AuctionCache } from "./auction";
-import { CacheConfig } from "./lib/entities";
 
+import { Logger, TransactionData } from "@connext/nxtp-utils";
+import Redis from "ioredis";
 
 //Redis Store I
 
@@ -15,21 +16,75 @@ import { CacheConfig } from "./lib/entities";
 export type ChainCache = {
   auctions: AuctionCache;
 };
+type StoreManagerParams = { redisUrl: string; logger: Logger; redis?: Redis.Redis };
 
 // TODO: This storage endpoint should be considered a stub for a future redis / permanent storage solution.
 export class TransactionCache {
   private readonly cache: Map<string, ChainCache> = new Map();
 
-  constructor(config: CacheConfig) {
-    for (const chain of Object.keys(config.chains)) {
-      this.cache.set(chain, {
-        auctions: new AuctionCache(),
-      });
+  private static instance: TransactionCache | undefined;
+
+  private readonly txData: Redis.Redis;
+  private readonly txStatus: Redis.Redis;
+
+  private readonly logger: Logger;
+
+  private constructor({ redisUrl, logger, redis }: StoreManagerParams) {
+    if (redis) {
+    } else {
+      this.txData = new Redis(redisUrl + "/1");
+      this.txStatus = new Redis(redisUrl + "/2");
+    }
+    this.logger = logger;
+  }
+
+  public static getInstance({ redisUrl, logger, redis }: StoreManagerParams): TransactionCache {
+    if (TransactionCache.instance) {
+      return TransactionCache.instance;
+    } else {
+      const store = new TransactionCache({ redis, redisUrl, logger });
+      TransactionCache.instance = store;
+      return store;
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public async getOpenTxNonce(chain: number): Promise<BigNumber> {
-    throw new Error("Not implemented");
+  public async getStatus(domain: string, nonce: string): Promise<Bid | undefined>{
+    const status = this.txStatus.scanStream({
+      match: `${domain}:${nonce}`,
+      count: 1,
+    });
+
+    const recordStatus = await status.read(1);
+    //todo: cast to status enum
+    return recordStatus;
+  }
+
+  public async getLatestNonce(domain: string): Promise<TransactionData | undefined>{
+    
+    //assuming theres a guranetee it starts from latest record
+
+    this.txData.zrevrangebyscore();
+    const stream = this.txData.scanStream({
+      // only returns keys following the pattern of `user:*`
+      match: `${domain}:*`,
+      // only return objects that match a given type,
+      // (requires Redis >= 6.0)
+      // returns approximately 100 elements per call
+      count: 1,
+    });
+
+    const latestRecordForDomain = await stream.read(1);
+    const parsedRecord = JSON.parse(latestRecordForDomain);
+
+    return parsedRecord;
+    
+  }
+
+  public async storeStatus(cache: any): Promise<void> {
+    await this.txStatus.set(cache.nxtpId, JSON.stringify(cache.bid));
+  }
+
+  public async storeTxData(data: any): Promise<void>{
+    await this.txData.set(data.nxtpId, JSON.stringify(""));
   }
 }
--- packages/auctioneer/src/adapters/RedisStoreManager.ts
+++ packages/auctioneer/src/adapters/RedisStoreManager.ts
@@ -1,7 +1,6 @@
 
 import { jsonifyError, Logger, TransactionData } from "@connext/nxtp-utils";
 import Redis from "ioredis";
-import { Stream } from "stream";
 import { Bid } from "../lib/types";
 
 type StoreManagerParams = { redisUrl: string; logger: Logger; redis?: Redis.Redis };
@@ -36,26 +35,24 @@ export class StoreManager {
   public async getStatus(domain: string, nonce: string): Promise<Bid | undefined>{
     const status = this.txStatus.scanStream({
       match: `${domain}:${nonce}`,
-      type: "zset",
       count: 1,
     });
 
     const recordStatus = await status.read(1);
     //todo: cast to status enum
     return recordStatus;
-
-    
   }
 
   public async getLatestNonce(domain: string): Promise<TransactionData | undefined>{
     
     //assuming theres a guranetee it starts from latest record
+
+    this.txData.zrevrangebyscore()
     const stream = this.txData.scanStream({
       // only returns keys following the pattern of `user:*`
       match: `${domain}:*`,
       // only return objects that match a given type,
       // (requires Redis >= 6.0)
-      type: "zset",
       // returns approximately 100 elements per call
       count: 1,
     });
@@ -67,7 +64,11 @@ export class StoreManager {
     
   }
 
-  public async save(cache: any): Promise<void> {
+  public async storeStatus(cache: any): Promise<void> {
     await this.redis.set(cache.nxtpId, JSON.stringify(cache.bid));
+  }
+
+  public async storeTxData(data: any): Promise<void>{
+    
   }
 }
