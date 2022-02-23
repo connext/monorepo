6057c3abd9bc604f325777b74abecbdc079d7dab
Liu <mailto123ads@gmail.com>
2022-02-23 09:34:20 -0500

    stable swap deploy script

---
+++ packages/contracts/deploy/deployStableswap.ts
@@ -0,0 +1,70 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments } = hre;
+
+  let deployer;
+  ({ deployer } = await hre.getNamedAccounts());
+  if (!deployer) {
+    [deployer] = await hre.getUnnamedAccounts();
+  }
+  console.log("deploying stable swap");
+  console.log("deployer: ", deployer);
+
+  /////////////////////////////////////////////////////////////////////////////////
+  ////  LP Token
+  /////////////////////////////////////////////////////////////////////////////////
+  const lpToken = await deployments.getOrNull("LPToken");
+  if (lpToken) {
+    console.log(`reusing "LPToken" at ${lpToken.address}`);
+  } else {
+    await deployments.deploy("LPToken", {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+    });
+
+    await deployments.execute(
+      "LPToken",
+      { from: deployer, log: true },
+      "initialize",
+      "Nxtp Stable LP Token",
+      "NxtpStableLPToken",
+    );
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////
+  ////  AmplificationUtils
+  /////////////////////////////////////////////////////////////////////////////////
+  await deployments.deploy("AmplificationUtils", {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+
+  /////////////////////////////////////////////////////////////////////////////////
+  ////  SwapUtils
+  /////////////////////////////////////////////////////////////////////////////////
+  await deployments.deploy("SwapUtils", {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+
+  /////////////////////////////////////////////////////////////////////////////////
+  ////  StableSwap
+  /////////////////////////////////////////////////////////////////////////////////
+  await deployments.deploy("StableSwap", {
+    from: deployer,
+    log: true,
+    libraries: {
+      SwapUtils: (await deployments.get("SwapUtils")).address,
+      AmplificationUtils: (await deployments.get("AmplificationUtils")).address,
+    },
+    skipIfAlreadyDeployed: true,
+  });
+};
+
+export default func;
+func.tags = ["StableSwap"];
