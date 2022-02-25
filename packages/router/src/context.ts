3771e09083c81a3dc6989e774785c99bcb5eb3fc
wanglonghong <wanglonghong4467@gmail.com>
2022-02-25 09:42:01 +0800

    cleanup context

--- packages/router/src/context.ts
+++ packages/router/src/context.ts
@@ -2,7 +2,7 @@ import { Wallet } from "ethers";
 import { Logger } from "@connext/nxtp-utils";
 import { ChainReader } from "@connext/nxtp-txservice";
 import { AuctioneerAPI } from "@connext/nxtp-adapters-auctioneer";
-import { TransactionCache } from "@connext/nxtp-adapters-cache";
+import { StoreManager } from "@connext/nxtp-adapters-cache";
 import { SubgraphReader } from "@connext/nxtp-adapters-subgraph";
 import { Web3Signer } from "@connext/nxtp-adapters-web3signer";
 
@@ -15,7 +15,7 @@ export type AppContext = {
     wallet: Wallet | Web3Signer; // Used for signing metatxs for bids.
     subgraph: SubgraphReader; // Aggregates subgraphs in a FallbackSubgraph for each chain.
     auctioneer: AuctioneerAPI; // Auctioneer HTTP API interface.
-    cache: TransactionCache; // Used to cache important data locally.
+    cache: StoreManager; // Used to cache important data locally.
     chainreader: ChainReader; // Used to read from the blockchain using RPC providers.
   };
   config: NxtpRouterConfig;
