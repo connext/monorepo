e0f107e3b43da3f3bb18176ac046ff1ed5745d7b
wanglonghong <wanglonghong4467@gmail.com>
2022-02-25 23:50:26 +0800

    chainId vs nomad

--- packages/utils/src/nomad.ts
+++ packages/utils/src/nomad.ts
@@ -1,36 +1,24 @@
-import { utils } from "ethers";
+const chainIdToDomainNameMapping: Map<number, string> = new Map([
+  // TODO: chainId vs nomad name should be added for all the networks
+  [1, "eth"], // Ethereum ('eth interpreted as int)
+  [1284, "beam"], // Moonbeam ('beam interpreted as int)
+]);
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export const getChainIdFromDomain = (domain: string): number => {
-  // TODO: get chainId from domain.
-  // TODO: move this function in utils after cleanup
-  return 1;
+export const getChainIdFromDomain = (domainName: string): number => {
+  // TODO: get chainId from domain name.
+  const filtered = [...chainIdToDomainNameMapping.entries()].filter((it) => it[1] === domainName);
+  if (!filtered || filtered.length != 1) throw new Error(`Duplicate domain name ${domainName}`);
+  return filtered[0][0];
 };
 
 export const getDomainFromChainId = (chainId: number): string => {
   // TODO: get domain from chainId.
   // TODO: move this function in utils after cleanup
-  return "1";
+  const domainName = chainIdToDomainNameMapping.get(chainId);
+  if (!domainName) throw new Error(`Cannot find corresponding Nomad domain for chainId ${chainId}`);
+
+  return getHexDomainFromString(domainName);
 };
-
-// Hex domains calculated using `getHexDomainFromString`
-const chainIdToDomainMapping: Map<number, number> = new Map([
-  [1, 0x657468], // Ethereum ('eth interpreted as int)
-  [1284, 0x6265616d], // Moonbeam ('beam interpreted as int)
-]);
-
-/**
- * Converts a chain id (listed at at chainlist.org) to a Nomad domain.
- *
- * @param chainId A chain id number
- * @returns A Nomad domain number in decimal
- */
-export function chainIdToDomain(chainId: number): number {
-  const domain = chainIdToDomainMapping.get(chainId);
-  if (!domain) throw new Error(`Cannot find corresponding Nomad domain for chainId ${chainId}`);
-
-  return domain;
-}
 
 /**
  * Converts a string (e.g. "eth" for Ethereum) to a decimal formatted Nomad
@@ -56,44 +44,4 @@ export function getDomainFromString(name: string): number {
 export function getHexDomainFromString(name: string): string {
   const domain = getDomainFromString(name);
   return "0x" + domain.toString(16);
-}
-
-/**
- * Converts a 20-byte (or other length) ID to a 32-byte ID.
- * Ensures that a bytes-like is 32 long. left-padding with 0s if not.
- *
- * @param data A string or array of bytes to canonize
- * @returns A Uint8Array of length 32
- * @throws if the input is undefined, or not exactly 20 or 32 bytes long
- */
-export function canonizeId(data?: utils.BytesLike): Uint8Array {
-  if (!data) throw new Error("Bad input. Undefined");
-
-  const buf = utils.arrayify(data);
-  if (buf.length > 32) throw new Error("Too long");
-  if (buf.length !== 20 && buf.length != 32) {
-    throw new Error("bad input, expect address or bytes32");
-  }
-  return utils.zeroPad(buf, 32);
-}
-
-/**
- * Converts an Nomad ID of 20 or 32 bytes to the corresponding EVM Address.
- *
- * For 32-byte IDs this enforces the EVM convention of using the LAST 20 bytes.
- *
- * @param data The data to truncate
- * @returns A 20-byte, 0x-prepended hex string representing the EVM Address
- * @throws if the data is not 20 or 32 bytes
- */
-export function evmId(data: utils.BytesLike): Address {
-  const u8a = utils.arrayify(data);
-
-  if (u8a.length === 32) {
-    return utils.hexlify(u8a.slice(12, 32));
-  } else if (u8a.length === 20) {
-    return utils.hexlify(u8a);
-  } else {
-    throw new Error(`Invalid id length. expected 20 or 32. Got ${u8a.length}`);
-  }
 }
