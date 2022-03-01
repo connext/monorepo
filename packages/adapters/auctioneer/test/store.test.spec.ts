6b6e745561d4ea3c2709cb9f7dad7608440c6c36
@base0010 <marcus@gatekeeper.network>
2022-03-01 05:44:46 -0700

    dockerizing sequencer, dep problem with auctioneer adapter

--- packages/adapters/auctioneer/test/store.test.spec.ts
+++ packages/adapters/auctioneer/test/store.test.spec.ts
@@ -5,6 +5,6 @@ import { Subscriptions } from "@connext/nxtp-adapters-cache/src/lib/entities";
 
 describe('Sequencer::Store Test', async () => {
   const subscriptions: Subscriptions = new Map();
-  const transactions = new TransactionsCache({ url:"http://localhost:6379", subscriptions: subscriptions });
-  
+  const transactions = new TransactionsCache({ url: "redis://localhost:6379", subscriptions: subscriptions });
+  await transactions.storeStatus({nxtpId:"0xdeadbeef", bid:{tprop:"test"}});
 })
