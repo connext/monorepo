76d5276f66b080630dfe4f3ebb9effb2aa505c6f
wanglonghong <wanglonghong4467@gmail.com>
2022-03-02 17:09:00 +0800

    stableSwap interface

--- packages/agents/router/src/lib/helpers/interface.ts
+++ packages/agents/router/src/lib/helpers/interface.ts
@@ -2,13 +2,17 @@ import { Interface } from "ethers/lib/utils";
 import {
   TransactionManager as TTransactionManager,
   ConnextPriceOracle as TConnextPriceOracle,
+  StableSwap as TStableSwap,
   ERC20 as TERC20,
 } from "@connext/nxtp-contracts/typechain-types";
 
 import TransactionManagerArtifact from "@connext/nxtp-contracts/artifacts/contracts/TransactionManager.sol/TransactionManager.json";
 import PriceOracleArtifact from "@connext/nxtp-contracts/artifacts/contracts/ConnextPriceOracle.sol/ConnextPriceOracle.json";
+import StableSwapArtifact from "@connext/nxtp-contracts/artifacts/contracts/StableSwap.sol/StableSwap.json";
 
 export const getTxManagerInerface = () =>
   new Interface(TransactionManagerArtifact.abi) as TTransactionManager["interface"];
 
 export const getPriceOracleInterface = () => new Interface(PriceOracleArtifact.abi) as TConnextPriceOracle["interface"];
+
+export const getStableSwapInterface = () => new Interface(StableSwapArtifact.abi) as TStableSwap["interface"];
