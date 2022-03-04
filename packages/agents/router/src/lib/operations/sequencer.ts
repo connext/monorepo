db52198512daf5820677577dfc7601e92c0ccb88
Sanchay Mittal <sanchaymittal@gmail.com>
2022-03-04 19:05:14 +0530

    new: sendBid operation

---
+++ packages/agents/router/src/lib/operations/sequencer.ts
@@ -0,0 +1,29 @@
+import { createLoggingContext, SignedBid, NxtpError } from "@connext/nxtp-utils";
+import { getContext } from "../../router";
+import axios, { AxiosResponse } from "axios";
+
+export const sendBid = async (bid: SignedBid): Promise<any> => {
+  const { requestContext, methodContext } = createLoggingContext(sendBid.name);
+  const { logger, config, chainData, routerAddress } = getContext();
+
+  /// TODO don't send the signature in logs, edit bid during logging
+  logger.info("Method start", requestContext, methodContext, { bid });
+
+  let response: AxiosResponse<string> = await axios.post(formatUrl(config.sequencerUrl, "bid"), {
+    bid,
+  });
+
+  if (!response || !response.data || response.data.length === 0) {
+    throw new NxtpError("error sendBid", { response });
+  } else {
+    return response.data;
+  }
+};
+
+export const formatUrl = (_url: string, endpoint: string, identifier?: string): string => {
+  let url = `${_url}/${endpoint}`;
+  if (identifier) {
+    url += `/${identifier}`;
+  }
+  return url;
+};
