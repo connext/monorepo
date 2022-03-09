763a8e116a2f8d78dbcf38cb09382f4df99b78e0
wanglonghong <wanglonghong4467@gmail.com>
2022-03-09 16:19:28 +0800

    unit tests for router

--- packages/agents/router/test/router.spec.ts
+++ packages/agents/router/test/router.spec.ts
@@ -2,30 +2,14 @@ import { expect, mkAddress } from "@connext/nxtp-utils";
 import { createStubInstance } from "sinon";
 import { TransactionService } from "@connext/nxtp-txservice";
 
-import { getSwapPoolMap } from "../src/router";
 import { constants } from "ethers";
 
 describe("Router", () => {
-  describe("#getSwapPoolMap", () => {
-    it("should error if router contract not present for a chain", async () => {
-      const txService = createStubInstance(TransactionService);
-      txService.getCode.onSecondCall().resolves("0x");
-      await expect(
-        getSwapPoolMap({
-          routerAddress: mkAddress(),
-          swapPools: [
-            {
-              name: "TEST",
-              assets: [
-                { chainId: 1, assetId: constants.AddressZero },
-                { chainId: 2, assetId: constants.AddressZero },
-              ],
-            },
-          ],
-          isRouterContract: true,
-          txService: txService as any,
-        }),
-      ).to.be.rejectedWith("Router Contract isn't deployed");
-    });
+  describe("#setupCache", () => {
+    it("should error if fulfill fails", async () => {});
+    it("happy case: should fulfill", async () => {});
+  });
+  describe("#setupSubgraphReader", () => {
+    it("happy case: sugraph setup should be done", async () => {});
   });
 });
