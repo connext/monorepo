4f0dd16268df1346e3a1395846d5339bf76fdf3f
Sanchay Mittal <sanchaymittal@gmail.com>
2022-03-09 14:18:32 +0530

    add: helpers interface in sequencer

---
+++ packages/agents/sequencer/src/lib/helpers/interface.ts
@@ -0,0 +1,22 @@
+import { Interface } from "ethers/lib/utils";
+import {
+  TransactionManager as TTransactionManager,
+  ConnextPriceOracle as TConnextPriceOracle,
+  TokenRegistry as TTokenRegistry,
+  StableSwap as TStableSwap,
+  ERC20 as TERC20,
+} from "@connext/nxtp-contracts/typechain-types";
+
+import TransactionManagerArtifact from "@connext/nxtp-contracts/artifacts/contracts/TransactionManager.sol/TransactionManager.json";
+import PriceOracleArtifact from "@connext/nxtp-contracts/artifacts/contracts/ConnextPriceOracle.sol/ConnextPriceOracle.json";
+import StableSwapArtifact from "@connext/nxtp-contracts/artifacts/contracts/StableSwap.sol/StableSwap.json";
+import TokenRegistryArtifact from "@connext/nxtp-contracts/artifacts/contracts/nomad-xapps/contracts/bridge/TokenRegistry.sol/TokenRegistry.json";
+
+export const getTxManagerInerface = () =>
+  new Interface(TransactionManagerArtifact.abi) as TTransactionManager["interface"];
+
+export const getTokenRegistryInterface = () => new Interface(TokenRegistryArtifact.abi) as TTokenRegistry["interface"];
+
+export const getPriceOracleInterface = () => new Interface(PriceOracleArtifact.abi) as TConnextPriceOracle["interface"];
+
+export const getStableSwapInterface = () => new Interface(StableSwapArtifact.abi) as TStableSwap["interface"];
