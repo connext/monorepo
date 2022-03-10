b9955a680aafd9ed50449e4efe41b29b1f244631
Sanchay Mittal <sanchaymittal@gmail.com>
2022-03-10 13:02:57 +0530

    add: sendToRelayer

---
+++ packages/agents/sequencer/src/lib/operations/relayer.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+import {
+  gelatoSend,
+  isChainSupportedByGelato,
+  SignedBid,
+  RequestContext,
+  createLoggingContext,
+  formatUrl,
+  gelatoRelayEndpoint,
+} from "@connext/nxtp-utils";
+import { getTxManagerInterface } from "@connext/nxtp-contracts/src";
+
+import { AppContext } from "../../context";
+
+export const sendToRelayer = async (context: AppContext, signedBid: SignedBid, _requestContext: RequestContext) => {
+  const {
+    logger,
+    chainData,
+    adapters: { chainreader, cache },
+    config,
+  } = context;
+  const { requestContext, methodContext } = createLoggingContext(sendToRelayer.name, _requestContext);
+  logger.info(`Method start: ${sendToRelayer.name}`, requestContext, methodContext, { signedBid });
+
+  const { bid } = signedBid;
+  const destinationChainId = chainData.get(bid.data.params.destinationDomain)!.chainId;
+  const destinationTransactionManagerAddress =
+    config.chains[bid.data.params.destinationDomain].deployments.transactionManager;
+
+  const encodedData = getTxManagerInterface().encodeFunctionData("fulfill", [bid.data]);
+
+  if (!isChainSupportedByGelato(destinationChainId)) {
+    throw new Error("Chain not supported by gelato.");
+  }
+
+  logger.info("Sending to Gelato network", requestContext, methodContext, {
+    encodedData,
+    destinationTransactionManagerAddress,
+    domain: bid.data.params.destinationDomain,
+  });
+
+  const result = await gelatoSend(
+    destinationChainId,
+    destinationTransactionManagerAddress,
+    encodedData,
+    bid.data.local,
+    bid.data.feePercentage,
+  );
+  const response = await axios.get(formatUrl(gelatoRelayEndpoint, "tasks", result.taskId));
+  // TODO: check response, if it didn't work, send the next!
+
+  logger.info("Sent to Gelato network", requestContext, methodContext, {
+    result,
+    taskId: result.taskId,
+    response: response.data,
+  });
+};
