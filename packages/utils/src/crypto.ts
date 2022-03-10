b5eca9f0da358050b0fbd98dadf0a683bc5becf0
Jake Kidd <jcb.kdd@gmail.com>
2022-03-10 13:44:12 -0800

    remove deprecated dependency

--- packages/utils/src/crypto.ts
+++ packages/utils/src/crypto.ts
@@ -1,4 +1,3 @@
-import { encrypt as libEncrypt } from "eth-sig-util";
 import { utils } from "ethers";
 import { publicKeyConvert } from "secp256k1";
 
@@ -10,10 +9,10 @@ declare const ethereum: any;
 //  * @param publicKey - Public key to encrypt with
 //  * @returns Encrypted message
 //  */
-export const encrypt = async (message: string, publicKey: string) => {
-  const buf = Buffer.from(JSON.stringify(libEncrypt(publicKey, { data: message }, "x25519-xsalsa20-poly1305")), "utf8");
-  return "0x" + buf.toString("hex");
-};
+// export const encrypt = async (message: string, publicKey: string) => {
+//   const buf = Buffer.from(JSON.stringify(libEncrypt(publicKey, { data: message }, "x25519-xsalsa20-poly1305")), "utf8");
+//   return "0x" + buf.toString("hex");
+// };
 
 export const ethereumRequest = async (method: string, params: string[]): Promise<any> => {
   // If ethereum.request() exists, the provider is probably EIP-1193 compliant.
