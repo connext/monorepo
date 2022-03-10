6e733482c1defd2649d51dd3e34acd86c007158b
wanglonghong <wanglonghong4467@gmail.com>
2022-03-10 18:44:16 +0800

    unknown -> any

--- packages/adapters/cache/src/lib/caches/cache.ts
+++ packages/adapters/cache/src/lib/caches/cache.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 import { Logger } from "@connext/nxtp-utils";
 
-import { CacheParams, StoreChannel, SubscriptionCallback, Subscriptions } from "../entities";
+import { CacheParams } from "../entities";
 
 /**
  * @classdesc Manages storage, updates, and retrieval of a set of data determined by use-case.
@@ -9,10 +9,8 @@ import { CacheParams, StoreChannel, SubscriptionCallback, Subscriptions } from "
 export abstract class Cache {
   protected readonly data!: Redis.Redis;
   protected readonly logger: Logger;
-  // public readonly subscriptions: Subscriptions;
 
   constructor({ url, mock, logger }: CacheParams) {
-    // this.subscriptions = subscriptions;
     this.logger = logger;
     if (mock) {
       const IoRedisMock = require("ioredis-mock");
