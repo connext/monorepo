832b00b31294eae147d00a74b8e18d82d4b7b4ab
Sanchay Mittal <sanchaymittal@gmail.com>
2022-03-14 13:57:52 +0530

    clean up sdk

--- packages/agents/sdk/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { NxtpSdk } from "./sdk";
-export { NxtpSdkBase } from "./sdkBase";
-export { getDeployedTransactionManagerContract } from "./transactionManager/transactionManager";
-export * from "./types";
-export { getMinExpiryBuffer } from "./utils";
