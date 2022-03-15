cfd609d1d44de2e486870ea3ec7f557ee7683e90
Rahul Sethuram <rahul@sethuram.com>
2022-03-15 21:20:55 +0400

    Add chains

--- packages/test-ui/src/components/Router.tsx
+++ packages/test-ui/src/components/Router.tsx
@@ -15,9 +15,9 @@ type RouterProps = {
 
 const decimals: Record<string, number> = {};
 
-const TESTNET_CHAINS = [421611, 97, 43113, 5, 42, 80001, 4, 3, 1287];
+const TESTNET_CHAINS = [421611, 97, 43113, 5, 42, 80001, 4, 3, 1287, 2221];
 
-const MAINNET_CHAINS = [1, 10, 56, 100, 137, 250, 1284, 1285, 2001, 42161, 43114];
+const MAINNET_CHAINS = [1, 10, 56, 100, 137, 250, 288, 1284, 1285, 2001, 42161, 43114, 192837465];
 
 /**
  * Gets hosted subgraph for applicable chains
