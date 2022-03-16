143b2fd4e62798cabf99ab0155c0e808298ffeed
@base0010 <marcus@gatekeeper.network>
2022-03-15 22:17:23 -0400

    pass var

--- ops/main.tf
+++ ops/main.tf
@@ -77,7 +77,8 @@ resource "aws_instance" "terraformed-router" {
     sudo usermod -a -G docker ec2-user
     sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    sudo docker-compose "${var.full_image_name}"
+    sudo docker pull "${var.full_image_name}"
+    sudo docker run "${var.full_image_name}"
     sudo touch /root/touch.txt
   EOF
   
