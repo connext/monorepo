0fed661b91aaf855030db88b0cda7b53e44265d3
@base0010 <marcus@gatekeeper.network>
2022-03-15 22:03:14 -0400

    pass var

--- ops/main.tf
+++ ops/main.tf
@@ -77,9 +77,8 @@ resource "aws_instance" "terraformed-router" {
     sudo usermod -a -G docker ec2-user
     sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    sudo git clone https://github.com/connext/nxtp.git
+    sudo docker-compose "${var.full_image_name}"
     sudo touch /root/touch.txt
-  EOD
   EOF
   
 }
