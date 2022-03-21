43d5498d7a848d1faac3f411d354cef08184813d
Rahul Sethuram <rahul@sethuram.com>
2022-03-21 11:51:09 -0500

    Tf stuff

--- ops/main.tf
+++ ops/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "nxtp-terraform"
+    key    = "state/"
+    region = "us-east-1"
+  }
+}
+
 variable "AWS_SECRET" {
   type = string
   description = "secret aws key"
