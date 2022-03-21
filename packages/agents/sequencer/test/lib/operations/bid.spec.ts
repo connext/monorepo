13170d7c3ab4cec58d284b3b879a427e95cb7d0c
wanglonghong <wanglonghong4467@gmail.com>
2022-03-21 21:10:23 +0800

    Update bid.spec.ts

--- packages/agents/sequencer/test/lib/operations/bid.spec.ts
+++ packages/agents/sequencer/test/lib/operations/bid.spec.ts
@@ -1,5 +1,48 @@
+import { mkAddress, FulfillArgs, Bid, expect } from "@connext/nxtp-utils";
 import { stub, restore, reset, SinonStub } from "sinon";
+import { handleBid } from "../../../src/lib/operations";
+import { mock } from "../../mock";
 
+const mockFulfillArgs: FulfillArgs[] = [
+  {
+    params: {
+      recipient: "0xbeefdead",
+      callTo: "0x",
+      callData: "0x0",
+      originDomain: "2000",
+      destinationDomain: "3000",
+    },
+    local: "0xdedddddddddddddd",
+    router: mkAddress("0xa"),
+    feePercentage: "0.1",
+    nonce: "1",
+    amount: "10",
+    relayerSignature: "0xsigsigsig",
+  },
+  {
+    params: {
+      recipient: "0xbeefdead",
+      callTo: "0x",
+      callData: "0x0",
+      originDomain: "2000",
+      destinationDomain: "3000",
+    },
+    local: "0xdedddddddddddddd",
+    router: mkAddress("0xb"),
+    feePercentage: "0.1",
+    nonce: "1",
+    amount: "10",
+    relayerSignature: "0xsigsigsig",
+  },
+];
+
+const mockBids = [
+  mock.entity.bid("0xtx111", mockFulfillArgs[0]),
+  mock.entity.bid("0xtx111", mockFulfillArgs[1]),
+  mock.entity.bid(),
+];
+
+const loggingContext = mock.loggingContext("BID-TEST");
 describe("Bid", () => {
   describe("#handleBid", () => {
     beforeEach(async () => {});
@@ -7,8 +50,16 @@ describe("Bid", () => {
       restore();
       reset();
     });
-    it("should error if input validation fails", async () => {});
-    it("happy case: should store bid to auction cache", async () => {});
+    it("should error if input validation fails", async () => {
+      const _bid: Bid = {
+        ...mockBids[0],
+        transactionId: 1,
+      };
+      expect(handleBid(_bid, loggingContext.requestContext)).to.eventually.be.rejectedWith("Params invalid");
+    });
+    it("happy case: should store bid to auction cache", async () => {
+      expect(handleBid(mockBids[0], loggingContext.requestContext)).to.eventually.be.ok;
+    });
   });
   describe("#bidSelection", () => {
     beforeEach(async () => {});
@@ -16,5 +67,6 @@ describe("Bid", () => {
       restore();
       reset();
     });
+    it("happy case: should send best bid to the relayer", async () => {});
   });
 });
