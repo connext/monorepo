48002b42901d6686f350470f3008745590386e86
wanglonghong <wanglonghong4467@gmail.com>
2022-03-21 17:09:39 +0800

    exclude from tests

---
+++ packages/agents/sequencer/test/lib/operations/bid.spec.ts
@@ -0,0 +1,20 @@
+import { stub, restore, reset, SinonStub } from "sinon";
+
+describe("Bid", () => {
+  describe("#handleBid", () => {
+    beforeEach(async () => {});
+    afterEach(() => {
+      restore();
+      reset();
+    });
+    it("should error if input validation fails", async () => {});
+    it("happy case: should store bid to auction cache", async () => {});
+  });
+  describe("#bidSelection", () => {
+    beforeEach(async () => {});
+    afterEach(() => {
+      restore();
+      reset();
+    });
+  });
+});
