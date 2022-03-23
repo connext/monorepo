4c75b94b4fdb586f82dc9dc5f5e6183d51fbbc1d
Jake Kidd <jcb.kdd@gmail.com>
2022-03-22 17:19:59 -0700

    await expect

--- packages/agents/router/test/bindings/metrics/index.spec.ts
+++ packages/agents/router/test/bindings/metrics/index.spec.ts
@@ -4,7 +4,7 @@ import * as binding from "../../../src/bindings/metrics";
 describe("Bindings:Metrics", async () => {
   describe("bindMetrics", () => {
     it("happy: should collect metrics", async () => {
-      expect(await binding.bindMetrics()).to.be.fulfilled;
+      await expect(binding.bindMetrics()).to.be.fulfilled;
     });
   });
 });
--- packages/agents/router/test/bindings/server/index.spec.ts
+++ packages/agents/router/test/bindings/server/index.spec.ts
@@ -4,7 +4,7 @@ import * as binding from "../../../src/bindings/server";
 describe("Bindings:Server", async () => {
   describe("#bindServer", () => {
     it("happy: should bind fastify server endpoints", async () => {
-      expect(await binding.bindServer()).to.be.fulfilled;
+      await expect(binding.bindServer()).to.be.fulfilled;
     });
   });
 });
