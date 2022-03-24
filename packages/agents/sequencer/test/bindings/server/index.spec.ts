b2b2053507a4d8fb3d2f1c80755ef30fc81c016f
wanglonghong <wanglonghong4467@gmail.com>
2022-03-24 22:27:24 +0800

    fix coverage

--- packages/agents/sequencer/test/bindings/server/index.spec.ts
+++ packages/agents/sequencer/test/bindings/server/index.spec.ts
@@ -1,20 +1,24 @@
 import { SinonStub, stub, restore, reset } from "sinon";
-import { expect, mkAddress } from "@connext/nxtp-utils";
+import { expect } from "@connext/nxtp-utils";
 import { FastifyInstance } from "fastify";
-import { parseEther } from "ethers/lib/utils";
 
-import * as binding from "../../../src/bindings/server";
+import * as BindingFns from "../../../src/bindings/server";
 import * as BidFns from "../../../src/lib/operations/bid";
 import { mock } from "../../mock";
+
 let fastifyApp: FastifyInstance;
-describe("Bindings:Server", async () => {
+describe("bindServer", () => {
   describe("#bindServer", () => {
     let handleBidStub: SinonStub;
-    before(async () => {
-      fastifyApp = await binding.bindServer();
+    beforeEach(() => {
       handleBidStub = stub(BidFns, "handleBid");
     });
+    after(() => {
+      restore();
+      reset();
+    });
     it("happy: should respond with `pong`", async () => {
+      fastifyApp = await BindingFns.bindServer();
       const response = await fastifyApp.inject({
         method: "GET",
         url: "/ping",
@@ -23,14 +27,14 @@ describe("Bindings:Server", async () => {
       expect(response.payload).to.be.eq("pong\n");
     });
     it("happy: should succeed to post a bid", async () => {
-      handleBidStub.resolves();
+      handleBidStub.resolves(null);
       const response = await fastifyApp.inject({
         method: "POST",
         url: "/bid",
         payload: mock.entity.bid(),
       });
       expect(response.statusCode).to.be.eq(200);
-      expect(response.payload).to.be.eq("pong\n");
+      expect(JSON.parse(response.payload).message).to.be.eq("Sent bid to auctioneer");
     });
     it("happy: should receive 500 error if handling the bid fails", async () => {
       handleBidStub.throws(new Error("Handling the bid failed!"));
