d25d7e17a26b36e064cf37fc0834c3db47c2ea11
wanglonghong <wanglonghong4467@gmail.com>
2022-03-28 16:39:28 +0800

    more cases for address.ts

--- packages/utils/test/helpers/address.spec.ts
+++ packages/utils/test/helpers/address.spec.ts
@@ -1,25 +1,44 @@
 import { expect } from "chai";
 
-import { getAddressFromPublicKey, compressPublicKey } from "../../src";
+import { getAddressFromPublicKey, compressPublicKey, validateAndParseAddress, mkAddress } from "../../src";
 
-const testPublicKey = "0x045a9d7e18d9110220ed939dccfdfb899ae92c57d1a321520c51d56e2750ca048e16257463175c0774b8041f03b66c824a664fa22d719dca18c6cf116f82d278a4"
+const testPublicKey =
+  "0x045a9d7e18d9110220ed939dccfdfb899ae92c57d1a321520c51d56e2750ca048e16257463175c0774b8041f03b66c824a664fa22d719dca18c6cf116f82d278a4";
 const testAddress = "0x24EEe0a64ecB052BF4686B35c776d5a97E0934Ac";
 const compressedPublicKey = "0x025a9d7e18d9110220ed939dccfdfb899ae92c57d1a321520c51d56e2750ca048e";
 
 describe("Helpers:Address", () => {
-    describe("#getAddressFromPublicKey", () => {
-        it(`Should return address ${testAddress} from pub key`, () => {
-            expect(getAddressFromPublicKey(testPublicKey)).to.be.deep.eq(
-                testAddress,
-            );
-        });
-    })
-    
-    describe("#compressPublicKey", () => {
-        it(`Should get compressed version of public key`, () => {
-            expect(compressPublicKey(testPublicKey)).to.be.deep.equal(
-                compressedPublicKey
-            )
-        })
-    })
-})
+  describe("#getAddressFromPublicKey", () => {
+    it(`Should return address ${testAddress} from pub key`, () => {
+      expect(getAddressFromPublicKey(testPublicKey)).to.be.deep.eq(testAddress);
+    });
+
+    it(`Should throw if pubKey is invalid`, () => {
+      const testPublicKey2 =
+        "0x045a9d7e18d9110220ed939dccfdfb899ae92c57d1a321520c51d56e2750ca048e16257463175c0774b8041f03b66c824a664fa22d719dca18c6cf116f82d278tt";
+      expect(() => getAddressFromPublicKey(testPublicKey2)).throw();
+    });
+  });
+
+  describe("#compressPublicKey", () => {
+    it(`Should get compressed version of public key`, () => {
+      const compressedForm = Uint8Array.from([
+        2, 90, 157, 126, 24, 217, 17, 2, 32, 237, 147, 157, 204, 253, 251, 137, 154, 233, 44, 87, 209, 163, 33, 82, 12,
+        81, 213, 110, 39, 80, 202, 4, 142,
+      ]);
+
+      expect(compressPublicKey(testPublicKey)).to.be.deep.equal(compressedForm);
+    });
+  });
+
+  describe("#validateAndParseAddress", () => {
+    it(`Should throw if the address is invalid`, () => {
+      const address = "0x123asd";
+      expect(() => validateAndParseAddress(address)).throw();
+    });
+    it(`Should return address if it is valid`, () => {
+      const address = mkAddress("0x123abc");
+      expect(validateAndParseAddress(address).toLowerCase()).to.be.eq(address.toLowerCase());
+    });
+  });
+});
