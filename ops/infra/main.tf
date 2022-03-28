ea539a2a13e79463ae4379ac7c473921e085de2c
carlomazzaferro <carlo.mazzaferro@olx.com>
2022-03-28 19:48:41 +0200

    infra set up

---
+++ ops/infra/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "nxtp-terraform-infra"
+    key    = "state/"
+    region = "us-east-1"
+  }
+}
+
+
+
+provider "aws" {
+  region = var.region
+}
+
+
+module "iam" {
+  source = "../modules/iam"
+}
