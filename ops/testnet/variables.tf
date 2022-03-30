55c56022e458d5524a768f84231c1d7a16f2dd77
carlomazzaferro <carlo.mazzaferro@olx.com>
2022-03-30 13:11:43 +0200

    latest docker image

--- ops/testnet/variables.tf
+++ ops/testnet/variables.tf
@@ -18,7 +18,7 @@ variable "environment" {
 variable "full_image_name_router" {
   type        = string
   description = "router image name"
-  default     = "ghcr.io/connext/router:sha-011c975"
+  default     = "ghcr.io/connext/router:sha-c3bc0e0"
 }
 
 variable "full_image_name_sequencer" {
