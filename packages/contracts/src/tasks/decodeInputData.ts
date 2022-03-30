9d978599694b5f568ffe6460022e9e2c8e837409
Rahul Sethuram <rahul@sethuram.com>
2022-03-30 18:55:58 +0400

    Fix decode script

--- packages/contracts/src/tasks/decodeInputData.ts
+++ packages/contracts/src/tasks/decodeInputData.ts
@@ -3,6 +3,7 @@ import { task } from "hardhat/config";
 
 export default task("decode-input-data", "Decodes input data")
   .addParam("inputData", "Input data")
+  .addParam("type", "Input data")
   .setAction(async ({ inputData, type }, { deployments }) => {
     console.log("inputData: ", inputData);
     console.log("type: ", type);
@@ -12,6 +13,9 @@ export default task("decode-input-data", "Decodes input data")
 
     if (type === "fulfill") {
       const decoded = txMinterface.decodeFunctionData("fulfill", inputData);
+      console.log("decoded: ", decoded);
+    } else if (type === "prepare") {
+      const decoded = txMinterface.decodeFunctionData("prepare", inputData);
       console.log("decoded: ", decoded);
     }
   });
