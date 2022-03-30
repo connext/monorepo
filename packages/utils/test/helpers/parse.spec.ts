0fe1275ab41d27fd2c797841a70e0921015956e4
wanglonghong <wanglonghong4467@gmail.com>
2022-03-30 23:06:26 +0800

    create gelato tests

--- packages/utils/test/helpers/parse.spec.ts
+++ packages/utils/test/helpers/parse.spec.ts
@@ -1,10 +1,25 @@
 import { expect } from "chai";
-import { parseHostname } from "../../src";
+import { parseHostname, formatUrl } from "../../src";
 
 describe("Helpers:Parse", () => {
   describe("#parseHostname", () => {
     it("happy", () => {
       expect(parseHostname("https://api.thegraph.com/subgraphs/")).to.be.eq("api.thegraph.com");
+    });
+  });
+  describe("#parseHostname", () => {
+    it("happy", () => {
+      expect(parseHostname("https://api.thegraph.com/subgraphs/")).to.be.eq("api.thegraph.com");
+    });
+  });
+  describe("#formatUrl", () => {
+    it("happy: should add indetifier if exists", () => {
+      expect(formatUrl("https://api.thegraph.com", "subgraphs", "hosted-service")).to.be.eq(
+        "https://api.thegraph.com/subgraphs/hosted-service",
+      );
+    });
+    it("happy: should not add indetifier if no exists", () => {
+      expect(formatUrl("https://api.thegraph.com", "subgraphs")).to.be.eq("https://api.thegraph.com/subgraphs");
     });
   });
 });
