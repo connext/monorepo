106d82312f623954ea134f60facd0eb3bcd1d47e
- <mailto123ads@gmail.com>
2022-03-31 09:34:25 +0800

    import just once

--- packages/deployments/contracts/contracts/StableSwap.sol
+++ packages/deployments/contracts/contracts/StableSwap.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "./lib/StableSwap/OwnerPausableUpgradeable.sol";
-import "./lib/StableSwap/SwapUtils.sol";
 import "./lib/StableSwap/AmplificationUtils.sol";
 import "./interfaces/IStableSwap.sol";
 
