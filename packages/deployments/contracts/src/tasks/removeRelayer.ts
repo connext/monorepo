802cf6ce62ac215ae881306c8e7a649a8de5f5cf
_ <mailto123ads@gmail.com>
2022-04-06 18:50:14 +0800

    add tasks for adding and removing relayer

---
+++ packages/deployments/contracts/src/tasks/removeRelayer.ts
@@ -0,0 +1,44 @@
+import { isAddress } from "ethers/lib/utils";
+import { task } from "hardhat/config";
+
+type TaskArgs = {
+  relayer: string;
+  connextAddress?: string;
+};
+
+export default task("remove-relayer", "Remove Relayer from whitelist")
+  .addParam("relayer", "The address of relayer to remove")
+  .addOptionalParam("connextAddress", "Override connext address")
+  .setAction(
+    async ({ relayer, connextAddress: _connextAddress }: TaskArgs, { deployments, getNamedAccounts, ethers }) => {
+      const namedAccounts = await getNamedAccounts();
+
+      console.log("relayer: ", relayer);
+
+      let connextAddress = _connextAddress;
+      if (!connextAddress) {
+        const connextDeployment = await deployments.get("Connext");
+        connextAddress = connextDeployment.address;
+      }
+      console.log("connextAddress: ", connextAddress);
+
+      const connext = await ethers.getContractAt("Connext", connextAddress);
+
+      if (!isAddress(relayer) || relayer === ethers.constants.AddressZero) {
+        throw new Error("Invalid Relayer address");
+      }
+
+      const approvedRelayer = await connext.approvedRelayer(relayer);
+      console.log("approvedRelayer: ", approvedRelayer);
+      if (!approvedRelayer) {
+        throw new Error("Not approved relayer");
+      }
+
+      const tx = await connext.setupRelayer(relayer, false, {
+        from: namedAccounts.deployer,
+      });
+      console.log("setupRelayer tx: ", tx);
+      const receipt = await tx.wait();
+      console.log("setupRelayer tx mined: ", receipt.transactionHash);
+    },
+  );
