3815d87564424a20d88ffa8a2d844cbaf61ddaf2
Jake Kidd <jcb.kdd@gmail.com>
2022-04-09 00:04:38 -0700

    server tests

--- packages/agents/sequencer/test/bindings/auctions/index.spec.ts
+++ packages/agents/sequencer/test/bindings/auctions/index.spec.ts
@@ -1,39 +1,28 @@
 import { expect, delay } from "@connext/nxtp-utils";
-import { stub, restore, reset, SinonStub } from "sinon";
+import { stub, SinonStub } from "sinon";
 
 import { ctxMock, getOperationsStub, getHelpersStub } from "../../globalTestHook";
 import { bindAuctions } from "../../../src/bindings";
-import * as BindAuctionsFns from "../../../src/bindings/auctions";
 
-describe("bidSelection", () => {
+describe("Bindings:Auctions", () => {
   describe("#bidSelection", () => {
-    let bidSelectionStub: SinonStub;
+    let selectBidsStub: SinonStub;
     beforeEach(() => {
-      getOperationsStub = stub(operations, "getOperations");
-      getOperationsStub.returns(mock);
-      bidSelectionStub = stub(BidFns, "bidSelection").resolves(null);
-    });
-
-    after(() => {
-      restore();
-      reset();
+      selectBidsStub = stub().resolves();
+      getOperationsStub.returns({
+        auctions: {
+          selectBids: selectBidsStub,
+        },
+      });
     });
 
     it("happy: should start an interval loop that calls polling fn", async () => {
       // Override the poll interval to 10ms so we can test the interval loop
-      await bindBidSelection(10);
+      await bindAuctions(10);
       // TODO: slight race here?
       await delay(20);
       ctxMock.config.mode.cleanup = true;
-      expect(bidSelectionStub.callCount).to.be.gte(1);
-    });
-
-    it("happy: default polling interval", async () => {
-      BindBidSelectionFns.BID_SELECTION_POLL_INTERVAL = 10;
-      await bindBidSelection();
-      await delay(20);
-      ctxMock.config.mode.cleanup = true;
-      expect(bidSelectionStub.callCount).to.be.gte(1);
+      expect(selectBidsStub.callCount).to.be.gte(1);
     });
   });
 });
