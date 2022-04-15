f18e3059c61c7b519aff9e1028bd8c3f60f29929
carlomazzaferro <carlo.mazzaferro@olx.com>
2022-04-15 17:56:27 +0200

    typo

--- ops/testnet/prod/main.tf
+++ ops/testnet/prod/main.tf
@@ -99,7 +99,7 @@ module "ecs" {
   source           = "../../modules/ecs"
   stage                   = var.stage
   environment             = var.environment
-  ecs_cluster_name_prefix = "nxtp-ecs-"
+  ecs_cluster_name_prefix = "nxtp-ecs"
   vpc_id           = module.network.vpc_id
   private_subnets  = module.network.private_subnets
   public_subnets   = module.network.public_subnets
