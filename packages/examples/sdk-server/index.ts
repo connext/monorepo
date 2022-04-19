1ff24f5579e1f32bce1d01b9047dad0bd0c473a2
Sanchay Mittal <sanchaymittal@gmail.com>
2022-04-19 16:23:09 +0200

    setup sdk-server

--- packages/examples/sdk-server/index.ts
+++ packages/examples/sdk-server/index.ts
@@ -1,14 +1,8 @@
-import {
-  jsonifyError,
-  NxtpError,
-  RemoveLiquidityRequest,
-  RemoveLiquidityRequestSchema,
-  RemoveLiquidityResponseSchema,
-  AdminRequest,
-  XCallArgsSchema,
-} from "@connext/nxtp-utils";
+import { RemoveLiquidityResponseSchema, AdminRequest, XCallArgsSchema, XCallArgs } from "@connext/nxtp-utils";
+import ethers, { providers } from "ethers";
+import * as fs from "fs";
 
-import { NxtpSdkConfigSchema, NxtpSdk } from "nxtp-sdk";
+import { NxtpSdkConfig, NxtpSdkConfigSchema, NxtpSdk } from "nxtp-sdk";
 import fastify, { FastifyInstance, FastifyReply } from "fastify";
 
 let sdkInstance: NxtpSdk;
@@ -18,33 +12,44 @@ export const sdkServer = () =>
     const server = fastify();
 
     server.addHook("onReady", async () => {
-      sdkBaseInstance = new NxtpSdk(config);
+      let configJson: Record<string, any> = {};
+      let configFile: any = {};
+
+      try {
+        configJson = JSON.parse(process.env.NXTP_CONFIG || "");
+      } catch (e: unknown) {
+        console.info("No NXTP_CONFIG exists, using config file and individual env vars");
+      }
+      try {
+        let json: string;
+        const path = process.env.NXTP_CONFIG_FILE ?? "config.json";
+        if (fs.existsSync(path)) {
+          json = fs.readFileSync(path, { encoding: "utf-8" });
+          configFile = JSON.parse(json);
+        }
+      } catch (e: unknown) {
+        console.error("Error reading config file!");
+        process.exit(1);
+      }
+      // return configFile;
+
+      const nxtpConfig: NxtpSdkConfig = {
+        chains: configJson.chains ? configJson.chains : configFile.chains,
+        logLevel: configJson.logLevel || configFile.logLevel || "info",
+      };
+
+      const signer = new ethers.Wallet(configJson.mnemonic || configFile.mnemonic);
+
+      sdkInstance = await NxtpSdk.create(nxtpConfig, signer);
     });
 
     server.get("/ping", (_, res) => api.get.ping(res));
 
-    server.get("/config", (_, res) => api.get.config(res));
-
-    server.post<{ Body: NxtpSdkConfigSchema }>(
-      "/create",
-      { schema: { body: NxtpSdkConfigSchema, response: NxtpSdk } },
-      async (req, res) => api.post.create(req.body, res, api.post.create),
-    );
-
-    server.post<{ Body: XCallArgsSchema }>(
+    server.post<{ Body: XCallArgs }>(
       "/xcall",
       { schema: { body: XCallArgsSchema, response: { "2xx": RemoveLiquidityResponseSchema } } },
-      async (req, res) => api.post.xcall(req.body, res, api.post.removeLiquidity),
+      async (req, res) => api.post.xcall(req.body),
     );
-
-    server.listen(config.server.port, config.server.host, (err, address) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      logger.info(`Server listening at ${address}`);
-      res(server);
-    });
   });
 
 export const api = {
@@ -52,29 +57,13 @@ export const api = {
     ping: async (res: FastifyReply) => {
       return res.status(200).send("pong\n");
     },
-    config: async (res: FastifyReply) => {
-      const {
-        adapters: { wallet },
-        logger,
-      } = getContext();
-      try {
-        return res.status(200).send({
-          signerAddress: await wallet.getAddress(),
-        });
-      } catch (e: unknown) {
-        const json = jsonifyError(e as NxtpError);
-        logger.error("Failed to get wallet address", undefined, undefined, json);
-        return res.status(500).send(json);
-      }
-    },
   },
   post: {
-    create: async (res: FastifyReply) => {
-      await NxtpSdk.create(config, signer, logger);
-      return res.status(200).send();
-    },
-    xcall: async (res: FastifyReply) => {
-      return res.status(500).send("Not implemented");
+    xcall: async (req: XCallArgs): Promise<providers.TransactionResponse> => {
+      // return re.status(500).send("Not implemented");
+      return {} as providers.TransactionResponse;
     },
   },
 };
+
+sdkServer();
