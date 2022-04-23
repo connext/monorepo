b99208cd1ee5f449a13789bc92c555de40b8b23b
Jake Kidd <jcb.kdd@gmail.com>
2022-04-23 13:53:02 -0700

    satisfying check marks

--- packages/integration/test/log.ts
+++ packages/integration/test/log.ts
@@ -22,9 +22,26 @@ if (!fs.existsSync(logdir)) {
  * - Fine control over what's printed, since we may want to use the saved logfiles as a validation step
  * for staging setups in the future.
  */
+enum LogLevel {
+  INIT = "INIT",
+  STEP = "STEP",
+  INFO = "INFO",
+  DONE = "DONE",
+  FAIL = "FAIL",
+}
+
+type LogContext = {
+  domain?: DomainInfo;
+  hash?: string;
+  etc?: any;
+};
+
 export const log = {
+  last: "",
   step: 0,
-  print: (mod: string, message: string, etc?: any) => {
+  print: (mod: string, level: LogLevel, _message: string, context: LogContext = {}) => {
+    const { hash, etc } = context;
+    const { chain, network } = context.domain ?? {};
     let append = "";
     if (etc) {
       for (const key of Object.keys(etc as object)) {
@@ -35,51 +52,40 @@ export const log = {
         }`;
       }
     }
+    let message = "";
+    message += level === LogLevel.STEP ? "\n" : "";
+    message += level !== LogLevel.INIT ? `[${level.toString()}]` : "";
+    message += log.step > 0 ? ` (${log.step})` : "";
+    message += chain ? ` {${chain}}` : "";
+    message += ` ${_message}`;
+    message += level === LogLevel.DONE ? " ✔" : "";
+    if (hash && network) {
+      message += `\n\tHash: ${hash}`;
+      message += `\n\tEtherscan: ${formatEtherscanLink({
+        hash,
+        network,
+      })}`;
+    }
     console.log(mod, message, append, "\x1b[0m");
     fs.appendFileSync(logfile, "\n" + message + append);
   },
   params: (params: string) => {
-    log.print("\x1b[35m\x1b[4m%s", "TEST PARAMETERS");
-    log.print("\x1b[35m%s", params);
+    log.print("\x1b[35m\x1b[4m%s\x1b[0m", LogLevel.INIT, "TEST PARAMETERS");
+    log.print("\x1b[35m%s", LogLevel.INIT, params);
   },
-  info: (_message: string, context: { domain?: DomainInfo; hash?: string; etc?: any } = {}) => {
-    const { domain, hash, etc } = context;
-    const { network, chain } = domain ?? {};
-    let message = `[INFO] (${log.step})`;
-    message += chain ? ` {${chain}}` : "";
-    message += ` ${_message}`;
-    if (hash) {
-      message += `\n\tHash: ${hash}`;
-      if (network) {
-        message += `\n\tEtherscan: ${formatEtherscanLink({
-          hash,
-          network,
-        })}`;
-      }
-    }
-    log.print("\x1b[36m%s", message, etc);
+  info: (message: string, context: LogContext = {}) => {
+    log.print("\x1b[36m%s", LogLevel.INFO, message, context);
   },
   next: (message: string) => {
-    log.step++;
-    log.print("\x1b[32m%s", `\n[STEP] (${log.step}) ${message}`);
-  },
-  fail: (_message: string, context: { domain: DomainInfo; hash?: string; etc?: any }) => {
-    const {
-      domain: { chain, network },
-      hash,
-      etc,
-    } = context;
-    let message = `[FAIL] (${log.step}) {${chain}} ${_message}`;
-    if (hash) {
-      message += ` Hash: ${hash}`;
-      if (network) {
-        message += `Etherscan: ${formatEtherscanLink({
-          hash,
-          network,
-        })}`;
-      }
+    if (log.last) {
+      log.print("\x1b[32m%s", LogLevel.DONE, log.last);
     }
-    log.print("\x1b[31m%s", message, etc);
-    process.exit(1);
+    log.step++;
+    log.print("\x1b[32m%s", LogLevel.STEP, message);
+    log.last = message;
+  },
+  fail: (message: string, context: LogContext) => {
+    log.print("\x1b[31m%s", LogLevel.FAIL, message, context);
+    throw new Error(message);
   },
 };
