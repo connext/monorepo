1f24035c85b5ff1a050c279938a6dbceb005b4fb
Lisandro Corbalan <lmcorbalan@gmail.com>
2022-04-28 11:54:43 -0300

    Test sponsoring fees execute logic

---
+++ packages/deployments/contracts/contracts/test/TestSponsorVault.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+
+import "../interfaces/ISponsorVault.sol";
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+contract TestSponsorVault is ISponsorVault {
+
+  uint256 liquidityFeeToSend;
+  uint256 liquidityFeeToReturn;
+  uint256 relayerFeeToSend;
+
+  receive() external payable {}
+
+  function setFeeValues(
+    uint256 _liquidityFeeToSend,
+    uint256 _liquidityFeeToReturn,
+    uint256 _relayerFeeToSend
+  ) external {
+    liquidityFeeToSend = _liquidityFeeToSend;
+    liquidityFeeToReturn = _liquidityFeeToReturn;
+    relayerFeeToSend = _relayerFeeToSend;
+  }
+
+  function reimburseLiquidityFees(address adopted, uint256 amount) external override returns (uint256) {
+    IERC20Upgradeable(adopted).transfer(msg.sender, liquidityFeeToSend);
+    return liquidityFeeToReturn;
+  }
+
+  function reimburseRelayerFees(uint32 originDomain, address to, uint256 relayerFee) external override {
+    to.call{value: relayerFeeToSend}("");
+  }
+
+}
