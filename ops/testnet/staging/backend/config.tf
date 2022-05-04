548e598138e6277992eac8b949966787297173da
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-05-04 21:49:07 +0200

    secrets configs

--- ops/testnet/staging/backend/config.tf
+++ ops/testnet/staging/backend/config.tf
@@ -1,9 +1,14 @@
+
 locals {
   poller_env_vars = [
-    { name = "ENVIRONMENT", value = var.environment }
+    { name = "ENVIRONMENT", value = var.environment },
+    { name = "DATABASE_URL", value = "postgres://${var.postgres_user}:${var.postgres_password}@${module.poller_db.db_instance_fixed_address}:${module.poller_db.db_instance_port}/connext" }
   ]
   postgrest_env_vars = [
     { name = "PGRST_ADMIN_SERVER_PORT", value = 3001 },
+    { name = "PGRST_DB_URI", value = "postgres://${var.postgres_user}:${var.postgres_password}@${module.poller_db.db_instance_fixed_address}:${module.poller_db.db_instance_port}/connext" },
+    { name = "PGRST_DB_SCHEMA", value = "public" },
+    { name = "PGRST_DB_ANON_ROLE", value = "query" },
     { name = "ENVIRONMENT", value = var.environment }
   ]
 }
