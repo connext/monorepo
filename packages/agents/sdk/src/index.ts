8f20e28b82ccef423681600836a36a723f8eb5a4
Sanchay Mittal <sanchaymittal@gmail.com>
2022-05-06 02:00:21 +0530

    expose new classes

--- packages/agents/sdk/src/index.ts
+++ packages/agents/sdk/src/index.ts
@@ -1,3 +1,6 @@
 export { NxtpSdk } from "./sdk";
 export { NxtpSdkBase } from "./sdkBase";
+export { NxtpSdkRouter } from "./sdkRouter";
+export { NxtpSdkStableSwap } from "./sdkStableSwap";
+export { NxtpSdkUtils } from "./sdkUtils";
 export { NxtpSdkConfig, NxtpSdkConfigSchema } from "./config";
