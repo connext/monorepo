7838f79641fc8b870469995b774754e249e3435a
Sanchay Mittal <sanchaymittal@gmail.com>
2022-05-08 10:41:08 +0530

    sdkBase unit test setup

---
+++ packages/agents/sdk/test/sdkBase.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "@connext/nxtp-utils";
+import { create } from "../src";
+import { mock } from "./mock";
+
+describe("SdkBase", () => {
+  describe("#create", () => {
+    it("happy: should work", async () => {});
+  });
+});
