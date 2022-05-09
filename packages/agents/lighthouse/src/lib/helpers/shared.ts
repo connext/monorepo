65d04c2eb70b6c89f6f9276f298bf4bfa862c64e
Sanchay Mittal <sanchaymittal@gmail.com>
2022-05-10 00:34:13 +0530

    clean up

--- packages/agents/lighthouse/src/lib/helpers/shared.ts
+++ packages/agents/lighthouse/src/lib/helpers/shared.ts
@@ -1,7 +1,9 @@
 import * as fs from "fs";
 
 import {
-  signHandleRelayerFeePayload as _signHandleRelayerFeePayload,
+  gelatoSend as _gelatoSend,
+  isChainSupportedByGelato as _isChainSupportedByGelato,
+  getGelatoRelayerAddress as _getGelatoRelayerAddress,
   getSubgraphHealth as _getSubgraphHealth,
   getSubgraphName as _getSubgraphName,
 } from "@connext/nxtp-utils";
@@ -9,39 +11,9 @@ import { utils } from "ethers";
 
 import { getContext } from "../../lighthouse";
 
-/**
- * Returns local asset address on destination domain corresponding to local asset on origin domain
- *
- * @param _originDomain
- * @param _originLocalAsset The asset sent over the bridge
- * @param _destinationDomain
- * @returns
- */
-export const getDestinationLocalAsset = async (
-  _originDomain: string,
-  _originLocalAsset: string,
-  _destinationDomain: string,
-): Promise<string> => {
-  const {
-    adapters: { subgraph },
-  } = getContext();
-
-  // get canonical asset from orgin domain.
-  const sendingDomainAsset = await subgraph.getAssetByLocal(_originDomain, _originLocalAsset);
-
-  const canonicalId = sendingDomainAsset!.canonicalId as string;
-
-  const destinationDomainAsset = await subgraph.getAssetByCanonicalId(Number(_destinationDomain), canonicalId);
-
-  const localAddress = destinationDomainAsset!.local;
-  return localAddress;
-};
-
 export const getTransactionId = (nonce: string, domain: string): string => {
   return utils.keccak256(utils.hexlify(utils.concat([utils.toUtf8Bytes(nonce), utils.toUtf8Bytes(domain)])));
 };
-
-export const signHandleRelayerFeePayload = _signHandleRelayerFeePayload;
 
 export const getSubgraphHealth = _getSubgraphHealth;
 
@@ -50,3 +22,9 @@ export const getSubgraphName = _getSubgraphName;
 export const existsSync = fs.existsSync;
 
 export const readFileSync = fs.readFileSync;
+
+export const gelatoSend = _gelatoSend;
+
+export const isChainSupportedByGelato = _isChainSupportedByGelato;
+
+export const getGelatoRelayerAddress = _getGelatoRelayerAddress;
