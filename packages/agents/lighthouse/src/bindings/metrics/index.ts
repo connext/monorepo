a42cbb1c6b26ebef0053c12ec162b66644dc0181
Sanchay Mittal <sanchaymittal@gmail.com>
2022-05-10 00:33:11 +0530

    cleanup

--- packages/agents/lighthouse/src/bindings/metrics/index.ts
+++
@@ -1,7 +0,0 @@
-import { collectDefaultMetrics } from "prom-client";
-
-export const bindMetrics = async () => {
-  // TODO: need to keep last block height? contractReader has done it already.
-  // TODO: export transactions with timestamps to Prometheus
-  collectDefaultMetrics({ prefix: "router_" });
-};
