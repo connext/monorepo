fd599a24f15bfae638481e76f5d54a4ab368a3e7
Gerardo Nardelli <patitonardelli@gmail.com>
2022-05-10 11:37:21 -0300

    Add NomadFacet

--- packages/deployments/contracts/contracts/diamond/facets/NomadFacet.sol
+++ packages/deployments/contracts/contracts/diamond/facets/NomadFacet.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.11;
 
-import {Home} from "../../../nomad-core/contracts/Home.sol";
-import {XAppConnectionManager} from "../../../nomad-core/contracts/XAppConnectionManager.sol";
-import {AppStorage} from "../../libraries/LibConnextStorage.sol";
+import {BaseConnextFacet} from "./BaseConnextFacet.sol";
+import {XAppConnectionManager} from "../../nomad-core/contracts/XAppConnectionManager.sol";
 
-import {BaseConnextFacet} from "../BaseConnextFacet.sol";
-
-contract XAppConnectionClientFacet is BaseConnextFacet {
+contract NomadFacet is BaseConnextFacet {
   // ============ Getters functions ============
 
   function xAppConnectionManager() public view returns (XAppConnectionManager) {
     return s.xAppConnectionManager;
+  }
+
+  function remotes(uint32 _domain) public view returns (bytes32) {
+    return s.remotes[_domain];
   }
 
   // ============ External functions ============
@@ -22,5 +23,14 @@ contract XAppConnectionClientFacet is BaseConnextFacet {
    */
   function setXAppConnectionManager(address _xAppConnectionManager) external onlyOwner {
     s.xAppConnectionManager = XAppConnectionManager(_xAppConnectionManager);
+  }
+
+  /**
+   * @notice Register the address of a Router contract for the same xApp on a remote chain
+   * @param _domain The domain of the remote xApp Router
+   * @param _router The address of the remote xApp Router
+   */
+  function enrollRemoteRouter(uint32 _domain, bytes32 _router) external onlyOwner {
+    s.remotes[_domain] = _router;
   }
 }
