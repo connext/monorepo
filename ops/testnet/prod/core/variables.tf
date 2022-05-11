77a64c135395bf91bb87f2529b64af0187717e2a
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-05-11 21:18:05 +0200

    add lighthouse

--- ops/testnet/prod/core/variables.tf
+++ ops/testnet/prod/core/variables.tf
@@ -27,13 +27,19 @@ variable "domain" {
 variable "full_image_name_router" {
   type        = string
   description = "router image name"
-  default     = "ghcr.io/connext/router:0.2.0-alpha.10"
+  default     = "ghcr.io/connext/router:0.2.0-alpha.11"
 }
 
 variable "full_image_name_sequencer" {
   type        = string
   description = "sequencer image name"
-  default     = "ghcr.io/connext/sequencer:0.2.0-alpha.10"
+  default     = "ghcr.io/connext/sequencer:0.2.0-alpha.14"
+}
+
+variable "full_image_name_lighthouse" {
+  type        = string
+  description = "router image name"
+  default     = "ghcr.io/connext/lighthouse:0.2.0-alpha.14"
 }
 
 variable "mnemonic" {
