38385098d5c85a6d7bb9da73147f4e8f088acaa0
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-05-11 10:23:29 +0200

    lighthouse terraform

--- ops/testnet/staging/core/config.tf
+++ ops/testnet/staging/core/config.tf
@@ -7,6 +7,10 @@ locals {
     { name = "NXTP_CONFIG", value = local.local_router_config },
     { name = "ENVIRONMENT", value = var.environment }
   ]
+  lighthouse_env_vars = [
+    { name = "NXTP_CONFIG", value = local.local_lighthouse_config },
+    { name = "ENVIRONMENT", value = var.environment }
+  ]
   web3signer_env_vars = [
     { name = "WEB3_SIGNER_PRIVATE_KEY", value = var.web3_signer_private_key },
     { name = "WEB3SIGNER_HTTP_HOST_ALLOWLIST", value = "*" }
@@ -104,3 +108,59 @@ locals {
     environment   = "staging"
   })
 }
+
+
+
+locals {
+  local_lighthouse_config = jsonencode({
+    redis = {
+      host = module.router_cache.redis_instance_address,
+      port = module.router_cache.redis_instance_port
+    },
+    logLevel     = "debug"
+    sequencerUrl = "https://${module.sequencer.service_endpoint}"
+    server = {
+      adminToken = var.admin_token_router
+      port       = 8080
+    }
+    chains = {
+      "1111" = {
+        providers = ["https://eth-rinkeby.alchemyapi.io/v2/${var.rinkeby_alchemy_key_1}", "https://rpc.ankr.com/eth_rinkeby"]
+        assets = [
+          {
+            name    = "TEST"
+            address = "0xB7b1d3cC52E658922b2aF00c5729001ceA98142C"
+          }
+        ]
+        subgraph = {
+          runtime = [
+            {
+              query  = "https://api.thegraph.com/subgraphs/name/connext/nxtp-amarok-runtime-staging-rinkeby"
+              health = "https://api.thegraph.com/index-node/graphql"
+            }
+          ]
+        }
+      }
+      "2221" = {
+        providers = ["https://eth-kovan.alchemyapi.io/v2/${var.kovan_alchemy_key_1}"]
+        assets = [
+          {
+            name    = "TEST"
+            address = "0xB5AabB55385bfBe31D627E2A717a7B189ddA4F8F"
+          }
+        ]
+        subgraph = {
+          runtime = [
+            {
+              query  = "https://api.thegraph.com/subgraphs/name/connext/nxtp-amarok-runtime-staging-kovan"
+              health = "https://api.thegraph.com/index-node/graphql"
+            }
+          ]
+        }
+      }
+    }
+    web3SignerUrl = "https://${module.web3signer.service_endpoint}"
+    environment   = "staging"
+  })
+}
+
