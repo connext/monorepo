7c236e23f469b22ae49028b2a4861de555f1204f
Rahul Sethuram <rahul@sethuram.com>
2022-05-20 18:26:03 +0400

    Try again

--- packages/agents/backend/poller/db/migrations/20220504052525_postgrest_access_control.sql
+++ packages/agents/backend/poller/db/migrations/20220504052525_postgrest_access_control.sql
@@ -1,14 +1,24 @@
 -- migrate:up
-create role query nologin;
+DO $do$ BEGIN IF EXISTS (
+  SELECT
+  FROM pg_catalog.pg_roles
+  WHERE rolname = 'query'
+) THEN RAISE NOTICE 'Role "query" already exists. Skipping.';
+ELSE create role query nologin;
+END IF;
+END $do$;
 grant usage on schema public to query;
 grant select on public.transfers to query;
-create role reader noinherit login password '3eadooor';
+DO $do$ BEGIN IF EXISTS (
+  SELECT
+  FROM pg_catalog.pg_roles
+  WHERE rolname = 'reader'
+) THEN RAISE NOTICE 'Role "reader" already exists. Skipping.';
+ELSE create role reader noinherit login password '3eadooor';
+END IF;
+END $do$;
 GRANT CONNECT ON DATABASE connext TO reader;
 grant usage on schema public to reader;
 grant select on public.transfers to reader;
 grant query to reader;
 -- migrate:down
-drop owned by query;
-drop role query;
-drop owned by reader;
-drop role reader;
