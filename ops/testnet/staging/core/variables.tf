b463e827442fc74a9d6e3717277fc75116b0bfe1
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-05-22 13:58:16 +0200

    staging and prod configs for daemon services

--- ops/testnet/staging/core/variables.tf
+++ ops/testnet/staging/core/variables.tf
@@ -28,19 +28,19 @@ variable "environment" {
 variable "full_image_name_router" {
   type        = string
   description = "router image name"
-  default     = "ghcr.io/connext/router:sha-0a63867"
+  default     = "ghcr.io/connext/router:sha-e271a40"
 }
 
 variable "full_image_name_sequencer" {
   type        = string
   description = "sequencer image name"
-  default     = "ghcr.io/connext/sequencer:sha-0a63867"
+  default     = "ghcr.io/connext/sequencer:sha-e271a40"
 }
 
 variable "full_image_name_lighthouse" {
   type        = string
   description = "router image name"
-  default     = "ghcr.io/connext/lighthouse:sha-0a63867"
+  default     = "ghcr.io/connext/lighthouse:sha-e271a40"
 }
 
 variable "mnemonic" {
