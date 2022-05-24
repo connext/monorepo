903f8a30ab431d1b922fe91be6a7da3046363430
LayneHaber <layne.haber@gmail.com>
2022-05-23 20:10:11 -0600

    chore: move files, tests passing

---
+++ packages/deployments/contracts/contracts_forge/utils/Mock.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import {TypedMemView, PromiseMessage, PromiseRouter} from "../../contracts/core/promise/PromiseRouter.sol";
+import {ICallback} from "../../contracts/core/promise/interfaces/ICallback.sol";
+import {BaseConnextFacet} from "../../contracts/core/connext/facets/BaseConnextFacet.sol";
+import {ISponsorVault} from "../../contracts/core/connext/interfaces/ISponsorVault.sol";
+
+contract MockHome {
+  function dispatch(
+    uint32 _destinationDomain,
+    bytes32 _recipientAddress,
+    bytes memory _messageBody
+  ) external {
+    1 == 1;
+  }
+}
+
+contract MockConnext {
+  function claim(address _recipient, bytes32[] calldata _transferIds) external {
+    1 == 1;
+  }
+}
+
+contract MockRelayerFeeRouter {
+  function send(
+    uint32 _domain,
+    address _recipient,
+    bytes32[] calldata _transactionIds
+  ) external {
+    1 == 1;
+  }
+}
+
+contract MockPromiseRouter is PromiseRouter {
+  using TypedMemView for bytes;
+  using TypedMemView for bytes29;
+  using PromiseMessage for bytes29;
+
+  function mockHandle(
+    address callbackAddress,
+    bool returnSuccess,
+    bytes calldata returnData
+  ) public {
+    bytes32 transferId = "A";
+
+    bytes memory message = PromiseMessage.formatPromiseCallback(transferId, callbackAddress, returnSuccess, returnData);
+    bytes29 _msg = message.ref(0).mustBePromiseCallback();
+
+    promiseMessages[transferId] = message;
+  }
+}
+
+contract MockCallback is ICallback {
+  function callback(
+    bytes32 transferId,
+    bool success,
+    bytes memory data
+  ) external {
+    require(data.length != 0);
+  }
+}
+
+contract TestSetterFacet is BaseConnextFacet {
+  function setTestRelayerFees(bytes32 _transferId, uint256 _fee) external {
+    s.relayerFees[_transferId] = _fee;
+  }
+
+  function setTestTransferRelayer(bytes32 _transferId, address _relayer) external {
+    s.transferRelayer[_transferId] = _relayer;
+  }
+
+  function setTestSponsorVault(address _sponsorVault) external {
+    s.sponsorVault = ISponsorVault(_sponsorVault);
+  }
+
+  function setTestApprovedRelayer(address _relayer, bool _approved) external {
+    s.approvedRelayers[_relayer] = _approved;
+  }
+
+  function setTestRouterBalances(
+    address _router,
+    address _local,
+    uint256 _amount
+  ) external {
+    s.routerBalances[_router][_local] = _amount;
+  }
+
+  function setTestApprovedRouter(address _router, bool _approved) external {
+    s.routerPermissionInfo.approvedRouters[_router] = _approved;
+  }
+}
