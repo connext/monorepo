514c4852d219ad9b867472cb5c24177f823f17de
wanglonghong <wanglonghong4467@gmail.com>
2022-05-25 20:35:52 +0800

    pass supported chains in router

--- packages/agents/router/src/router.ts
+++ packages/agents/router/src/router.ts
@@ -6,6 +6,7 @@ import {
   jsonifyError,
   Logger,
   RequestContext,
+  ChainData,
 } from "@connext/nxtp-utils";
 import { SubgraphReader } from "@connext/nxtp-adapters-subgraph";
 import { StoreManager } from "@connext/nxtp-adapters-cache";
@@ -140,8 +141,15 @@ export const setupSubgraphReader = async (requestContext: RequestContext): Promi
   const { logger, chainData, config } = context;
   const methodContext = createMethodContext(setupSubgraphReader.name);
 
+  const supportedDomains = [...Object.keys(config.chains)];
+  const supportedChainData: Map<string, ChainData> = new Map();
+  for (const supportedDomain of supportedDomains) {
+    if (chainData.has(supportedDomain)) {
+      supportedChainData.set(supportedDomain, chainData.get(supportedDomain));
+    }
+  }
   logger.info("Subgraph reader setup in progress...", requestContext, methodContext, {});
-  const subgraphReader = await SubgraphReader.create(chainData, config.environment, config.subgraphPrefix);
+  const subgraphReader = await SubgraphReader.create(supportedChainData, config.environment, config.subgraphPrefix);
 
   // Pull support for domains that don't have a subgraph.
   const supported: Record<string, boolean> = subgraphReader.supported;
