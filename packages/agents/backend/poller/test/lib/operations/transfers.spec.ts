ab4ba875d24afad8c48d11a2cfdef8fffbd8d129
preethamr <preethamr@gmail.com>
2022-05-25 17:18:36 -0700

    Remove subgraphHealth

--- packages/agents/backend/poller/test/lib/operations/transfers.spec.ts
+++ packages/agents/backend/poller/test/lib/operations/transfers.spec.ts
@@ -84,7 +84,6 @@ const mockNoBlockNumber: Map<string, number> = new Map();
 mockNoBlockNumber.set("99999", 1234567);
 
 describe("Backend operations", () => {
-  let getSubgraphHealthStub: SinonStub;
   let mockContext: backend.AppContext;
 
   beforeEach(() => {
@@ -131,62 +130,18 @@ describe("Backend operations", () => {
   });
 
   it("should poll subgraph with block zero", async () => {
-    getSubgraphHealthStub = stub(SharedFns, "getSubgraphHealth");
-    getSubgraphHealthStub.resolves({
-      chainHeadBlock: 0,
-      latestBlock: 0,
-      lastHealthyBlock: 0,
-      network: "mocknet",
-      fatalError: undefined,
-      health: "healthy",
-      synced: true,
-      url: "http://example.com",
-    });
     await expect(poller()).to.eventually.not.be.rejected;
   });
 
   it("should poll subgraph with mock non zero block", async () => {
-    getSubgraphHealthStub = stub(SharedFns, "getSubgraphHealth");
-    getSubgraphHealthStub.resolves({
-      chainHeadBlock: 1234567,
-      latestBlock: 1234567,
-      lastHealthyBlock: 100,
-      network: "mocknet",
-      fatalError: undefined,
-      health: "healthy",
-      synced: true,
-      url: "https://example.com",
-    });
     await expect(poller()).to.eventually.not.be.rejected;
   });
 
   it("should poll subgraph with mock backend", async () => {
-    getSubgraphHealthStub = stub(SharedFns, "getSubgraphHealth");
-    getSubgraphHealthStub.resolves({
-      chainHeadBlock: 1234567,
-      latestBlock: 1234567,
-      lastHealthyBlock: 100,
-      network: "mocknet",
-      fatalError: undefined,
-      health: "healthy",
-      synced: true,
-      url: "http://example.com",
-    });
     await expect(poller()).to.eventually.not.be.rejected;
   });
 
   it("should poll subgraph with mock backend empty response", async () => {
-    getSubgraphHealthStub = stub(SharedFns, "getSubgraphHealth");
-    getSubgraphHealthStub.resolves({
-      chainHeadBlock: 1234567,
-      latestBlock: 1234567,
-      lastHealthyBlock: 100,
-      network: "mocknet",
-      fatalError: undefined,
-      health: "healthy",
-      synced: true,
-      url: "http://example.com",
-    });
     (mockContext.adapters.subgraph.getOriginTransfers as SinonStub).resolves([]);
     (mockContext.adapters.subgraph.getDestinationTransfers as SinonStub).resolves([]);
 
@@ -194,17 +149,6 @@ describe("Backend operations", () => {
   });
 
   it("should poll subgraph with mock backend no block number", async () => {
-    getSubgraphHealthStub = stub(SharedFns, "getSubgraphHealth");
-    getSubgraphHealthStub.resolves({
-      chainHeadBlock: 1234567,
-      latestBlock: 1234567,
-      lastHealthyBlock: 100,
-      network: "mocknet",
-      fatalError: undefined,
-      health: "healthy",
-      synced: true,
-      url: "http://example.com",
-    });
     (mockContext.adapters.subgraph.getLatestBlockNumber as SinonStub).resolves(mockNoBlockNumber);
     (mockContext.adapters.subgraph.getOriginTransfers as SinonStub).resolves([]);
     (mockContext.adapters.subgraph.getDestinationTransfers as SinonStub).resolves([]);
