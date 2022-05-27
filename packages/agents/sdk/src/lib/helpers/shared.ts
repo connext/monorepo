7b02949e6459415d27d430ff0611643bca8c61df
Sanchay Mittal <sanchaymittal@gmail.com>
2022-05-27 08:30:55 +0530

    new: validateUri() & axiosGetRequest()

--- packages/agents/sdk/src/lib/helpers/shared.ts
+++ packages/agents/sdk/src/lib/helpers/shared.ts
@@ -1,4 +1,33 @@
-import { getChainData as _getChainData, getChainIdFromDomain as _getChainIdFromDomain } from "@connext/nxtp-utils";
+import {
+  ajv,
+  TUrl,
+  getChainData as _getChainData,
+  getChainIdFromDomain as _getChainIdFromDomain,
+} from "@connext/nxtp-utils";
+import axios from "axios";
+
+import { UriInvalid } from "../errors/index";
 
 export const getChainData = _getChainData;
 export const getChainIdFromDomain = _getChainIdFromDomain;
+
+export const validateUri = (uri: string): void => {
+  const validateInput = ajv.compile(TUrl);
+  const validInput = validateInput(uri);
+  if (!validInput) {
+    const msg = validateInput.errors?.map((err: any) => `${err.instancePath} - ${err.message}`).join(",");
+    throw new UriInvalid({
+      paramsError: msg,
+      uri,
+    });
+  }
+};
+
+export const axiosGetRequest = async (uri: string): Promise<any> => {
+  try {
+    const response = await axios.get(uri);
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
