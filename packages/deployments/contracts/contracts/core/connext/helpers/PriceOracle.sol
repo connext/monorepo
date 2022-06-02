ac43cb874022e4c2a9b88aaf3a4a9c3dba44e96c
liu <mailto123ads@gmail.com>
2022-06-02 20:58:15 +0800

    solidity upgraded to 0.8.14

--- packages/deployments/contracts/contracts/core/connext/helpers/PriceOracle.sol
+++ packages/deployments/contracts/contracts/core/connext/helpers/PriceOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.11;
+pragma solidity 0.8.14;
 
 abstract contract PriceOracle {
   /// @notice Indicator that this is a PriceOracle contract (for inspection)
