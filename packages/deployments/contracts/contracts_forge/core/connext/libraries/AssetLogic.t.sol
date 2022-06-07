f53e1e7dd5d3d8f5b1dfd1cb7655d20c794c8833
wanglonghong <wanglonghong4467@gmail.com>
2022-06-07 19:56:39 +0800

    Revert case

--- packages/deployments/contracts/contracts_forge/core/connext/libraries/AssetLogic.t.sol
+++ packages/deployments/contracts/contracts_forge/core/connext/libraries/AssetLogic.t.sol
@@ -5,13 +5,17 @@ import "../facets/FacetHelper.sol";
 import "../../../../contracts/core/connext/libraries/AssetLogic.sol";
 import {BaseConnextFacet} from "../../../../contracts/core/connext/facets/BaseConnextFacet.sol";
 import {ConnextMessage} from "../../../../contracts/core/connext/libraries/ConnextMessage.sol";
-import {LibConnextStorage, AppStorage} from "../../../../contracts/core/connext/libraries/LibConnextStorage.sol";
+import {LibConnextStorage, AppStorage, PausedFunctions} from "../../../../contracts/core/connext/libraries/LibConnextStorage.sol";
+import {ITokenRegistry} from "../../../../contracts/core/connext/interfaces/ITokenRegistry.sol";
+
+import "../../../../lib/forge-std/src/console.sol";
 
 contract AssetLogicTest is BaseConnextFacet, FacetHelper {
   // ============ Storage ============
   uint32 domain = 1;
   address canonical = address(2);
   address asset = address(3);
+  address adopted = address(4);
   bytes32 canonicalTokenId = bytes32(abi.encodePacked(canonical));
   uint32 canonicalDomain = domain;
 
@@ -20,18 +24,32 @@ contract AssetLogicTest is BaseConnextFacet, FacetHelper {
   function setUp() public {
     // set defaults
     setDefaults();
+  }
+
+  // should work
+  function test_AssetLogic_swapFromLocalAssetIfNeeded_works() public {
     vm.mockCall(
       _tokenRegistry,
       abi.encodeWithSelector(ITokenRegistry.getTokenId.selector),
       abi.encode(canonicalDomain, canonicalTokenId)
     );
-  }
-
-  // should work
-  function test_AssetLogic_swapFromLocalAssetIfNeeded_works() public {
     s.canonicalToAdopted[canonicalTokenId] = asset;
     (uint256 _amount, address _asset) = AssetLogic.swapFromLocalAssetIfNeeded(asset, 10000);
     assertEq(_amount, 10000);
     assertEq(_asset, asset);
+  }
+
+  // should revert
+  function test_AssetLogic_swapFromLocalAssetIfNeeded_revertIfSwappingPaused() public {
+    s.canonicalToAdopted[canonicalTokenId] = adopted;
+    s._paused = PausedFunctions.Swap;
+    vm.mockCall(
+      _tokenRegistry,
+      abi.encodeWithSelector(ITokenRegistry.getTokenId.selector),
+      abi.encode(canonicalDomain, canonicalTokenId)
+    );
+    vm.expectRevert(AssetLogic.AssetLogic__swapFromLocalAssetIfNeeded_swapPaused.selector);
+
+    AssetLogic.swapFromLocalAssetIfNeeded(asset, 10000);
   }
 }
