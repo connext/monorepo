0ae2f3fbb2c08e04b3d085ac6d462b2284d031a2
LayneHaber <layne.haber@gmail.com>
2022-06-07 08:01:47 -0700

    stub test cases

--- packages/deployments/contracts/contracts_forge/core/connext/libraries/AssetLogic.t.sol
+++ packages/deployments/contracts/contracts_forge/core/connext/libraries/AssetLogic.t.sol
@@ -26,18 +26,63 @@ contract AssetLogicTest is BaseConnextFacet, FacetHelper {
     setDefaults();
   }
 
-  // should work
-  function test_AssetLogic_swapFromLocalAssetIfNeeded_works() public {
-    vm.mockCall(
-      _tokenRegistry,
-      abi.encodeWithSelector(ITokenRegistry.getTokenId.selector),
-      abi.encode(canonicalDomain, canonicalTokenId)
-    );
-    s.canonicalToAdopted[canonicalTokenId] = asset;
-    (uint256 _amount, address _asset) = AssetLogic.swapFromLocalAssetIfNeeded(asset, 10000);
-    assertEq(_amount, 10000);
-    assertEq(_asset, asset);
-  }
+  // ============ stableSwapPoolExist ============
+
+  function test_AssetLogic_stableSwapPoolExist_works() public {}
+
+  // ============ getTokenIndexFromStableSwapPool ============
+
+  function test_AssetLogic_getTokenIndexFromStableSwapPool_failsIfNotFound() public {}
+
+  function test_AssetLogic_getTokenIndexFromStableSwapPool_works() public {}
+
+  // ============ handleIncomingAsset ============
+
+  function test_AssetLogic_handleIncomingAsset_failsIfValueBadWhenNative() public {}
+
+  function test_AssetLogic_handleIncomingAsset_failsIfValueBad() public {}
+
+  function test_AssetLogic_handleIncomingAsset_works() public {}
+
+  function test_AssetLogic_handleIncomingAsset_worksWithNative() public {}
+
+  function test_AssetLogic_handleIncomingAsset_worksWithNativeAndFee() public {}
+
+  function test_AssetLogic_handleIncomingAsset_worksWithFeeOnTransfer() public {}
+
+  // ============ wrapNativeAsset ============
+
+  function test_AssetLogic_wrapNativeAsset_works() public {}
+
+  // ============ transferAssetToContract ============
+
+  function test_AssetLogic_transferAssetToContract_works() public {}
+
+  function test_AssetLogic_transferAssetToContract_worksWithFeeOnTransfer() public {}
+
+  // ============ transferAssetFromContract ============
+
+  function test_AssetLogic_transferAssetFromContract_failsIfNoAsset() public {}
+
+  function test_AssetLogic_transferAssetFromContract_works() public {}
+
+  function test_AssetLogic_transferAssetFromContract_worksIfZero() public {}
+
+  function test_AssetLogic_transferAssetFromContract_worksForNative() public {}
+
+  // ============ swapToLocalAssetIfNeeded ============
+
+  function test_AssetLogic_swapToLocalAssetIfNeeded_failsIfPaused() public {}
+
+  function test_AssetLogic_swapToLocalAssetIfNeeded_worksIfZero() public {}
+
+  // does not swap if already local
+  function test_AssetLogic_swapToLocalAssetIfNeeded_worksIfLocal() public {}
+
+  // works
+  function test_AssetLogic_swapToLocalAssetIfNeeded_works() public {}
+
+  // ============ swapFromLocalAssetIfNeeded ============
 
   // should revert
   function testFail_AssetLogic_swapFromLocalAssetIfNeeded_revertIfSwappingPaused() public {
@@ -56,5 +101,23 @@ contract AssetLogicTest is BaseConnextFacet, FacetHelper {
     // vm.expectRevert(AssetLogic.AssetLogic__swapFromLocalAssetIfNeeded_swapPaused.selector);
 
     AssetLogic.swapFromLocalAssetIfNeeded(asset, 10000);
+  }
+
+  function test_AssetLogic_swapFromLocalAssetIfNeeded_worksIfZero() public {}
+
+  // does not swap if already adopted
+  function test_AssetLogic_swapFromLocalAssetIfNeeded_worksIfAdopted() public {}
+
+  // should work
+  function test_AssetLogic_swapFromLocalAssetIfNeeded_works() public {
+    vm.mockCall(
+      _tokenRegistry,
+      abi.encodeWithSelector(ITokenRegistry.getTokenId.selector),
+      abi.encode(canonicalDomain, canonicalTokenId)
+    );
+    s.canonicalToAdopted[canonicalTokenId] = asset;
+    (uint256 _amount, address _asset) = AssetLogic.swapFromLocalAssetIfNeeded(asset, 10000);
+    assertEq(_amount, 10000);
+    assertEq(_asset, asset);
   }
 }
