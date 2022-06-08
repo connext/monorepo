cab226aa3ad4343880d0249b271acfbc2ec20b50
Sanchay Mittal <sanchaymittal@gmail.com>
2022-06-08 13:13:41 +0800

    moraar unit test

--- packages/deployments/contracts/contracts_forge/core/connext/libraries/SwapUtils.t.sol
+++ packages/deployments/contracts/contracts_forge/core/connext/libraries/SwapUtils.t.sol
@@ -61,6 +61,8 @@ contract SwapUtilsTest is ForgeHelper {
 
   SwapUtils.Swap swap;
 
+  uint256 preciseA;
+
   address lpTokenReceiver = address(1);
   uint256 lpTokenSupply = uint256(1000);
 
@@ -68,7 +70,8 @@ contract SwapUtilsTest is ForgeHelper {
   uint256 token2Balance = uint256(1000);
 
   uint256 amount = uint256(10);
-  uint8 tokenIndex = uint8(0);
+  uint8 tokenIndexFrom = uint8(0);
+  uint8 tokenIndexTo = uint8(1);
 
   // ============ Setup ============
 
@@ -102,6 +105,8 @@ contract SwapUtilsTest is ForgeHelper {
       tokenPrecisionMultipliers,
       balances
     );
+
+    preciseA = AmplificationUtils._getAPrecise(swap);
   }
 
   // ============ Utils ============
@@ -111,7 +116,7 @@ contract SwapUtilsTest is ForgeHelper {
   // Should work
   function test_SwapUtils__calculateWithdrawOneToken_works() public {
     vm.mockCall(address(lpToken), abi.encodeWithSelector(IERC20.totalSupply.selector), abi.encode(lpTokenSupply));
-    uint256 res = SwapUtils.calculateWithdrawOneToken(swap, amount, tokenIndex);
+    uint256 res = SwapUtils.calculateWithdrawOneToken(swap, amount, tokenIndexFrom);
     assertEq(res, uint256(19));
   }
 
@@ -122,7 +127,7 @@ contract SwapUtilsTest is ForgeHelper {
     uint256 dy;
     uint256 newY;
     uint256 currentY;
-    (dy, newY, currentY) = SwapUtils.calculateWithdrawOneTokenDY(swap, tokenIndex, amount, lpTokenSupply);
+    (dy, newY, currentY) = SwapUtils.calculateWithdrawOneTokenDY(swap, tokenIndexFrom, amount, lpTokenSupply);
     assertEq(dy, uint256(19));
     assertEq(newY, uint256(980));
     assertEq(currentY, uint256(lpTokenSupply));
@@ -131,13 +136,76 @@ contract SwapUtilsTest is ForgeHelper {
   // ============ getYD ============
 
   // Should work
-  //   function test_SwapUtils__getYD_works() public {
-  //     uint256 dy;
-  //     uint256 newY;
-  //     uint256 currentY;
-  //     (dy, newY, currentY) = SwapUtils.getYD(, tokenIndex, amount, lpTokenSupply);
-  //     assertEq(dy, uint256(19));
-  //     assertEq(newY, uint256(980));
-  //     assertEq(currentY, uint256(lpTokenSupply));
-  //   }
+  function test_SwapUtils__getYD_works() public {
+    uint256[] memory xp = SwapUtils._xp(swap);
+
+    uint256 d0 = SwapUtils.getD(xp, preciseA);
+    uint256 d1 = d0.sub(amount.mul(d0).div(lpTokenSupply));
+
+    uint256 newY = SwapUtils.getYD(preciseA, tokenIndexFrom, xp, d1);
+
+    assertEq(newY, uint256(980));
+  }
+
+  // ============ getD ============
+
+  // Should work
+  function test_SwapUtils__getD_works() public {
+    uint256[] memory xp = SwapUtils._xp(swap);
+    uint256 d0 = SwapUtils.getD(xp, preciseA);
+
+    assertEq(d0, uint256(2000));
+  }
+
+  // ============ _xp ============
+
+  // Should work
+  function test_SwapUtils___xp_works() public {
+    uint256[] memory xp = SwapUtils._xp(swap);
+
+    uint256 numTokens = balances.length;
+    for (uint256 i = 0; i < numTokens; i++) {
+      assertEq(xp[0], balances[0].mul(tokenPrecisionMultipliers[0]));
+    }
+  }
+
+  // ============ getVirtualPrice ============
+
+  // Should work
+  function test_SwapUtils__getVirtualPrice_worksWithSupplyZero() public {
+    uint256 virtualPrice = SwapUtils.getVirtualPrice(swap);
+
+    assertEq(virtualPrice, uint256(0));
+  }
+
+  // Should work
+  function test_SwapUtils__getVirtualPrice_worksWithSupply() public {
+    vm.mockCall(address(lpToken), abi.encodeWithSelector(IERC20.totalSupply.selector), abi.encode(lpTokenSupply));
+    uint256 virtualPrice = SwapUtils.getVirtualPrice(swap);
+
+    uint256[] memory xp = SwapUtils._xp(swap);
+    uint256 d = SwapUtils.getD(xp, preciseA);
+
+    uint256 verify = d.mul(10**uint256(SwapUtils.POOL_PRECISION_DECIMALS)).div(lpTokenSupply);
+
+    assertEq(virtualPrice, uint256(verify));
+  }
+
+  // ============ getY ============
+
+  // Should work
+  function test_SwapUtils__getY_works() public {
+    uint256[] memory xp = SwapUtils._xp(swap);
+    // uint256 y = SwapUtils.getY(preciseA, tokenIndexFrom, tokenIndexTo, x, xp);
+  }
+
+  // ============ calculateSwap ============
+
+  // Should work
+  function test_SwapUtils__calculateSwap_works() public {
+    uint256 dy = SwapUtils.calculateSwap(swap, tokenIndexFrom, tokenIndexTo, amount);
+
+    console.logUint(dy);
+    assertEq(dy, uint256(9));
+  }
 }
