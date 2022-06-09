fcc382bf096367b341a83eaf0bf0f8cea1a9f178
Rahul Sethuram <rahul@sethuram.com>
2022-06-09 11:50:43 -0700

    Handler

--- packages/integration/test/helpers/enrollHandlers.ts
+++ packages/integration/test/helpers/enrollHandlers.ts
@@ -1,3 +1,14 @@
 import { Wallet } from "ethers";
 
-export const enrollHandlers = async (_wallet: Wallet) => {};
+export const enrollHandlers = async (
+  handlers: {
+    domain: string;
+    connextHandler: string;
+    promiseRouterUpgradeBeaconProxy: string;
+    relayerFeeRouterUpgradeBeaconProxy: string;
+  }[],
+  wallet: Wallet,
+) => {
+  for (const handler of ["ConnextHandler", "PromiseRouterUpgradeBeaconProxy", "RelayerFeeRouterUpgradeBeaconProxy"]) {
+  }
+};
