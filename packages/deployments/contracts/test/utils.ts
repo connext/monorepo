fc1bb8eb1c5eb9dcf07d1b1de7d2cfefefc5742f
Jake Kidd <jcb.kdd@gmail.com>
2022-06-16 16:13:10 -0700

    fix import

--- packages/deployments/contracts/test/utils.ts
+++ packages/deployments/contracts/test/utils.ts
@@ -22,7 +22,7 @@ import {
   BridgeFacet,
   ProposedOwnableFacet,
   IERC20,
-} from "../typechain-types";
+} from "../src/typechain-types";
 import { Artifact } from "hardhat/types";
 
 export const MAX_FEE_PER_GAS = BigNumber.from("975000000");
