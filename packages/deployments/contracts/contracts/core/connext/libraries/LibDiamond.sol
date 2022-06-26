8f0061e5016ddcd4f8cfcb77d3c59d3ba2944241
LayneHaber <layne.haber@gmail.com>
2022-06-26 18:17:42 -0400

    allow for first cut

--- packages/deployments/contracts/contracts/core/connext/libraries/LibDiamond.sol
+++ packages/deployments/contracts/contracts/core/connext/libraries/LibDiamond.sol
@@ -97,8 +97,11 @@ library LibDiamond {
     address _init,
     bytes memory _calldata
   ) internal {
-    uint256 time = diamondStorage().acceptanceTimes[keccak256(abi.encode(_diamondCut))];
-    require(time > 0 && time < block.timestamp, "LibDiamond: delay not elapsed");
+    DiamondStorage storage ds = diamondStorage();
+    if (ds.facetAddresses.length != 0) {
+      uint256 time = ds.acceptanceTimes[keccak256(abi.encode(_diamondCut))];
+      require(time > 0 && time < block.timestamp, "LibDiamond: delay not elapsed");
+    } // Otherwise, this is the first instance of deployment and it can be set automatically
     for (uint256 facetIndex; facetIndex < _diamondCut.length; facetIndex++) {
       IDiamondCut.FacetCutAction action = _diamondCut[facetIndex].action;
       if (action == IDiamondCut.FacetCutAction.Add) {
