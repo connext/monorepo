f5cf4da5b1de3e9c4466a0ba231f4e5122f416c3
Rahul Sethuram <rahul@sethuram.com>
2022-06-27 10:56:11 -0400

    Fix migration

--- packages/agents/cartographer/poller/db/migrations/20220618065158_checkpoints.sql
+++ packages/agents/cartographer/poller/db/migrations/20220618065158_checkpoints.sql
@@ -1,10 +1,7 @@
 -- migrate:up
-
-CREATE TABLE public.checkpoints (
+CREATE TABLE IF NOT EXISTS public.checkpoints (
     check_name character varying(255) NOT NULL PRIMARY KEY,
     check_point numeric DEFAULT 0 NOT NULL
 );
-
-
 -- migrate:down
-
+DROP TABLE public.checkpoints;
