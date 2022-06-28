55f9b782acd42e51c723511c863b381be304b6ac
wanglonghong <wanglonghong4467@gmail.com>
2022-06-28 22:29:32 +0800

    Comment out nomad-sdk integration

--- packages/agents/router/src/lib/operations/execute.ts
+++ packages/agents/router/src/lib/operations/execute.ts
@@ -121,20 +121,22 @@ export const execute = async (params: OriginTransfer): Promise<void> => {
     });
   }
 
-  const { originBlacklisted, destinationBlacklisted } = await getBlacklist(originDomain, destinationDomain);
-
   logger.debug("Signed payloads", requestContext, methodContext, {
     rounds: Object.keys(signatures),
     // Sanitized with ellipsis.
     sigs: Object.values(signatures).map((s) => s.slice(0, 6) + ".."),
   });
 
-  if (originBlacklisted || destinationBlacklisted) {
-    throw new NomadHomeBlacklisted({
-      originDomainBlacklisted: originBlacklisted,
-      destinationBlacklisted: destinationBlacklisted,
-    });
-  }
+  // Need to make sure if nomad-sdk handles an error in case of bad rpc before integrating.
+  // Test code base: https://codesandbox.io/s/nomad-integration-testing-h8q00t?file=/index.js
+
+  // const { originBlacklisted, destinationBlacklisted } = await getBlacklist(originDomain, destinationDomain);
+  // if (originBlacklisted || destinationBlacklisted) {
+  //   throw new NomadHomeBlacklisted({
+  //     originDomainBlacklisted: originBlacklisted,
+  //     destinationBlacklisted: destinationBlacklisted,
+  //   });
+  // }
 
   if (callData !== "0x") {
     const code = await txservice.getCode(+destinationDomain, to);
