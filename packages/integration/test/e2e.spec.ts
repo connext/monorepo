1acba38128589c2bd5e2a88082c5e313020609c9
wanglonghong <wanglonghong4467@gmail.com>
2022-07-06 23:47:19 +0800

    a couple of fixes

--- packages/integration/test/e2e.spec.ts
+++ packages/integration/test/e2e.spec.ts
@@ -37,7 +37,7 @@ const PARAMETERS = {
     CHAIN: 1337,
     RPC: ["http://localhost:8547"],
     DEPLOYMENTS: {
-      ConnextHandler: "0x8273e4B8ED6c78e252a9fCa5563Adfcc75C91b2A",
+      ConnextHandler: "0xF08dF3eFDD854FEDE77Ed3b2E515090EEe765154",
       PromiseRouterUpgradeBeaconProxy: "0xbaAA2a3237035A2c7fA2A33c76B44a8C6Fe18e87",
       RelayerFeeRouterUpgradeBeaconProxy: "0xEcFcaB0A285d3380E488A39B4BB21e777f8A4EaC",
     },
@@ -47,7 +47,7 @@ const PARAMETERS = {
     CHAIN: 1338,
     RPC: ["http://localhost:8546"],
     DEPLOYMENTS: {
-      ConnextHandler: "0x8273e4B8ED6c78e252a9fCa5563Adfcc75C91b2A",
+      ConnextHandler: "0xF08dF3eFDD854FEDE77Ed3b2E515090EEe765154",
       PromiseRouterUpgradeBeaconProxy: "0xbaAA2a3237035A2c7fA2A33c76B44a8C6Fe18e87",
       RelayerFeeRouterUpgradeBeaconProxy: "0xEcFcaB0A285d3380E488A39B4BB21e777f8A4EaC",
       TokenRegistry: "0x75c35C980C0d37ef46DF04d31A140b65503c0eEd",
