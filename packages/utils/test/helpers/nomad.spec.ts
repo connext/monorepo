46d351ad923bd263bc43d121ac9a243de6438083
just-a-node <eye1717@gmail.com>
2022-07-08 14:07:34 -0600

    Make mock chainIds different from domainIds for better testing

--- packages/utils/test/helpers/nomad.spec.ts
+++ packages/utils/test/helpers/nomad.spec.ts
@@ -18,11 +18,11 @@ describe("Helpers:Nomad", () => {
     });
     it("happy: should get chainId from domain with the `chainData` argument", async () => {
       const chainId = await getChainIdFromDomain("1337", mock.chainData());
-      expect(chainId).to.be.eq(1337);
+      expect(chainId).to.be.eq(13337);
     });
     it("happy: should get chainId from domain using `getChainData`", async () => {
       const chainId = await getChainIdFromDomain("1337");
-      expect(chainId).to.be.eq(1337);
+      expect(chainId).to.be.eq(13337);
     });
   });
   describe("#getDomainFromChainId", () => {
@@ -30,11 +30,11 @@ describe("Helpers:Nomad", () => {
       expect(getDomainFromChainId(2000, mock.chainData())).to.be.eventually.throw();
     });
     it("happy: should get domain from chainId with the `chainData` argument", async () => {
-      const domain = await getDomainFromChainId(1337, mock.chainData());
+      const domain = await getDomainFromChainId(13337, mock.chainData());
       expect(domain).to.be.eq("1337");
     });
     it("happy: should get domain from chainId using `getChainData`", async () => {
-      const domain = await getDomainFromChainId(1337);
+      const domain = await getDomainFromChainId(13337);
       expect(domain).to.be.eq("1337");
     });
   });
