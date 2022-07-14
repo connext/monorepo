0816e2a89dc5274487f5beeb1a53d07df923d898
wanglonghong <wanglonghong4467@gmail.com>
2022-07-15 02:06:37 +0800

    rm unused schemas

--- packages/utils/src/peripherals/subgraph.ts
+++ packages/utils/src/peripherals/subgraph.ts
@@ -1,24 +1,3 @@
-// TODO: Make an actual error type for this?
-type SubgraphHealthError = {
-  message: string;
-  block: number;
-  handler: any;
-};
-
-export type SubgraphHealth = {
-  chainHeadBlock: number;
-  latestBlock: number;
-  lastHealthyBlock: number | undefined;
-  network: string;
-  fatalError: SubgraphHealthError | undefined;
-  health:
-    | "healthy" // Subgraph syncing normally
-    | "unhealthy" // Subgraph syncing but with errors
-    | "failed"; // Subgraph halted due to errors
-  synced: boolean;
-  url: string;
-};
-
 export type SubgraphQueryMetaParams = {
   maxBlockNumber: number;
   latestNonce: number;
