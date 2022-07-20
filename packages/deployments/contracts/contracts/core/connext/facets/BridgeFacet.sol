ef60e440f7f59bedd291424ea4cf475e8469e28f
Liu <mailto123ads@gmail.com>
2022-07-20 10:40:32 -0400

    fix sponsors full amount

--- packages/deployments/contracts/contracts/core/connext/facets/BridgeFacet.sol
+++ packages/deployments/contracts/contracts/core/connext/facets/BridgeFacet.sol
@@ -688,8 +688,14 @@ contract BridgeFacet is BaseConnextFacet {
         // balance read about it
 
         uint256 starting = IERC20(_asset).balanceOf(address(this));
+        uint256 liquidityFee = _getFastTransferAmount(
+          _args.amount,
+          (s.LIQUIDITY_FEE_DENOMINATOR - s.LIQUIDITY_FEE_NUMERATOR),
+          s.LIQUIDITY_FEE_DENOMINATOR
+        );
+
         (bool success, bytes memory data) = address(s.sponsorVault).call(
-          abi.encodeWithSelector(s.sponsorVault.reimburseLiquidityFees.selector, _asset, _args.amount, _args.params.to)
+          abi.encodeWithSelector(s.sponsorVault.reimburseLiquidityFees.selector, _asset, liquidityFee, _args.params.to)
         );
 
         if (success) {
