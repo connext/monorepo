151f4be13d0c75a7a74a1886ebe73800d3b50624
Rahul Sethuram <rahul@sethuram.com>
2022-07-20 16:41:17 +0200

    Fix build

--- packages/agents/router/src/subscriber/subscriber.ts
+++ packages/agents/router/src/subscriber/subscriber.ts
@@ -76,9 +76,6 @@ export const makeSubscriber = async (_configOverride?: NxtpRouterConfig) => {
     context.adapters.contracts = getContractInterfaces();
     context.adapters.mqClient = await setupMq(
       context.config.messageQueue.uri as string,
-      context.config.messageQueue.port as number,
-      context.config.messageQueue.user as string,
-      context.config.messageQueue.pass as string,
       context.logger,
       requestContext,
     );
