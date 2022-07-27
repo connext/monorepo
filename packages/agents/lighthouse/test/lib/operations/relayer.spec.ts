f3ca7705118e3a457935c8971aba0e795f0e9ad4
Sanchay Mittal <sanchaymittal@gmail.com>
2022-07-27 18:26:32 +0400

    fix: unit test

--- packages/agents/lighthouse/test/lib/operations/relayer.spec.ts
+++ packages/agents/lighthouse/test/lib/operations/relayer.spec.ts
@@ -9,20 +9,14 @@ describe("Operations:Relayer", () => {
   let mockContext: any;
   let connextRelayerSendStub: SinonStub;
   let getGelatoRelayerAddressStub: SinonStub;
-  let getGasEstimateWithRevertCodeStub: SinonStub;
-  let externalRelayerSendStub: SinonStub;
   beforeEach(() => {
     mockContext = stubContext();
     connextRelayerSendStub = stub().resolves();
     getGelatoRelayerAddressStub = stub().resolves(mkAddress("0x111"));
-    getGasEstimateWithRevertCodeStub = stub().resolves();
-    externalRelayerSendStub = stub().resolves();
     getHelpersStub.returns({
       relayer: {
         connextRelayerSend: connextRelayerSendStub,
-        externalRelayerSend: externalRelayerSendStub,
         getGelatoRelayerAddress: getGelatoRelayerAddressStub,
-        getGasEstimateWithRevertCode: getGasEstimateWithRevertCodeStub,
       },
     });
   });
@@ -33,47 +27,56 @@ describe("Operations:Relayer", () => {
   describe("#sendToRelayer", () => {
     it("should not try to send the payload to the connext relayer if relayerUrl is undefined", async () => {
       ctxMock.config.relayerUrl = undefined;
-      externalRelayerSendStub.resolves();
+
       connextRelayerSendStub.resolves();
       const executeArgs = mock.entity.executeArgs();
       const transferId = mkBytes32();
       await sendToRelayer(executeArgs, encodedDataMock, transferId, requestContext);
       expect(connextRelayerSendStub.callCount).to.be.eq(0);
       expect(getGelatoRelayerAddressStub.callCount).to.be.eq(1);
-      expect(getGasEstimateWithRevertCodeStub.callCount).to.be.eq(1);
-      expect(externalRelayerSendStub.callCount).to.be.eq(1);
+      expect(ctxMock.adapters.chainreader.getGasEstimateWithRevertCode).to.be.calledOnceWith(
+        Number(executeArgs.params.destinationDomain),
+      );
+      expect(ctxMock.adapters.relayer.send).to.be.calledOnceWith(
+        Number(mock.chain.B),
+        ctxMock.config.chains[mock.domain.B].deployments.connext,
+        "0xabcde",
+        requestContext,
+      );
     });
     it("should not send the payload if gas estimation fails", async () => {
-      externalRelayerSendStub.resolves();
-      getGasEstimateWithRevertCodeStub.throws(new Error("Gas estimation failed"));
       const executeArgs = mock.entity.executeArgs();
       const transferId = mkBytes32();
       await sendToRelayer(executeArgs, encodedDataMock, transferId, requestContext);
       expect(connextRelayerSendStub.callCount).to.be.eq(1);
-      expect(getGelatoRelayerAddressStub.callCount).to.be.eq(1);
-      expect(externalRelayerSendStub.callCount).to.be.eq(0);
+      expect(ctxMock.adapters.chainreader.getGasEstimateWithRevertCode).to.be.calledOnceWith(
+        Number(executeArgs.params.destinationDomain),
+      );
     });
     it("should send the payload to the backup relayer successfully", async () => {
-      externalRelayerSendStub.resolves();
       connextRelayerSendStub.resolves({ taskId: mkBytes32("0x12345") });
       const executeArgs = mock.entity.executeArgs();
       const transferId = mkBytes32();
       await sendToRelayer(executeArgs, encodedDataMock, transferId, requestContext);
       expect(connextRelayerSendStub.callCount).to.be.eq(1);
       expect(getGelatoRelayerAddressStub.callCount).to.be.eq(0);
-      expect(getGasEstimateWithRevertCodeStub.callCount).to.be.eq(0);
-      expect(externalRelayerSendStub.callCount).to.be.eq(0);
     });
     it("should send the payload to the gelato relayer successfully!", async () => {
-      externalRelayerSendStub.resolves();
       connextRelayerSendStub.throws();
       const executeArgs = mock.entity.executeArgs();
       const transferId = mkBytes32();
       await sendToRelayer(executeArgs, encodedDataMock, transferId, requestContext);
       expect(connextRelayerSendStub.callCount).to.be.eq(1);
       expect(getGelatoRelayerAddressStub.callCount).to.be.eq(1);
-      expect(getGasEstimateWithRevertCodeStub.callCount).to.be.eq(1);
-      expect(externalRelayerSendStub.callCount).to.be.eq(1);
+      expect(ctxMock.adapters.chainreader.getGasEstimateWithRevertCode).to.be.calledOnceWith(
+        Number(executeArgs.params.destinationDomain),
+      );
+      expect(ctxMock.adapters.relayer.send).to.be.calledOnceWith(
+        Number(mock.chain.B),
+        ctxMock.config.chains[mock.domain.B].deployments.connext,
+        "0xabcde",
+        requestContext,
+      );
     });
   });
 });
