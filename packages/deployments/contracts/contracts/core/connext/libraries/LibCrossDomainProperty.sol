7145e4e0d249eed97ccf991dfb2313689ec0b82b
LayneHaber <layne.haber@gmail.com>
2022-07-28 15:49:39 -0600

    fix: review comments

--- packages/deployments/contracts/contracts/core/connext/libraries/LibCrossDomainProperty.sol
+++ packages/deployments/contracts/contracts/core/connext/libraries/LibCrossDomainProperty.sol
@@ -3,6 +3,27 @@ pragma solidity 0.8.15;
 
 import {TypedMemView} from "../../../nomad-core/libs/TypedMemView.sol";
 
+/**
+ * @notice This library includes functions to help parse the values appended
+ * to the `msg.data` when a call is executed by the `Executor`.
+ *
+ * During a crosschain transfer, a user specifies `callData` to be executed on
+ * the destination domain via `.call`. Prior to executing this `callData`, the
+ * `Executor` will append the following properties to the calldata:
+ * - origin - the origin domain
+ * - originSender - `msg.sender` of `xcall`
+ * - amount - the amount the `.call` is executed with
+ *
+ * This library provides helpers to parse these properties out from and append
+ * them to the calldata.
+ *
+ * ***IMPORTANT NOTE ON USAGE***
+ * It is important to keep in mind that ANYONE can append bogus properties to the
+ * `msg.data` a function receives. To ensure checks such as:
+ *      _admin == LibCrossDomainProperty.originSender(msg.data);
+ * are valid, checking these properties should be paired with enforcing the msg.sender
+ * of the call is the `Executor` contract on that domain.
+ */
 library LibCrossDomainProperty {
   // ============ Libraries ============
 
@@ -17,48 +38,74 @@ library LibCrossDomainProperty {
   // ============ Internal Functions ============
 
   /**
-   * @notice
-   * @param
-   * @param
-   * @param
-   * @param
-   * @return
+   * @notice Appends the supplied properties to the end of the callData
+   * @dev See warning at the top of the file about usage
+   * @param _amount The amount the call is executed with
+   * @param _originDomain The originating domain of the call
+   * @param _originSender The msg.sender of the xcall
+   * @param _calldata The function data to be executed with the .call
+   * @return bytes A byte array containing the calldata and all properties
    */
   function formatCalldataWithProperties(
     uint256 _amount,
     uint32 _originDomain,
     address _originSender,
-    bytes memory callData
+    bytes memory _calldata
   ) internal returns (bytes memory) {
     // Convert data to bytes
     bytes memory properties = abi.encodePacked(_amount, _originDomain, _originSender);
-    return bytes.concat(callData, properties);
+    return bytes.concat(_calldata, properties);
   }
 
-  function amount(bytes memory data) internal returns (uint256) {
+  /**
+   * @notice Parses the amount from the msg.data
+   * @dev See warning at the top of the file about usage
+   * @param _data The msg.data sent by executor
+   * @return uint256 Amount to execute call with
+   */
+  function amount(bytes memory _data) internal returns (uint256) {
     // create view
-    bytes29 typed = data.ref(0);
-    // The data will be packed with the properties appended to the data
-    uint256 dataLen = typed.len() - PROPERTIES_LENGTH;
+    bytes29 typed = _data.ref(0);
     // before the domain = calldata
-    return typed.indexUint(dataLen, 32);
+    return typed.indexUint(callDataLength(typed), 32);
   }
 
-  function origin(bytes memory data) internal returns (uint32) {
+  /**
+   * @notice Parses the origin domain from the msg.data
+   * @dev See warning at the top of the file about usage
+   * @param _data The msg.data sent by executor
+   * @return uint32 origin domain
+   */
+  function origin(bytes memory _data) internal returns (uint32) {
     // create view
-    bytes29 typed = data.ref(0);
-    // The data will be packed with the properties appended to the data
-    uint256 dataLen = typed.len() - PROPERTIES_LENGTH;
+    bytes29 typed = _data.ref(0);
     // before the domain = calldata + amount
-    return uint32(typed.indexUint(dataLen + 32, 4));
+    return uint32(typed.indexUint(callDataLength(typed) + 32, 4));
   }
 
-  function originSender(bytes memory data) internal returns (address) {
+  /**
+   * @notice Parses the origin msg.sender from the msg.data
+   * @dev See warning at the top of the file about usage
+   * @param _data The msg.data sent by executor
+   * @return address The msg.sender of the initial `xcall`
+   */
+  function originSender(bytes memory _data) internal returns (address) {
     // create view
-    bytes29 typed = data.ref(0);
-    // The data will be packed with the properties appended to the data
-    uint256 dataLen = typed.len() - PROPERTIES_LENGTH;
+    bytes29 typed = _data.ref(0);
     // before the domain = calldata + amount + domain
-    return typed.indexAddress(dataLen + 36);
+    return typed.indexAddress(callDataLength(typed) + 36);
+  }
+
+  /**
+   * @notice Used internally to get the length of the calldata included
+   * @dev See warning at the top of the file about usage
+   * @param _view The msg.data sent by executor cast as a TypedMemView
+   * @return uint256 Length of the calldata
+   */
+  function callDataLength(bytes29 _view) internal returns (uint256) {
+    uint256 len = _view.len();
+    require(len >= PROPERTIES_LENGTH, "!length");
+    // The data will be packed with the properties appended to the data
+    return len - PROPERTIES_LENGTH;
   }
 }
