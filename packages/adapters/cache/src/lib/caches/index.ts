644ec033d3a1428de08c7b0ca0e86e27b4f81204
wanglonghong <wanglonghong4467@gmail.com>
2022-08-10 23:35:46 +0800

    setup lighthouse cache

--- packages/adapters/cache/src/lib/caches/index.ts
+++ packages/adapters/cache/src/lib/caches/index.ts
@@ -4,3 +4,4 @@ export { AuctionsCache } from "./auctions";
 export { ConsumersCache } from "./consumers";
 export { RoutersCache } from "./routers";
 export { TasksCache } from "./tasks";
+export { LightHouseCache } from "./lighthouse";
