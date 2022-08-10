e9f77c9b42d1c6de02f18e1a43775e4a22f0bc2b
jakekidd <jcb.kdd@gmail.com>
2022-08-09 17:40:38 -0700

    deployment script updates

--- packages/deployments/contracts/deploy/01_deployMessaging.ts
+++ packages/deployments/contracts/deploy/01_deployMessaging.ts
@@ -2,49 +2,93 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { BigNumber, constants, Wallet } from "ethers";
 
-import { chainIdToDomain } from "../src";
+import { chainIdToDomain, mustGetEnv } from "../src";
 
 // TODO: Should be made a step in the deployment process.
 // Should replace the nomad steps.
 
-// The chain ID of the hub. For production environment, should be Ethereum Mainet.
-const HUB_CHAIN_ID = 1;
+const DEFAULT_PROCESS_GAS = BigNumber.from("200000");
+const DEFAULT_RESERVE_GAS = BigNumber.from("100000");
+
 // Contract prefixes for Connector contracts.
 const HUB_PREFIX = "L1";
 const SPOKE_PREFIX = "L2";
-// Map of chain ID => config for all spoke chains.
-const CHAIN_TO_CONFIG: {
-  [chain: number]: {
-    prefix: string; // The chain's name and the Connector name prefix.
-    amb: string; // Official AMB contract address.
-    processGas: BigNumber;
-    reserveGas: BigNumber;
+
+type RouterAddresses = {
+  bridgeRouter: string;
+  // promiseRouter: string;
+  // relayerFeeRouter: string;
+};
+
+type ProtocolConfig = {
+  // The chain ID of the hub. For production environment, should be Ethereum Mainnet (1).
+  hub: number;
+  configs: {
+    // Map of chain ID => configs.
+    [chain: number]: {
+      prefix: string; // The chain's name and the Connector name prefix.
+      amb: string; // Official AMB contract address.
+      processGas: BigNumber;
+      reserveGas: BigNumber;
+    };
   };
+};
+
+const PROTOCOL_CONFIGS: {
+  local: ProtocolConfig;
+  testnet: ProtocolConfig;
+  mainnet: ProtocolConfig;
 } = {
-  // NOTE: Eth mainnet (chain ID = 1) intentionally not included here.
-  10: {
-    prefix: "Optimism",
-    amb: "",
-    processGas: BigNumber.from("200_000"),
-    reserveGas: BigNumber.from("200_000"),
+  local: {
+    hub: 1337,
+    configs: {
+      // TODO: Configs for 1337, 1338
+    },
   },
-  100: {
-    prefix: "Gnosis",
-    amb: "",
-    processGas: BigNumber.from("2_000_000"),
-    reserveGas: BigNumber.from("200_000"),
+  testnet: {
+    hub: 4,
+    configs: {
+      // TODO: Configs for rinkeby, goerli, ropsten, etc.
+    },
+  },
+  mainnet: {
+    hub: 1,
+    configs: {
+      1: {
+        prefix: "Eth",
+        amb: "",
+        processGas: DEFAULT_PROCESS_GAS,
+        reserveGas: DEFAULT_RESERVE_GAS,
+      },
+      10: {
+        prefix: "Optimism",
+        amb: "",
+        // TODO: 2mil gas for opti (going L1 => L2)? Is that correct?
+        processGas: BigNumber.from("2000000"),
+        reserveGas: DEFAULT_RESERVE_GAS,
+      },
+      100: {
+        prefix: "Gnosis",
+        amb: "",
+        processGas: DEFAULT_PROCESS_GAS,
+        reserveGas: DEFAULT_RESERVE_GAS,
+      },
+    },
   },
 };
 
 // Format the arguments for Connector contract constructor.
-const formatConnectorArgs = (args: {
-  deploymentChainId: number;
-  mirrorChainId: number;
-  rootManager: string;
-  bridgeRouter: string;
-}): any[] => {
-  const { deploymentChainId, mirrorChainId, rootManager, bridgeRouter } = args;
-  const config = CHAIN_TO_CONFIG[deploymentChainId];
+const formatConnectorArgs = (
+  protocol: ProtocolConfig,
+  args: {
+    deploymentChainId: number;
+    mirrorChainId: number;
+    rootManager: string;
+    routers: RouterAddresses;
+  },
+): any[] => {
+  const { deploymentChainId, mirrorChainId, rootManager, routers } = args;
+  const config = protocol.configs[deploymentChainId];
 
   const deploymentDomain = BigNumber.from(chainIdToDomain(deploymentChainId).toString());
   const mirrorDomain = BigNumber.from(chainIdToDomain(mirrorChainId).toString());
@@ -52,7 +96,7 @@ const formatConnectorArgs = (args: {
     deploymentDomain,
     config.amb,
     rootManager,
-    bridgeRouter,
+    routers.bridgeRouter,
     config.processGas,
     config.reserveGas,
     // Mirror domain should be known.
@@ -66,7 +110,8 @@ const formatConnectorArgs = (args: {
 const handleDeployMainnet = async (
   hre: HardhatRuntimeEnvironment,
   deployer: Wallet,
-  bridgeRouter: string,
+  protocol: ProtocolConfig,
+  routers: RouterAddresses,
 ): Promise<void> => {
   // Deploy RootManager.
   console.log("Deploying RootManager...");
@@ -81,19 +126,20 @@ const handleDeployMainnet = async (
   // TODO: Deploy EthMainnetConnector.
 
   // Loop through every HubConnector and deploy.
-  for (const mirrorChain of Object.keys(CHAIN_TO_CONFIG)) {
+  const { configs } = protocol;
+  for (const mirrorChain of Object.keys(configs)) {
     const mirrorChainId = +mirrorChain;
-    const prefix = CHAIN_TO_CONFIG[mirrorChainId].prefix + HUB_PREFIX;
+    const prefix = configs[mirrorChainId].prefix + HUB_PREFIX;
     const contract = `${prefix}Connector`;
     console.log(`Deploying ${contract}...`);
     const deployment = await hre.deployments.deploy(contract, {
       contract,
       from: deployer.address,
-      args: formatConnectorArgs({
-        deploymentChainId: HUB_CHAIN_ID,
+      args: formatConnectorArgs(protocol, {
+        deploymentChainId: protocol.hub,
         mirrorChainId,
         rootManager: rootManager.address,
-        bridgeRouter,
+        routers,
       }),
       skipIfAlreadyDeployed: true,
       log: true,
@@ -105,8 +151,9 @@ const handleDeployMainnet = async (
 const handleDeploySpoke = async (
   hre: HardhatRuntimeEnvironment,
   deployer: Wallet,
+  protocol: ProtocolConfig,
   deploymentChainId: number,
-  bridgeRouter: string,
+  routers: RouterAddresses,
 ): Promise<void> => {
   // Deploy RootManager.
   console.log("Deploying RootManager...");
@@ -119,17 +166,19 @@ const handleDeploySpoke = async (
   });
   console.log(`RootManager deployed to ${rootManager.address}`);
 
-  const prefix = CHAIN_TO_CONFIG[deploymentChainId].prefix + SPOKE_PREFIX;
+  // Deploy the Connector contract for this Spoke chain.
+  const { configs } = protocol;
+  const prefix = configs[deploymentChainId].prefix + SPOKE_PREFIX;
   const contract = `${prefix}Connector`;
   console.log(`Deploying ${contract}...`);
   const deployment = await hre.deployments.deploy(contract, {
     contract,
     from: deployer.address,
-    args: formatConnectorArgs({
+    args: formatConnectorArgs(protocol, {
       deploymentChainId,
-      mirrorChainId: HUB_CHAIN_ID,
+      mirrorChainId: protocol.hub,
       rootManager: rootManager.address,
-      bridgeRouter,
+      routers,
     }),
     skipIfAlreadyDeployed: true,
     log: true,
@@ -154,6 +203,14 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment): Promise<voi
   console.log("\n============================= Deploying Messaging Contracts ===============================");
   console.log("deployer: ", deployer.address);
 
+  const env = mustGetEnv();
+  const network = env === "production" ? "mainnet" : env === "staging" ? "testnet" : "local";
+  const protocol = PROTOCOL_CONFIGS[network];
+
+  if (!protocol.configs[protocol.hub]) {
+    throw new Error(`Network ${network} is not supported!`);
+  }
+
   // Deploy BridgeRouter.
   console.log("Deploying BridgeRouter...");
   const bridgeRouter = await hre.deployments.deploy("BridgeRouter", {
@@ -166,13 +223,17 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment): Promise<voi
   console.log(`BridgeRouter deployed to ${bridgeRouter.address}`);
 
   // Handle deployment for RootManager and Connector(s).
-  if (chain === HUB_CHAIN_ID.toString()) {
-    await handleDeployMainnet(hre, deployer, bridgeRouter.address);
+  if (chain === protocol.toString()) {
+    await handleDeployMainnet(hre, deployer, protocol, {
+      bridgeRouter: bridgeRouter.address,
+    });
   } else {
-    if (!Object.keys(CHAIN_TO_CONFIG).includes(chain)) {
-      throw new Error("Invalid chain for deployment!");
+    if (!Object.keys(protocol.configs).includes(chain)) {
+      throw new Error(`Invalid chain (${chain}) for deployment!`);
     }
-    await handleDeploySpoke(hre, deployer, +chain, bridgeRouter.address);
+    await handleDeploySpoke(hre, deployer, protocol, +chain, {
+      bridgeRouter: bridgeRouter.address,
+    });
   }
 };
 
