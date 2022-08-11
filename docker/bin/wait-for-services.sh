45bcdbbbdfc762584c4c6222b0aeaccacede6196
wanglonghong <wanglonghong4467@gmail.com>
2022-08-11 16:51:46 +0800

    increase attempt count

--- docker/bin/wait-for-services.sh
+++ docker/bin/wait-for-services.sh
@@ -28,10 +28,10 @@ function wait_for_service() {
     local attempt=1
 
     until curl -f --max-time 1 "http://localhost:${PORT}/ping" &>/dev/null; do
-        echo "${attempt}/12: Service not up, sleeping ${attempt} seconds..."
+        echo "${attempt}/30: Service not up, sleeping ${attempt} seconds..."
         sleep ${attempt}
         attempt=$((attempt + 1))
-        if [[ ${attempt} == 12 ]]
+        if [[ ${attempt} == 30 ]]
         then
             echo -e "\033[31mERROR\033[m: Waited too long for ${SERVICE} to become available!"
             exit 1
