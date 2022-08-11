c915cb460c6c6ce456ebeaa5bd4ccd4fff18b90e
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-08-11 10:13:34 +0200

    scaling service based on cpu and memory

--- ops/testnet/staging/core/main.tf
+++ ops/testnet/staging/core/main.tf
@@ -149,7 +149,7 @@ module "sequencer_subscriber" {
   loadbalancer_port        = 80
   cpu                      = 1024
   memory                   = 2048
-  instance_count           = 10
+  instance_count           = 5
   timeout                  = 180
   ingress_cdir_blocks      = ["0.0.0.0/0"]
   ingress_ipv6_cdir_blocks = []
@@ -158,6 +158,15 @@ module "sequencer_subscriber" {
   container_env_vars       = local.sequencer_env_vars
 }
 
+module "sequencer_subscriber_auto_scaling" {
+  source           = "../../../modules/auto-scaling"
+  stage            = var.stage
+  environment      = var.environment
+  domain           = var.domain
+  ecs_service_name = module.sequencer_subscriber.service_name
+  ecs_cluster_name = module.ecs.ecs_cluster_name
+
+}
 module "web3signer" {
   source                   = "../../../modules/service"
   stage                    = var.stage
