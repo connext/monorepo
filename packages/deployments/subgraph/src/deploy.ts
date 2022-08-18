95e48bfbec71fe12e9e26bc6890ef801b53fe201
Rahul Sethuram <rahul@sethuram.com>
2022-08-18 16:13:12 +0400

    Dynamic deploy

--- packages/deployments/subgraph/src/deploy.ts
+++ packages/deployments/subgraph/src/deploy.ts
@@ -5,6 +5,9 @@ import util from "util";
 import YAML from "yaml";
 import yamlToJson from "js-yaml";
 
+import ConnextHandler_DiamondProxy_1337 from "../../contracts/deployments/local_1337/ConnextHandler_DiamondProxy.json";
+import ConnextHandler_DiamondProxy_1338 from "../../contracts/deployments/local_1338/ConnextHandler_DiamondProxy.json";
+
 const exec = util.promisify(_exec);
 
 export type Network = {
@@ -67,6 +70,13 @@ const run = async () => {
 
   for (const n of networksToDeploy) {
     console.log(n);
+    if (n.network === "local_1337") {
+      n.address = ConnextHandler_DiamondProxy_1337.address;
+    }
+
+    if (n.network === "local_1338") {
+      n.address = ConnextHandler_DiamondProxy_1338.address;
+    }
 
     /// prepare
     jsonFile.dataSources = (jsonFile.dataSources ?? []).map((ds: any) => {
