f5b0d26ce021a3670d24a4ecb5b9c1971318fc06
liu <mailto123ads@gmail.com>
2022-08-22 23:05:39 +0800

    update PolygonL1Connector

--- packages/deployments/contracts/contracts/core/messaging/connectors/PolygonConnector.sol
+++ packages/deployments/contracts/contracts/core/messaging/connectors/PolygonConnector.sol
@@ -2,18 +2,20 @@
 pragma solidity 0.8.15;
 
 import {CrossChainEnabledPolygonChild} from "@openzeppelin/contracts/crosschain/polygon/CrossChainEnabledPolygonChild.sol";
-import {FxBaseRootTunnel} from "https://github.com/fx-portal/contracts/blob/main/contracts/tunnel/FxBaseRootTunnel.sol";
 
 import {IRootManager} from "../interfaces/IRootManager.sol";
 import {Connector} from "./Connector.sol";
 
 // address constant MUMBAI_FX_CHILD = 0xCf73231F28B7331BBe3124B907840A94851f9f11;
-// address constant GOERLI_CHECKPOINT_MANAGER = 0x2890bA17EfE978480615e330ecB65333b880928e;
 // address constant GOERLI_FX_ROOT = 0x3d1d3E34f7fB6D26245E6640E1c50710eFFf15bA;
 
 // address constant MAINNET_FX_CHILD = 0x8397259c983751DAf40400790063935a11afa28a;
-// address constant MAINNET_CHECKPOINT_MANAGER = 0x86E4Dc95c7FBdBf52e33D563BbDB00823894C287;
 // address constant MAINNET_FX_ROOT = 0xfe5e5D361b2ad62c541bAb87C45a0B9B018389a2;
+
+// FX_ROOT contract
+interface PolygonL1AMB {
+  function sendMessageToChild(address receiver, bytes calldata data) external;
+}
 
 contract PolygonL2Connector is Connector, CrossChainEnabledPolygonChild {
   event MessageSent(bytes message);
@@ -30,7 +32,7 @@ contract PolygonL2Connector is Connector, CrossChainEnabledPolygonChild {
     uint256 _reserveGas,
     address _fxChild
   )
-    Connector(_domain, _mirrorDomain, _amb, _rootManager, _mirrorConnector, _mirrorProcessGas, _processGas, _reserveGas),
+    Connector(_domain, _mirrorDomain, _amb, _rootManager, _mirrorConnector, _mirrorProcessGas, _processGas, _reserveGas)
     CrossChainEnabledPolygonChild(_fxChild)
   {}
 
@@ -54,7 +56,7 @@ contract PolygonL2Connector is Connector, CrossChainEnabledPolygonChild {
   }
 }
 
-contract PolygonL1Connector is Connector, FxBaseRootTunnel  {
+contract PolygonL1Connector is Connector {
   // ============ Constructor ============
   constructor(
     uint32 _domain,
@@ -65,11 +67,9 @@ contract PolygonL1Connector is Connector, FxBaseRootTunnel  {
     uint256 _mirrorProcessGas,
     uint256 _processGas,
     uint256 _reserveGas,
-    address _checkPointManager,
-    address _fxRoot
+    address _checkPointManager
   )
-    Connector(_domain, _mirrorDomain, _amb, _rootManager, _mirrorConnector, _mirrorProcessGas, _processGas, _reserveGas),
-    FxBaseRootTunnel(_checkPointManager, _fxRoot)
+    Connector(_domain, _mirrorDomain, _amb, _rootManager, _mirrorConnector, _mirrorProcessGas, _processGas, _reserveGas)
   {}
 
   // ============ Private fns ============
@@ -79,7 +79,10 @@ contract PolygonL1Connector is Connector, FxBaseRootTunnel  {
   }
 
   function _sendMessage(bytes memory _data) internal override {
-    sendMessageToChild(_data);
+    // FX_ROOT on L1 chain will call below
+    // bytes memory data = abi.encode(msg.sender, _receiver, _data);
+    // stateSender.syncState(fxChild, data);
+    PolygonL1AMB(AMB).sendMessageToChild(mirrorConnector, _data);
   }
 
   function _processMessage(bytes memory _data) internal override {
