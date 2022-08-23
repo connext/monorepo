f78ce6c69e57424299153c3d2f1a85c2e9ad3ef5
Sanchay Mittal <sanchaymittal@gmail.com>
2022-08-23 08:25:49 +0530

    updates

--- packages/deployments/subgraph/src/amarok-stableswap-staging/mapping.ts
+++ packages/deployments/subgraph/src/amarok-stableswap-staging/mapping.ts
@@ -16,39 +16,8 @@ import {
 } from "../../generated/Connext/StableSwap";
 import { MetaData } from "../../generated/schema";
 
-// handleAddLiquidity
-
 const DEFAULT_META_ID = "0xmetaid";
 
-
-// event TokenSwap(address indexed buyer, uint256 tokensSold, uint256 tokensBought, uint128 soldId, uint128 boughtId);
-
-//   event RemoveLiquidity(address indexed provider, uint256[] tokenAmounts, uint256 lpTokenSupply);
-//   event RemoveLiquidityOne(
-//     address indexed provider,
-//     uint256 lpTokenAmount,
-//     uint256 lpTokenSupply,
-//     uint256 boughtId,
-//     uint256 tokensBought
-//   );
-//   event RemoveLiquidityImbalance(
-//     address indexed provider,
-//     uint256[] tokenAmounts,
-//     uint256[] fees,
-//     uint256 invariant,
-//     uint256 lpTokenSupply
-//   );
-//   event RampA(uint256 oldA, uint256 newA, uint256 initialTime, uint256 futureTime);
-//   event StopRampA(uint256 currentA, uint256 time);
-
-
-//   event AddLiquidity(
-//     address indexed provider,
-//     uint256[] tokenAmounts,
-//     uint256[] fees,
-//     uint256 invariant,
-//     uint256 lpTokenSupply
-//   );
 export function handleAddLiquidity(event: AddLiquidity): void {
   let metaDataId = DEFAULT_META_ID;
   let metaData = MetaData.load(metaDataId);
