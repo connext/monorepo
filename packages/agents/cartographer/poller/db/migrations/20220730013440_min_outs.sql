f427be7b3cbc27151a07e71d957d1122058dc904
jaycoinsolv <jay@CoinSolv.io>
2022-08-24 15:34:04 +0530

    addition of new hourly metrics and transfer schema change

--- packages/agents/cartographer/poller/db/migrations/20220730013440_min_outs.sql
+++ packages/agents/cartographer/poller/db/migrations/20220730013440_min_outs.sql
@@ -1,4 +1,4 @@
 -- migrate:up
-ALTER TABLE transfers DROP COLUMN slippage_tol;
-ALTER TABLE transfers ADD COLUMN destination_min_out numeric;
+-- ALTER TABLE transfers DROP COLUMN slippage_tol;
+-- ALTER TABLE transfers ADD COLUMN destination_min_out numeric;
 -- migrate:down
