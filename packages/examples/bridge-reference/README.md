e888c3f95830c1111f26554716c8bf680ba4645e
just-a-node <eye1717@gmail.com>
2022-08-25 17:52:46 -0600

    Add new test ui

---
+++ packages/examples/bridge-reference/README.md
@@ -0,0 +1,3 @@
+## Bridge UI reference implementation
+
+A reference implementation of a bridge UI. Used in this monorepo to test SDK functionality.
---
+++ packages/examples/bridge-reference/src/utils/context.tsx
@@ -0,0 +1,16 @@
+import React, { createContext, useReducer } from 'react';
+
+export function createCtx<StateType, ActionType>(reducer: React.Reducer<StateType, ActionType>, initialState: StateType) {
+    const defaultDispatch: React.Dispatch<ActionType> = () => initialState;
+    const ctx = createContext({
+        state: initialState,
+        dispatch: defaultDispatch
+    });
+
+    function Provider(props: React.PropsWithChildren<{}>) {
+        const [state, dispatch] = useReducer<React.Reducer<StateType, ActionType>>(reducer, initialState);
+        return <ctx.Provider value={{ state, dispatch }} {...props} />;
+    }
+
+    return [ctx, Provider] as const;
+}
