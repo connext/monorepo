bed1d1f54625c877413b083a93371f2d760dda6d
LayneHaber <layne.haber@gmail.com>
2022-08-25 18:51:05 -0600

    feat: add set xapp connection manager task

--- packages/deployments/contracts/hardhat.config.ts
+++ packages/deployments/contracts/hardhat.config.ts
@@ -45,6 +45,7 @@ import "./tasks/setMirrorConnectors";
 import "./tasks/addConnextions";
 import "./tasks/setBridgeRouter";
 import "./tasks/addSequencer";
+import "./tasks/setXAppConnectionManager";
 
 const urlOverride = process.env.ETH_PROVIDER_URL;
 const chainId = parseInt(process.env.CHAIN_ID ?? "1337", 10);
