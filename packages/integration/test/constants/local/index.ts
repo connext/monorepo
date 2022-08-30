e00cc531f0f7f679fd99ab636aa76a912706e14e
wanglonghong <wanglonghong4467@gmail.com>
2022-08-30 21:19:57 +0800

    hardcode AMB deployments

--- packages/integration/test/constants/local/index.ts
+++ packages/integration/test/constants/local/index.ts
@@ -45,3 +45,36 @@ export const PARAMETERS = {
     DEPLOYMENTS: null, // Must be set at runtime!
   },
 };
+
+export const AmbDeployments = {
+  RootManager: {
+    domain: "1337",
+    address: "0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0",
+    l1connectors: {
+      A: {
+        domain: "1337",
+        name: "MainnetL1Connector",
+        address: "0xF12b5dd4EAD5F743C6BaA640B0216200e89B60Da",
+      },
+      B: {
+        domain: "1338",
+        name: "OptimismL1Connector",
+        address: "0xf25186B5081Ff5cE73482AD761DB0eB0d25abfBF",
+      },
+    },
+  },
+  Connectors: {
+    A: {
+      domain: "1337",
+      type: "l1",
+      name: "MainnetL1Connector",
+      address: "0xF12b5dd4EAD5F743C6BaA640B0216200e89B60Da",
+    },
+    B: {
+      domain: "1338",
+      type: "l2",
+      name: "OptimismL2Connector",
+      address: "0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0",
+    },
+  },
+};
