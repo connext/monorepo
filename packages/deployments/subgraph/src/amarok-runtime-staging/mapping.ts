98c36ad7d85e83024c81273faef995bc3b5e9d7a
sanchaymittal <sanchaymittal@gmail.com>
2022-09-01 15:04:45 +0530

    new: dispatch, Process, AggregateRootUpdated

--- packages/deployments/subgraph/src/amarok-runtime-staging/mapping.ts
+++ packages/deployments/subgraph/src/amarok-runtime-staging/mapping.ts
@@ -19,7 +19,7 @@ import {
   RouterRecipientSet,
   MaxRoutersPerTransferUpdated,
 } from "../../generated/Connext/ConnextHandler";
-import { Dispatch, Process } from "../../generated/Connector/Connector";
+import { Dispatch, Process, AggregateRootUpdated } from "../../generated/Connector/Connector";
 import {
   Asset,
   AssetBalance,
@@ -30,9 +30,14 @@ import {
   OriginTransfer,
   DestinationTransfer,
   Setting,
+  OriginMessage,
+  DestinationMessage,
+  AggregateRoot
 } from "../../generated/schema";
 
 const DEFAULT_MAX_ROUTERS_PER_TRANSFER = 5;
+const DEFAULT_AGGREGATE_ROOTS_ID = "AGGREGATE_ROOTS";
+
 export function handleRelayerAdded(event: RelayerAdded): void {
   let relayerId = event.params.relayer.toHex();
   let relayer = Relayer.load(relayerId);
@@ -407,12 +412,41 @@ export function handleReconciled(event: Reconciled): void {
 
 export function handleDispatch(event: Dispatch): void {
   // Dispatch(bytes32 leaf, uint256 index, bytes32 root, bytes message);
-  // initiate origin transfer: save message, root
+  let message = OriginMessage.load(event.params.leaf.toHexString());
+  if (message == null) {
+    message = new OriginMessage(event.params.leaf.toHexString());
+  }
+
+  message.leaf = event.params.leaf;
+  message.index = event.params.index;
+  message.root = event.params.root;
+  message.message = event.params.message;
+
+  message.save();
 }
 
 export function handleProcess(event: Process): void {
-  // Dispatch(bytes32 leaf, uint256 index, bytes32 root, bytes message);
-  // initiate destination transfer: save processed: boolean
+  let message = DestinationMessage.load(event.params.leaf.toHexString());
+  if (message == null) {
+    message = new DestinationMessage(event.params.leaf.toHexString());
+  }
+
+  message.leaf = event.params.leaf;
+  message.processed = event.params.success;
+  message.returnData = event.params.returnData;
+
+  message.save();
+}
+
+export function handleAggregateRootUpdated(event: AggregateRootUpdated): void {
+
+  let aggregateRoot = AggregateRoot.load(event.params.current.toHexString());
+  if (aggregateRoot == null) {
+    aggregateRoot = new AggregateRoot(event.params.current.toHexString());
+  }
+
+  aggregateRoot.root = event.params.current;
+  aggregateRoot.save();
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
