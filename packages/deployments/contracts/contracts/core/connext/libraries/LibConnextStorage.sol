1d9bdd6d4a08910e6ca409623a2549448009154a
LayneHaber <layne.haber@gmail.com>
2022-09-14 13:57:34 -0600

    remove temporary structs, update call params to be all inclusive

--- packages/deployments/contracts/contracts/core/connext/libraries/LibConnextStorage.sol
+++ packages/deployments/contracts/contracts/core/connext/libraries/LibConnextStorage.sol
@@ -19,97 +19,58 @@ struct TokenId {
 }
 
 /**
- * @dev FIXME: When we are at the point where we want to fix the xcall interface
- * by removing the struct-based arguments, remove this and keep the CallParams below
- *
- * @notice These are the params the user supplies to `xcall` of the `CallParams`
- */
-struct UserFacingCallParams {
-  address to;
-  bytes callData;
-  uint32 destinationDomain;
-  address delegate;
-  uint256 slippage;
-}
-
-/**
  * @notice These are the call parameters that will remain constant between the
  * two chains. They are supplied on `xcall` and should be asserted on `execute`
  * @property to - The account that receives funds, in the event of a crosschain call,
  * will receive funds if the call fails.
  *
- * @param to - The address you are sending funds (and potentially data) to
- * @param callData - The data to execute on the receiving chain. If no crosschain call is needed, then leave empty.
  * @param originDomain - The originating domain (i.e. where `xcall` is called). Must match nomad domain schema
  * @param destinationDomain - The final domain (i.e. where `execute` / `reconcile` are called). Must match nomad domain schema
+ * @param canonicalDomain - The canonical domain of the asset you are bridging
+ * @param to - The address you are sending funds (and potentially data) to
  * @param delegate - An address who can execute txs on behalf of `to`, in addition to allowing relayers
  * @param receiveLocal - If true, will use the local nomad asset on the destination instead of adopted.
+ * @param callData - The data to execute on the receiving chain. If no crosschain call is needed, then leave empty.
  * @param slippage - Slippage user is willing to accept from original amount in expressed in BPS (i.e. if
  * a user takes 1% slippage, this is expressed as 1_000)
+ * @param originSender - The msg.sender of the xcall
+ * @param bridgedAmt - The amount sent over the bridge (after potential AMM on xcall)
+ * @param normalizedIn - The amount sent to `xcall`, normalized to 18 decimals
+ * @param nonce - The nonce on the origin domain used to ensure the transferIds are unique
+ * @param canonicalId - The unique identifier of the canonical token corresponding to bridge assets
  */
-// FIXME: make this the struct containing all information used to generate a transfer id.
-// should be internal from the perspective of the user
 struct CallParams {
-  address to;
-  bytes callData;
   uint32 originDomain;
   uint32 destinationDomain;
+  uint32 canonicalDomain;
+  address to;
   address delegate;
   bool receiveLocal;
+  bytes callData;
   uint256 slippage;
-}
-
-// FIXME: this should be moved into the CallParams struct
-// used in PortalFacet to fix stack too deep errors
-struct TransferIdGenerationInformation {
   address originSender;
   uint256 bridgedAmt;
   uint256 normalizedIn;
   uint256 nonce;
   bytes32 canonicalId;
-  uint32 canonicalDomain;
-}
-
-/**
- * @notice The arguments you supply to the `xcall` function called by user on origin domain
- * @param params - The CallParams. These are consistent across sending and receiving chains
- * @param asset - The asset the caller sent with the transfer. Can be the adopted, canonical,
- * or the representational asset.
- * @param amount - The amount of transferring asset supplied by the user in the `xcall`.
- */
-struct XCallArgs {
-  UserFacingCallParams params;
-  address asset; // Could be adopted, local, or canonical.
-  uint256 amount;
 }
 
 /**
  * @notice
  * @param params - The CallParams. These are consistent across sending and receiving chains.
- * @param local - The local asset for the transfer, will be swapped to the adopted asset if
- * appropriate.
  * @param routers - The routers who you are sending the funds on behalf of.
  * @param routerSignatures - Signatures belonging to the routers indicating permission to use funds
  * for the signed transfer ID.
  * @param sequencer - The sequencer who assigned the router path to this transfer.
  * @param sequencerSignature - Signature produced by the sequencer for path assignment accountability
  * for the path that was signed.
- * @param amount - The amount forwarded from the bridge
- * @param nonce - The nonce used to generate transfer ID.
- * @param normalizedIn - The amount the user sent in on `xcall`, normalized to 18 decimals
- * @param originSender - The msg.sender of the xcall on origin domain.
  */
 struct ExecuteArgs {
   CallParams params;
-  address local; // local representation of canonical token
   address[] routers;
   bytes[] routerSignatures;
   address sequencer;
   bytes sequencerSignature;
-  uint256 amount;
-  uint256 nonce;
-  uint256 normalizedIn;
-  address originSender;
 }
 
 /**
