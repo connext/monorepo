39d43ef86cff7a8b6b6a4d6968a7dccdcf2046e2
wanglonghong <wanglonghong4467@gmail.com>
2022-09-16 23:48:27 +0800

    rebuild typechain

--- packages/deployments/contracts/src/typechain-types/factories/contracts/core/index.ts
+++ packages/deployments/contracts/src/typechain-types/factories/contracts/core/index.ts
@@ -2,7 +2,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export * as connext from "./connext";
-export * as promise from "./promise";
 export * as relayerFee from "./relayer-fee";
 export { Router__factory } from "./Router__factory";
 export { Version__factory } from "./Version__factory";
