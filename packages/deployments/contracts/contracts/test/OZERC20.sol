090e357d3f3f7e7c4337b54ecb95635b513410ce
preethamr <preethamr@gmail.com>
2022-09-20 15:33:53 -0700

    Implement permit extension

--- packages/deployments/contracts/contracts/test/OZERC20.sol
+++ packages/deployments/contracts/contracts/test/OZERC20.sol
@@ -8,7 +8,9 @@ pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
@@ -35,7 +37,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
  * functions have been added to mitigate the well-known issues around setting
  * allowances. See {IERC20-approve}.
  */
-abstract contract ERC20 is IERC20, IERC20Permit {
+abstract contract ERC20 is IERC20, IERC20Permit, EIP712 {
   using SafeMath for uint256;
 
   mapping(address => uint256) private balances;
@@ -44,8 +46,28 @@ abstract contract ERC20 is IERC20, IERC20Permit {
 
   uint256 private supply;
 
-  bytes32 private immutable _PERMIT_TYPEHASH =
+  using Counters for Counters.Counter;
+
+  mapping(address => Counters.Counter) private _nonces;
+
+  // solhint-disable-next-line var-name-mixedcase
+  bytes32 private constant _PERMIT_TYPEHASH =
     keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+  /**
+   * @dev In previous versions `_PERMIT_TYPEHASH` was declared as `immutable`.
+   * However, to ensure consistency with the upgradeable transpiler, we will continue
+   * to reserve a slot.
+   * @custom:oz-renamed-from _PERMIT_TYPEHASH
+   */
+  // solhint-disable-next-line var-name-mixedcase
+  bytes32 private _PERMIT_TYPEHASH_DEPRECATED_SLOT;
+
+  /**
+   * @dev Initializes the {EIP712} domain separator using the `name` parameter, and setting `version` to `"1"`.
+   *
+   * It's a good idea to use the same `name` that is defined as the ERC20 token name.
+   */
+  constructor(string memory name) EIP712(name, "1") {}
 
   struct Token {
     string name;
@@ -299,7 +321,7 @@ abstract contract ERC20 is IERC20, IERC20Permit {
   function permit(
     address owner,
     address spender,
-    uint256 amount,
+    uint256 value,
     uint256 deadline,
     uint8 v,
     bytes32 r,
@@ -307,40 +329,39 @@ abstract contract ERC20 is IERC20, IERC20Permit {
   ) public virtual override {
     require(block.timestamp <= deadline, "ERC20Permit: expired deadline");
 
-    bytes32 structHash = keccak256(
-      abi.encode(_PERMIT_TYPEHASH, owner, spender, amount, nonces[owner].current(), deadline)
-    );
+    bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline));
 
-    // TODO: Gen hash
-    bytes32 hash;
+    bytes32 hash = _hashTypedDataV4(structHash);
 
     address signer = ECDSA.recover(hash, v, r, s);
     require(signer == owner, "ERC20Permit: invalid signature");
 
-    nonces[owner].increment();
-    _approve(owner, spender, amount);
+    _approve(owner, spender, value);
   }
 
   /**
    * @dev See {IERC20Permit-nonces}.
    */
-  function nonces(address owner) public view override returns (uint256) {
-    return nonces[owner].current();
+  function nonces(address owner) public view virtual override returns (uint256) {
+    return _nonces[owner].current();
   }
 
   /**
    * @dev See {IERC20Permit-DOMAIN_SEPARATOR}.
    */
+  // solhint-disable-next-line func-name-mixedcase
   function DOMAIN_SEPARATOR() external view override returns (bytes32) {
-    // return
-    //   keccak256(
-    //     abi.encode(
-    //       keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-    //       keccak256(bytes(name)),
-    //       keccak256(bytes(version)),
-    //       chainid,
-    //       address(this)
-    //     )
-    //   );
+    return _domainSeparatorV4();
+  }
+
+  /**
+   * @dev "Consume a nonce": return the current value and increment.
+   *
+   * _Available since v4.1._
+   */
+  function _useNonce(address owner) internal virtual returns (uint256 current) {
+    Counters.Counter storage nonce = _nonces[owner];
+    current = nonce.current();
+    nonce.increment();
   }
 }
