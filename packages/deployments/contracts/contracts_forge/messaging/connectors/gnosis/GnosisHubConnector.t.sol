a902a3ac408f6bd11c0998bbecb26d31cf0f5a7a
wanglonghong <wanglonghong4467@gmail.com>
2022-09-22 12:32:50 +0800

    Necessary test functions

--- packages/deployments/contracts/contracts_forge/messaging/connectors/gnosis/GnosisHubConnector.t.sol
+++ packages/deployments/contracts/contracts_forge/messaging/connectors/gnosis/GnosisHubConnector.t.sol
@@ -3,4 +3,27 @@ pragma solidity 0.8.15;
 
 import "../../../utils/ConnectorHelper.sol";
 
-contract GnosisHubConnectorTest is ConnectorHelper {}
+contract GnosisHubConnectorTest is ConnectorHelper {
+  function setup() public {}
+
+  // ============ GnosisHubConnector.verifySender ============
+  function test_GnosisHubConnector__verifySender_shouldWorkIfTrue() public {}
+
+  function test_GnosisHubConnector__verifySender_shouldWorkIfFalse() public {}
+
+  function test_GnosisHubConnector__verifySender_shouldFailIfCallerNotAmb() public {}
+
+  // ============ GnosisHubConnector._sendMessage ============
+  function test_GnosisHubConnector__sendMessage_shouldWork() public {}
+
+  function test_GnosisHubConnector__sendMessage_shouldFailIfDataLengthNot32() public {}
+
+  // ============ GnosisHubConnector._processMessage ============
+  function test_GnosisHubConnectr__processMessage_shouldWork() public {}
+
+  function test_GnosisHubConnectr__processMessage_shouldFailIfSenderNotVerified() public {}
+
+  function test_GnosisHubConnectr__processMessage_shouldFailIfSourceChainIdMismatch() public {}
+
+  function test_GnosisHubConnectr__processMessage_shouldFailIfDataLengthNot32() public {}
+}
--- packages/deployments/contracts/contracts_forge/messaging/connectors/gnosis/GnosisSpokeConnector.t.sol
+++ packages/deployments/contracts/contracts_forge/messaging/connectors/gnosis/GnosisSpokeConnector.t.sol
@@ -3,4 +3,25 @@ pragma solidity 0.8.15;
 
 import "../../../utils/ConnectorHelper.sol";
 
-contract GnosisSpokeConnectorTest is ConnectorHelper {}
+contract GnosisSpokeConnectorTest is ConnectorHelper {
+  function setup() public {}
+
+  // ============ GnosisSpokeConnector.verifySender ============
+  function test_GnosisSpokeConnector__verifySender_shouldWorkIfTrue() public {}
+
+  function test_GnosisSpokeConnector__verifySender_shouldWorkIfFalse() public {}
+
+  function test_GnosisSpokeConnector__verifySender_shouldFailIfCallerNotAmb() public {}
+
+  // ============ GnosisSpokeConnector._sendMessage ============
+  function test_GnosisSpokeConnector__sendMessage_shouldWork() public {}
+
+  // ============ GnosisSpokeConnector._processMessage ============
+  function test_GnosisSpokeConnector__processMessage_shouldWork() public {}
+
+  function test_GnosisSpokeConnector__processMessage_shouldFailIfSenderNotVerified() public {}
+
+  function test_GnosisSpokeConnector__processMessage_shouldFailIfSourceChainIdMismatch() public {}
+
+  function test_GnosisSpokeConnector__processMessage_shouldFailIfDataLengthNot32() public {}
+}
