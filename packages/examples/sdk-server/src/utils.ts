1a033d58d817b341f7ec1475bd81cd9de3ca4ff9
just-a-node <eye1717@gmail.com>
2022-09-23 12:17:15 -0600

    getTransfersByUser endpt

--- packages/examples/sdk-server/src/utils.ts
+++ packages/examples/sdk-server/src/utils.ts
@@ -2,66 +2,21 @@ import { NxtpSdkUtils } from "@connext/nxtp-sdk";
 import { FastifyInstance } from "fastify";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
-import {
-  getCanonicalFromLocalSchema,
-  getLPTokenAddressSchema,
-  getLPTokenUserBalanceSchema,
-  getPoolTokenIndexSchema,
-  getPoolTokenBalanceSchema,
-  getPoolTokenUserBalanceSchema,
-  getPoolTokenAddressSchema,
-  getVirtualPriceSchema,
-  calculateSwapSchema,
-  calculateTokenAmountSchema,
-  calculateRemoveSwapLiquiditySchema,
-  getPoolSchema,
-  getUserPoolsSchema,
-  addLiquiditySchema,
-  removeLiquiditySchema,
-  swapSchema,
-} from "./types/api";
+import { getTransfersByUserSchema } from "./types/api";
 
 export const utilsRoutes = async (server: FastifyInstance, sdkUtilsInstance: NxtpSdkUtils): Promise<any> => {
   const s = server.withTypeProvider<TypeBoxTypeProvider>();
 
-  s.get(
+  s.post(
     "/getTransfersByUser",
     {
       schema: {
-        params: getTransfersByUsersSchema,
+        body: getTransfersByUserSchema,
       },
     },
     async (request, reply) => {
-      const { domainId, tokenAddress } = request.params;
-      const res = await sdkUtilsInstance.getCanonicalFromLocal(domainId, tokenAddress);
-      reply.status(200).send(res);
-    },
-  );
-
-  s.get(
-    "/getLPTokenAddress/:domainId/:canonicalId",
-    {
-      schema: {
-        params: getLPTokenAddressSchema,
-      },
-    },
-    async (request, reply) => {
-      const { domainId, canonicalId } = request.params;
-      const res = await sdkUtilsInstance.getLPTokenAddress(domainId, canonicalId);
-      reply.status(200).send(res);
-    },
-  );
-
-  s.post(
-    "/calculateSwap",
-    {
-      schema: {
-        body: calculateSwapSchema,
-      },
-    },
-    async (request, reply) => {
-      const { domainId, canonicalId, tokenIndexFrom, tokenIndexTo, amount } = request.body;
-      const res = await sdkUtilsInstance.calculateSwap(domainId, canonicalId, tokenIndexFrom, tokenIndexTo, amount);
+      const { params } = request.body;
+      const res = await sdkUtilsInstance.getTransfersByUser(params);
       reply.status(200).send(res);
     },
   );
