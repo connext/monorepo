8c8e20e4b33d7e2b9885d16b43406e132d7f219e
LayneHaber <layne.haber@gmail.com>
2022-10-02 13:42:15 -0600

    make all clients pausable

--- packages/deployments/contracts/contracts/messaging/WatcherClient.sol
+++ packages/deployments/contracts/contracts/messaging/WatcherClient.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity 0.8.15;
 
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+
 import {ProposedOwnable} from "../shared/ProposedOwnable.sol";
 import {WatcherManager} from "./WatcherManager.sol";
 
@@ -9,7 +11,7 @@ import {WatcherManager} from "./WatcherManager.sol";
  * Contracts can inherit this contract to be able to use the watcher manager's shared watcher set.
  */
 
-contract WatcherClient is ProposedOwnable {
+contract WatcherClient is ProposedOwnable, Pausable {
   // ============ Events ============
   event WatcherManagerChanged(address watcherManager);
 
@@ -34,5 +36,14 @@ contract WatcherClient is ProposedOwnable {
   function setWatcherManager(address _watcherManager) external onlyOwner {
     require(_watcherManager != address(watcherManager), "already watcher manager");
     emit WatcherManagerChanged(_watcherManager);
+  }
+
+  function unpause() external onlyOwner whenPaused {
+    _unpause();
+  }
+
+  // ============ Watcher fns ============
+  function pause() external onlyWatcher whenNotPaused {
+    _pause();
   }
 }
