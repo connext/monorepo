420e48de1d12ca3521ce8d2fd9d24cef4688e6e4
LayneHaber <layne.haber@gmail.com>
2022-10-03 16:36:05 -0600

    fix integration + rebuild

--- packages/deployments/contracts/src/typechain-types/factories/contracts/core/connext/interfaces/index.ts
+++ packages/deployments/contracts/src/typechain-types/factories/contracts/core/connext/interfaces/index.ts
@@ -2,6 +2,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export { IAavePool__factory } from "./IAavePool__factory";
+export { IBridgeHook__factory } from "./IBridgeHook__factory";
+export { IBridgeRouter__factory } from "./IBridgeRouter__factory";
 export { IBridgeToken__factory } from "./IBridgeToken__factory";
 export { IConnextHandler__factory } from "./IConnextHandler__factory";
 export { IDiamondCut__factory } from "./IDiamondCut__factory";
@@ -12,4 +14,5 @@ export { IGasTokenOracle__factory } from "./IGasTokenOracle__factory";
 export { IPriceOracle__factory } from "./IPriceOracle__factory";
 export { IStableSwap__factory } from "./IStableSwap__factory";
 export { ITokenExchange__factory } from "./ITokenExchange__factory";
+export { IWeth__factory } from "./IWeth__factory";
 export { IXReceiver__factory } from "./IXReceiver__factory";
