47c3910858a495092383a382754603ec5aece5c3
wanglonghong <wanglonghong4467@gmail.com>
2022-10-07 22:54:12 +0800

    read rpc endpoints from hardhat config

--- packages/deployments/contracts/src/init/config/index.ts
+++ packages/deployments/contracts/src/init/config/index.ts
@@ -1,5 +1,19 @@
+import * as fs from "fs";
+
 import { ProtocolStack } from "../helpers";
 
-export const getProtocolStack = (network: "testnet" | "mainnet", env: "staging" | "prod"): ProtocolStack => {
+/**
+ * Load the protocol stack from the default config
+ * @param network - The network identifier which can be either `mainnet` or `testnet`
+ * @param env - The smart contract environment which can be either `staging` or `prod`
+ * @param chainIds - The array of chain Ids you wanna initialize.
+ *      If not set, it will run the init script for the all chains.
+ */
+export const getDefaulProtocolStack = (
+  network: "testnet" | "mainnet",
+  env: "staging" | "prod",
+  chainIds?: number[],
+): ProtocolStack | undefined => {
+  const json_filename = `${network}.${env}.json`;
   throw new Error("Not implemented yet");
 };
