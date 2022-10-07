96c5ffed9e29152822948260793f50724ca205d6
wanglonghong <wanglonghong4467@gmail.com>
2022-10-07 22:24:33 +0800

    create folder structure for zero config

---
+++ packages/deployments/contracts/src/init/config/index.ts
@@ -0,0 +1,5 @@
+import { ProtocolStack } from "../helpers";
+
+export const getProtocolStack = (network: "testnet" | "mainnet", env: "staging" | "prod"): ProtocolStack => {
+  throw new Error("Not implemented yet");
+};
---
+++ packages/deployments/contracts/src/init/config/rpc.ts
@@ -0,0 +1,18 @@
+export const PUBLIC_RPCS = [
+  {
+    chain: 5,
+    rpc: "https://rpc.ankr.com/eth_goerli",
+  },
+  {
+    chain: 420,
+    rpc: "https://goerli.optimism.io",
+  },
+  {
+    chain: 80001,
+    rpc: "https://rpc.ankr.com/polygon_mumbai",
+  },
+  {
+    chain: 100100,
+    rpc: "https://rpc-chiado.gnosistestnet.com",
+  },
+];
