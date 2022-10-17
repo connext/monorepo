cf2e36487cea4b67a221cd02e83988c04e0221d0
Rahul Sethuram <rahul@sethuram.com>
2022-10-17 09:22:19 +0400

    Hardcode 950k gas

--- packages/agents/sequencer/src/adapters/relayer/gelato.ts
+++ packages/agents/sequencer/src/adapters/relayer/gelato.ts
@@ -1,8 +1,11 @@
 import { createLoggingContext, RequestContext } from "@connext/nxtp-utils";
+import { BigNumber } from "ethers";
 
 import { RelayerSendFailed } from "../../lib/errors";
 import { getHelpers } from "../../lib/helpers";
 import { getContext } from "../../sequencer";
+
+const GAS_LIMIT_FOR_RELAYER = 950_000;
 
 export const getRelayerAddress = async (chainId: number): Promise<string> => {
   const { logger } = getContext();
@@ -43,7 +46,12 @@ export const send = async (
     data: encodedData,
   };
 
-  const response = await gelatoSDKSend(request, gelatoApiKey, {}, logger);
+  const response = await gelatoSDKSend(
+    request,
+    gelatoApiKey,
+    { gasLimit: BigNumber.from(GAS_LIMIT_FOR_RELAYER) },
+    logger,
+  );
 
   if (!response) {
     throw new RelayerSendFailed({ response: response });
