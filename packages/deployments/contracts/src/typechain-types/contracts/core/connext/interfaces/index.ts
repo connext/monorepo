3b34a72b583dbb1dccab6fb6502e27309de1ae5b
Rahul Sethuram <rahul@sethuram.com>
2022-10-19 10:01:36 +0400

    Deployments

--- packages/deployments/contracts/src/typechain-types/contracts/core/connext/interfaces/index.ts
+++ packages/deployments/contracts/src/typechain-types/contracts/core/connext/interfaces/index.ts
@@ -8,8 +8,6 @@ export type { IDiamondCut } from "./IDiamondCut";
 export type { IDiamondLoupe } from "./IDiamondLoupe";
 export type { IERC165 } from "./IERC165";
 export type { IERC20Extended } from "./IERC20Extended";
-export type { IGasTokenOracle } from "./IGasTokenOracle";
 export type { IPriceOracle } from "./IPriceOracle";
 export type { IStableSwap } from "./IStableSwap";
-export type { ITokenExchange } from "./ITokenExchange";
 export type { IXReceiver } from "./IXReceiver";
