f6c1afbf4a47a49807456fa654deb3b22df43a4f
just-a-node <eye1717@gmail.com>
2022-10-25 17:51:04 -0600

    Temp unblock subgraph builds

--- packages/deployments/subgraph/src/amarok-runtime-v0/mapping/index.ts
+++ packages/deployments/subgraph/src/amarok-runtime-v0/mapping/index.ts
@@ -2,6 +2,5 @@ export * from "./asset";
 export * from "./relayer";
 export * from "./routers";
 export * from "./spokeConnector";
-export * from "./stableSwap";
 export * from "./transfers";
 export * from "./sequencer";
