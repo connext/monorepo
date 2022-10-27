c0680082ea3a8c871a14e82a72b239b4881b04d5
sanchaymittal <sanchaymittal@gmail.com>
2022-10-27 12:51:34 +0100

    add: back the stableswap changes at v0

--- packages/deployments/subgraph/src/amarok-runtime-v0/mapping/index.ts
+++ packages/deployments/subgraph/src/amarok-runtime-v0/mapping/index.ts
@@ -2,5 +2,6 @@ export * from "./asset";
 export * from "./relayer";
 export * from "./routers";
 export * from "./spokeConnector";
+export * from "./stableSwap";
 export * from "./transfers";
 export * from "./sequencer";
