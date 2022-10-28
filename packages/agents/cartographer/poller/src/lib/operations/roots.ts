97125c66a15c00eeb9135445e41133194ce16742
Rahul Sethuram <rahul@sethuram.com>
2022-10-28 15:32:04 +0400

    Fix to use all hubs

--- packages/agents/cartographer/poller/src/lib/operations/roots.ts
+++ packages/agents/cartographer/poller/src/lib/operations/roots.ts
@@ -45,29 +45,32 @@ export const updatePropagatedRoots = async () => {
   const {
     adapters: { subgraph, database },
     logger,
+    domains,
   } = getContext();
   const { requestContext, methodContext } = createLoggingContext(updatePropagatedRoots.name);
 
-  // TODO: should replace below hardcoded value
-  const domain = "1735353714";
-  const offset = await database.getCheckPoint("propagated_root_");
-  const limit = 100;
+  const metas = await subgraph.getConnectorMeta(domains);
+  const hubs = new Set(metas.map((m) => m.hubDomain));
+  for (const hub of [...hubs]) {
+    const offset = await database.getCheckPoint("propagated_root_" + hub);
+    const limit = 100;
 
-  logger.debug("Retrieving propagated roots", requestContext, methodContext, {
-    domain: domain,
-    offset: offset,
-    limit: limit,
-  });
+    logger.debug("Retrieving propagated roots", requestContext, methodContext, {
+      domain: hub,
+      offset: offset,
+      limit: limit,
+    });
 
-  const propagatedRoots: PropagatedRoot[] = await subgraph.getGetPropagatedRoots(domain, offset, limit);
+    const propagatedRoots: PropagatedRoot[] = await subgraph.getGetPropagatedRoots(hub, offset, limit);
 
-  // Reset offset at the end of the cycle.
-  const newOffset = propagatedRoots.length == 0 ? 0 : propagatedRoots[propagatedRoots.length - 1].count;
-  if (offset === 0 || newOffset > offset) {
-    // TODO: Add a working transaction wraper
-    await database.savePropagatedRoots(propagatedRoots);
+    // Reset offset at the end of the cycle.
+    const newOffset = propagatedRoots.length == 0 ? 0 : propagatedRoots[propagatedRoots.length - 1].count;
+    if (offset === 0 || newOffset > offset) {
+      // TODO: Add a working transaction wraper
+      await database.savePropagatedRoots(propagatedRoots);
 
-    await database.saveCheckPoint("propagated_root_", newOffset);
-    logger.debug("Saved propageted roots", requestContext, methodContext, { offset: newOffset });
+      await database.saveCheckPoint("propagated_root_" + hub, newOffset);
+      logger.debug("Saved propageted roots", requestContext, methodContext, { offset: newOffset });
+    }
   }
 };
