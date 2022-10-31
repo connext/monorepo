bc24fd1fc7e7eb45d7358f920a70e06c63daf1f0
preethamr <preethamr@gmail.com>
2022-10-31 14:22:07 -0700

    Fix relayer web2signer url

--- ops/testnet/staging/core/config.tf
+++ ops/testnet/staging/core/config.tf
@@ -266,6 +266,6 @@ locals {
       }
     }
     environment   = var.stage
-    web3SignerUrl = "https://${module.relayer.service_endpoint}"
+    web3SignerUrl = "https://${module.relayer_web3signer.service_endpoint}"
   })
 }
