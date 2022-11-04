f168866ca4df13be4bab814e0a2d2578bf448224
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2022-11-04 22:00:04 +0800

    fix forge test from rounding issue

--- packages/deployments/contracts/contracts_forge/core/connext/facets/SwapAdminFacet.t.sol
+++ packages/deployments/contracts/contracts_forge/core/connext/facets/SwapAdminFacet.t.sol
@@ -600,8 +600,8 @@ contract SwapAdminFacetTest is SwapAdminFacet, StableSwapFacet, FacetHelper {
     // set timestamp to +100000 seconds
     vm.warp(blockTimestamp + 100000);
     assertEq(this.getSwapA(_canonicalKey), 47);
-    assertEq(this.getSwapAPrecise(_canonicalKey), 4794);
-    assertEq(this.getSwapVirtualPrice(_canonicalKey), 1000115870150391394);
+    assertEq(this.getSwapAPrecise(_canonicalKey), 4793);
+    assertEq(this.getSwapVirtualPrice(_canonicalKey), 1000115610744866006);
 
     // set timestamp to the end of ramp period
     vm.warp(endTimestamp);
