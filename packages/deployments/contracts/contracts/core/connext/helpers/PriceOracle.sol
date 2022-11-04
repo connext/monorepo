515a1464f6d5a89aa6ee6b35fdcf65370f073993
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2022-11-04 11:31:54 +0800

    Revert "Merge branch '2155-spearbit-3-priceoracle' into spearbit-109"
    This reverts commit 2a7a651f16822b41749ca0f9bf2101b8f563d7cb, reversing
    changes made to df0ea03fa26b662015bc30440e41fbffcdcf0fc6.

--- packages/deployments/contracts/contracts/core/connext/helpers/PriceOracle.sol
+++ packages/deployments/contracts/contracts/core/connext/helpers/PriceOracle.sol
@@ -8,9 +8,9 @@ abstract contract PriceOracle {
   /**
    * @notice Get the price of a token
    * @param token The token to get the price of
-   * @return price The asset price mantissa (scaled by 1e18).
+   * @return The asset price mantissa (scaled by 1e18).
    *  Zero means the price is unavailable.
-   * @return source The source of the price. enum (NA, DIRECT, CHAINLINK, DEX, V1_ORACLE)
+   * @return The source of the price. enum (NA, DIRECT, CHAINLINK, DEX, V1_ORACLE)
    */
-  function getTokenPrice(address token) external view virtual returns (uint256 price, uint256 source);
+  function getTokenPrice(address token) external view virtual returns (uint256, uint256);
 }
