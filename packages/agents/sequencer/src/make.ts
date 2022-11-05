e461364aa2a90ea1bbb6fe04bca82b77746fa170
sanchaymittal <sanchaymittal@gmail.com>
2022-11-05 23:47:04 +0000

    new: make for subscriber & publisher

---
+++ packages/agents/sequencer/src/make.ts
@@ -0,0 +1,25 @@
+import { makePublisher as _makePublisher, makeSubscriber as _makeSubscriber, setupContext } from "./sequencer";
+
+import { SequencerConfig } from "./lib/entities";
+
+export const makeSubscriber = async (_configOverride?: SequencerConfig) => {
+  /// MARK - Context
+  await setupContext(_configOverride);
+
+  _makeSubscriber();
+};
+
+export const makePublisher = async (_configOverride?: SequencerConfig) => {
+  /// MARK - Context
+  await setupContext(_configOverride);
+
+  _makePublisher();
+};
+
+export const makeSequencer = async (_configOverride?: SequencerConfig) => {
+  /// MARK - Context
+  await setupContext(_configOverride);
+
+  _makeSubscriber();
+  _makePublisher();
+};
