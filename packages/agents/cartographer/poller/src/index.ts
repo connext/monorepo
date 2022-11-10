c07f5feb3017331dbcfb1e5be89467fbe5c0a28c
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-11-10 08:11:48 +0100

    initial work on getting lambda to build

---
+++ packages/agents/cartographer/poller/src/index.ts
@@ -0,0 +1,15 @@
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+
+import { makeMessagesPoller } from "./pollers/messagePoller";
+
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  makeMessagesPoller();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world",
+    }),
+  };
+};
