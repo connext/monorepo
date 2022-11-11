267025b4302663a2bc4812d49ba68327cf07d5f1
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2022-11-11 22:20:33 +0800

    add comment allow only fixed decimal token

--- packages/deployments/contracts/contracts/core/connext/facets/SwapAdminFacet.sol
+++ packages/deployments/contracts/contracts/core/connext/facets/SwapAdminFacet.sol
@@ -97,7 +97,8 @@ contract SwapAdminFacet is BaseConnextFacet {
    * @param _key the hash of the canonical id and domain for token
    * @param _pooledTokens an array of ERC20s this pool will accept
    * @param decimals the decimals to use for each pooled token,
-   * eg 8 for WBTC. Cannot be larger than POOL_PRECISION_DECIMALS
+   * eg 8 for WBTC. Cannot be larger than POOL_PRECISION_DECIMALS(18)
+   * Only fixed decimal tokens are allowed.
    * @param lpTokenName the long-form name of the token to be deployed
    * @param lpTokenSymbol the short symbol for the token to be deployed
    * @param _a the amplification coefficient * n ** (n - 1). See the
