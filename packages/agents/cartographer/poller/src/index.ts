2b36d06823346ae925daef5e68081719f3fce821
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-11-13 22:55:36 +0100

    docs

--- packages/agents/cartographer/poller/src/index.ts
+++ packages/agents/cartographer/poller/src/index.ts
@@ -1,14 +1,12 @@
-import tracer from "dd-trace";
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 
 import { makePoller } from "./pollers";
 
-tracer.init({ profiling: true, runtimeMetrics: true });
-
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-  makePoller();
+
+  await makePoller();
 
   return {
     statusCode: 200,
