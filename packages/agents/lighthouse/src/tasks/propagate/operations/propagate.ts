40f720c27c2fb4db5992218187350ae628c431d3
Rahul Sethuram <rahul@sethuram.com>
2022-11-14 20:24:29 -0500

    Propagate

--- packages/agents/lighthouse/src/tasks/propagate/operations/propagate.ts
+++ packages/agents/lighthouse/src/tasks/propagate/operations/propagate.ts
@@ -1,8 +1,9 @@
 import { sendWithRelayerWithBackup } from "@connext/nxtp-adapters-relayer";
-import { createLoggingContext } from "@connext/nxtp-utils";
+import { createLoggingContext, RequestContext } from "@connext/nxtp-utils";
 
 import { encodePropagate } from "../../../mockable";
 import { NoChainIdForHubDomain, RootManagerPropagateWrapperNotFound } from "../errors";
+import { getPropagateParams as getPropagateParamsArbitrum } from "../helpers/arbitrum";
 import { getContext } from "../propagate";
 
 export type ExtraPropagateParams = {
@@ -10,7 +11,17 @@ export type ExtraPropagateParams = {
   value: string;
 };
 
-const getParamsForDomainFn: Record<string, () => Promise<ExtraPropagateParams>> = {};
+const getParamsForDomainFn: Record<
+  string,
+  (
+    spokeDomain: string,
+    spokeChainId: number,
+    hubChainId: number,
+    requestContext: RequestContext,
+  ) => Promise<ExtraPropagateParams>
+> = {
+  "1634886255": getPropagateParamsArbitrum,
+};
 
 export const propagate = async () => {
   const {
@@ -22,22 +33,22 @@ export const propagate = async () => {
   const { requestContext, methodContext } = createLoggingContext(propagate.name);
   logger.info("Starting propagate operation", requestContext, methodContext);
   const domains = await subgraph.getDomainsForHub(config.hubDomain);
+  const hubChainId = chainData.get(config.hubDomain)?.chainId;
+  if (!hubChainId) {
+    throw new NoChainIdForHubDomain(config.hubDomain, requestContext, methodContext);
+  }
+
   const params: ExtraPropagateParams[] = await Promise.all(
     domains.map(async (domain) => {
       logger.info("Starting propagation for domain", requestContext, methodContext, { domain });
       const getParamsForDomain = getParamsForDomainFn[domain];
       let params: ExtraPropagateParams = { encodedData: "0x", value: "0" };
       if (getParamsForDomain) {
-        params = await getParamsForDomain();
+        params = await getParamsForDomain(domain, chainData.get(domain)?.chainId, hubChainId, requestContext);
       }
       return params;
     }),
   );
-
-  const hubChainId = chainData.get(config.hubDomain)?.chainId;
-  if (!hubChainId) {
-    throw new NoChainIdForHubDomain(config.hubDomain, requestContext, methodContext);
-  }
 
   const target = contracts.rootManagerPropagateWrapper(hubChainId, config.environment === "staging" ? "Staging" : "");
   if (!target) {
