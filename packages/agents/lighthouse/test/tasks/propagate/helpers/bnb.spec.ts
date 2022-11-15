35eb8594c15e777145b86ba8e6d977947daec34f
wanglonghong <wanglonghong4467@gmail.com>
2022-11-15 22:31:45 +0800

    necessary test functions

---
+++ packages/agents/lighthouse/test/tasks/propagate/helpers/bnb.spec.ts
@@ -0,0 +1,11 @@
+import { SinonStub, stub } from "sinon";
+
+describe("Helpers: Bnb", () => {
+  describe("#getPropagateParams", () => {
+    it("should throw an error if no provider for spoke domain", async () => {});
+    it("should throw an error if no provider for hub domain", async () => {});
+    it("should throw an error if no spoke connector", async () => {});
+    it("should throw an error if no hub connector", async () => {});
+    it("should return necessary data successfully", async () => {});
+  });
+});
--- packages/agents/lighthouse/test/tasks/propagate/operations/propagate.spec.ts
+++ packages/agents/lighthouse/test/tasks/propagate/operations/propagate.spec.ts
@@ -1,16 +1,9 @@
-import { BaseRequestContext, createRequestContext, expect, Logger, mock, RelayerTaskStatus } from "@connext/nxtp-utils";
 import { SinonStub, stub } from "sinon";
-
-import * as ProcessFromRootFns from "../../../../src/tasks/processFromRoot/operations/processFromRoot";
-import * as MockableFns from "../../../../src/mockable";
-import { processFromRootCtxMock } from "../../../globalTestHook";
-import { ProcessConfigNotAvailable } from "../../../../src/tasks/processFromRoot/errors";
-import { mockTaskId } from "@connext/nxtp-adapters-relayer/test/mock";
-import { Relayer } from "@connext/nxtp-adapters-relayer";
-import { ChainReader } from "@connext/nxtp-txservice";
 
 describe("Operations: Propagate", () => {
   describe("#propagate", () => {
     it("should throw an error if no hub domain id", async () => {});
+    it("should throw an error if root manager not found", async () => {});
+    it("should send encoded data to relayer succesfully", async () => {});
   });
 });
