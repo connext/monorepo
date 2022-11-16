b734ddfb63cac38ad960eb0195fb01ddf6f3f632
LayneHaber <layne.haber@gmail.com>
2022-11-16 12:20:24 -0500

    fix: intentionally revert, comments

--- packages/deployments/contracts/contracts/messaging/connectors/polygon/PolygonHubConnector.sol
+++ packages/deployments/contracts/contracts/messaging/connectors/polygon/PolygonHubConnector.sol
@@ -53,6 +53,14 @@ contract PolygonHubConnector is HubConnector, FxBaseRootTunnel {
   }
 
   function _setMirrorConnector(address _mirrorConnector) internal override {
+    // NOTE: FxBaseRootTunnel has the following code in their `setFxChildTunnel`:
+    // ```
+    // require(fxChildTunnel == address(0x0), "FxBaseRootTunnel: CHILD_TUNNEL_ALREADY_SET");
+    // ```
+    // Which means this function will revert if updating the `mirrorConnector`. In that case, in
+    // changes  the
+    // hub connector should also be redeployed
+    require(mirrorConnector == address(0), "!empty");
     super._setMirrorConnector(_mirrorConnector);
 
     setFxChildTunnel(_mirrorConnector);
