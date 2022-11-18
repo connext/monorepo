d1c57769e378f799cf22b9b5dd97dc68e33c5888
LayneHaber <layne.haber@gmail.com>
2022-11-18 13:15:53 -0500

    use balanceOf

--- packages/deployments/contracts/contracts/core/connext/facets/TokenFacet.sol
+++ packages/deployments/contracts/contracts/core/connext/facets/TokenFacet.sol
@@ -452,7 +452,13 @@ contract TokenFacet is BaseConnextFacet {
       // Sanity check: no value custodied if on canonical domain
       address canonicalAsset = TypeCasts.bytes32ToAddress(_canonical.id);
       // Check custodied amount for the given canonical asset address.
-      if (s.custodied[canonicalAsset] > 0) {
+      // NOTE: if the `cap` is not set, the `custodied` value will not continue to be updated,
+      // so you must use the `balanceOf` for accurate accounting. If there are funds held
+      // on these contracts, then when you remove the asset id, the assets cannot be bridged back and
+      // become worthless. This means the bridged assets would become worthless.
+      // An attacker could prevent admins from removing an asset by sending funds to this contract,
+      // but all of the liquidity should already be removed before this function is called.
+      if (IERC20(canonicalAsset).balanceOf(address(this)) > 0) {
         revert TokenFacet__removeAssetId_remainsCustodied();
       }
     } else {
--- packages/deployments/contracts/contracts_forge/core/connext/facets/TokenFacet.t.sol
+++ packages/deployments/contracts/contracts_forge/core/connext/facets/TokenFacet.t.sol
@@ -251,8 +251,7 @@ contract TokenFacetTest is TokenFacet, FacetHelper {
       TokenId(s.domain, _canonicalId),
       asset,
       adoptedAssetId,
-      stableSwap,
-      100000 ether
+      stableSwap
     );
   }
 
@@ -276,7 +275,7 @@ contract TokenFacetTest is TokenFacet, FacetHelper {
     vm.prank(_owner);
     // no error message given bc shouldnt be able to find function
     vm.expectRevert();
-    this.setupAssetWithDeployedRepresentation(canonical, address(asset), address(asset), address(0), 0);
+    this.setupAssetWithDeployedRepresentation(canonical, address(asset), address(asset), address(0));
   }
 
   function test_TokenFacet__setupAssetWithDeployedRepresentation_failIfOnRemoteAndCannotBurn() public {
@@ -290,7 +289,7 @@ contract TokenFacetTest is TokenFacet, FacetHelper {
     vm.prank(_owner);
     // no error message given bc shouldnt be able to find function
     vm.expectRevert();
-    this.setupAssetWithDeployedRepresentation(canonical, address(asset), address(asset), address(0), 0);
+    this.setupAssetWithDeployedRepresentation(canonical, address(asset), address(asset), address(0));
   }
 
   function test_TokenFacet__addStableSwapPool_success() public {
@@ -330,6 +329,7 @@ contract TokenFacetTest is TokenFacet, FacetHelper {
     setupAssetAndAssert(_adopted, bytes4(""));
 
     s.custodied[_canonical] = 0;
+    vm.mockCall(_local, abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)), abi.encode(0));
     removeAssetAndAssert(utils_calculateCanonicalHash(), _deployedLocal, _adopted);
   }
 
