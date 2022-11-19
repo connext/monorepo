3b83162758b05356b1d2b9b750108907a4917011
LayneHaber <layne.haber@gmail.com>
2022-11-19 10:20:20 -0500

    fix

--- packages/deployments/contracts/contracts/messaging/connectors/polygon/PolygonSpokeConnector.sol
+++ packages/deployments/contracts/contracts/messaging/connectors/polygon/PolygonSpokeConnector.sol
@@ -73,13 +73,12 @@ contract PolygonSpokeConnector is SpokeConnector, FxBaseChildTunnel {
   function _processMessage(bytes memory _data) internal override {}
 
   function _setMirrorConnector(address _mirrorConnector) internal override {
-    // NOTE: FxBaseRootTunnel has the following code in their `setFxChildTunnel`:
+    // NOTE: FxBaseChildTunnel has the following code in their `setFxRootTunnel`:
     // ```
-    // require(fxChildTunnel == address(0x0), "FxBaseRootTunnel: CHILD_TUNNEL_ALREADY_SET");
+    // require(setFxRootTunnel == address(0x0), "FxBaseChildTunnel: ROOT_TUNNEL_ALREADY_SET");
     // ```
     // Which means this function will revert if updating the `mirrorConnector`. In that case, in
-    // changes  the
-    // hub connector should also be redeployed
+    // changes the spoke connector should also be redeployed
     super._setMirrorConnector(_mirrorConnector);
 
     setFxRootTunnel(_mirrorConnector);
