44f92863e40ae1e8cdadb8ef2aa4cadddf869d87
just-a-node <eye1717@gmail.com>
2022-11-23 14:03:07 -0700

    Explicit decimals in config

--- packages/deployments/contracts/src/init/config/mainnet/production.ts
+++ packages/deployments/contracts/src/init/config/mainnet/production.ts
@@ -16,6 +16,7 @@ export const MAINNET_PRODUCTION_INIT_CONFIG: InitConfig = {
       canonical: {
         domain: "6648936",
         address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        decimals: 6,
       },
       representations: {
         "1869640809": {
@@ -45,6 +46,7 @@ export const MAINNET_PRODUCTION_INIT_CONFIG: InitConfig = {
       canonical: {
         domain: "6648936",
         address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        decimals: 18,
       },
       representations: {
         "1869640809": {
