404304ad183d5ecef710787127edb2d8f683fd80
just-a-node <eye1717@gmail.com>
2022-11-28 19:43:40 -0700

    Add pricefeed

---
+++ packages/agents/sdk/src/lib/priceFeed/PriceFeed.ts
@@ -0,0 +1,85 @@
+import Coinbase from "./Coinbase";
+
+const cache: {
+  [tokenSymbol: string]: Promise<any> | null;
+} = {};
+
+const cacheTimestamps: {
+  [tokenSymbol: string]: number | null;
+} = {};
+
+export type ApiKeys = {
+  coingecko?: string;
+};
+
+interface Service {
+  getPriceByTokenSymbol(symbol: string): Promise<number>;
+}
+
+class PriceFeed {
+  cacheTimeMs = 5 * 60 * 1000;
+  apiKeys: ApiKeys = {};
+  services: Service[] = [];
+
+  aliases: { [tokenSymbol: string]: string } = {
+    WETH: "ETH",
+    WMATIC: "MATIC",
+    XDAI: "DAI",
+    WXDAI: "DAI",
+  };
+
+  constructor(apiKeysMap: ApiKeys = {}) {
+    if (apiKeysMap) {
+      this.apiKeys = apiKeysMap;
+    }
+    this.setServices();
+  }
+
+  setApiKeys(apiKeysMap: ApiKeys = {}) {
+    this.apiKeys = apiKeysMap;
+    this.setServices();
+  }
+
+  private setServices() {
+    this.services = [new Coinbase()];
+  }
+
+  async getPriceByTokenSymbol(tokenSymbol: string) {
+    if (this.aliases[tokenSymbol]) {
+      tokenSymbol = this.aliases[tokenSymbol];
+    }
+    if ((await cache[tokenSymbol]) && cacheTimestamps[tokenSymbol]) {
+      const isRecent = cacheTimestamps[tokenSymbol]! > Date.now() - this.cacheTimeMs;
+      if (isRecent) {
+        return cache[tokenSymbol];
+      }
+    }
+    const promise = this._getPriceByTokenSymbol(tokenSymbol);
+    cache[tokenSymbol] = promise;
+    cacheTimestamps[tokenSymbol] = Date.now();
+    return promise;
+  }
+
+  async _getPriceByTokenSymbol(tokenSymbol: string) {
+    const errors: Error[] = [];
+    for (const service of this.services) {
+      try {
+        const price = await service.getPriceByTokenSymbol(tokenSymbol);
+        if (price === null) {
+          throw new Error(`null price for ${tokenSymbol}`);
+        }
+        return price;
+      } catch (err: any) {
+        const isLastService = this.services.indexOf(service) === this.services.length - 1;
+        errors.push(err.message);
+        if (isLastService) {
+          cache[tokenSymbol] = null;
+          cacheTimestamps[tokenSymbol] = null;
+          throw new Error(`PriceFeed error(s): ${errors.join(" ")}`);
+        }
+      }
+    }
+  }
+}
+
+export default PriceFeed;
