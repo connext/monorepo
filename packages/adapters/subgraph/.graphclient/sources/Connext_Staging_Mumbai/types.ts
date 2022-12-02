4ab75e9686038fa5b7504b9ef742cb489f05dfa0
wanglonghong <wanglonghong4467@gmail.com>
2022-12-02 16:35:57 +0800

    rebuild graphclient

--- packages/adapters/subgraph/.graphclient/sources/Connext_Staging_Mumbai/types.ts
+++ packages/adapters/subgraph/.graphclient/sources/Connext_Staging_Mumbai/types.ts
@@ -24,6 +24,7 @@ export type Scalars = {
 export type stagingmumbai_AggregateRoot = {
   id: Scalars['ID'];
   root: Scalars['stagingmumbai_Bytes'];
+  blockNumber: Scalars['BigInt'];
 };
 
 export type stagingmumbai_AggregateRoot_filter = {
@@ -41,13 +42,22 @@ export type stagingmumbai_AggregateRoot_filter = {
   root_not_in?: InputMaybe<Array<Scalars['stagingmumbai_Bytes']>>;
   root_contains?: InputMaybe<Scalars['stagingmumbai_Bytes']>;
   root_not_contains?: InputMaybe<Scalars['stagingmumbai_Bytes']>;
+  blockNumber?: InputMaybe<Scalars['BigInt']>;
+  blockNumber_not?: InputMaybe<Scalars['BigInt']>;
+  blockNumber_gt?: InputMaybe<Scalars['BigInt']>;
+  blockNumber_lt?: InputMaybe<Scalars['BigInt']>;
+  blockNumber_gte?: InputMaybe<Scalars['BigInt']>;
+  blockNumber_lte?: InputMaybe<Scalars['BigInt']>;
+  blockNumber_in?: InputMaybe<Array<Scalars['BigInt']>>;
+  blockNumber_not_in?: InputMaybe<Array<Scalars['BigInt']>>;
   /** Filter for the block changed event. */
   _change_block?: InputMaybe<stagingmumbai_BlockChangedFilter>;
 };
 
 export type stagingmumbai_AggregateRoot_orderBy =
   | 'id'
-  | 'root';
+  | 'root'
+  | 'blockNumber';
 
 export type stagingmumbai_Asset = {
   id: Scalars['ID'];
