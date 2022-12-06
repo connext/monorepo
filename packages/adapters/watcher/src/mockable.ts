88cc7d7492f8557ecd88ac1d4c73d90894826ba1
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2022-12-06 22:00:49 +0800

    twilio alert test

--- packages/adapters/watcher/src/mockable.ts
+++ packages/adapters/watcher/src/mockable.ts
@@ -1,4 +1,16 @@
 import { axiosGet as _axiosGet, axiosPost as _axiosPost } from "@connext/nxtp-utils";
+import { Twilio as _Twilio } from "twilio";
+import { MessageInstance } from "twilio/lib/rest/api/v2010/account/message";
 
 export const axiosGet = _axiosGet;
 export const axiosPost = _axiosPost;
+
+export const Twilio = _Twilio;
+export const sendMessageViaTwilio = async (
+  accountSid: string,
+  authToken: string,
+  textContext: { body: string; to: string; from: string },
+): Promise<MessageInstance> => {
+  const client = new Twilio(accountSid, authToken);
+  return await client.messages.create(textContext);
+};
---
+++ packages/adapters/watcher/test/alert/sms.spec.ts
@@ -0,0 +1,37 @@
+import { SinonStub, stub } from "sinon";
+
+import { WatcherConfig } from "../../src/config";
+import { TEST_REPORT } from "../utils";
+import { alertViaSms, alertViaTelegram } from "../../src/alert";
+import { expect } from "@connext/nxtp-utils";
+import * as Mockable from "../../src/mockable";
+
+describe("Watcher Adapter: sms", () => {
+  let config: WatcherConfig = {
+    twilioNumber: "123324523523",
+    twilioAccountSid: "test",
+    twilioAuthToken: "test",
+    twilioToPhoneNumbers: ["21348728349"],
+  };
+  beforeEach(() => {});
+
+  describe("#alertViaSms", () => {
+    beforeEach(() => {});
+
+    it("should throw if chatId or apiKey is invalid", async () => {
+      await expect(alertViaSms(TEST_REPORT, { ...config, twilioNumber: undefined })).to.be.rejectedWith(
+        "alertViaSms: Twilio config is invalid!",
+      );
+    });
+
+    it("should success if config is valid", async () => {
+      let twilioStub: SinonStub;
+      twilioStub = stub(Mockable, "sendMessageViaTwilio").resolves();
+
+      await expect(alertViaSms(TEST_REPORT, config)).to.not.rejected;
+      expect(twilioStub.callCount).to.be.eq(1);
+
+      twilioStub.restore();
+    });
+  });
+});
