3fb583ad1e2415c6f2fe336d2c31f3952e3e53c9
wanglonghong <wanglonghong4467@gmail.com>
2022-12-20 23:27:26 +0800

    hub staging update

--- packages/deployments/subgraph/src/amarok-hub-staging/mapping/merkleTreeManager.ts
+++ packages/deployments/subgraph/src/amarok-hub-staging/mapping/merkleTreeManager.ts
@@ -1,27 +1,31 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 
-import { LeavesInserted } from "../../../generated/MerkleTreeManager/MerkleTreeManager";
+import { LeafInserted } from "../../../generated/MerkleTreeManager/MerkleTreeManager";
 import { AggregatedMessageRoot, RootAggregated } from "../../../generated/schema";
 
-export function handleLeavesInserted(event: LeavesInserted): void {
+export function handleLeafInserted(event: LeafInserted): void {
   const key = event.params.root.toHexString();
-  const numRootsAggregated = event.params.leaves.length;
-  const preCount = event.params.count.minus(BigInt.fromI32(numRootsAggregated));
 
-  for (let i = 0; i < numRootsAggregated; i++) {
-    const leaf = event.params.leaves[i].toHexString();
-    const index = preCount.plus(BigInt.fromI32(i));
+  const leaf = event.params.leaf.toHexString();
 
-    let instance = AggregatedMessageRoot.load(`${key}-${index}`); // Should ALWAYS be null.
-    if (instance == null) {
-      instance = new AggregatedMessageRoot(`${key}-${index}`);
-    }
+  // Index of insertion for each leaf will be (whatever the aggregate tree count was before
+  // insertion of all these message roots) + index in the array of message roots.
+  // TODO: obviously this is unnecessary, but preserving the old logic for easy debug in case this is off
+  // @wang please look at this
+  const aggregateTreePreCount = event.params.count.minus(BigInt.fromI32(1));
+  const index = aggregateTreePreCount.plus(BigInt.fromI32(1));
 
-    const rootAggregatedInstance = RootAggregated.load(leaf);
+  let instance = AggregatedMessageRoot.load(`${key}-${index}`); // Should ALWAYS be null.
+  if (instance == null) {
+    instance = new AggregatedMessageRoot(`${key}-${index}`);
+  }
+
+  const rootAggregatedInstance = RootAggregated.load(leaf);
+  if (rootAggregatedInstance != null) {
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     instance.domain = rootAggregatedInstance!.domain;
-    instance.index = index;
-    instance.receivedRoot = Bytes.fromHexString(leaf);
-    instance.save();
   }
+  instance.index = index;
+  instance.receivedRoot = Bytes.fromHexString(leaf);
+  instance.save();
 }
