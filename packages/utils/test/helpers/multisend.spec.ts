cf540a026b3f89ae210bbf0a2469e26e8730f086
jakekidd <jcb.kdd@gmail.com>
2022-12-20 16:23:19 -0800

    fix multisend util unit test

--- packages/utils/test/helpers/multisend.spec.ts
+++ packages/utils/test/helpers/multisend.spec.ts
@@ -1,10 +1,10 @@
-import { expect, mkAddress } from "@connext/nxtp-utils";
-import { BigNumber } from "ethers";
+import { BigNumber, utils } from "ethers";
+import { expect, mkAddress, MultisendAbi } from "@connext/nxtp-utils";
 
-import { makeMultisendTransactions, MultisendTransaction, MultisendTransactionOperation } from "../../src";
+import { encodeMultisendCall, MultisendTransaction, MultisendTransactionOperation } from "../../src";
 
 describe("Helpers:Multisend", () => {
-  describe("#getMultisendTransaction", () => {
+  describe("#encodeMultisendCall", () => {
     it("converts the given tx info into valid multisend transaction calldata", () => {
       const tx: MultisendTransaction = {
         operation: MultisendTransactionOperation.CALL,
@@ -12,12 +12,17 @@ describe("Helpers:Multisend", () => {
         data: "0xc0ffee",
         value: BigNumber.from("987654321"),
       };
-      const result = makeMultisendTransactions([tx]);
-      expect(result).to.be.eq(
+      const expected =
         "0x001230000000000000000000000000000000000000000000000000000000000000" +
-          "000000000000000000000000000000003ade68b100000000000000000000000000000" +
-          "00000000000000000000000000000000003c0ffee",
-      );
+        "000000000000000000000000000000003ade68b100000000000000000000000000000" +
+        "00000000000000000000000000000000003c0ffee";
+
+      const result = encodeMultisendCall([tx]);
+
+      // Encode expected result using the Multisend ABI; should match result from encodeMultisendCall method.
+      const iface = new utils.Interface(MultisendAbi);
+      const encoded = iface.encodeFunctionData("multiSend", [expected]);
+      expect(result).to.be.eq(encoded);
     });
   });
 });
