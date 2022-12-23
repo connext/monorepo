f93f029aea2ffd8c62a920f82182290c97ab3952
Rahul Sethuram <rahul@sethuram.com>
2022-12-23 19:29:20 +0400

    Update gelato version

--- packages/agents/sequencer/src/mockable.ts
+++ packages/agents/sequencer/src/mockable.ts
@@ -1,5 +1,3 @@
 import { sendWithRelayerWithBackup as _sendWithRelayerWithBackup } from "@connext/nxtp-adapters-relayer";
-import { GelatoRelaySDK } from "@gelatonetwork/relay-sdk";
 
 export const sendWithRelayerWithBackup = _sendWithRelayerWithBackup;
-export const getEstimatedFee = GelatoRelaySDK.getEstimatedFee;
