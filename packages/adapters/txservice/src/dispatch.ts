13a959396714aac1ee1d99644c1b03d7cd08af56
LayneHaber <layne.haber@gmail.com>
2023-01-09 20:33:35 -0700

    feat: gas price override enabled

--- packages/adapters/txservice/src/dispatch.ts
+++ packages/adapters/txservice/src/dispatch.ts
@@ -1,4 +1,4 @@
-import { BigNumber, Signer, providers, utils } from "ethers";
+import { BigNumber, Signer, constants, providers, utils } from "ethers";
 import PriorityQueue from "p-queue";
 import {
   createLoggingContext,
@@ -351,7 +351,11 @@ export class TransactionDispatch extends RpcProviderAggregator {
    *
    * @returns A list of receipts or errors that occurred for each.
    */
-  public async send(minTx: WriteTransaction, context: RequestContext): Promise<providers.TransactionReceipt> {
+  public async send(
+    minTx: WriteTransaction,
+    context: RequestContext,
+    gasPrice?: BigNumber,
+  ): Promise<providers.TransactionReceipt> {
     const method = this.send.name;
     const { requestContext, methodContext } = createLoggingContext(method, context);
     const txsId = getUuid();
@@ -381,7 +385,7 @@ export class TransactionDispatch extends RpcProviderAggregator {
           // that, if we get past this method, we can *generally* assume that the transaction will go through on submit - although it's
           // still possible to revert due to a state change below.
           const attemptedNonces: number[] = [];
-          const [gasLimit, gasPrice, nonceInfo] = await Promise.all([
+          const [gasLimit, _gasPrice, nonceInfo] = await Promise.all([
             this.estimateGas(minTx),
             this.getGasPrice(requestContext),
             this.determineNonce(attemptedNonces),
@@ -389,9 +393,10 @@ export class TransactionDispatch extends RpcProviderAggregator {
           let { nonce, backfill, transactionCount } = nonceInfo;
 
           // TODO: Remove hardcoded (exposed gasLimitInflation config var should replace this).
+          const override = (gasPrice ?? constants.Zero);
           const gas: Gas = {
             limit: gasLimit,
-            price: gasPrice,
+            price: override.gt(_gasPrice) ? override : _gasPrice,
           };
           if (this.domain === 1634886255) {
             gas.limit = BigNumber.from(10_000_000);
