8af48d8cab89496568781c9bdc795341866880b7
Rahul Sethuram <rahul@sethuram.com>
2023-01-12 09:55:28 +0400

    Fix build

--- packages/agents/sequencer/src/mockable.ts
+++ packages/agents/sequencer/src/mockable.ts
@@ -1,3 +1,5 @@
 import { sendWithRelayerWithBackup as _sendWithRelayerWithBackup } from "@connext/nxtp-adapters-relayer";
+import { calculateRelayerFee as _calculateRelayerFee } from "@connext/nxtp-utils";
 
 export const sendWithRelayerWithBackup = _sendWithRelayerWithBackup;
+export const calculateRelayerFee = _calculateRelayerFee;
