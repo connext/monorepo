9a0f9e713fc38315a8f51e5c944d615908ef1444
Rahul Sethuram <rahul@sethuram.com>
2023-01-12 16:43:38 +0400

    Better error handling

--- packages/utils/src/helpers/axios.ts
+++ packages/utils/src/helpers/axios.ts
@@ -17,21 +17,7 @@ export const axiosPost = async <T = any, R = AxiosResponse<T>, D = any>(
     const response = await axios.post<T, R, D>(url, data, config);
     return response;
   } catch (error: unknown) {
-    const errorObj: any = {};
-    if ((error as AxiosError<T, D>).response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      errorObj.data = (error as AxiosError<T, D>).response!.data;
-      errorObj.status = (error as AxiosError<T, D>).response!.status;
-      errorObj.headers = (error as AxiosError<T, D>).response!.headers;
-    } else if ((error as AxiosError<T, D>).request) {
-      errorObj.request = (error as AxiosError<T, D>).request;
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      errorObj.message = (error as AxiosError<T, D>).message;
-    }
-    errorObj.config = (error as AxiosError<T, D>).config;
-    throw new AxiosQueryError(url, "post", data, errorObj);
+    throw new AxiosQueryError(url, "post", data, (error as AxiosError).toJSON());
   }
 };
 
@@ -41,20 +27,6 @@ export const axiosGet = async <T = any, R = AxiosResponse<T>, D = any>(url: stri
     const response = await axios.get<T, R, D>(url, data as any);
     return response;
   } catch (error: unknown) {
-    const errorObj: any = {};
-    if ((error as AxiosError<T, D>).response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      errorObj.data = (error as AxiosError<T, D>).response!.data;
-      errorObj.status = (error as AxiosError<T, D>).response!.status;
-      errorObj.headers = (error as AxiosError<T, D>).response!.headers;
-    } else if ((error as AxiosError<T, D>).request) {
-      errorObj.request = (error as AxiosError<T, D>).request;
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      errorObj.message = (error as AxiosError<T, D>).message;
-    }
-    errorObj.config = (error as AxiosError<T, D>).config;
-    throw new AxiosQueryError(url, "get", undefined, errorObj);
+    throw new AxiosQueryError(url, "get", undefined, (error as AxiosError).toJSON());
   }
 };
