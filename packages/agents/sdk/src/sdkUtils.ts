cd07f600f6a0294956d315e69a99f1f436fb4069
just-a-node <eye1717@gmail.com>
2023-01-13 12:47:37 -0700

    Combine getTransfers queries with optional params

--- packages/agents/sdk/src/sdkUtils.ts
+++ packages/agents/sdk/src/sdkUtils.ts
@@ -43,130 +43,37 @@ export class NxtpSdkUtils extends NxtpSdkShared {
     return await axiosGetRequest(uri);
   }
 
-  async getTransfersByUser(params: {
-    userAddress: string;
+  async getTransfers(params: {
+    userAddress?: string;
+    routerAddress?: string;
     status?: XTransferStatus;
+    transferId?: string;
+    transactionHash?: string;
     range?: { limit?: number; offset?: number };
   }): Promise<any> {
-    const { userAddress, status, range } = params;
+    const { userAddress, routerAddress, status, transferId, transactionHash, range } = params;
 
-    const userIdentifier = `xcall_tx_origin=eq.${userAddress.toLowerCase()}&`;
-    const statusIdentifier = status ? `status=eq.${status}` : "";
+    const userIdentifier = userAddress ? `xcall_tx_origin=eq.${userAddress.toLowerCase()}&` : "";
+    const routerIdentifier = routerAddress ? `routers=cs.%7B${routerAddress.toLowerCase()}%7D&` : "";
+    const statusIdentifier = status ? `status=eq.${status}&` : "";
+    const transferIdIdentifier = transferId ? `transfer_id=eq.${transferId.toLowerCase()}&` : "";
+    const transactionHashIdentifier = transactionHash
+      ? `xcall_transaction_hash=eq.${transactionHash.toLowerCase()}&`
+      : "";
 
-    const searchIdentifier = userIdentifier + statusIdentifier;
+    const searchIdentifier =
+      userIdentifier + routerIdentifier + statusIdentifier + transferIdIdentifier + transactionHashIdentifier;
 
     const limit = range?.limit ? range.limit : 10;
     const offset = range?.offset ? range.offset : 0;
 
-    const rangeIdentifier = `&limit=${limit}&offset=${offset}`;
-    const orderIdentifier = `&order=xcall_timestamp.desc`;
+    const rangeIdentifier = `limit=${limit}&offset=${offset}&`;
+    const orderIdentifier = `order=xcall_timestamp.desc`;
 
     const uri = formatUrl(
       this.config.cartographerUrl!,
       "transfers?",
       searchIdentifier + rangeIdentifier + orderIdentifier + `&${transfersCastForUrl}`,
-    );
-
-    // Validate uri
-    validateUri(uri);
-
-    return await axiosGetRequest(uri);
-  }
-
-  async getTransfers(params: { range?: { limit?: number; offset?: number } }): Promise<any> {
-    const { range } = params;
-
-    const limit = range?.limit ? range.limit : 10;
-    const offset = range?.offset ? range.offset : 0;
-
-    const rangeIdentifier = `limit=${limit}&offset=${offset}`;
-    const orderIdentifier = `&order=xcall_timestamp.desc`;
-
-    const uri = formatUrl(
-      this.config.cartographerUrl!,
-      "transfers?",
-      rangeIdentifier + orderIdentifier + `&${transfersCastForUrl}`,
-    );
-
-    // Validate uri
-    validateUri(uri);
-
-    return await axiosGetRequest(uri);
-  }
-
-  async getTransfersByStatus(params: {
-    status: XTransferStatus;
-    range?: { limit?: number; offset?: number };
-  }): Promise<any> {
-    const { status, range } = params;
-
-    const statusIdentifier = `status=eq.${status}`;
-
-    const limit = range?.limit ? range.limit : 10;
-    const offset = range?.offset ? range.offset : 0;
-
-    const rangeIdentifier = `&limit=${limit}&offset=${offset}`;
-    const orderIdentifier = `&order=xcall_timestamp.desc`;
-
-    const uri = formatUrl(
-      this.config.cartographerUrl!,
-      "transfers?",
-      statusIdentifier + rangeIdentifier + orderIdentifier + `&${transfersCastForUrl}`,
-    );
-
-    // Validate uri
-    validateUri(uri);
-
-    return await axiosGetRequest(uri);
-  }
-
-  async getTransfersByRouter(params: {
-    routerAddress: string;
-    status?: XTransferStatus;
-    range?: { limit?: number; offset?: number };
-  }): Promise<any> {
-    const { routerAddress, status, range } = params;
-
-    const routerIdentifier = `routers=cs.%7B${routerAddress.toLowerCase()}%7D&`;
-    const statusIdentifier = status ? `status=eq.${status}` : "";
-
-    const searchIdentifier = routerIdentifier + statusIdentifier;
-
-    const limit = range?.limit ? range.limit : 10;
-    const offset = range?.offset ? range.offset : 0;
-
-    const rangeIdentifier = `&limit=${limit}&offset=${offset}`;
-    const orderIdentifier = `&order=xcall_timestamp.desc`;
-
-    const uri = formatUrl(
-      this.config.cartographerUrl!,
-      "transfers?",
-      searchIdentifier + rangeIdentifier + orderIdentifier + `&${transfersCastForUrl}`,
-    );
-
-    // Validate uri
-    validateUri(uri);
-
-    return await axiosGetRequest(uri);
-  }
-
-  async getTransferById(transferId: string): Promise<any> {
-    const uri = formatUrl(
-      this.config.cartographerUrl!,
-      "transfers?",
-      `transfer_id=eq.${transferId.toLowerCase()}&${transfersCastForUrl}`,
-    );
-    // Validate uri
-    validateUri(uri);
-
-    return await axiosGetRequest(uri);
-  }
-
-  async getTransferByTransactionHash(transactionHash: string): Promise<any> {
-    const uri = formatUrl(
-      this.config.cartographerUrl!,
-      "transfers?",
-      `xcall_transaction_hash=eq.${transactionHash.toLowerCase()}&${transfersCastForUrl}`,
     );
 
     // Validate uri
