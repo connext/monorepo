fffcc2670aa3ccb945d6af9138655f62c90358f4
LayneHaber <layne.haber@gmail.com>
2023-01-16 20:26:04 -0700

    fix: watcher adapter tests

--- packages/adapters/watcher/test/pause/pauser.spec.ts
+++ packages/adapters/watcher/test/pause/pauser.spec.ts
@@ -18,12 +18,13 @@ describe("Watcher Adapter: Pauser", () => {
   let txservice: SinonStubbedInstance<TransactionService>;
   let context: VerifierContext;
   let pauser: Pauser;
+  let logger: Logger;
 
   beforeEach(() => {
     txservice = createStubInstance(TransactionService, {
       readTx: Promise.resolve(readTxResult),
     });
-    const logger = createStubInstance(Logger);
+    logger = createStubInstance(Logger);
     context = {
       domains,
       txservice,
@@ -40,16 +41,16 @@ describe("Watcher Adapter: Pauser", () => {
 
   describe("#pause", () => {
     beforeEach(() => {
-      txservice.getGasPrice.resolves(BigNumber.from(1000))
+      txservice.getGasPrice.resolves(BigNumber.from(1000));
     });
 
     it("should return empty array when domains empty", async () => {
-      let result = await pauser.pause("for test", []);
+      let result = await pauser.pause(requestContext, "for test", []);
       expect(result).to.be.empty;
     });
 
     it("should fail if deployment not exist", async () => {
-      const result = await pauser.pause("for test", ["112312312312312"]);
+      const result = await pauser.pause(requestContext, "for test", ["112312312312312"]);
       expect(result[0].domain).to.be.equal("112312312312312");
       expect(result[0].paused).to.be.equal(false);
       expect(result[0].relevantTransaction).to.be.equal("");
@@ -62,17 +63,26 @@ describe("Watcher Adapter: Pauser", () => {
       txservice.readTx.callsFake(async (): Promise<string> => {
         return "0x0000000000000000000000000000000000000000000000000000000000000001"; // true
       });
-      const result = await pauser.pause("for test", domains);
+      const result = await pauser.pause(requestContext, "for test", domains);
       expect(result[0].domain).to.be.equal(domain);
       expect(result[0].paused).to.be.equal(false);
       expect(result[0].relevantTransaction).to.be.equal("");
       expect(result[0].error.toString()).to.be.equal("Error: Already Paused");
     });
 
+    it("should fail if getGasPrice fails", async () => {
+      txservice.getGasPrice.throws("failure");
+      const result = await pauser.pause(requestContext, "for test", domains);
+      expect(result[0].domain).to.be.equal(domain);
+      expect(result[0].paused).to.be.equal(false);
+      expect(result[0].relevantTransaction).to.be.equal("");
+      expect(result[0].error.toString()).to.be.equal("failure");
+    });
+
     it("should fail if pause tx failed", async () => {
       txservice.sendTx.throws("send tx failed");
 
-      const result = await pauser.pause("for test", domains);
+      const result = await pauser.pause(requestContext, "for test", domains);
       expect(result[0].domain).to.be.equal(domain);
       expect(result[0].paused).to.be.equal(false);
       expect(result[0].relevantTransaction).to.be.equal("");
@@ -84,13 +94,29 @@ describe("Watcher Adapter: Pauser", () => {
         return mock.ethers.receipt({ transactionHash: mkHash("0x1") });
       });
 
-      const result = await pauser.pause("for test", domains);
+      const result = await pauser.pause(requestContext, "for test", domains);
       expect(result[0].domain).to.be.equal(domain);
       expect(result[0].paused).to.be.equal(true);
       expect(result[0].relevantTransaction).to.be.equal(mkHash("0x1"));
       expect(result[0].error).to.be.null;
 
       expect(txservice.sendTx).to.have.been.callCount(1);
+    });
+
+    it("should handle a single domain failing", async () => {
+      domains.push("1236565");
+      txservice.sendTx.callsFake(async (): Promise<providers.TransactionReceipt> => {
+        return mock.ethers.receipt({ transactionHash: mkHash("0x1") });
+      });
+      const result = await pauser.pause(requestContext, "test", domains);
+      expect(result[0].domain).to.be.equal(domains[0]);
+      expect(result[0].paused).to.be.equal(true);
+      expect(result[0].relevantTransaction).to.be.equal(mkHash("0x1"));
+      expect(result[0].error).to.be.null;
+      expect(result[1].domain).to.be.equal(domains[1]);
+      expect(result[1].paused).to.be.equal(false);
+      expect(result[1].relevantTransaction).to.be.equal("");
+      expect(result[1].error.toString()).to.be.equal("Error: Cannot find corresponding chainId for domain " + domains[1]);
     });
   });
 });
