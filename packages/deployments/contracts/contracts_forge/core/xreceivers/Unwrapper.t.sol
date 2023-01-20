92522e32398e228b021fa22a8b91f5673ad8794b
LayneHaber <layne.haber@gmail.com>
2023-01-20 13:31:17 -0700

    WIP: unwrapper tests

--- packages/deployments/contracts/contracts_forge/core/xreceivers/Unwrapper.t.sol
+++ packages/deployments/contracts/contracts_forge/core/xreceivers/Unwrapper.t.sol
@@ -4,18 +4,14 @@ pragma solidity 0.8.17;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {Unwrapper, IWrapper} from "../../../contracts/core/xreceivers/Unwrapper.sol";
-import {Orphanage} from "../../../contracts/core/xreceivers/Orphanage.sol";
 
 import "../../utils/ForgeHelper.sol";
 import "../../utils/Mock.sol";
 
 contract UnwrapperTest is ForgeHelper {
   // ============ Events ============
-
-  event UnwrappingFailed(address recipient, bytes reason);
-  event SendUnwrappedFailed(address recipient, bytes reason);
-  event TransferWrappedFailed(address recipient, bytes reason);
-  event WrongAsset(address recipient, address asset, bytes reason);
+  event FundsDelivered(address recipient, address asset, uint256 amount);
+  event WrongAsset(address recipient, address asset);
 
   // ============ Storage ============
   address constant MOCK_CONNEXT = address(999999);
@@ -43,6 +39,8 @@ contract UnwrapperTest is ForgeHelper {
     utils_setUpMockErc20(true); // Default transferSuccess = true.
 
     unwrapper = new Unwrapper(MOCK_CONNEXT, MOCK_WRAPPER);
+    assertEq(unwrapper.CONNEXT(), MOCK_CONNEXT);
+    assertEq(address(unwrapper.WRAPPER()), MOCK_WRAPPER);
 
     vm.label(MOCK_CONNEXT, "Connext");
     vm.label(MOCK_WRAPPER, "Wrapper");
@@ -58,12 +56,15 @@ contract UnwrapperTest is ForgeHelper {
   }
 
   // ============ Unwrapper.xReceive ============
-  function test_Unwrapper__xReceive_works() public {
+  function test_Unwrapper__xReceive_works(uint256 amount, address recipient) public {
     address asset = MOCK_WRAPPER;
-    uint256 amount = 10 ether;
+    bytes memory callData = abi.encode(recipient);
+
+    vm.expectEmit(true, true, true, true);
+    emit FundsDelivered(recipient, asset, amount);
 
     vm.prank(MOCK_CONNEXT);
-    unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
+    unwrapper.xReceive(transferId, amount, asset, originSender, origin, callData);
   }
 
   function test_Unwrapper__xReceive_worksIfRecipientIsZero() public {
@@ -102,64 +103,12 @@ contract UnwrapperTest is ForgeHelper {
     unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
   }
 
-  function test_Unwrapper__xReceive_emitsEventIfNonWrapperAssetAndERC20TransferFails() public {
-    utils_setUpMockErc20(false);
-    address asset = MOCK_ERC20; // xReceive will be getting a random non-wrapper erc20 asset.
-    uint256 amount = 10 ether;
-
-    vm.expectEmit(true, true, true, true);
-    emit WrongAsset(recipient, asset, "wrong asset used for unwrap and fallback transfer failed");
-
-    vm.prank(MOCK_CONNEXT);
-    unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
-  }
-
   function test_Unwrapper__xReceive_emitsEventIfNonWrapperAssetAndERC20TransferReverts() public {
     address asset = address(new RevertingERC20()); // xReceive will be getting a non-wrapper, reverting erc20 asset.
     uint256 amount = 10 ether;
 
     vm.expectEmit(true, true, true, true);
-    emit WrongAsset(recipient, asset, abi.encodeWithSignature("Error(string)", "test transfer error"));
-
-    vm.prank(MOCK_CONNEXT);
-    unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
-  }
-
-  function test_Unwrapper__xReceive_orphansIfWithdrawRevertsAndTransferWETHReverts() public {
-    address asset = address(new RevertingERC20()); // xReceive will be getting an asset that reverts on `withdraw` and `transfer`.
-    unwrapper = new Unwrapper(MOCK_CONNEXT, asset); // set the asset as the target wrapper asset
-    uint256 amount = 10 ether;
-
-    vm.expectEmit(true, true, true, true);
-    emit UnwrappingFailed(recipient, abi.encodeWithSignature("Error(string)", "test withdraw error"));
-    emit TransferWrappedFailed(recipient, abi.encodeWithSignature("Error(string)", "test transfer error"));
-
-    vm.prank(MOCK_CONNEXT);
-    unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
-  }
-
-  function test_Unwrapper__xReceive_orphansIfWithdrawRevertsAndTransferWETHFails() public {
-    address asset = address(new RevertingERC20()); // xReceive will be getting an asset that reverts on `withdraw` and `transfer`.
-    unwrapper = new Unwrapper(MOCK_CONNEXT, asset); // set the asset as the target wrapper asset
-    uint256 amount = 10 ether;
-
-    // Make the transfer fail, not revert
-    vm.mockCall(asset, abi.encodeWithSelector(IWrapper.transfer.selector), abi.encode(false));
-
-    vm.expectEmit(true, true, true, true);
-    emit UnwrappingFailed(recipient, abi.encodeWithSignature("Error(string)", "test withdraw error"));
-    emit TransferWrappedFailed(recipient, abi.encodeWithSignature("fallback transfer of wrapped asset failed"));
-
-    vm.prank(MOCK_CONNEXT);
-    unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
-  }
-
-  function test_Unwrapper__xReceive_emitsEventifWithdrawSucceedsAndSendETHFails() public {
-    address asset = MOCK_WRAPPER;
-    uint256 amount = 10 ether;
-
-    vm.expectEmit(true, true, true, true);
-    emit SendUnwrappedFailed(recipient, "unwrap succeeded but sending unwrapped asset to recipient failed");
+    emit WrongAsset(recipient, asset);
 
     vm.prank(MOCK_CONNEXT);
     unwrapper.xReceive(transferId, amount, asset, originSender, origin, callDataWithRecipient);
