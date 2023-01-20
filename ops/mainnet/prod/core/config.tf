600dcc290f79c10cd7e1d2c69f9d70a37325a098
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-01-20 18:24:05 +0800

    remove alchemy optimism rpc

--- ops/mainnet/prod/core/config.tf
+++ ops/mainnet/prod/core/config.tf
@@ -433,7 +433,7 @@ locals {
         }]
       },
       "1869640809" = {
-        providers = ["https://opt-mainnet.g.alchemy.com/v2/${var.optimism_alchemy_key_0}", "https://opt-mainnet.g.alchemy.com/v2/${var.optimism_alchemy_key_1}", "https://1rpc.io/op", "https://mainnet.optimism.io", "https://rpc.ankr.com/optimism"]
+        providers = ["https://endpoints.omniatech.io/v1/op/mainnet/public", "https://1rpc.io/op", "https://mainnet.optimism.io", "https://rpc.ankr.com/optimism"]
         assets = [{
           name    = "USDC"
           address = "0x85FB8e2903Ad92A2ab0C6a725806636666ee2Ab4"
