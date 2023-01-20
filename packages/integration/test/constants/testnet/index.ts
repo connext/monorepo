1fe8eb254e3656ffc7750c1caea9636bc6a908fe
wanglonghong <wanglonghong4467@gmail.com>
2023-01-20 10:10:24 +0800

    Revert "fix integration config"
    This reverts commit e88dfe9057f661a51f739f7fb0a937467a857ce0.

--- packages/integration/test/constants/testnet/index.ts
+++ packages/integration/test/constants/testnet/index.ts
@@ -335,7 +335,6 @@ export const SEQUENCER_CONFIG: Promise<SequencerConfig> = (async (): Promise<Seq
       url: "postgres://postgres:qwerty@localhost:5432/connext?sslmode=disable",
     },
     relayerFeeTolerance: 20,
-    excludeListFromRelayerFee: [],
   };
 })();
 
