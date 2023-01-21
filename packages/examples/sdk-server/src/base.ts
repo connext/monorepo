9f0c5556544d4976930e65e4e2fedd6e4d057690
just-a-node <eye1717@gmail.com>
2023-01-20 23:25:36 -0700

    Add bumpTransfer endpt

--- packages/examples/sdk-server/src/base.ts
+++ packages/examples/sdk-server/src/base.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { Static, Type } from "@sinclair/typebox";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
-import { NxtpSdkBase, SdkXCallParamsSchema, SdkXCallParams } from "@connext/nxtp-sdk";
+import { NxtpSdkBase, SdkXCallParamsSchema, SdkXCallParams, SdkBumpTransferParamsSchema } from "@connext/nxtp-sdk";
 import { TIntegerString } from "@connext/nxtp-utils";
 
 import { approveIfNeededSchema, getCanonicalTokenIdSchema, calculateCanonicalKeySchema } from "./types/api";
@@ -53,6 +53,19 @@ export const baseRoutes = async (server: FastifyInstance, sdkBaseInstance: NxtpS
     async (request, reply) => {
       const { domainId, assetId, amount, infiniteApprove } = request.body;
       const txReq = await sdkBaseInstance.approveIfNeeded(domainId, assetId, amount, infiniteApprove);
+      reply.status(200).send(txReq);
+    },
+  );
+
+  s.post(
+    "/bumpTransfer",
+    {
+      schema: {
+        body: SdkBumpTransferParamsSchema,
+      },
+    },
+    async (request, reply) => {
+      const txReq = await sdkBaseInstance.bumpTransfer(request.body);
       reply.status(200).send(txReq);
     },
   );
