698521ca2273fc6f90c175ef779be7dfbd7a9e88
Rahul Sethuram <rahul@sethuram.com>
2023-01-23 10:03:47 +0400

    Use Gelato relayer in processFromRoot

--- packages/deployments/contracts/src/typechain-types/contracts/shared/libraries/index.ts
+++ packages/deployments/contracts/src/typechain-types/contracts/shared/libraries/index.ts
@@ -3,7 +3,4 @@
 /* eslint-disable */
 import type * as multisendSol from "./Multisend.sol";
 export type { multisendSol };
-import type * as unwrapperSol from "./Unwrapper.sol";
-export type { unwrapperSol };
-export type { Orphanage } from "./Orphanage";
 export type { TypedMemView } from "./TypedMemView";
