082fca6bb3ce5190afd5b89ec71182e230f0ead7
preethamr <preethamr@gmail.com>
2023-01-22 23:43:34 -0800

    Disable Sequencer dd-trace

--- packages/agents/sequencer/src/index.ts
+++ packages/agents/sequencer/src/index.ts
@@ -1,9 +1,6 @@
-import tracer from "dd-trace";
-
 import { makePublisher, makeSubscriber, makeSequencer } from "./make";
 
 export { SequencerConfig, ChainConfig } from "./lib/entities";
 export { makePublisher, makeSubscriber, makeSequencer };
 
-tracer.init({ profiling: true, runtimeMetrics: true });
 makeSequencer();
