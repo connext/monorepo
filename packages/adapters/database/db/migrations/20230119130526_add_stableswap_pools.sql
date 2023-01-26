59d7ad5b468b93adced049ce5d2fb3521a18371f
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-01-26 16:50:54 +0800

    daily swap volume schema

--- packages/adapters/database/db/migrations/20230119130526_add_stableswap_pools.sql
+++ packages/adapters/database/db/migrations/20230119130526_add_stableswap_pools.sql
@@ -48,6 +48,20 @@ CREATE  OR REPLACE VIEW public.hourly_swap_volume AS (
     FROM public.stableswap_exchanges swap
     GROUP BY 1,2,3
     );
+
+CREATE  OR REPLACE VIEW public.daily_swap_volume AS ( 
+    SELECT 
+        swap.pool_id,
+        swap.domain,
+        date_trunc('day', to_timestamp(swap.timestamp))::date AS swap_day,
+        SUM((swap.tokens_sold
+         + swap.tokens_bought) / 2) As volume,
+        COUNT(
+            swap.pool_id
+        ) AS swap_count
+    FROM public.stableswap_exchanges swap
+    GROUP BY 1,2,3
+    );
               
 
 GRANT SELECT ON public.stableswap_pools to query;
@@ -56,9 +70,12 @@ GRANT SELECT ON public.stableswap_exchanges to query;
 GRANT SELECT ON public.stableswap_exchanges to reader;
 GRANT SELECT ON public.hourly_swap_volume to query;
 GRANT SELECT ON public.hourly_swap_volume to reader;
+GRANT SELECT ON public.daily_swap_volume to query;
+GRANT SELECT ON public.daily_swap_volume to reader;
 
 
 -- migrate:down
 DROP TABLE IF EXISTS public.stableswap_pools;
 DROP TABLE IF EXISTS public.stableswap_exchanges;
 DROP TABLE IF EXISTS public.hourly_swap_volume;
+DROP TABLE IF EXISTS public.daily_swap_volume;
