8accfc50c4972dcb8919eb4916e9359acbffcf63
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-01-27 10:30:05 +0800

    Update queries.ts

--- packages/adapters/subgraph/src/lib/operations/queries.ts
+++ packages/adapters/subgraph/src/lib/operations/queries.ts
@@ -874,7 +874,7 @@ export const getStableSwapPoolsQuery = (domain: string) => {
   const prefix = config.sources[domain].prefix;
   const queryString = `
   ${prefix}_stableSwaps ( 
-    first: 100
+    first: 1000
   ) {
     ${STABLESWAP_POOL_ENTITY}
   }`;
