76d66875474297b3ef9e57181e25bb6eb5871132
wanglonghong <wanglonghong4467@gmail.com>
2023-01-31 23:26:40 +0800

    nit

--- packages/agents/sdk/src/lib/helpers/permit/index.ts
+++ packages/agents/sdk/src/lib/helpers/permit/index.ts
@@ -2,5 +2,5 @@ import { createTypedData } from "./shared";
 
 export const createTypedDataFns: Record<string, any> = {
   nextUSDC: createTypedData,
-  usdc: createTypedData,
+  USDC: createTypedData,
 };
