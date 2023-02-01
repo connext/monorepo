054bb00913c5d4f2d08be518e3139e0398a3dc9b
Rahul Sethuram <rahul@sethuram.com>
2023-02-01 15:24:54 +0400

    Add retries by default

--- packages/utils/src/helpers/axios.ts
+++ packages/utils/src/helpers/axios.ts
@@ -2,6 +2,8 @@ import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 
 import { jsonifyError, NxtpError } from "../types";
 
+import { delay } from "./time";
+
 export class AxiosQueryError extends NxtpError {
   constructor(url: string, method: "get" | "post", data: any, errorObj: any) {
     super(`Error sending axios request to url ${url}`, { url, data, method, error: errorObj }, AxiosQueryError.name);
@@ -12,29 +14,46 @@ export const axiosPost = async <T = any, R = AxiosResponse<T>, D = any>(
   url: string,
   data?: D,
   config?: AxiosRequestConfig<D>,
+  numAttempts = 5,
+  retryDelay = 2000,
 ): Promise<R> => {
-  try {
-    const response = await axios.post<T, R, D>(url, data, config);
-    return response;
-  } catch (error: unknown) {
-    // eslint-disable-next-line import/no-named-as-default-member
-    if (axios.isAxiosError(error)) {
-      throw new AxiosQueryError(url, "post", data, { error: error.toJSON(), status: error.response?.status });
+  let error;
+  for (let i = 0; i < numAttempts; i++) {
+    try {
+      const response = await axios.post<T, R, D>(url, data, config);
+      return response;
+    } catch (err: unknown) {
+      // eslint-disable-next-line import/no-named-as-default-member
+      if (axios.isAxiosError(err)) {
+        error = { error: err.toJSON(), status: err.response?.status };
+      }
+      error = jsonifyError(err as NxtpError);
     }
-    throw new AxiosQueryError(url, "post", data, jsonifyError(error as NxtpError));
+    await delay(retryDelay);
   }
+  throw new AxiosQueryError(url, "post", data, error);
 };
 
-export const axiosGet = async <T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D): Promise<R> => {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const response = await axios.get<T, R, D>(url, data as any);
-    return response;
-  } catch (error: unknown) {
-    // eslint-disable-next-line import/no-named-as-default-member
-    if (axios.isAxiosError(error)) {
-      throw new AxiosQueryError(url, "get", data, { error: error.toJSON(), status: error.response?.status });
+export const axiosGet = async <T = any, R = AxiosResponse<T>, D = any>(
+  url: string,
+  data?: D,
+  numAttempts = 5,
+  retryDelay = 2000,
+): Promise<R> => {
+  let error;
+  for (let i = 0; i < numAttempts; i++) {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const response = await axios.get<T, R, D>(url, data as any);
+      return response;
+    } catch (err: unknown) {
+      // eslint-disable-next-line import/no-named-as-default-member
+      if (axios.isAxiosError(err)) {
+        error = { error: err.toJSON(), status: err.response?.status };
+      }
+      error = jsonifyError(err as NxtpError);
     }
-    throw new AxiosQueryError(url, "get", data, jsonifyError(error as NxtpError));
+    await delay(retryDelay);
   }
+  throw new AxiosQueryError(url, "get", data, error);
 };
