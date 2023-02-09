29f61bb51e310162caf07e4db7b6ad23b1beb488
Jake Kidd <jcb.kdd@gmail.com>
2023-02-09 15:13:31 -0800

    re-org and naming nits; stub for upgrading in motherforker

---
+++ packages/deployments/contracts/contracts_forge/upgrade/BridgeFacet.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+import {MotherForker} from "../utils/MotherForker.sol";
+
+contract BridgeFacetUpgradeTest is MotherForker {
+  // TODO: Test upgraded forked deployment of Connext here to make sure fast and slow paths are working!
+}
---
+++ packages/deployments/contracts/contracts_forge/utils/MotherForker.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+import {ForgeHelper} from "../utils/ForgeHelper.sol";
+
+/**
+ * @notice A 'ForkHelper' used to initialize all the applicable forks (found in foundry.toml).
+ * @dev Should be inherited and used in the `upgrade` suite of tests to basically test upgradability
+ * of live contracts.
+ */
+abstract contract MotherForker is ForgeHelper {
+  // Hardcode the networks to participate in (should match foundry.toml)
+  string[2][] public NETWORKS; // [[name, rpc], [name, rpc], ...]
+  uint256[] public FORK_IDS;
+
+  // ============ Test set up ============
+  function setUp() public {
+    createForks();
+  }
+
+  // ============ Utils ==================
+  // Create a fork for each network.
+  function utils_createForks() internal {
+    NETWORKS = vm.rpcUrls();
+    for (uint256 i; i < NETWORKS.length; i++) {
+      FORK_IDS[i] = vm.createFork(NETWORKS[i][1]);
+    }
+  }
+
+  function utils_upgradeDiamond(address diamond) internal {
+    // TODO: Need a pre-generated diamond cut proposal - one that will be different than on-chain - that we're
+    // testing here. Should come from config?
+    // TODO: Should submit a pre-generated diamond cut proposal to the Connext Diamond, zoom forward 7 days,
+    // and then accept the proposal.
+  }
+}
