f2fbd48f6799c640c4bb7c2ddc5e202f2de9932b
Jake Kidd <jcb.kdd@gmail.com>
2023-02-13 12:56:18 -0800

    get connext address from env

--- packages/deployments/contracts/contracts_forge/utils/MotherForker.sol
+++ packages/deployments/contracts/contracts_forge/utils/MotherForker.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
+
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {ForgeHelper} from "../utils/ForgeHelper.sol";
 
 /**
@@ -12,10 +14,11 @@ abstract contract MotherForker is ForgeHelper {
   string[2][] public NETWORKS; // [[name, rpc], [name, rpc], ...]
   uint256[] public FORK_IDS;
 
+  mapping(uint256 => address) public connextAddressByFork;
+
   // ============ Test set up ============
   function setUp() public {
     utils_createForks();
-    utils_assertCanSelectAllForks();
   }
 
   // ============ Utils ==================
@@ -23,7 +26,15 @@ abstract contract MotherForker is ForgeHelper {
   function utils_createForks() internal {
     NETWORKS = vm.rpcUrls();
     for (uint256 i; i < NETWORKS.length; i++) {
-      FORK_IDS[i] = vm.createFork(NETWORKS[i][1]);
+      uint256 forkId = vm.createSelectFork(NETWORKS[i][1]);
+      FORK_IDS[i] = forkId;
+      // Assert that the fork is selectable (has been selected above).
+      assertEq(vm.activeFork(), forkId);
+
+      // Form the key we'll use to identify the env vars needed.
+      string memory key = Strings.toString(block.chainid);
+      // Get address of the connext diamond on the fork.
+      connextAddressByFork[forkId] = cheats.envAddress(string.concat("CONNEXT_ADDR_", key));
     }
   }
 
@@ -32,14 +43,5 @@ abstract contract MotherForker is ForgeHelper {
     // testing here. Should come from config?
     // TODO: Should submit a pre-generated diamond cut proposal to the Connext Diamond, zoom forward 7 days,
     // and then accept the proposal.
-  }
-
-  function utils_assertCanSelectAllForks() public {
-    // Loop through all the created forks and ensure we can select them.
-    for (uint256 i; i < FORK_IDS.length; i++) {
-      uint256 forkId = FORK_IDS[i];
-      vm.selectFork(forkId);
-      assertEq(vm.activeFork(), forkId);
-    }
   }
 }
