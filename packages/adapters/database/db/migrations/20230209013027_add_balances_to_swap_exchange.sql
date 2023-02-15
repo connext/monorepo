6b0d4e6bdf9b3b036150b85568b69e557992ac3e
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-02-15 22:37:07 +0800

    add fee to db - schema

--- packages/adapters/database/db/migrations/20230209013027_add_balances_to_swap_exchange.sql
+++ packages/adapters/database/db/migrations/20230209013027_add_balances_to_swap_exchange.sql
@@ -3,4 +3,4 @@ ALTER TABLE public.stableswap_exchanges
 ADD COLUMN IF NOT EXISTS balances numeric [] DEFAULT array[]::numeric[] NOT NULL;
 
 -- migrate:down
-
+ALTER TABLE root_messages DROP COLUMN balances;
