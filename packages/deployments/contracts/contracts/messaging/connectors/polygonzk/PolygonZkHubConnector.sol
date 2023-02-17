dd02334ccb6def9879ebf7212b3438614f06d76b
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-02-17 14:27:32 +0800

    polygon zkevm connector draft

---
+++ packages/deployments/contracts/contracts/messaging/connectors/polygonzk/PolygonZkHubConnector.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.8.17;
+
+import {IRootManager} from "../../interfaces/IRootManager.sol";
+
+import {HubConnector, Connector} from "../HubConnector.sol";
+
+import {BasePolygonZk} from "./BasePolygonZk.sol";
+
+contract PolygonZkHubConnector is HubConnector, BasePolygonZk {
+  // ============ Constructor ============
+  constructor(
+    uint32 _domain,
+    uint32 _mirrorDomain,
+    address _amb,
+    address _rootManager,
+    address _mirrorConnector,
+    uint32 _mirrorNetworkId
+  ) HubConnector(_domain, _mirrorDomain, _amb, _rootManager, _mirrorConnector) BasePolygonZk(_mirrorNetworkId) {}
+
+  // ============ Private fns ============
+  function _verifySender(address _expected) internal view override returns (bool) {
+    require(msg.sender == AMB, "!amb");
+    return _expected == mirrorConnector;
+  }
+
+  /**
+   * @dev Handles an incoming `outboundRoot`
+   */
+  function _processMessageFrom(address sender, bytes memory _data) internal override(BasePolygonZk) {
+    require(_verifySender(sender), "!l2Connector");
+
+    // set the outbound root for mirror domain
+    IRootManager(ROOT_MANAGER).aggregate(MIRROR_DOMAIN, bytes32(_data));
+  }
+
+  function _sendMessage(bytes memory _data, bytes memory _encodedData) internal override {
+    _sendMessage(AMB, mirrorConnector, _data, _encodedData);
+  }
+}
---
+++ packages/deployments/contracts/contracts/messaging/interfaces/ambs/polygonzk/IBridgeMessageReceiver.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.8.17;
+
+/**
+ * @dev Define interface for PolygonZkEVM Bridge message receiver
+ */
+interface IBridgeMessageReceiver {
+  function onMessageReceived(
+    address originAddress,
+    uint32 originNetwork,
+    bytes memory data
+  ) external returns (bool);
+}
