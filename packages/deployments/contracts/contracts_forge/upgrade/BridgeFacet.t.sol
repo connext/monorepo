9a3ab88f0506d859288366d8bb7205d8e43ca104
Layne Haber <layne.haber@gmail.com>
2023-02-24 11:21:44 -0700

    feat: invoke propose script from foundry

--- packages/deployments/contracts/contracts_forge/upgrade/BridgeFacet.t.sol
+++ packages/deployments/contracts/contracts_forge/upgrade/BridgeFacet.t.sol
@@ -5,11 +5,15 @@ import {MotherForker} from "../utils/MotherForker.sol";
 contract BridgeFacetUpgradeTest is MotherForker {
   // TODO: Test upgraded forked deployment of Connext here to make sure fast and slow paths are working!
 
+  function setUp() public {}
+
   function test_fastPath() public {
-    // Loop through all forks as sending chains.
-    for (uint256 i; i < FORK_IDS.length; i++) {
-      uint256 forkId = FORK_IDS[i];
-      vm.selectFork(forkId);
-    }
+    // // Loop through all forks as sending chains.
+    // for (uint256 i; i < FORK_IDS.length; i++) {
+    //   uint256 forkId = FORK_IDS[i];
+    //   vm.selectFork(forkId);
+    // }
+
+    utils_generateProposalFile();
   }
 }
--- packages/deployments/contracts/contracts_forge/utils/MotherForker.sol
+++ packages/deployments/contracts/contracts_forge/utils/MotherForker.sol
@@ -21,31 +21,49 @@ abstract contract MotherForker is ForgeHelper {
 
   mapping(uint256 => ForkInfo) public forkInfo;
 
-  // ============ Test set up ============
-  function setUp() public {
-    utils_createForks();
-  }
-
   // ============ Utils ==================
   // Create a fork for each network.
   function utils_createForks() internal {
-    NETWORKS = vm.rpcUrls();
-    for (uint256 i; i < NETWORKS.length; i++) {
-      uint256 forkId = vm.createSelectFork(NETWORKS[i][1]);
-      FORK_IDS[i] = forkId;
-      // Assert that the fork is selectable (has been selected above).
-      assertEq(vm.activeFork(), forkId);
+    // TODO: .rpcUrls() is not working
+    // NETWORKS = vm.rpcUrls();
+    // for (uint256 i; i < NETWORKS.length; i++) {
+    //   uint256 forkId = vm.createSelectFork(NETWORKS[i][1]);
+    //   FORK_IDS[i] = forkId;
+    //   // Assert that the fork is selectable (has been selected above).
+    //   assertEq(vm.activeFork(), forkId);
+    //   // Form the key we'll use to identify the env vars needed.
+    //   string memory key = Strings.toString(block.chainid);
+    //   // Get address of the connext diamond on the fork.
+    //   address connext = vm.envAddress(string.concat("CONNEXT_", key));
+    //   string memory delimiter = ",";
+    //   address[] memory assets = vm.envAddress(string.concat("ASSETS_", key), delimiter);
+    //   forkInfo[forkId] = ForkInfo({connext: connext, assets: assets});
+    // }
+  }
 
-      // Form the key we'll use to identify the env vars needed.
-      string memory key = Strings.toString(block.chainid);
-      // Get address of the connext diamond on the fork.
-      address connext = vm.envAddress(string.concat("CONNEXT_", key));
-
-      string memory delimiter = ",";
-      address[] memory assets = vm.envAddress(string.concat("ASSETS_", key), delimiter);
-
-      forkInfo[forkId] = ForkInfo({connext: connext, assets: assets});
-    }
+  function utils_generateProposalFile() internal {
+    // generate the diamond cut proposal by running `propose` via cli:
+    // yarn workspace @connext/smart-contracts propose --env "production" --chains 1 10 56 100 137 42161
+    string[] memory args = new string[](18);
+    args[0] = "yarn";
+    args[1] = "workspace";
+    args[2] = "@connext/smart-contracts";
+    args[3] = "propose";
+    args[4] = "--env";
+    args[5] = "production";
+    args[6] = "--chains";
+    args[7] = "1";
+    args[8] = "--chains";
+    args[9] = "10";
+    args[10] = "--chains";
+    args[11] = "56";
+    args[12] = "--chains";
+    args[13] = "100";
+    args[14] = "--chains";
+    args[15] = "137";
+    args[16] = "--chains";
+    args[17] = "42161";
+    vm.ffi(args);
   }
 
   function utils_upgradeDiamond(address diamond) internal {
