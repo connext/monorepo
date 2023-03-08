7d82039ae6ba097b67544bef636932c4cc198da4
sanchaymittal <sanchaymittal@gmail.com>
2023-03-08 12:30:34 +0700

    graph client update

--- packages/adapters/subgraph/.graphclient/sources/Connext_Optimism/types.ts
+++ packages/adapters/subgraph/.graphclient/sources/Connext_Optimism/types.ts
@@ -68,6 +68,7 @@ export type optimism_AggregateRoot_orderBy =
 export type optimism_Asset = {
   id: Scalars['ID'];
   key?: Maybe<Scalars['optimism_Bytes']>;
+  decimal?: Maybe<Scalars['BigInt']>;
   canonicalId?: Maybe<Scalars['optimism_Bytes']>;
   canonicalDomain?: Maybe<Scalars['BigInt']>;
   adoptedAsset?: Maybe<Scalars['optimism_Bytes']>;
@@ -200,6 +201,7 @@ export type optimism_AssetBalance_orderBy =
   | 'asset'
   | 'asset__id'
   | 'asset__key'
+  | 'asset__decimal'
   | 'asset__canonicalId'
   | 'asset__canonicalDomain'
   | 'asset__adoptedAsset'
@@ -254,6 +256,14 @@ export type optimism_Asset_filter = {
   key_not_in?: InputMaybe<Array<Scalars['optimism_Bytes']>>;
   key_contains?: InputMaybe<Scalars['optimism_Bytes']>;
   key_not_contains?: InputMaybe<Scalars['optimism_Bytes']>;
+  decimal?: InputMaybe<Scalars['BigInt']>;
+  decimal_not?: InputMaybe<Scalars['BigInt']>;
+  decimal_gt?: InputMaybe<Scalars['BigInt']>;
+  decimal_lt?: InputMaybe<Scalars['BigInt']>;
+  decimal_gte?: InputMaybe<Scalars['BigInt']>;
+  decimal_lte?: InputMaybe<Scalars['BigInt']>;
+  decimal_in?: InputMaybe<Array<Scalars['BigInt']>>;
+  decimal_not_in?: InputMaybe<Array<Scalars['BigInt']>>;
   canonicalId?: InputMaybe<Scalars['optimism_Bytes']>;
   canonicalId_not?: InputMaybe<Scalars['optimism_Bytes']>;
   canonicalId_gt?: InputMaybe<Scalars['optimism_Bytes']>;
@@ -330,6 +340,7 @@ export type optimism_Asset_filter = {
 export type optimism_Asset_orderBy =
   | 'id'
   | 'key'
+  | 'decimal'
   | 'canonicalId'
   | 'canonicalDomain'
   | 'adoptedAsset'
@@ -821,6 +832,7 @@ export type optimism_DestinationTransfer_orderBy =
   | 'asset'
   | 'asset__id'
   | 'asset__key'
+  | 'asset__decimal'
   | 'asset__canonicalId'
   | 'asset__canonicalDomain'
   | 'asset__adoptedAsset'
@@ -1327,6 +1339,7 @@ export type optimism_OriginTransfer_orderBy =
   | 'asset'
   | 'asset__id'
   | 'asset__key'
+  | 'asset__decimal'
   | 'asset__canonicalId'
   | 'asset__canonicalDomain'
   | 'asset__adoptedAsset'
@@ -2162,6 +2175,7 @@ export type optimism_RouterDailyTVL_orderBy =
   | 'asset'
   | 'asset__id'
   | 'asset__key'
+  | 'asset__decimal'
   | 'asset__canonicalId'
   | 'asset__canonicalDomain'
   | 'asset__adoptedAsset'
