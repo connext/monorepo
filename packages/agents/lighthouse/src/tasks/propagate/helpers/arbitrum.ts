9f60f2fe01d36fe96e82b288e2d5c4d9a5968818
Rahul Sethuram <rahul@sethuram.com>
2023-03-11 15:44:28 +0400

    Bump gas levels to see

--- packages/agents/lighthouse/src/tasks/propagate/helpers/arbitrum.ts
+++ packages/agents/lighthouse/src/tasks/propagate/helpers/arbitrum.ts
@@ -83,7 +83,7 @@ export const getPropagateParams = async (
       l2SpokeConnector.address,
       l1Provider,
     );
-    const gasLimitForAutoRedeem = L1ToL2MessageGasParams.gasLimit.mul(5);
+    const gasLimitForAutoRedeem = L1ToL2MessageGasParams.gasLimit.mul(15);
     logger.info(`Got message gas params`, requestContext, methodContext, {
       maxFeePerGas: L1ToL2MessageGasParams.maxFeePerGas.toString(),
       maxSubmissionCost: L1ToL2MessageGasParams.maxSubmissionFee.toString(),
@@ -91,7 +91,7 @@ export const getPropagateParams = async (
       gasLimitForAutoRedeem: gasLimitForAutoRedeem.toString(),
     });
 
-    submissionPriceWei = L1ToL2MessageGasParams.maxSubmissionFee.mul(5).toString();
+    submissionPriceWei = L1ToL2MessageGasParams.maxSubmissionFee.mul(10).toString();
     // multiply gasLimit by 15 to be successful in auto-redeem
     maxGas = gasLimitForAutoRedeem.toString();
     callValue = BigNumber.from(submissionPriceWei).add(gasPriceBid.mul(maxGas)).toString();
