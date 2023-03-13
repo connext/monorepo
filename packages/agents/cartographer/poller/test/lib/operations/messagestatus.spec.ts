c5179618c6046005ddea0a8792ff67b6bcb00d68
wanglonghong <wanglonghong4467@gmail.com>
2023-03-13 13:46:14 +0800

    mvp for message status unit test

---
+++ packages/agents/cartographer/poller/test/lib/operations/messagestatus.spec.ts
@@ -0,0 +1,11 @@
+import { SinonStub } from "sinon";
+import { expect } from "@connext/nxtp-utils";
+import { mockDestinationSubgraphResponse, mockOriginSubgraphResponse } from "@connext/nxtp-adapters-subgraph/test/mock";
+
+import { mockContext } from "../../globalTestHook";
+import { updateBackoffs, updateTransfers } from "../../../src/lib/operations/transfers";
+
+describe("MessageStatus operations", () => {
+  describe("#updateMessageStatus", () => {});
+  describe("#getMessageStatus", () => {});
+});
