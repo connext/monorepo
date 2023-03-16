e5d4e3ea58276f6ed2e47f51f7e21ee464530978
Layne Haber <layne.haber@gmail.com>
2023-03-16 09:50:19 -0600

    refactor: complete implementation

--- packages/deployments/contracts/contracts/core/xreceivers/ConnextPoolLiquidity.sol
+++ packages/deployments/contracts/contracts/core/xreceivers/ConnextPoolLiquidity.sol
@@ -7,6 +7,8 @@ import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 import {IXReceiver} from "../connext/interfaces/IXReceiver.sol";
 import {IConnext} from "../connext/interfaces/IConnext.sol";
+import {TokenId} from "../connext/libraries/TokenId.sol";
+import {AssetLogic} from "../connext/libraries/AssetLogic.sol";
 
 contract ConnextPoolLiquidity is IXReceiver {
   // ============ Libraries ============
@@ -15,7 +17,23 @@ contract ConnextPoolLiquidity is IXReceiver {
 
   // ============ Events ============
 
-  event CrossLiquidityAdded(bytes32 transferId, address lp, uint256 received, uint256 deposited, address recipient);
+  /**
+   * @notice Emitted when liquidity is added to a pool
+   * @param transferId The transfer id
+   * @param recipient The recipient of the lp tokens
+   * @param lp The address of the lp token
+   * @param received The amount of lp tokens received
+   * @param deposited The address of token deposited
+   * @param depositedAmount The amount of tokens deposited
+   */
+  event CrossLiquidityAdded(
+    bytes32 indexed transferId,
+    address indexed recipient,
+    address indexed lp,
+    uint256 received,
+    address deposited,
+    uint256 depositedAmount
+  );
 
   // ============ Constants ============
 
@@ -24,10 +42,6 @@ contract ConnextPoolLiquidity is IXReceiver {
    * @notice The connext address on this domain
    */
   IConnext public connext;
-
-  /**
-   * @notice Get the LP token
-   */
 
   // ============= Modifiers ==============
 
@@ -46,6 +60,15 @@ contract ConnextPoolLiquidity is IXReceiver {
 
   // ============ External Functions ============
 
+  /**
+   * @notice Called by the connext bridge to add liquidity to a pool
+   * @dev Will add single-sided liquidity in the asset associated with the transfer.
+   * @param _transferId The transfer id
+   * @param _amount The amount of tokens to add to LP pool
+   * @param _asset The asset to add to the LP pool
+   * @param _callData The encoded data for the transfer, should be the encoded recipient
+   * of LP tokens on this chain.
+   */
   function xReceive(
     bytes32 _transferId,
     uint256 _amount,
@@ -53,36 +76,89 @@ contract ConnextPoolLiquidity is IXReceiver {
     address, //_originSender,
     uint32, // _origin,
     bytes memory _callData
-  ) external override onlyConnext returns (bytes memory) {
+  ) external override onlyConnext returns (bytes memory ret) {
     // Sanity check: recipient in calldata is nonzero
     address recipient = abi.decode(_callData, (address));
     require(recipient != address(0), "!recipient");
 
-    // 1. Calculate min amount to mint
-    bytes32 key = bytes32(0); // the hash of canonical token data
-    uint256[] memory amounts = new uint256[](2); // TODO: best way to derive from pool tokens
-    amounts[0] = _amount; // [local, adopted]
-    amounts[1] = 0;
+    // Sanity check: _amount is nonzero
+    require(_amount > 0, "!amount");
 
-    // 2. Get the LP token
+    // Sanity check: _asset is nonzero
+    require(_asset != address(0), "!asset");
+
+    // Get the canonical identifier for the pool
+    bytes32 key = getKey(_asset);
+
+    // Get starting lp token balance
     address lp = connext.getSwapLPToken(key);
-    uint256 start = IERC20(lp).balanceOf(address(this));
 
-    // 3. Approve the connext contract to spend the deposited token
-    IERC20(_asset).safeApprove(address(connext), _amount);
+    // Add liquidity and distribute LP tokens
+    uint256 received = addLiquidityAndDistributeLP(key, _amount, _asset, lp, recipient);
 
-    // 4. Call `connext.addLiquidity`
-    connext.addSwapLiquidity(key, amounts, connext.calculateSwapTokenAmount(key, amounts, true), block.timestamp + 1);
-
-    // 5. Send LP tokens to recipient in `_callData`
-    uint256 received = IERC20(lp).balanceOf(address(this)) - start;
-    IERC20(lp).safeTransfer(recipient, received);
-
-    // 6. Emit event
-    emit CrossLiquidityAdded(_transferId, lp, received, _amount, recipient);
+    // Emit event
+    emit CrossLiquidityAdded(_transferId, recipient, lp, received, _asset, _amount);
   }
 
-  // ============ Public Functions ============
-
   // ============ Private Functions ============
+
+  /**
+   * @notice Generates the amounts[] array for the connext.addLiquidity call
+   * @dev Inserts _amount at the index for _asset in the pools stored at key
+   */
+  function addLiquidityAndDistributeLP(
+    bytes32 _key,
+    uint256 _amount,
+    address _asset,
+    address _lp,
+    address _recipient
+  ) private returns (uint256) {
+    // Get starting lp token balance
+    uint256 start = IERC20(_lp).balanceOf(address(this));
+
+    // Generate amounts input
+    uint256[] memory amounts = generateAmounts(_key, _amount, _asset);
+
+    // Calculate the minimun to add
+    uint256 min = connext.calculateSwapTokenAmount(_key, amounts, true);
+
+    // Approve amount
+    IERC20(_asset).safeApprove(address(connext), _amount);
+
+    // Call connext.addLiquidity
+    connext.addSwapLiquidity(_key, amounts, min, block.timestamp + 1);
+
+    // Approve amount
+    IERC20(_asset).safeApprove(address(connext), 0);
+
+    // Send LP tokens to recipient in `_callData`
+    uint256 received = IERC20(_asset).balanceOf(address(this)) - start;
+    IERC20(_asset).safeTransfer(_recipient, received);
+    return received;
+  }
+
+  /**
+   * @notice Generates the amounts[] array for the connext.addLiquidity call
+   * @dev Inserts _amount at the index for _asset in the pools stored at key
+   */
+  function generateAmounts(bytes32 key, uint256 _amount, address _asset) private view returns (uint256[] memory) {
+    // Generate the amount array
+    uint256 amountsLen = connext.getSwapStorage(key).pooledTokens.length;
+    // TODO: send directly to recipient?
+    require(amountsLen > 0, "!pool");
+    uint256[] memory amounts = new uint256[](amountsLen);
+
+    // Insert _amount at the index for _asset
+    amounts[connext.getSwapTokenIndex(key, _asset)] = _amount;
+    return amounts;
+  }
+
+  /**
+   * @notice Get the hash of the canonical identifier for the asset
+   * @param _asset The asset to get the canonical identifier for
+   */
+  function getKey(address _asset) private view returns (bytes32) {
+    TokenId memory id = connext.getTokenId(_asset);
+    return AssetLogic.calculateCanonicalHash(id.id, id.domain);
+  }
 }
