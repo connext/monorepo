b3bc51f198d555040d223c7429c96dceb2d6199a
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-03-22 00:39:32 +0800

    update deploy config

--- packages/deployments/contracts/deployConfig/shared.ts
+++ packages/deployments/contracts/deployConfig/shared.ts
@@ -131,8 +131,6 @@ export const MESSAGING_PROTOCOL_CONFIGS: Record<string, MessagingProtocolConfig>
           hub: {
             // https://goerli.etherscan.io/address/0x5b47E1A08Ea6d985D6649300584e6722Ec4B1383#code
             optimismPortal: "0x5b47E1A08Ea6d985D6649300584e6722Ec4B1383",
-            // https://goerli.etherscan.io/address/0xE6Dfba0953616Bacab0c9A8ecb3a9BBa77FC15c0
-            l2OutputOracle: "0xE6Dfba0953616Bacab0c9A8ecb3a9BBa77FC15c0",
           },
         },
       },
@@ -314,8 +312,6 @@ export const MESSAGING_PROTOCOL_CONFIGS: Record<string, MessagingProtocolConfig>
             // TODO: should be updated to mainnet
             // https://goerli.etherscan.io/address/0x5b47E1A08Ea6d985D6649300584e6722Ec4B1383#code
             optimismPortal: "0x5b47E1A08Ea6d985D6649300584e6722Ec4B1383",
-            // https://goerli.etherscan.io/address/0xE6Dfba0953616Bacab0c9A8ecb3a9BBa77FC15c0
-            l2OutputOracle: "0xE6Dfba0953616Bacab0c9A8ecb3a9BBa77FC15c0",
             gasCap: DEFAULT_PROCESS_GAS,
           },
           spoke: {
