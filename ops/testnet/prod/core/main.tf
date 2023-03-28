9b24950719a3d73f501230f19d47205e1df71d37
Rahul Sethuram <rahul@sethuram.com>
2023-03-28 10:12:51 +0400

    Propagate every 30 min

--- ops/testnet/prod/core/main.tf
+++ ops/testnet/prod/core/main.tf
@@ -287,7 +287,7 @@ module "lighthouse_propagate_cron" {
   environment         = var.environment
   stage               = var.stage
   container_env_vars  = merge(local.lighthouse_env_vars, { LIGHTHOUSE_SERVICE = "propagate" })
-  schedule_expression = "rate(5 minutes)"
+  schedule_expression = "rate(30 minutes)"
 }
 
 module "lighthouse_sendoutboundroot_cron" {
