dd462d2673ca6bb4fd5886abb9811fef12c60903
Rahul Sethuram <rahul@connext.network>
2023-04-17 13:28:40 +0800

    Testnet prod (#3993)
    * Testnet sync (#3990)
    
    * fix: upgraded docs to latest (#3980)
    
    * fix: upgraded docs to latest
    
    * fix: added requested changes
    
    ---------
    
    Co-authored-by: Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
    
    * feat: mark based on leaf count (#3989)
    
    ---------
    
    Co-authored-by: Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
    Co-authored-by: Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
    Co-authored-by: wanglonghong <wanglonghong4467@gmail.com>
    
    * testnet sync (#3992)
    
    * fix: upgraded docs to latest (#3980)
    
    * fix: upgraded docs to latest
    
    * fix: added requested changes
    
    ---------
    
    Co-authored-by: Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
    
    * feat: mark based on leaf count (#3989)
    
    * fix: exclude null entities
    
    ---------
    
    Co-authored-by: Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
    Co-authored-by: Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
    
    ---------
    
    Co-authored-by: Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
    Co-authored-by: Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
    Co-authored-by: wanglonghong <wanglonghong4467@gmail.com>

--- packages/adapters/database/src/client.ts
+++ packages/adapters/database/src/client.ts
@@ -329,6 +329,7 @@ export const saveProcessedRootMessages = async (
         {
           spoke_domain,
           processed: true,
+          leaf_count: dc.isNotNull,
         },
         { limit: 1, order: { by: "leaf_count", direction: "DESC" } },
       )
