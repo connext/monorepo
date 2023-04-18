fb42b2cc497fd33a35f27119e98dfd19aab33630
wanglonghong <wanglonghong4467@gmail.com>
2023-04-18 16:57:48 +0800

    feat: still sketch

---
+++ packages/agents/chain-abstraction/src/origin/swap-and-xcall.ts
@@ -0,0 +1,20 @@
+import { providers } from "ethers";
+
+import { SwapAndXCallParams } from "./types";
+
+/**
+ * Prepares `SwapAndXCall` inputs and encodes the calldata. Returns `providers.TransactionRequest` object to be sent to the RPC provider.
+ *
+ * @param originDomain - The origin domain ID.
+ * @param destinationDomain - The destination domain ID.
+ * @param fromAsset - The address of the asset to swap from.
+ * @param toAsset - The address of the asset to swap to.
+ * @param amountIn - The number of `fromAsset` tokens.
+ * @param to - The address to send the asset and call with the calldata on the destination.
+ * @param delegate - (optional) The fallback address on the destination domain which defaults to `to`.
+ * @param slippage - (optional) Maximum acceptable slippage in BPS which defaults to 300. For example, a value of 300 means 3% slippage.
+ * @param calldata - (optional) Calldata to execute (can be empty: "0x").
+ */
+export const prepareSwapAndXCall = (params: SwapAndXCallParams): Promise<providers.TransactionRequest> => {
+  throw new Error("ToDo");
+};
---
+++ packages/agents/chain-abstraction/src/origin/types.ts
@@ -0,0 +1,16 @@
+import { Type, Static } from "@sinclair/typebox";
+import { TAddress, TIntegerString } from "@connext/nxtp-utils";
+
+export const SwapAndXCallParamsSchema = Type.Object({
+  originDomain: TIntegerString,
+  destinationDomain: TIntegerString,
+  fromAsset: TAddress,
+  toAsset: TAddress,
+  amountIn: TIntegerString,
+  to: Type.String(),
+  delegate: Type.Optional(TAddress),
+  slippage: Type.Optional(TIntegerString),
+  callData: Type.Optional(Type.String()),
+});
+
+export type SwapAndXCallParams = Static<typeof SwapAndXCallParamsSchema>;
