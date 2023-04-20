838609e87fffc65a7786e5dad5ec25b7edbf64ed
wanglonghong <wanglonghong4467@gmail.com>
2023-04-20 17:13:19 +0800

    refactor: combine swapper and swapdata

--- packages/agents/chain-abstraction/src/helpers/index.ts
+++ packages/agents/chain-abstraction/src/helpers/index.ts
@@ -1,3 +1,47 @@
 export * from "./abis";
-export * from "./swapper";
-export * from "./swapdata";
+import { Swapper } from "../types";
+
+import { SwapDataCallback, getSwapDataForOneInch } from "./swapdata";
+
+export const SwapperMapping: Record<string, { type: Swapper; address: string; swapdata: SwapDataCallback }> = {
+  "6648936": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // ETH mainnet
+  "1869640809": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // Optimism
+  "6450786": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // BNB Chain
+  "6778479": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // Gnosis Chain
+  "1886350457": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // Polygon
+  "1634886255": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // Arbitrum One
+  "2053862243": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // zkSync2 mainnet
+  "1887071085": {
+    type: Swapper.OneInch,
+    address: "",
+    swapdata: getSwapDataForOneInch,
+  }, // Polygon zkEVM
+};
