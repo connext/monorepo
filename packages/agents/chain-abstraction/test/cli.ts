2331ae229807e921fc644f48a5e7e4bed1b99d69
wanglonghong <wanglonghong4467@gmail.com>
2023-04-23 11:58:43 +0800

    test: integrate getPoolFeeForUniV3

--- packages/agents/chain-abstraction/test/cli.ts
+++ packages/agents/chain-abstraction/test/cli.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 import { Wallet, providers, Contract, BigNumber, constants } from "ethers";
-import { getXCallCallData, prepareSwapAndXCall } from "../src";
+import { getPoolFeeForUniV3, getXCallCallData, prepareSwapAndXCall } from "../src";
 import { DestinationCallDataParams, Swapper, XReceiveTarget } from "../src/types";
 config();
 
@@ -112,25 +112,29 @@ const testMidasProtocolTarget = async () => {
   const BNB_NATIVE = constants.AddressZero;
   const BNB_USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
   const amountIn = BigNumber.from("5000000000000000");
+  const fromAsset = BNB_NATIVE;
+  const toAsset = BNB_USDC;
 
   // DESTINATION SIDE
   const POLYGON_WETH = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+  const POLYGON_USDC = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
   const POLYGON_CTOKEN_WETH = "0xD809c769A04246855fee98423B180C7CCa6bF07c"; // https://app.midascapital.xyz/137/pool/5
-  const fromAsset = BNB_NATIVE;
-  const toAsset = BNB_USDC;
   const midasProtocolTarget = "0x5d7663c5483A46e7794b652aF8f155775E4F390C";
 
   // Params for calldata generation
   const POLYGON_DOMAIN_ID = "1886350457";
+  const POLYGON_RPC_URL = "https://polygon.llamarpc.com";
   const target = XReceiveTarget.MidasProtocol;
   const swapper = Swapper.UniV3;
+  const poolFee = await getPoolFeeForUniV3(POLYGON_DOMAIN_ID, POLYGON_RPC_URL, POLYGON_WETH, POLYGON_USDC);
+
   const params: DestinationCallDataParams = {
     fallback: signerAddress,
     swapForwarderData: {
       toAsset: POLYGON_WETH,
       swapData: {
         amountOutMin: "0",
-        poolFee: "500",
+        poolFee: poolFee,
       },
       forwardCallData: {
         cTokenAddress: POLYGON_CTOKEN_WETH,
