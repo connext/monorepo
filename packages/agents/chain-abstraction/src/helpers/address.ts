37466ada323f85b457cced9ed2c1455cab0ad9fe
wanglonghong <wanglonghong4467@gmail.com>
2023-04-24 21:09:57 +0800

    3971 chain abstraction package (#3994)
    * feat: directory sketch
    
    * feat: add chain-abstraction package to workspaces
    
    * feat: cleanup
    
    * feat: still sketch
    
    * feat: add more optional params
    
    * refactor: add todo functions
    
    * feat: swapData to todo
    
    * feat: still todo
    
    * feat: add steps for prepareSwapAndXCall
    
    * feat: mvp for prepareSwapAndXCall
    
    * feat: hardcoded abis
    
    * feat: docstring
    
    * feat: docstring
    
    * refactor: start with a simple solution
    
    * refactor: combine swapper and swapdata
    
    * feat: mvp of calculateRouteForSwapAndXCall
    
    * feat: this is the cross-chain slippage
    
    * feat: type definition done
    
    * feat: kick off getXcallCallData
    
    * feat: separate swapdata between origin and destination
    
    * feat: no need to make it async
    
    * feat: separate use
    
    * feat: origin, destination
    
    * feat: getXCallCallData
    
    * feat: swapData for destination
    
    * feat: clarify usecase
    
    * feat: export functions
    
    * feat: oneinch api
    
    * fix: prepareSwapAndXCall
    
    * Update package.json
    
    * fix: lint
    
    * test: kick off testing on polygon
    
    * test: swapAndXcall passed
    
    https://connextscan.io/tx/0x8e392a8af02e10a0d989d92c48922ec81515f68b44bf74b0634a5ea30f49ce81?src=search
    
    * test: swapAndXCall + relayerFeeInTransacting works
    
    https://connextscan.io/tx/0x0ea9db61a4a7ac8c4586d2f4acb0d524a43f2f3f60bf3c523b922e194b895418?src=search
    
    * feat: update SwapAndXCall deployment
    
    * feat: separate swapper on origin/destination
    
    * docs: wrong trace link
    
    * feat: support getting poolFee
    
    * Update index.ts
    
    * feat: create getPoolFeeForUniV3
    
    * feat: prepare to get liquidity
    
    * feat: sort by liquidity
    
    * test: integrate getPoolFeeForUniV3
    
    * fix: lint
    
    * feat: ci/cd ops

---
+++ packages/agents/chain-abstraction/src/helpers/address.ts
@@ -0,0 +1,12 @@
+export const DEPLOYED_ADDRESSES: Record<string, Record<string, string>> = {
+  swapandxcall: {
+    "6648936": "", // ETH mainnet
+    "1869640809": "", // Optimism
+    "6450786": "0x6840600Bc9B74fA6B3BDF1503cb31A7AdF061c1D", // BNB Chain
+    "6778479": "", // Gnosis Chain
+    "1886350457": "0x697075f4A3Ce358d125281134e98d594D8Bb472e", // Polygon
+    "1634886255": "", // Arbitrum One
+    "2053862243": "", // zkSync2 mainnet
+    "1887071085": "", // Polygon zkEVM
+  },
+};
---
+++ packages/agents/chain-abstraction/src/libs/destination/Mean.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns the forward calldata for the `MeanFinaceTarget` contract.
+ */
+export const generateForwardCallData = (_args: any): string => {
+  throw new Error("ToDo");
+};
