a14c3fa965511065a8fa4001a7af0295c8710124
Layne Haber <layne.haber@gmail.com>
2023-04-25 15:49:59 -0600

    WIP: testing setup

---
+++ packages/deployments/contracts/contracts_forge/core/connext/helpers/RelayerProxyHub.t.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "../../../utils/ForgeHelper.sol";
+import "../../../../contracts/core/connext/helpers/RelayerProxyHub.sol";
+
+contract RelayerProxyHubTest is ForgeHelper {
+  enum AutonolasPriorityFunction {
+    Propagate,
+    ProcessFromRoot,
+    ProposeAggregateRoot,
+    FinalizeAndPropagate
+  }
+  // ============ Events ============
+  event RootManagerChanged(address rootManager, address oldRootManager);
+  event PropagateCooldownChanged(uint256 propagateCooldown, uint256 oldPropagateCooldown);
+  event HubConnectorChanged(address hubConnector, address oldHubConnector, uint32 chain);
+  event AutonolasChanged(address updated, address previous);
+  event AutonolasPriorityChanged(AutonolasPriorityFunction fn, uint8 updated, uint8 previous);
+
+  // ============ Storage ============
+  address _connext = address(12312);
+  address _spokeConnector = address(12321222);
+  address _gelatoRelayer = address(123444412);
+  address _feeCollector = address(12335555);
+  address _keep3r = address(12335556);
+  address _rootManager = address(12312);
+  address _autonolas = address(134325213);
+  uint256 _propagateCooldown = 12321222;
+  address _hubConnector = address(123444412);
+  uint32 _chain = 123;
+
+  RelayerProxyHub proxy;
+
+  // ============ Setup ============
+  function setUp() public {
+    utils_deployAndAssert();
+  }
+
+  // ============ Utils ============
+  function utils_deployAndAssert() public {
+    vm.expectEmit(true, true, true, true);
+    emit RootManagerChanged(_rootManager, address(0));
+
+    vm.expectEmit(true, true, true, true);
+    emit PropagateCooldownChanged(_propagateCooldown, 0);
+
+    vm.expectEmit(true, true, true, true);
+    emit AutonolasChanged(_autonolas, address(0));
+
+    vm.expectEmit(true, true, true, true);
+    emit HubConnectorChanged(_hubConnector, address(0), _chain);
+
+    address[] memory connectors = new address[](1);
+    connectors[0] = _hubConnector;
+    uint32[] memory chains = new uint32[](1);
+    chains[0] = _chain;
+
+    proxy = new RelayerProxyHub(
+      _connext,
+      _spokeConnector,
+      _gelatoRelayer,
+      _feeCollector,
+      _keep3r,
+      _rootManager,
+      _autonolas,
+      _propagateCooldown,
+      connectors,
+      chains
+    );
+  }
+
+  // ============ isWorkableBySender ============
+  // fails if:
+  // - sender != autonolas
+  // - autonolasPriority > 0
+  // - serviceable block
+  function test_RelayerProxyHub__propagateKeep3r_works() public {
+    assertEq(proxy.autonolas(), _autonolas);
+  }
+}
