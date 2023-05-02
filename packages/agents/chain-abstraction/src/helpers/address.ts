574041e86faeb669044ac716b465003becdef949
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-05-02 12:41:55 +0800

    4087 no need to swap in case of from and to asset is same in chain abstraction sdk (#4088)
    * feat: update swap and xcall contract
    
    * feat: check if from and to is same
    
    * fix: unit tests

--- packages/agents/chain-abstraction/src/helpers/address.ts
+++ packages/agents/chain-abstraction/src/helpers/address.ts
@@ -2,9 +2,9 @@ export const DEPLOYED_ADDRESSES: Record<string, Record<string, string>> = {
   swapandxcall: {
     "6648936": "", // ETH mainnet
     "1869640809": "", // Optimism
-    "6450786": "0x6840600Bc9B74fA6B3BDF1503cb31A7AdF061c1D", // BNB Chain
+    "6450786": "0xB7E327b0484641Aa59b3F9ffDbEa4b4872a701fD", // BNB Chain
     "6778479": "", // Gnosis Chain
-    "1886350457": "0x697075f4A3Ce358d125281134e98d594D8Bb472e", // Polygon
+    "1886350457": "0x56D087121E5CC320002A4D826B08832FA9E729Bc", // Polygon
     "1634886255": "", // Arbitrum One
     "2053862243": "", // zkSync2 mainnet
     "1887071085": "", // Polygon zkEVM
