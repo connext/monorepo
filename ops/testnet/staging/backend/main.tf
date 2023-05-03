6b43be550db77b190dbb25ec3b9df7c3f688db28
preethamr <preethamr@users.noreply.github.com>
2023-05-03 07:46:29 -0700

    Update carto storage tf config

--- ops/testnet/staging/backend/main.tf
+++ ops/testnet/staging/backend/main.tf
@@ -27,8 +27,8 @@ module "cartographer_db" {
   source                = "../../../modules/db"
   identifier            = "rds-postgres-cartographer-${var.environment}-${var.stage}"
   instance_class        = "db.t3.medium"
-  allocated_storage     = 10
-  max_allocated_storage = 20
+  allocated_storage     = 30
+  max_allocated_storage = 50
 
 
   name     = "connext" // db name
