ba412307070cb0e2ceda6cffefb736ffe6adad90
sanchaymittal <sanchaymittal@gmail.com>
2023-05-09 10:28:40 +0900

    fix: sequencer unit test

--- packages/agents/sequencer/test/lib/helpers/relayerfee.spec.ts
+++ packages/agents/sequencer/test/lib/helpers/relayerfee.spec.ts
@@ -26,7 +26,7 @@ describe("Helpers:RelayerFee", () => {
     >;
     beforeEach(() => {
       safeCalculateRelayerFeeStub = stub(MockableFns, "calculateRelayerFee");
-      safeGetConversionRateStub = stub(MockableFns, "getConversionRate").resolves(1000);
+      safeGetConversionRateStub = stub(MockableFns, "safeGetConversionRate").resolves(1000);
       getDecimalsForAssetStub = stub(MockableFns, "getDecimalsForAsset").resolves(6);
     });
 
