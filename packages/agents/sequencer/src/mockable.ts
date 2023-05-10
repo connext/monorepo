35695c37c199a97bec9a282a679db89e54fab80f
Rahul Sethuram <rahul@sethuram.com>
2023-05-10 13:41:15 +0530

    Fix mockable

--- packages/agents/sequencer/src/mockable.ts
+++ packages/agents/sequencer/src/mockable.ts
@@ -3,11 +3,9 @@ import {
   calculateRelayerFee as _calculateRelayerFee,
   safeGetConversionRate as _safeGetConversionRate,
   getDecimalsForAsset as _getDecimalsForAsset,
-  safeGetConversionRate as _safeGetConversionRate,
 } from "@connext/nxtp-utils";
 
 export const sendWithRelayerWithBackup = _sendWithRelayerWithBackup;
 export const calculateRelayerFee = _calculateRelayerFee;
 export const safeGetConversionRate = _safeGetConversionRate;
 export const getDecimalsForAsset = _getDecimalsForAsset;
-export const safeGetConversionRate = _safeGetConversionRate;
