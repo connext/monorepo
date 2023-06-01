a82862939af8db8e8ba156e329c63882c1dd31b5
preethamr <preethamr@users.noreply.github.com>
2023-05-31 19:59:44 -0700

    fix: sequencer exec data not found in the cache (#4383)

--- packages/agents/sequencer/src/lib/operations/execute/slowpath.ts
+++ packages/agents/sequencer/src/lib/operations/execute/slowpath.ts
@@ -54,6 +54,11 @@ export const storeSlowPathData = async (executorData: ExecutorData, _requestCont
     });
     const queue = config.messageQueue.queues.find((it) => it.name == transfer.xparams!.originDomain);
     await channel.prefetch(queue?.limit || 1);
+    // Set status before publish
+    // Avoid a race condition where the message is consumed before the status is set
+    // If publish fails we we will have bad state, but publish is HA so we should be fine
+    await cache.executors.setExecStatus(transferId, ExecStatus.Enqueued);
+    await cache.executors.storeExecutorData(executorData);
     channel.publish(
       config.messageQueue.exchanges[0].name,
       transfer.xparams!.originDomain,
@@ -65,8 +70,6 @@ export const storeSlowPathData = async (executorData: ExecutorData, _requestCont
       message: message,
     });
 
-    await cache.executors.setExecStatus(transferId, ExecStatus.Enqueued);
-    await cache.executors.storeExecutorData(executorData);
     logger.info("Created a executor tx", requestContext, methodContext, { transferId, executorData });
   } else {
     // The executor data status here is Pending/Cancelled.
