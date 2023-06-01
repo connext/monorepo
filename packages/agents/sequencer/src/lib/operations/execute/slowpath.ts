d0c6e518cacc2b31cdb211f157d470e7d19bc690
wanglonghong <wanglonghong4467@gmail.com>
2023-06-01 17:27:53 +0800

    fix: slowpath cache issue

--- packages/agents/sequencer/src/lib/operations/execute/slowpath.ts
+++ packages/agents/sequencer/src/lib/operations/execute/slowpath.ts
@@ -6,6 +6,7 @@ import {
   ExecutorDataSchema,
   ExecStatus,
   jsonifyError,
+  getNtpTimeSeconds,
 } from "@connext/nxtp-utils";
 
 import { getContext, SlippageErrorPatterns } from "../../../sequencer";
@@ -58,7 +59,24 @@ export const storeSlowPathData = async (executorData: ExecutorData, _requestCont
   await cache.transfers.storeTransfers([transfer]);
 
   // Ensure that the executor data for this transfer hasn't expired.
-  const status = await cache.executors.getExecStatus(transferId);
+  let status = await cache.executors.getExecStatus(transferId);
+  if (status != ExecStatus.None) {
+    const lastExecTime = await cache.executors.getExecStatusTime(transferId);
+    const elapsed = (getNtpTimeSeconds() - lastExecTime) * 1000;
+    if (elapsed > config.executionWaitTime) {
+      logger.info("Executor merits retry", requestContext, methodContext, { transferId: transferId, status });
+      // Publish this transferId to sequencer subscriber to retry execution
+      status = ExecStatus.None;
+      await cache.executors.setExecStatus(transferId, status);
+    } else {
+      logger.info("Transfer awaiting execution", requestContext, methodContext, {
+        elapsed,
+        waitTime: config.executionWaitTime,
+        status,
+      });
+    }
+  }
+
   if (status === ExecStatus.Completed) {
     throw new ExecuteSlowCompleted({ transferId });
   } else if (status === ExecStatus.None) {
