3fb0df84756c72a7c362724d4e4c450d5b275f71
just-a-node <eye1717@gmail.com>
2023-06-02 16:19:27 -0600

    feat: add sdkShared types

--- packages/agents/sdk/src/interfaces/index.ts
+++ packages/agents/sdk/src/interfaces/index.ts
@@ -42,7 +42,197 @@ export type AssetData = {
   id: string;
 };
 
+export type RouterBalance = {
+  address: string;
+  asset_canonical_id: string;
+  asset_domain: string;
+  router_address: string;
+  balance: number;
+  local: string;
+  adopted: string;
+  canonical_id: string;
+  canonical_domain: string;
+  domain: string;
+  key: string;
+  id: string;
+  fees_earned: number;
+};
+
 export type ConnextSupport = { name: string; chainId: number; domainId: string; assets: string[] };
+
+/************************************
+SDK Shared Types
+*************************************/
+
+export const SdkGetConversionRateParamsSchema = Type.Object({
+  chainId: Type.Number(),
+});
+
+export type SdkGetConversionRateParams = Static<typeof SdkGetConversionRateParamsSchema>;
+
+export const SdkGetProviderParamsSchema = Type.Object({
+  domainId: Type.String(),
+});
+
+export type SdkGetProviderParams = Static<typeof SdkGetProviderParamsSchema>;
+
+export const SdkGetDeploymentAddressParamsSchema = Type.Object({
+  domainId: Type.String(),
+  deploymentName: Type.Enum({
+    connext: "connext",
+    multisend: "multisend",
+    unwrapper: "unwrapper",
+    stableSwap: "stableSwap",
+  }),
+});
+
+export type SdkGetDeploymentAddressParams = Static<typeof SdkGetDeploymentAddressParamsSchema>;
+
+export const SdkGetConnextParamsSchema = Type.Object({
+  domainId: Type.String(),
+});
+
+export type SdkGetConnextParams = Static<typeof SdkGetConnextParamsSchema>;
+
+export const SdkGetERC20ParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+
+export type SdkGetERC20Params = Static<typeof SdkGetERC20ParamsSchema>;
+
+export const SdkGetChainIdParamsSchema = Type.Object({
+  domainId: Type.String(),
+});
+
+export type SdkGetChainIdParams = Static<typeof SdkGetChainIdParamsSchema>;
+
+export const SdkDomainToChainNameParamsSchema = Type.Object({
+  domainId: Type.String(),
+});
+
+export type SdkDomainToChainNameParams = Static<typeof SdkDomainToChainNameParamsSchema>;
+
+export const SdkChainIdToDomainParamsSchema = Type.Object({
+  chainId: Type.Number(),
+});
+
+export type SdkChainIdToDomainParams = Static<typeof SdkChainIdToDomainParamsSchema>;
+
+export const SdkDomainToChainIdParamsSchema = Type.Object({
+  domainId: Type.Number(),
+});
+
+export type SdkDomainToChainIdParams = Static<typeof SdkDomainToChainIdParamsSchema>;
+
+export const SdkGetBlockNumberFromUnixTimestampParamsSchema = Type.Object({
+  domainId: Type.String(),
+  unixTimestamp: Type.Number(),
+});
+
+export type SdkGetBlockNumberFromUnixTimestampParams = Static<typeof SdkGetBlockNumberFromUnixTimestampParamsSchema>;
+
+export const SdkApproveIfNeededParamsSchema = Type.Object({
+  domainId: Type.String(),
+  assetId: Type.String(),
+  amount: Type.String(),
+  infiniteApprove: Type.Boolean(),
+});
+
+export type SdkApproveIfNeededParams = Static<typeof SdkApproveIfNeededParamsSchema>;
+
+export const SdkGetActiveLiquidityParamsSchema = Type.Object({
+  domain: Type.Optional(Type.String()),
+  local: Type.Optional(Type.String()),
+});
+
+export type SdkGetActiveLiquidityParams = Static<typeof SdkGetActiveLiquidityParamsSchema>;
+
+export const SdkGetAssetsDataByDomainAndAddressParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+
+export type SdkGetAssetsDataByDomainAndAddressParams = Static<typeof SdkGetAssetsDataByDomainAndAddressParamsSchema>;
+
+export const SdkGetAssetsWithSameCanonicalParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+export type SdkGetAssetsWithSameCanonicalParams = Static<typeof SdkGetAssetsWithSameCanonicalParamsSchema>;
+
+export const SdkGetAssetsDataByDomainAndKeyParamsSchema = Type.Object({
+  domainId: Type.String(),
+  key: Type.String(),
+});
+
+export type SdkGetAssetsDataByDomainAndKeyParams = Static<typeof SdkGetAssetsDataByDomainAndKeyParamsSchema>;
+
+export const SdkIsNextAssetParamsSchema = Type.Object({
+  tokenAddress: Type.String(),
+});
+
+export type SdkIsNextAssetParams = Static<typeof SdkIsNextAssetParamsSchema>;
+
+export const SdkChangeSignerAddressParamsSchema = Type.Object({
+  signerAddress: Type.String(),
+});
+
+export type SdkChangeSignerAddressParams = Static<typeof SdkChangeSignerAddressParamsSchema>;
+
+export const LogSchema = Type.Object({
+  blockNumber: Type.Number(),
+  blockHash: Type.String(),
+  transactionIndex: Type.Number(),
+  removed: Type.Boolean(),
+  address: Type.String(),
+  data: Type.String(),
+  topics: Type.Array(Type.String()),
+  transactionHash: Type.String(),
+  logIndex: Type.Number(),
+});
+
+export type LogType = Static<typeof LogSchema>;
+
+export const SdkParseConnextTransactionReceiptParamsSchema = Type.Object({
+  to: Type.String(),
+  from: Type.String(),
+  contractAddress: Type.String(),
+  transactionIndex: Type.Number(),
+  root: Type.Optional(Type.String()),
+  gasUsed: TIntegerString,
+  logsBloom: Type.String(),
+  blockHash: Type.String(),
+  transactionHash: Type.String(),
+  logs: Type.Array(LogSchema), // Assuming you have LogSchema defined
+  blockNumber: Type.Number(),
+  confirmations: Type.Number(),
+  cumulativeGasUsed: TIntegerString,
+  effectiveGasPrice: TIntegerString,
+  byzantium: Type.Boolean(),
+  type: Type.Number(),
+  status: Type.Optional(Type.Number()),
+});
+
+export type SdkParseConnextTransactionReceiptParams = Static<typeof SdkParseConnextTransactionReceiptParamsSchema>;
+
+export const SdkCalculateCanonicalKeyParamsSchema = Type.Object({
+  domainId: Type.String(),
+  canonicalId: Type.String(),
+});
+
+export type SdkCalculateCanonicalKeyParams = Static<typeof SdkCalculateCanonicalKeyParamsSchema>;
+
+export const SdkGetCanonicalTokenIdParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+
+export type SdkGetCanonicalTokenIdParams = Static<typeof SdkGetCanonicalTokenIdParamsSchema>;
+
+/************************************
+SDK Base Types
+*************************************/
 
 export const SdkXCallParamsSchema = Type.Object({
   origin: TIntegerString,
@@ -110,19 +300,3 @@ export const SdkCalculateAmountReceivedParamsSchema = Type.Object({
 });
 
 export type SdkCalculateAmountReceivedParams = Static<typeof SdkUpdateSlippageParamsSchema>;
-
-export type RouterBalance = {
-  address: string;
-  asset_canonical_id: string;
-  asset_domain: string;
-  router_address: string;
-  balance: number;
-  local: string;
-  adopted: string;
-  canonical_id: string;
-  canonical_domain: string;
-  domain: string;
-  key: string;
-  id: string;
-  fees_earned: number;
-};
