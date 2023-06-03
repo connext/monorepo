54ec10aeed24d074198dba56241e867f28345838
just-a-node <eye1717@gmail.com>
2023-06-03 11:17:34 -0600

    feat: add sdkPool types

--- packages/agents/sdk/src/interfaces/index.ts
+++ packages/agents/sdk/src/interfaces/index.ts
@@ -60,6 +60,36 @@ export type RouterBalance = {
 
 export type ConnextSupport = { name: string; chainId: number; domainId: string; assets: string[] };
 
+export const PoolAssetSchema = Type.Object({
+  address: Type.String(),
+  name: Type.String(),
+  symbol: Type.String(),
+  decimals: Type.Number(),
+  index: Type.Number(),
+  balance: TIntegerString,
+});
+
+export const PoolSchema = Type.Object({
+  domainId: Type.String(),
+  name: Type.String(),
+  symbol: Type.String(),
+  local: PoolAssetSchema,
+  adopted: PoolAssetSchema,
+  lpTokenAddress: Type.String(),
+  canonicalHash: Type.String(),
+  balances: Type.Array(TIntegerString),
+  decimals: Type.Array(Type.Number()),
+  invariant: TIntegerString,
+  initialA: TIntegerString,
+  initialATime: Type.Number(),
+  futureA: TIntegerString,
+  futureATime: Type.Number(),
+  currentA: TIntegerString,
+  swapFee: Type.String(),
+  adminFee: Type.String(),
+  address: Type.Optional(Type.String()),
+});
+
 /************************************
 SDK Shared Types
 *************************************/
@@ -292,3 +322,341 @@ export const SdkCalculateAmountReceivedParamsSchema = Type.Object({
 });
 
 export type SdkCalculateAmountReceivedParams = Static<typeof SdkCalculateAmountReceivedParamsSchema>;
+
+/************************************
+SDK Pool Types
+*************************************/
+
+// calculateSwap
+export const SdkCalculateSwapParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  tokenIndexFrom: Type.Number(),
+  tokenIndexTo: Type.Number(),
+  amount: TIntegerString,
+});
+export type SdkCalculateSwapParams = Static<typeof SdkCalculateSwapParamsSchema>;
+
+// calculateSwapLocal
+export const SdkCalculateSwapLocalParamsSchema = Type.Object({
+  domainId: Type.String(),
+  pool: PoolSchema,
+  tokenAddress: Type.String(),
+  tokenIndexFrom: Type.Number(),
+  tokenIndexTo: Type.Number(),
+  amount: TIntegerString,
+});
+export type SdkCalculateSwapLocalParams = Static<typeof SdkCalculateSwapLocalParamsSchema>;
+
+// getSwapOut
+export const SdkGetSwapOutParamsSchema = Type.Object({
+  pool: PoolSchema,
+  x: TIntegerString,
+  xp: Type.Array(TIntegerString),
+  tokenIndexFrom: Type.Optional(Type.Number()),
+  tokenIndexTo: Type.Optional(Type.Number()),
+});
+export type SdkGetSwapOutParams = Static<typeof SdkGetSwapOutParamsSchema>;
+
+// scientificToBigInt
+export const SdkScientificToBigIntParamsSchema = Type.Object({
+  scientificNotationString: Type.String(),
+});
+export type SdkScientificToBigIntParams = Static<typeof SdkScientificToBigIntParamsSchema>;
+
+// calculateTokenAmount
+export const SdkCalculateTokenAmountParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amounts: Type.Array(Type.String()),
+  isDeposit: Type.Optional(Type.Boolean()),
+});
+export type SdkCalculateTokenAmountParams = Static<typeof SdkCalculateTokenAmountParamsSchema>;
+
+// calculateRemoveSwapLiquidity
+export const SdkCalculateRemoveSwapLiquidityParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amount: Type.String(),
+});
+export type SdkCalculateRemoveSwapLiquidityParams = Static<typeof SdkCalculateRemoveSwapLiquidityParamsSchema>;
+
+// calculateRemoveSwapLiquidityOneToken
+export const SdkCalculateRemoveSwapLiquidityOneTokenParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amount: Type.String(),
+  index: Type.Number(),
+});
+export type SdkCalculateRemoveSwapLiquidityOneTokenParams = Static<
+  typeof SdkCalculateRemoveSwapLiquidityOneTokenParamsSchema
+>;
+
+// calculatePriceImpact
+export const SdkCalculatePriceImpactParamsSchema = Type.Object({
+  tokenInputAmount: TIntegerString,
+  tokenOutputAmount: TIntegerString,
+  virtualPrice: Type.Optional(TIntegerString),
+  isDeposit: Type.Optional(Type.Boolean()),
+});
+export type SdkCalculatePriceImpactParams = Static<typeof SdkCalculatePriceImpactParamsSchema>;
+
+// calculateAddLiquidityPriceImpact
+export const SdkCalculateAddLiquidityPriceImpactParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amountX: Type.String(),
+  amountY: Type.String(),
+});
+export type SdkCalculateAddLiquidityPriceImpactParams = Static<typeof SdkCalculateAddLiquidityPriceImpactParamsSchema>;
+
+// calculateRemoveLiquidityPriceImpact
+export const SdkCalculateRemoveLiquidityPriceImpactParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amountX: Type.String(),
+  amountY: Type.String(),
+});
+export type SdkCalculateRemoveLiquidityPriceImpactParams = Static<
+  typeof SdkCalculateRemoveLiquidityPriceImpactParamsSchema
+>;
+
+// calculateSwapPriceImpact
+export const SdkCalculateSwapPriceImpactParamsSchema = Type.Object({
+  domainId: Type.String(),
+  amountX: Type.String(),
+  tokenX: Type.String(),
+  tokenY: Type.String(),
+});
+export type SdkCalculateSwapPriceImpactParams = Static<typeof SdkCalculateSwapPriceImpactParamsSchema>;
+
+// getTokenPrice
+export const SdkGetTokenPriceParamsSchema = Type.Object({
+  tokenSymbol: Type.String(),
+});
+export type SdkGetTokenPriceParams = Static<typeof SdkGetTokenPriceParamsSchema>;
+
+// getLPTokenAddress
+export const SdkGetLPTokenAddressParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+export type SdkGetLPTokenAddressParams = Static<typeof SdkGetLPTokenAddressParamsSchema>;
+
+// getTokenUserBalance
+export const SdkGetTokenUserBalanceParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  userAddress: Type.String(),
+});
+export type SdkGetTokenUserBalanceParams = Static<typeof SdkGetTokenUserBalanceParamsSchema>;
+
+// getPoolTokenIndex
+export const SdkGetPoolTokenIndexParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  poolTokenAddress: Type.String(),
+});
+export type SdkGetPoolTokenIndexParams = Static<typeof SdkGetPoolTokenIndexParamsSchema>;
+
+// getPoolTokenDecimals
+export const SdkGetPoolTokenDecimalsParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  poolTokenAddress: Type.String(),
+});
+export type SdkGetPoolTokenDecimalsParams = Static<typeof SdkGetPoolTokenDecimalsParamsSchema>;
+
+// getPoolTokenBalance
+export const SdkGetPoolTokenBalanceParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  poolTokenAddress: Type.String(),
+  _index: Type.Optional(Type.Number()),
+});
+export type SdkGetPoolTokenBalanceParams = Static<typeof SdkGetPoolTokenBalanceParamsSchema>;
+
+// getPoolTokenAddress
+export const SdkGetPoolTokenAddressParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  index: Type.Number(),
+});
+export type SdkGetPoolTokenAddressParams = Static<typeof SdkGetPoolTokenAddressParamsSchema>;
+
+// getVirtualPrice
+export const SdkGetVirtualPriceParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+export type SdkGetVirtualPriceParams = Static<typeof SdkGetVirtualPriceParamsSchema>;
+
+// getRepresentation
+export const SdkGetRepresentationParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+export type SdkGetRepresentationParams = Static<typeof SdkGetRepresentationParamsSchema>;
+
+// getAdopted
+export const SdkGetAdoptedParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+export type SdkGetAdoptedParams = Static<typeof SdkGetAdoptedParamsSchema>;
+
+// getTokenSwapEvents
+export const SdkGetTokenSwapEventsParamsSchema = Type.Object({
+  key: Type.Optional(Type.String()),
+  buyer: Type.Optional(Type.String()),
+  transactionHash: Type.Optional(Type.String()),
+  startTimestamp: Type.Optional(Type.Number()),
+  endTimestamp: Type.Optional(Type.Number()),
+  range: Type.Optional(
+    Type.Object({
+      limit: Type.Optional(Type.Number()),
+      offset: Type.Optional(Type.Number()),
+    }),
+  ),
+});
+export type SdkGetTokenSwapEventsParams = Static<typeof SdkGetTokenSwapEventsParamsSchema>;
+
+// getPoolData
+export const SdkGetPoolDataParamsSchema = Type.Object({
+  key: Type.Optional(Type.String()),
+  domainId: Type.Optional(Type.String()),
+  lpTokenAddress: Type.Optional(Type.String()),
+});
+export type SdkGetPoolDataParams = Static<typeof SdkGetPoolDataParamsSchema>;
+
+// addLiquidity
+export const SdkAddLiquidityParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amounts: Type.Array(Type.String()),
+  minToMint: Type.Optional(Type.String()),
+  deadline: Type.Optional(Type.Number()),
+});
+export type SdkAddLiquidityParams = Static<typeof SdkAddLiquidityParamsSchema>;
+
+// removeLiquidityOneToken
+export const SdkRemoveLiquidityOneTokenParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  withdrawTokenAddress: Type.String(),
+  amount: Type.String(),
+  minAmount: Type.Optional(Type.String()),
+  deadline: Type.Optional(Type.Number()),
+});
+export type SdkRemoveLiquidityOneTokenParams = Static<typeof SdkRemoveLiquidityOneTokenParamsSchema>;
+
+// removeLiquidity
+export const SdkRemoveLiquidityParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amount: Type.String(),
+  minAmounts: Type.Optional(Type.Array(Type.String())),
+  deadline: Type.Optional(Type.Number()),
+});
+export type SdkRemoveLiquidityParams = Static<typeof SdkRemoveLiquidityParamsSchema>;
+
+// removeLiquidityImbalance
+export const SdkRemoveLiquidityImbalanceParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  amounts: Type.Array(Type.String()),
+  maxBurnAmount: Type.Optional(Type.String()),
+  deadline: Type.Optional(Type.Number()),
+});
+export type SdkRemoveLiquidityImbalanceParams = Static<typeof SdkRemoveLiquidityImbalanceParamsSchema>;
+
+// swap
+export const SdkSwapParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  from: Type.String(),
+  to: Type.String(),
+  amount: Type.String(),
+  minDy: Type.Optional(Type.Number()),
+  deadline: Type.Optional(Type.Number()),
+});
+export type SdkSwapParams = Static<typeof SdkSwapParamsSchema>;
+
+// getPool
+export const SdkGetPoolParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+});
+export type SdkGetPoolParams = Static<typeof SdkGetPoolParamsSchema>;
+
+// getUserPools
+export const SdkGetUserPoolsParamsSchema = Type.Object({
+  domainId: Type.String(),
+  userAddress: Type.String(),
+});
+export type SdkGetUserPoolsParams = Static<typeof SdkGetUserPoolsParamsSchema>;
+
+// getYieldStatsForDays
+export const SdkGetYieldStatsForDaysParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  unixTimestamp: Type.Number(),
+  days: Type.Number(),
+});
+export type SdkGetYieldStatsForDaysParams = Static<typeof SdkGetYieldStatsForDaysParamsSchema>;
+
+// calculateYield
+export const SdkCalculateYieldParamsSchema = Type.Object({
+  feesEarned: Type.Number(),
+  principal: Type.Number(),
+  days: Type.Number(),
+});
+export type SdkCalculateYieldParams = Static<typeof SdkCalculateYieldParamsSchema>;
+
+// getYieldData
+export const SdkGetYieldDataParamsSchema = Type.Object({
+  domainId: Type.String(),
+  tokenAddress: Type.String(),
+  days: Type.Optional(Type.Number()),
+});
+export type SdkGetYieldDataParams = Static<typeof SdkGetYieldDataParamsSchema>;
+
+// getLiquidityMiningAprPerPool
+export const SdkGetLiquidityMiningAprPerPoolParamsSchema = Type.Object({
+  totalTokens: Type.Number(),
+  totalBlocks: Type.Number(),
+  numPools: Type.Number(),
+  tokenSymbol: Type.String(),
+  poolTVL: Type.Number(),
+});
+export type SdkGetLiquidityMiningAprPerPoolParams = Static<typeof SdkGetLiquidityMiningAprPerPoolParamsSchema>;
+
+// getHourlySwapVolume
+export const SdkGetHourlySwapVolumeParamsSchema = Type.Object({
+  key: Type.Optional(Type.String()),
+  domainId: Type.Optional(Type.String()),
+  startTimestamp: Type.Optional(Type.Number()),
+  endTimestamp: Type.Optional(Type.Number()),
+  range: Type.Optional(
+    Type.Object({
+      limit: Type.Optional(Type.Number()),
+      offset: Type.Optional(Type.Number()),
+    }),
+  ),
+});
+export type SdkGetHourlySwapVolumeParams = Static<typeof SdkGetHourlySwapVolumeParamsSchema>;
+
+// getDailySwapVolume
+export const SdkGetDailySwapVolumeParamsSchema = Type.Object({
+  key: Type.Optional(Type.String()),
+  domainId: Type.Optional(Type.String()),
+  startTimestamp: Type.Optional(Type.Number()),
+  endTimestamp: Type.Optional(Type.Number()),
+  range: Type.Optional(
+    Type.Object({
+      limit: Type.Optional(Type.Number()),
+      offset: Type.Optional(Type.Number()),
+    }),
+  ),
+});
+export type SdkGetDailySwapVolumeParams = Static<typeof SdkGetDailySwapVolumeParamsSchema>;
