efeda61670e9ca5328c5620b83109dd956c94753
just-a-node <eye1717@gmail.com>
2023-06-08 17:12:50 -0600

    chore: all wrapped sdkBase tests

--- packages/agents/sdk-wrapper/test/sdkBase.spec.ts
+++ packages/agents/sdk-wrapper/test/sdkBase.spec.ts
@@ -1,45 +1,42 @@
-import { reset, restore, stub, SinonStub, createStubInstance, SinonStubbedInstance } from "sinon";
-import { expect } from "@connext/nxtp-utils";
-import { getConnextInterface, ChainReader } from "@connext/nxtp-txservice";
-import { providers, BigNumber, utils, constants } from "ethers";
+import { reset, restore, stub, SinonStub } from "sinon";
+import { providers, BigNumber } from "ethers";
 import { mock } from "./mock";
 import { SdkBase } from "../src/sdkBase";
-import { SdkUtils } from "../src/sdkUtils";
-import { SdkPool } from "../src/sdkPool";
 import * as MockableFns from "../src/mockable";
 
-import * as ConfigFns from "@connext/sdk-core/src/config";
-import * as SharedFns from "../src/lib/helpers/shared";
-import { SdkXCallParams } from "../src/interfaces";
+import {
+  SdkXCallParams,
+  SdkBumpTransferParams,
+  SdkUpdateSlippageParams,
+  SdkEstimateRelayerFeeParams,
+  SdkCalculateAmountReceivedParams,
+} from "@connext/sdk-core";
+import { expect } from "@connext/nxtp-utils";
 
 const mockConfig = mock.config();
 const mockChainData = mock.chainData();
-const mockDeployments = mock.contracts.deployments();
-
 const mockConnextAddress = mockConfig.chains[mock.domain.A].deployments!.connext;
+
 const chainId = +mock.chain.A;
+const relayerFee = BigNumber.from("1");
+const mockXTransfer = mock.entity.xtransfer();
+const mockGenericTxRequest: providers.TransactionRequest = {
+  to: mockConnextAddress,
+  data: "0x",
+  from: mock.config().signerAddress,
+  value: relayerFee,
+  chainId,
+};
 
 describe("SdkBase", () => {
   let sdkBase: SdkBase;
-  let sdkUtils: SdkUtils;
-  let sdkPool: SdkPool;
-  let config: ConfigFns.SdkConfig;
   let axiosPostStub: SinonStub;
-
-  let chainreader: SinonStubbedInstance<ChainReader>;
+  let expectedBaseUri: string;
 
   beforeEach(async () => {
-    chainreader = createStubInstance(ChainReader);
-    config = ConfigFns.getEnvConfig(mockConfig, mockChainData, mockDeployments);
     axiosPostStub = stub(MockableFns, "axiosPost");
-    stub(ConfigFns, "getConfig").resolves({ nxtpConfig: config, chainData: mockChainData });
-    stub(SharedFns, "domainToChainId").returns(chainId);
-    stub(SharedFns, "axiosGetRequest").resolves([]);
-
     sdkBase = await SdkBase.create(mockConfig, undefined, mockChainData);
-    sdkUtils = await SdkUtils.create(mockConfig, undefined, mockChainData);
-    sdkPool = await SdkPool.create(mockConfig, undefined, mockChainData);
-    (sdkBase as any).chainreader = chainreader;
+    expectedBaseUri = sdkBase.baseUri;
   });
 
   afterEach(() => {
@@ -62,60 +59,120 @@ describe("SdkBase", () => {
   });
 
   describe("#xcall", () => {
-    let getConversionRateStub: SinonStub;
-    let getDecimalsForAssetStub: SinonStub;
-    let getHardcodedGasLimitsStub: SinonStub;
-    let relayerFee = BigNumber.from("1");
+    it("happy: should send request with correct params", async () => {
+      const expectedEndpoint = "/xcall";
+      const expectedArgs: SdkXCallParams = {
+        ...mock.entity.xcallArgs(),
+        origin: mock.entity.callParams().originDomain,
+        relayerFee: relayerFee.toString(),
+        receiveLocal: false,
+        wrapNativeOnOrigin: false,
+        unwrapNativeOnDestination: false,
+      };
 
-    const mockXCallArgs = mock.entity.xcallArgs();
-    const standardXCallData: string = getConnextInterface().encodeFunctionData(
-      "xcall(uint32,address,address,address,uint256,uint256,bytes)",
-      [
-        mockXCallArgs.destination,
-        mockXCallArgs.to,
-        mockXCallArgs.asset,
-        mockXCallArgs.delegate,
-        mockXCallArgs.amount,
-        mockXCallArgs.slippage,
-        mockXCallArgs.callData,
-      ],
-    );
-
-    const mockXCallRequest: providers.TransactionRequest = {
-      to: mockConnextAddress,
-      data: standardXCallData,
-      from: mock.config().signerAddress,
-      value: relayerFee,
-      chainId,
-    };
-
-    const origin = mock.entity.callParams().originDomain;
-    const sdkXCallArgs: SdkXCallParams = {
-      ...mock.entity.xcallArgs(),
-      origin,
-      relayerFee: relayerFee.toString(),
-      receiveLocal: false,
-      wrapNativeOnOrigin: false,
-      unwrapNativeOnDestination: false,
-    };
-
-    beforeEach(() => {
-      getConversionRateStub = stub(SharedFns, "getConversionRate");
-      getDecimalsForAssetStub = stub(SharedFns, "getDecimalsForAsset");
-      getHardcodedGasLimitsStub = stub(SharedFns, "getHardcodedGasLimits");
-    });
-
-    afterEach(() => {
-      restore();
-      reset();
-    });
-
-    it("happy: should work if ERC20", async () => {
       axiosPostStub.resolves({
-        data: mockXCallRequest,
+        data: mockGenericTxRequest,
+        status: 200,
       });
-      const res = await sdkBase.xcall(sdkXCallArgs);
-      expect(res).to.be.deep.eq(mockXCallRequest);
+
+      const res = await sdkBase.xcall(expectedArgs);
+
+      expect(axiosPostStub).to.have.been.calledWithExactly(expectedBaseUri + expectedEndpoint, expectedArgs);
+      expect(res).to.be.deep.eq(mockGenericTxRequest);
+    });
+  });
+
+  describe("#bumpTransfer", () => {
+    it("happy: should send request with correct params", async () => {
+      const expectedEndpoint = "/bumpTransfer";
+      const expectedArgs: SdkBumpTransferParams = {
+        domainId: mockXTransfer.xparams.originDomain,
+        transferId: mockXTransfer.transferId,
+        asset: mockXTransfer.origin!.assets.transacting.asset,
+        relayerFee: relayerFee.toString(),
+      };
+
+      axiosPostStub.resolves({
+        data: mockGenericTxRequest,
+        status: 200,
+      });
+
+      const res = await sdkBase.bumpTransfer(expectedArgs);
+
+      expect(axiosPostStub).to.have.been.calledWithExactly(expectedBaseUri + expectedEndpoint, expectedArgs);
+      expect(res).to.be.deep.eq(mockGenericTxRequest);
+    });
+  });
+
+  describe("#updateSlippage", () => {
+    it("happy: should send request with correct params", async () => {
+      const expectedEndpoint = "/updateSlippage";
+      const expectedArgs: SdkUpdateSlippageParams = {
+        domainId: mockXTransfer.xparams.originDomain,
+        transferId: mockXTransfer.transferId,
+        slippage: "100",
+      };
+
+      axiosPostStub.resolves({
+        data: mockGenericTxRequest,
+        status: 200,
+      });
+
+      const res = await sdkBase.updateSlippage(expectedArgs);
+
+      expect(axiosPostStub).to.have.been.calledWithExactly(expectedBaseUri + expectedEndpoint, expectedArgs);
+      expect(res).to.be.deep.eq(mockGenericTxRequest);
+    });
+  });
+
+  describe("#estimateRelayerFee", () => {
+    it("happy: should send request with correct params", async () => {
+      const expectedEndpoint = "/estimateRelayerFee";
+      const expectedArgs: SdkEstimateRelayerFeeParams = {
+        originDomain: mockXTransfer.xparams.originDomain,
+        destinationDomain: mockXTransfer.xparams.destinationDomain,
+      };
+      const expectedRes = BigNumber.from(1);
+
+      axiosPostStub.resolves({
+        data: expectedRes,
+        status: 200,
+      });
+
+      const res = await sdkBase.estimateRelayerFee(expectedArgs);
+
+      expect(axiosPostStub).to.have.been.calledWithExactly(expectedBaseUri + expectedEndpoint, expectedArgs);
+      expect(res).to.be.deep.eq(expectedRes);
+    });
+  });
+
+  describe("#calculateAmountReceived", () => {
+    it("happy: should send request with correct params", async () => {
+      const expectedEndpoint = "/calculateAmountReceived";
+      const expectedArgs: SdkCalculateAmountReceivedParams = {
+        originDomain: mockXTransfer.xparams.originDomain,
+        destinationDomain: mockXTransfer.xparams.destinationDomain,
+        originTokenAddress: mockXTransfer.origin!.assets.transacting.asset,
+        amount: "100",
+        receiveLocal: false,
+        checkFastLiquidity: false,
+      };
+      const expectedRes = BigNumber.from(1);
+
+      axiosPostStub.resolves({
+        data: expectedRes,
+        status: 200,
+      });
+
+      const res = await sdkBase.calculateAmountReceived(
+        expectedArgs.originDomain,
+        expectedArgs.destinationDomain,
+        expectedArgs.originTokenAddress,
+        expectedArgs.amount,
+      );
+
+      expect(axiosPostStub).to.have.been.calledWithExactly(expectedBaseUri + expectedEndpoint, expectedArgs);
+      expect(res).to.be.deep.eq(expectedRes);
     });
   });
 });
