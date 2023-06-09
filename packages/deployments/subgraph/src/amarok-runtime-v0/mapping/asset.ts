6915e86c5d5f7eb192d35b65ebe525ab43a4d696
Layne Haber <layne.haber@gmail.com>
2023-06-09 10:37:34 -0600

    refactor: key on canonical hash; remove asset status

--- packages/deployments/subgraph/src/amarok-runtime-v0/mapping/asset.ts
+++ packages/deployments/subgraph/src/amarok-runtime-v0/mapping/asset.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prefer-const */
 import { AssetAdded, AssetRemoved } from "../../../generated/Connext/Connext";
-import { Asset, AssetStatus } from "../../../generated/schema";
+import { Asset } from "../../../generated/schema";
 
 import { getTokenDecimals } from "./helper";
 
 /// MARK - Assets
 export function handleAssetAdded(event: AssetAdded): void {
-  let assetId = event.params.localAsset.toHex();
+  let assetId = event.params.key;
   let asset = Asset.load(assetId);
   if (asset == null) {
     asset = new Asset(assetId);
@@ -18,24 +18,17 @@ export function handleAssetAdded(event: AssetAdded): void {
   asset.adoptedAsset = event.params.adoptedAsset;
   asset.localAsset = event.params.localAsset;
   asset.blockNumber = event.block.number;
+  asset.status = true;
 
-  let assetStatus = AssetStatus.load(event.params.key.toHex());
-  if (assetStatus == null) {
-    assetStatus = new AssetStatus(event.params.key.toHex());
-  }
-  assetStatus.status = true;
-
-  asset.status = assetStatus.id;
-
-  assetStatus.save();
   asset.save();
 }
 
 export function handleAssetRemoved(event: AssetRemoved): void {
-  let assetStatus = AssetStatus.load(event.params.key.toHex());
-  if (assetStatus == null) {
-    assetStatus = new AssetStatus(event.params.key.toHex());
+  let asset = Asset.load(event.params.key.toHex());
+  if (!asset) {
+    // Nothing to do, no asset record exists so status is already false
+    return;
   }
-  assetStatus.status = false;
-  assetStatus.save();
+  asset.status = false;
+  asset.save();
 }
