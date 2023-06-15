5006275bc6140bb86773368e3b7385f6838b6576
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-06-15 11:12:53 +0800

    fix: unit test for consensys lh test

--- packages/agents/lighthouse/test/tasks/sendOutboundRoot/helpers/consensys.spec.ts
+++ packages/agents/lighthouse/test/tasks/sendOutboundRoot/helpers/consensys.spec.ts
@@ -8,7 +8,7 @@ describe("Helpers: Consensys", () => {
     it("should get params", async () => {
       const res = await getSendOutboundRootParams(mock.domain.A);
       expect(res).to.deep.eq({
-        _fee: utils.parseEther("0.01").toString(),
+        _fee: utils.parseEther("0.001").toString(),
         _encodedData: "0x",
       });
     });
