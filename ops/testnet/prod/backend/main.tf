19ef83ee444dfffeb39523c44468118c075c8b12
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-06-16 14:29:48 +0200

    fix: capacity variables and definitions

--- ops/testnet/prod/backend/main.tf
+++ ops/testnet/prod/backend/main.tf
@@ -181,6 +181,8 @@ module "sdk_server_auto_scaling" {
   domain           = var.domain
   ecs_service_name = module.sdk-server.service_name
   ecs_cluster_name = module.ecs.ecs_cluster_name
+  min_capacity     = 2
+  max_capacity     = 10
 }
 
 
