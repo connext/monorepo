f7a631ea48d8c357031db11054c8fb8e07762b50
just-a-node <eye1717@gmail.com>
2023-06-16 11:12:08 -0600

    chore: rm dupe autoscaling config from merge

--- ops/testnet/staging/backend/main.tf
+++ ops/testnet/staging/backend/main.tf
@@ -124,15 +124,6 @@ module "sdk-server" {
   domain                   = var.domain
 }
 
-module "sdk_server_auto_scaling" {
-  source           = "../../../modules/auto-scaling"
-  stage            = var.stage
-  environment      = var.environment
-  domain           = var.domain
-  ecs_service_name = module.sdk-server.service_name
-  ecs_cluster_name = module.ecs.ecs_cluster_name
-}
-
 module "sdk_server_cache" {
   source                        = "../../../modules/redis"
   stage                         = var.stage
