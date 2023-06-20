011954a03ef2566f26ec4216182ea414e5b9751a
just-a-node <eye1717@gmail.com>
2023-06-19 18:55:45 -0600

    feat: get rid of unnecessary chainreader

--- packages/agents/sdk-wrapper/src/sdkShared.ts
+++ packages/agents/sdk-wrapper/src/sdkShared.ts
@@ -1,4 +1,4 @@
-import { getContractInterfaces, ConnextContractInterfaces, ChainReader } from "@connext/nxtp-txservice";
+import { getContractInterfaces, ConnextContractInterfaces } from "@connext/nxtp-txservice";
 import {
   Logger,
   ChainData,
@@ -17,7 +17,6 @@ export class SdkShared {
   readonly config: SdkConfig;
   readonly chainData: Map<string, ChainData>;
   readonly contracts: ConnextContractInterfaces;
-  protected readonly chainreader: ChainReader;
   protected readonly logger: Logger;
 
   readonly baseUri: string;
@@ -27,7 +26,6 @@ export class SdkShared {
     this.logger = logger;
     this.chainData = chainData;
     this.contracts = getContractInterfaces();
-    this.chainreader = new ChainReader(logger.child({ module: "ChainReader" }), config.chains);
     this.baseUri =
       this.config.network === "local"
         ? "http://localhost:8080"
