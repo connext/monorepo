dfc2a849ae7444b772f3ba19685c1765d5b45c95
Eddie <eye1717@gmail.com>
2023-06-21 04:35:07 -0600

    Sdk wrapper 2.1.0 alpha.0 (#4560)
    * feat: copy sdk types to remove dependency
    
    * fix: missing options
    
    * fix: default param will not work with optional ordering
    
    * chore: use specific version of dep
    
    * fix: trailing slash
    
    * chore: move to sdk-types
    
    * feat: copy contract types
    
    * feat: get rid of unnecessary chainreader
    
    * feat: remove dependency on txservice
    
    * fix: missing dep
    
    * chore: update yarn lock
    
    * chore: update yarn.lock
    
    * fix: add core sdk as devDep
    
    * fix: package typo
    
    * fix: changeSignerAddress should change local config
    
    * feat: local config options override for sdkBase methods
    
    * feat: local config options override for sdkShared methods
    
    * feat: local config options override for sdkPool methods
    
    * feat: local config options override for sdkRouter methods
    
    * feat: init wrapper changelog
    
    * chore: bump sdk-core
    
    * chore: bump utils

--- packages/agents/sdk-wrapper/src/sdkBase.ts
+++ packages/agents/sdk-wrapper/src/sdkBase.ts
@@ -1,13 +1,13 @@
 import { Logger, createLoggingContext, ChainData } from "@connext/nxtp-utils";
 import { providers, BigNumber, BigNumberish } from "ethers";
-import {
+
+import type {
   SdkConfig,
   SdkXCallParams,
   SdkBumpTransferParams,
   SdkUpdateSlippageParams,
   SdkEstimateRelayerFeeParams,
-} from "@connext/sdk-core";
-
+} from "./sdk-types";
 import { axiosPost } from "./mockable";
 import { SdkShared } from "./sdkShared";
 
@@ -28,7 +28,12 @@ export class SdkBase extends SdkShared {
     const { requestContext, methodContext } = createLoggingContext(this.xcall.name);
     this.logger.info("Method start", requestContext, methodContext, { params });
 
-    const response = await axiosPost(`${this.baseUri}/xcall`, params);
+    const _options = params.options ?? {
+      chains: this.config.chains,
+      signerAddress: this.config.signerAddress,
+    };
+
+    const response = await axiosPost(`${this.baseUri}/xcall`, { ...params, options: _options });
     return response.data;
   }
 
@@ -36,7 +41,12 @@ export class SdkBase extends SdkShared {
     const { requestContext, methodContext } = createLoggingContext(this.updateSlippage.name);
     this.logger.info("Method start", requestContext, methodContext, { params });
 
-    const response = await axiosPost(`${this.baseUri}/updateSlippage`, params);
+    const _options = params.options ?? {
+      chains: this.config.chains,
+      signerAddress: this.config.signerAddress,
+    };
+
+    const response = await axiosPost(`${this.baseUri}/updateSlippage`, { ...params, options: _options });
     return response.data;
   }
 
@@ -44,7 +54,12 @@ export class SdkBase extends SdkShared {
     const { requestContext, methodContext } = createLoggingContext(this.bumpTransfer.name);
     this.logger.info("Method start", requestContext, methodContext, { params });
 
-    const response = await axiosPost(`${this.baseUri}/bumpTransfer`, params);
+    const _options = params.options ?? {
+      chains: this.config.chains,
+      signerAddress: this.config.signerAddress,
+    };
+
+    const response = await axiosPost(`${this.baseUri}/bumpTransfer`, { ...params, options: _options });
     return response.data;
   }
 
