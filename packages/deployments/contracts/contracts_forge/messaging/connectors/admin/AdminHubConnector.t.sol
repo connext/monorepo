ffb76a848c8488002d6a06fb0947a8cf3f68e178
Layne Haber <layne.haber@gmail.com>
2023-06-22 17:31:01 -0600

    fix: cleaner test utilities; add fuzzing; add proper asserts

--- packages/deployments/contracts/contracts_forge/messaging/connectors/admin/AdminHubConnector.t.sol
+++ packages/deployments/contracts/contracts_forge/messaging/connectors/admin/AdminHubConnector.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 import {MockSpokeConnector} from "../../../utils/Mock.sol";
 import {SpokeConnector} from "../../../../contracts/messaging/connectors/SpokeConnector.sol";
 import {AdminHubConnector} from "../../../../contracts/messaging/connectors/admin/AdminHubConnector.sol";
-import {RootManager} from "../../../../contracts/messaging/RootManager.sol";
+import {RootManager, ProposedOwnable} from "../../../../contracts/messaging/RootManager.sol";
 import {WatcherManager} from "../../../../contracts/messaging/WatcherManager.sol";
 import {MerkleTreeManager} from "../../../../contracts/messaging/MerkleTreeManager.sol";
 import {Message} from "../../../../contracts/messaging/libraries/Message.sol";
@@ -13,9 +13,11 @@ import {RateLimited} from "../../../../contracts/messaging/libraries/RateLimited
 import "../../../utils/ForgeHelper.sol";
 
 contract AdminHubConnectorTest is ForgeHelper {
-  event MessageSent(bytes data, bytes encodedData, address caller);
+  event MessageProcessed(bytes data, address caller);
+  event RootReceived(uint32 domain, bytes32 receivedRoot, uint256 queueIndex);
+  event RootsAggregated(bytes32 aggregateRoot, uint256 count, bytes32[] aggregatedMessageRoots);
 
-  error ProposedOwnable__onlyOwner_notOwner();
+  // error ProposedOwnable__onlyOwner_notOwner();
 
   using stdStorage for StdStorage;
 
@@ -23,9 +25,9 @@ contract AdminHubConnectorTest is ForgeHelper {
   uint32 BNB_DOMAIN = 6450786;
   uint32 ETH_DOMAIN = 6648936;
   address watcher = 0x43DB577bB3DD02989Dc3DC8e65E61a27d6914386;
-  address owner = 0x4d50a469fc788a3c0CdC8Fd67868877dCb246625;
   SpokeConnector ETH_SPOKE_CONNECTOR = SpokeConnector(payable(0xF7c4d7dcEc2c09A15f2Db5831d6d25eAEf0a296c));
   RootManager ROOT_MANAGER = RootManager(0xd5d61E9dfb6680Cba8353988Ba0337802811C2e1);
+  address owner;
 
   // ============ config
   AdminHubConnector adminHubConnector;
@@ -33,25 +35,20 @@ contract AdminHubConnectorTest is ForgeHelper {
   // ============ Setup ============
   function setUp() public {
     vm.createSelectFork("https://eth.llamarpc.com", 17528353);
+    owner = ROOT_MANAGER.owner();
     vm.prank(owner);
     adminHubConnector = new AdminHubConnector(ETH_DOMAIN, BNB_DOMAIN, address(ROOT_MANAGER));
   }
 
-  // ============ utils ============
-
-  function test_AdminHubConnector__addSpokeRootToAggregate_doesNotWorkIfNotOwner() public {
+  // ============ Utils ============
+  function utils_insertAdminConnector(uint32 _domain) internal {
     vm.prank(watcher);
-    ROOT_MANAGER.removeConnector(BNB_DOMAIN);
+    ROOT_MANAGER.removeConnector(_domain);
     vm.prank(owner);
-    ROOT_MANAGER.addConnector(BNB_DOMAIN, address(adminHubConnector));
-    vm.prank(address(1));
-    vm.expectRevert(ProposedOwnable__onlyOwner_notOwner.selector);
-    adminHubConnector.addSpokeRootToAggregate(
-      bytes32(0x0000000000000000000000000000000000000000000000000000000000000001)
-    );
+    ROOT_MANAGER.addConnector(_domain, address(adminHubConnector));
   }
 
-  function test_AdminHubConnector__addSpokeRootToAggregate_works() public {
+  function utils_getConnectors() internal view returns (address[] memory) {
     address[] memory connectors = new address[](6);
     connectors[0] = ROOT_MANAGER.connectors(0);
     connectors[1] = ROOT_MANAGER.connectors(1);
@@ -59,65 +56,78 @@ contract AdminHubConnectorTest is ForgeHelper {
     connectors[3] = ROOT_MANAGER.connectors(3);
     connectors[4] = ROOT_MANAGER.connectors(4);
     connectors[5] = ROOT_MANAGER.connectors(5);
+    return connectors;
+  }
 
-    uint256[] memory fees = new uint256[](6);
-    fees[0] = 0;
-    fees[1] = 0;
-    fees[2] = 0;
-    fees[3] = 0;
-    fees[4] = 0;
-    fees[5] = 0;
+  function utils_getFeesAndEncodedData(
+    uint256 _numConnectors
+  ) internal pure returns (uint256[] memory, bytes[] memory) {
+    uint256[] memory fees = new uint256[](_numConnectors);
+    bytes[] memory encodedData = new bytes[](_numConnectors);
+    for (uint256 i = 0; i < _numConnectors; i++) {
+      encodedData[i] = hex"";
+      fees[i] = 0;
+    }
+    return (fees, encodedData);
+  }
 
-    bytes[] memory encodedData = new bytes[](6);
-    encodedData[0] = hex"";
-    encodedData[1] = hex"";
-    encodedData[2] = hex"";
-    encodedData[3] = hex"";
-    encodedData[4] = hex"";
-    encodedData[5] = hex"";
+  // ============ addSpokeRootToAggregate ============
+
+  function test_AdminHubConnector__addSpokeRootToAggregate_doesNotWorkIfNotOwner(
+    bytes32 _data,
+    address _sender
+  ) public {
+    // fuzz testing assumptions
+    vm.assume(_sender != owner);
+    vm.assume(_data != bytes32(0));
+
+    // run test
+    utils_insertAdminConnector(BNB_DOMAIN);
+    vm.prank(_sender);
+    vm.expectRevert(ProposedOwnable.ProposedOwnable__onlyOwner_notOwner.selector);
+    adminHubConnector.addSpokeRootToAggregate(_data);
+  }
+
+  function test_AdminHubConnector__addSpokeRootToAggregate_failsIfEmpty() public {
+    utils_insertAdminConnector(BNB_DOMAIN);
+    vm.prank(owner);
+    vm.expectRevert("empty");
+    adminHubConnector.addSpokeRootToAggregate(bytes32(0));
+  }
+
+  function test_AdminHubConnector__addSpokeRootToAggregate_works(bytes32 _rootToInsert) public {
+    vm.assume(_rootToInsert != bytes32(0));
+
+    // get all inputs to root manager
+    address[] memory connectors = utils_getConnectors();
+    (uint256[] memory fees, bytes[] memory encodedData) = utils_getFeesAndEncodedData(connectors.length);
+
+    // ensure the rm will not propagate without a new spoke root added
     vm.expectRevert("redundant root");
     ROOT_MANAGER.propagate(connectors, fees, encodedData);
 
-    vm.prank(watcher);
-    ROOT_MANAGER.removeConnector(BNB_DOMAIN);
-    vm.prank(owner);
-    ROOT_MANAGER.addConnector(BNB_DOMAIN, address(adminHubConnector));
-    vm.prank(owner);
-    adminHubConnector.addSpokeRootToAggregate(
-      bytes32(0x80E1EE5091DFA6F8B129E28436D323ED6DA70F0A6D39B722323272017001F7E7)
-    );
+    // add the admin connector
+    utils_insertAdminConnector(BNB_DOMAIN);
+    // Refresh the connectors
+    connectors = utils_getConnectors();
 
+    // use admin connector to insert on spoke root (and assert events)
+    (, uint128 last) = ROOT_MANAGER.pendingInboundRoots();
+    vm.expectEmit(true, true, true, true, address(ROOT_MANAGER));
+    emit RootReceived(BNB_DOMAIN, _rootToInsert, last + 1);
+
+    vm.expectEmit(true, true, true, true, address(adminHubConnector));
+    emit MessageProcessed(abi.encode(_rootToInsert), owner);
+
+    vm.prank(owner);
+    adminHubConnector.addSpokeRootToAggregate(_rootToInsert);
+
+    // Ensure added root is propagated
     vm.roll(block.number + ROOT_MANAGER.delayBlocks() + 1); // roll enough blocks to pass delay
-
-    connectors = new address[](6);
-    connectors[0] = ROOT_MANAGER.connectors(0);
-    connectors[1] = ROOT_MANAGER.connectors(1);
-    connectors[2] = ROOT_MANAGER.connectors(2);
-    connectors[3] = ROOT_MANAGER.connectors(3);
-    connectors[4] = ROOT_MANAGER.connectors(4);
-    connectors[5] = ROOT_MANAGER.connectors(5);
-
-    fees = new uint256[](6);
-    fees[0] = 0;
-    fees[1] = 0;
-    fees[2] = 0;
-    fees[3] = 0;
-    fees[4] = 0;
-    fees[5] = 0;
-
-    encodedData = new bytes[](6);
-    encodedData[0] = hex"";
-    encodedData[1] = hex"";
-    encodedData[2] = hex"";
-    encodedData[3] = hex"";
-    encodedData[4] = hex"";
-    encodedData[5] = hex"";
-
+    // NOTE: this event check ensures the root manager emitted the event, but not the data within
+    // the event payload
+    vm.expectEmit(true, false, false, false, address(ROOT_MANAGER));
+    emit RootsAggregated(bytes32(0), 0, new bytes32[](1));
     ROOT_MANAGER.propagate(connectors, fees, encodedData);
-
-    vm.roll(block.number + ETH_SPOKE_CONNECTOR.delayBlocks() + 1); // roll enough blocks to pass delay
-
-    // generate proof for this tx: https://connextscan.io/tx/0x25b854f9c3c2177eacab398b0909aadfd5d0b380a33833bfc9903db74c6e0156?src=search
-    // was sent before the latest root was added, so it should work
   }
 }
