b95ae2cee5bf5ed3774730ce6be3b81ff6bb3e7e
just-a-node <eye1717@gmail.com>
2023-06-26 16:32:46 -0600

    chore: make stubbable

--- packages/agents/chain-abstraction/src/libs/shared/index.ts
+++ packages/agents/chain-abstraction/src/libs/shared/index.ts
@@ -1,3 +1,8 @@
+import { domainToChainId as _domainToChainId, chainIdToDomain as _chainIdToDomain } from "@connext/nxtp-utils";
+
+export const domainToChainId = _domainToChainId;
+export const chainIdToDomain = _chainIdToDomain;
+
 export * from "./quote";
 export * from "./address";
 export * from "./slippage";
