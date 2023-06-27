f7188563be49bc1769e54b844d4d4e1befcf4ac8
just-a-node <eye1717@gmail.com>
2023-06-27 17:39:29 -0600

    fix: handle more obj types

--- packages/agents/sdk-wrapper/test/utils.ts
+++ packages/agents/sdk-wrapper/test/utils.ts
@@ -1,29 +1,25 @@
-import { BigNumber } from "ethers";
-
-type HexNumber = {
-  type: "BigNumber";
-  hex: string;
-};
+import { BigNumber, BigNumberish } from "ethers";
 
 export function convertBigNumberObject<T extends Record<string, any>>(
-  res: T,
-): { [K in keyof T]: T[K] extends HexNumber ? BigNumber : T[K] } {
+  obj: T,
+): { [K in keyof T]: T[K] extends BigNumberish ? BigNumber : T[K] } {
   const result: any = {};
 
-  for (const key in res) {
-    const value = res[key];
+  for (const key in obj) {
+    const value = obj[key];
 
-    // If value is an object and not an array, recurse
-    if (value && typeof value === "object" && !Array.isArray(value)) {
-      if (value.type === "BigNumber") {
-        result[key] = BigNumber.from(value.hex);
-      } else {
-        result[key] = convertBigNumberObject(value);
-      }
+    if (typeof value === "object" && value !== null && "type" in value && "hex" in value) {
+      result[key] = BigNumber.from(value.hex);
+    } else if (Array.isArray(value)) {
+      result[key] = value.map((item: any) =>
+        typeof item === "object" && item !== null && "type" in item && "hex" in item ? BigNumber.from(item.hex) : item,
+      );
+    } else if (typeof value === "object" && !Array.isArray(value)) {
+      result[key] = convertBigNumberObject(value);
     } else {
       result[key] = value;
     }
   }
 
-  return result;
+  return result as { [K in keyof T]: T[K] extends BigNumberish ? BigNumber : T[K] };
 }
