a6fb2fd2d36fca25d387451a6b6debe46d0f72d7
wanglonghong <wanglonghong4467@gmail.com>
2023-07-05 19:56:37 +0800

    ci: add mainnet ops (#4636)

--- ops/mainnet/prod/core/main.tf
+++ ops/mainnet/prod/core/main.tf
@@ -297,6 +297,25 @@ module "sequencer_web3signer" {
   container_env_vars       = local.sequencer_web3signer_env_vars
 }
 
+module "lighthouse_prover_cron" {
+  source              = "../../../modules/lambda"
+  ecr_repository_name = "nxtp-lighthouse"
+  docker_image_tag    = var.lighthouse_image_tag
+  container_family    = "lighthouse-prover"
+  environment         = var.environment
+  stage               = var.stage
+  container_env_vars = merge(local.lighthouse_env_vars, {
+    LIGHTHOUSE_SERVICE = "prover-pub"
+  })
+  schedule_expression    = "rate(5 minutes)"
+  timeout                = 900
+  memory_size            = 10240
+  lambda_in_vpc          = true
+  private_subnets        = module.network.private_subnets
+  lambda_security_groups = flatten([module.network.allow_all_sg, module.network.ecs_task_sg])
+
+}
+
 module "lighthouse_prover_subscriber" {
   source                   = "../../../modules/service"
   stage                    = var.stage
