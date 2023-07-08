1433659a3da7b4e9d2adac6f77e67b7c633ea533
Layne Haber <layne.haber@gmail.com>
2023-07-08 13:33:46 -0600

    feat: add replay test for old transfers

--- packages/deployments/contracts/contracts_forge/fork/WormholeConnectorFork.t.sol
+++ packages/deployments/contracts/contracts_forge/fork/WormholeConnectorFork.t.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
 import {RootManager} from "../../contracts/messaging/RootManager.sol";
 import {MerkleTreeManager} from "../../contracts/messaging/MerkleTreeManager.sol";
 import {HubConnector} from "../../contracts/messaging/connectors/HubConnector.sol";
 import {SpokeConnector} from "../../contracts/messaging/connectors/SpokeConnector.sol";
 
 import "../utils/MotherForker.sol";
+import "../utils/ForgeHelper.sol";
 import {DeploymentLookup} from "../utils/DeploymentLookup.sol";
+import {WormholeUpgradeTransfers} from "./constants/WormholeUpgradeTransfers.sol";
 
 /**
  * @notice For this test suite to function properly, the connectors must be deployed
@@ -16,11 +20,13 @@ import {DeploymentLookup} from "../utils/DeploymentLookup.sol";
 contract WormholeConnectorForkTest is MotherForker {
   // ============ Libraries ============
   using stdJson for string;
+  using stdStorage for StdStorage;
   using Strings for string;
   using Strings for uint256;
 
   // ============ Storage ============
   DeploymentLookup deploymentLookup = new DeploymentLookup();
+  WormholeUpgradeTransfers transfers = new WormholeUpgradeTransfers();
 
   // ============ Deployed contracts
   SpokeConnector newSpokeConnector;
@@ -41,6 +47,8 @@ contract WormholeConnectorForkTest is MotherForker {
   function setUp() public {
     // make deployment lookup persistent
     vm.makePersistent(address(deploymentLookup));
+    // make test constants persistent
+    vm.makePersistent(address(transfers));
     // Create the fork
     utils_setupForkingEnv();
   }
@@ -78,12 +86,12 @@ contract WormholeConnectorForkTest is MotherForker {
     // console.log("spoke merkle:", address(merkle));
   }
 
-  function utils_upgradeConnectors() internal {
-    utils_selectFork(56);
+  function utils_upgradeConnectors(uint256 _spoke, uint256 _hub) internal {
+    utils_selectFork(_spoke);
     uint32 domain = uint32(bnbConnext.domain());
 
     // 0. remove the connector from root manager if it exists
-    utils_selectFork(1);
+    utils_selectFork(_hub);
     if (rootManager.isDomainSupported(domain)) {
       //   console.log("removing connector");
       vm.prank(rootManager.owner());
@@ -92,7 +100,7 @@ contract WormholeConnectorForkTest is MotherForker {
     }
 
     // 1. set the arborist on spoke
-    utils_selectFork(56);
+    utils_selectFork(_spoke);
     if (merkle.arborist() != address(newSpokeConnector)) {
       //   console.log("setting arborist");
       vm.prank(merkle.owner());
@@ -115,14 +123,56 @@ contract WormholeConnectorForkTest is MotherForker {
     // NOTE: no need to put in an if statement because it must be in this state
     // to get past step 0
     // console.log("adding connector");
-    utils_selectFork(1);
+    utils_selectFork(_hub);
     vm.prank(rootManager.owner());
     rootManager.addConnector(domain, address(newHubConnector));
     require(rootManager.isDomainSupported(domain), "failed to add connector");
   }
 
   // ============ Tests ==================
-  function test_testing() public {
-    utils_upgradeConnectors();
+  function test_WormholeConnectorFork__shouldNotReplayTransfer() public {
+    // Upgrade the bsc connectors
+    utils_upgradeConnectors(56, 1);
+
+    // Get on spoke fork
+    utils_selectFork(56);
+
+    // Get the transfer constants
+    WormholeUpgradeTransfers.Transfer memory transfer = transfers.getCompletedToBscTransfer();
+
+    // Insert the aggregate root on the spoke
+    // NOTE: this could be any future aggregate root, the paths would just change
+    uint256 startBlock = block.number - 10_000;
+    stdstore
+      .target(address(newSpokeConnector))
+      .sig("pendingAggregateRoots(bytes32)")
+      .with_key(transfer.aggregateRoot)
+      .depth(0)
+      .checked_write(startBlock);
+    require(
+      newSpokeConnector.pendingAggregateRoots(transfer.aggregateRoot) == startBlock,
+      "failed to insert aggregate root"
+    );
+
+    // Get initial supply of the local asset for the transfer
+    uint256 initialSupply = IERC20(transfer.local).totalSupply();
+    uint256 initialBalance = IERC20(transfer.local).balanceOf(address(bnbConnext));
+
+    // Try to prove and process the completed transfer
+    // NOTE: We do *NOT* expect this call to revert, but we expect it to be a
+    // no-op on the execution layer (no balance/supply changes on local asset)
+    SpokeConnector.Proof[] memory proofs = new SpokeConnector.Proof[](1);
+    proofs[0] = transfer.proof;
+    newSpokeConnector.proveAndProcess(proofs, transfer.aggregateRoot, transfer.aggregatePath, transfer.aggregateIndex);
+
+    require(newSpokeConnector.provenAggregateRoots(transfer.aggregateRoot), "failed to prove aggregate root");
+    require(
+      newSpokeConnector.messages(keccak256(transfer.proof.message)) == SpokeConnector.MessageStatus.Processed,
+      "failed to prove message"
+    );
+
+    // Get initial supply of the local asset for the transfer
+    require(initialSupply == IERC20(transfer.local).totalSupply(), "replayed transfer");
+    require(initialBalance == IERC20(transfer.local).balanceOf(address(bnbConnext)), "connext balance changed");
   }
 }
