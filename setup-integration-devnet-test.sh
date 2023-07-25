23ad3aab5c8e702f5a28415e324cda760272b26c
wanglonghong <wanglonghong4467@gmail.com>
2023-07-25 17:24:26 +0800

    feat: deploy chains, deploy contracts, deploy subgraph

--- setup-integration-devnet-test.sh
+++ setup-integration-devnet-test.sh
@@ -1,117 +1,63 @@
 #!/bin/bash
 ##### Config Variables
 LOCALHOST="127.0.0.1"
-# LOCALHOST="localhost"
-SEQUENCER_PORT="8081"
-ROUTER_PORT="8080"
-DEFAULT_MNEMONIC="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
-router_publisher_image="${ROUTER_PUBLISHER_IMAGE:-'router-publisher:latest'}"
-router_subscriber_image="${ROUTER_SUBSCRIBER_IMAGE:-'router-subscriber:latest'}"
-router_executor_image="${ROUTER_EXECUTOR_IMAGE:-'router-executor:latest'}"
-sequencer_server_image="${SEQUENCER_SERVER_IMAGE:-'sequencer-server:latest'}"
-sequencer_publisher_image="${SEQUENCER_PUBLISHER_IMAGE:-'sequencer-publisher:latest'}"
-sequencer_subscriber_image="${SEQUENCER_SUBSCRIBER_IMAGE:-'sequencer-subscriber:latest'}"
-relayer_image="${RELAYER_IMAGE:-'relayer:latest'}"
-cartographer_transfers_image="${CARTOGRAPHER_TRANSFERS_IMAGE:-'cartographer-transfers:latest'}"
-cartographer_routers_image="${CARTOGRAPHER_ROUTERS_IMAGE:-'cartographer-routers:latest'}"
-lighthouse_image="${LIGHTHOUSE_IMAGE:-'lighthouse:latest'}"
-WEB3_SIGNER_PRIVATE_KEY_ROUTER="0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3" # 0x627306090abaB3A6e1400e9345bC60c78a8BEf57
-WEB3_SIGNER_PRIVATE_KEY_SEQUENCER="0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f" # 0xf17f52151EbEF6C7334FAD080c5704D77216b732
-WEB3_SIGNER_PRIVATE_KEY_RELAYER="0x09ac2359ab897a5ffc47d7c67df32abbf9c006e00fa2ba36580e0cecc58834fe" # 0xc5842D5870622B406a71eeC1EcB2Df01D9dF5C28
-RELAYER_URL="http://relayer:8082"
+
+##### Devnet Deployments
+echo "Starting devnets..."
+yarn workspace @connext/smart-contracts run spawn-devnet
 #####
 
-##### Global .env Configuration
-echo "
-# Global test environment
-ROUTER_PORT=${ROUTER_PORT}
-SEQUENCER_PORT=${SEQUENCER_PORT}
-
-# Agent configuration
-NXTP_SEQUENCER=http://${LOCALHOST}:${SEQUENCER_PORT}
-NXTP_MNEMONIC=${DEFAULT_MNEMONIC}
-NXTP_SERVER_HOST=http://${LOCALHOST}
-NXTP_SERVER_PORT=${ROUTER_PORT}
-
-SEQ_SERVER_HOST=http://${LOCALHOST}
-SEQ_SERVER_PORT=${SEQUENCER_PORT}
-SEQ_ENVIRONMENT=production
-SEQ_RELAYER_URL=${RELAYER_URL}
-
-NXTP_CONFIG=config.local.json
-SEQ_CONFIG=config.local.json
-RELAYER_CONFIG=config.local.json
-
-NXTP_ENVIRONMENT=local
-NXTP_NOMAD_ENVIRONMENT=staging
-
-MNEMONIC=${DEFAULT_MNEMONIC}
-WEB3_SIGNER_PRIVATE_KEY_ROUTER=${WEB3_SIGNER_PRIVATE_KEY_ROUTER}
-WEB3_SIGNER_PRIVATE_KEY_SEQUENCER=${WEB3_SIGNER_PRIVATE_KEY_SEQUENCER}
-WEB3_SIGNER_PRIVATE_KEY_RELAYER=${WEB3_SIGNER_PRIVATE_KEY_RELAYER}
-
-# Images used for building docker containers
-ROUTER_PUBLISHER_IMAGE=${router_publisher_image}
-ROUTER_SUBSCRIBER_IMAGE=${router_subscriber_image}
-ROUTER_EXECUTOR_IMAGE=${router_executor_image}
-SEQUENCER_SERVER_IMAGE=${sequencer_server_image}
-SEQUENCER_PUBLISHER_IMAGE=${sequencer_publisher_image}
-SEQUENCER_SUBSCRIBER_IMAGE=${sequencer_subscriber_image}
-RELAYER_IMAGE=${relayer_image}
-CARTOGRAPHER_TRANSFERS_IMAGE=${cartographer_transfers_image}
-CARTOGRAPHER_ROUTERS_IMAGE=${cartographer_routers_image}
-LIGHTHOUSE_IMAGE=${lighthouse_image}
-
-# Optional:
-# AUCTION_ROUND_DEPTH
-# NXTP_SUBGRAPH_POLL_INTERVAL
-# NXTP_CACHE_POLL_INTERVAL
-" > .env
-#####
-
-##### Delete previous local_1337 and local_1338 chain deployment records if they exist.
-rm -rf -- packages/deployments/contracts/deployments/local_1337
-rm -rf -- packages/deployments/contracts/deployments/local_1338
-#####
-
-##### Local chains, graph nodes, and IPFS.
-echo "Starting 1337 and 1338 local chains..."
-docker compose -f docker-compose.chains.yaml up -d --force-recreate
+##### IPFS, postgres, graph nodes.
+echo "Starting ipfs, postgres and graph-node..."
+docker compose -f docker-compose.devnets.yaml up -d --force-recreate
 sleep 5
 #####
 
 ##### Contract Deployments
-echo "Deploying contracts to 1337..."
-# MNEMONIC="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" ENV=production CHAIN_ID=1338 ETH_PROVIDER_URL=http://localhost:8547 yarn workspace @connext/smart-contracts hardhat deploy --network local_1337 --tags local
-MNEMONIC=${DEFAULT_MNEMONIC} ENV=production CHAIN_ID=1337 ETH_PROVIDER_URL=http://${LOCALHOST}:8547 yarn workspace @connext/smart-contracts hardhat deploy --network local_1337 --tags local
-echo "Deployed contracts to 1337"
+echo "Deploying contracts to the hub chain - mainnet-devnet"
+### TODO: Add a command to apply the necessary changes for mainnet-devnet.
+echo "Deployed contracts to mainnet-devnet"
 
-echo "Deploying contracts to 1338..."
-# MNEMONIC="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" ENV=production CHAIN_ID=1338 ETH_PROVIDER_URL=http://localhost:8546 yarn workspace @connext/smart-contracts hardhat deploy --network local_1338 --tags local
-MNEMONIC=${DEFAULT_MNEMONIC} ENV=production CHAIN_ID=1338 ETH_PROVIDER_URL=http://${LOCALHOST}:8546 yarn workspace @connext/smart-contracts hardhat deploy --network local_1338 --tags local
-echo "Deployed contracts to 1338"
+echo "Deploying contracts to the spoke chain - optimism-devnet"
+### TODO: Add a command to apply the necessary changes for optimism-devnet.
+echo "Deployed contracts to optimism-devnet"
+
+echo "Deploying contracts to the spoke chain - arbitrum-devnet"
+### TODO: Add a command to apply the necessary changes for arbitrum-devnet.
+echo "Deployed contracts to arbitrum-devnet"
+#####
+
+##### Loading .env file contents
+echo "Loading .env contents"
+source .env
+echo ".env contents loaded"
 #####
 
 ##### Subgraph Deployments
-echo "Building subgraph..."
-yarn workspace @connext/nxtp-subgraph prepare:local_1337
+echo "Building subgraph for the hub chain - mainnet-devnet..."
+yarn workspace @connext/nxtp-subgraph prepare:devnet:amarok-hub-v0
 yarn workspace @connext/nxtp-subgraph codegen
-echo "Built subgraph"
+echo "Built subgraph for the hub chain"
 
-echo "Deploying subgraph to 1337..."
-yarn workspace @connext/nxtp-subgraph create-local-1337
-yarn workspace @connext/nxtp-subgraph deploy-local-1337 -l v0.0.1
-echo "Deployed subgraph to 1337"
+echo "Deploying subgraph to the hub chain - mainnet-devnet..."
+yarn workspace @connext/nxtp-subgraph create:hub_devnet_mainnet --node MAINNET_DEVNET_RPC_URL
+yarn workspace @connext/nxtp-subgraph deploy:hub_devnet_mainnet --node MAINNET_DEVNET_RPC_URL -l v0.0.1
+echo "Deployed subgraph to the hub chain - mainnet-devnet"
 
-echo "Building subgraph..."
-yarn workspace @connext/nxtp-subgraph prepare:local_1338
+echo "Building subgraph for the spoke chains - optimism-devnet & arbitrum-devnet..."
+yarn workspace @connext/nxtp-subgraph prepare:devnet:amarok-runtime-v0
 yarn workspace @connext/nxtp-subgraph codegen
-echo "Built subgraph"
+echo "Built subgraph for the spoke chains"
 
-echo "Deploying subgraph to 1338..."
-yarn workspace @connext/nxtp-subgraph create-local-1338
-yarn workspace @connext/nxtp-subgraph deploy-local-1338 -l v0.0.1
-echo "Deployed subgraph to 1338"
+echo "Deploying subgraph to the spoke chain - optimism-devnet..."
+yarn workspace @connext/nxtp-subgraph create:spoke_devnet_optimism --node OPTIMISM_DEVNET_RPC_URL
+yarn workspace @connext/nxtp-subgraph deploy:spoke_devnet_optimism --node OPTIMISM_DEVNET_RPC_URL -l v0.0.1
+echo "Deployed subgraphs to the optimism-devnet"
+
+echo "Deploying subgraph to the hub chain - arbitrum-devnet..."
+yarn workspace @connext/nxtp-subgraph create:spoke_devnet_arbitrum --node ARBITRUM_DEVNET_RPC_URL
+yarn workspace @connext/nxtp-subgraph deploy:spoke_devnet_arbitrum --node ARBITRUM_DEVNET_RPC_URL -l v0.0.1
+echo "Deployed subgraph to the arbitrum-devnet"
 #####
 
 ##### Off-Chain Agents
