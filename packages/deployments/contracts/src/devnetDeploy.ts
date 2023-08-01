a5142f58eb2898e6b16cd01ba3e330503e5e723d
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-08-01 20:21:46 +0800

    feat: 4718 smart contracts deploy dev net (#4757)
    * feat: install forge-deploy npm
    
    * fix: remove forge-deploy npm
    
    * feat: forge-deploy npm
    
    * feat: forge deploy basically works
    
    * feat: basically works
    
    * feat: remove forge-deploy
    
    * feat: deploy forge
    
    * forge install: solidity-stringutils
    
    * feat: solidity-stingutils
    
    * feat: messaging & connext deploy
    
    * feat: use gnosis instead of arbitrum
    
    * feat: multisend contract deploy
    
    * feat: deploy relayer proxy hub contract
    
    * chore: yarn.lock

---
+++ packages/deployments/contracts/src/devnetDeploy.ts
@@ -0,0 +1,64 @@
+import util from "util";
+import { exec as _exec } from "child_process";
+
+import { config as dotenvConfig } from "dotenv";
+import { Wallet } from "ethers";
+
+dotenvConfig();
+
+const exec = util.promisify(_exec);
+
+const { MAINNET_DEVNET_RPC_URL, OPTIMISM_DEVNET_RPC_URL, GNOSIS_DEVNET_RPC_URL, MNEMONIC, TENDERLY_ACCOUNT_ID } =
+  process.env;
+
+const deployToDevnets = async () => {
+  if (!MAINNET_DEVNET_RPC_URL || !OPTIMISM_DEVNET_RPC_URL || !GNOSIS_DEVNET_RPC_URL) {
+    throw new Error("Not found devnet rpcs");
+  }
+
+  const chainConfigs = [
+    {
+      network: "mainnet",
+      rpc: MAINNET_DEVNET_RPC_URL,
+    },
+    {
+      network: "optimism",
+      rpc: OPTIMISM_DEVNET_RPC_URL,
+    },
+    {
+      network: "gnosis",
+      rpc: GNOSIS_DEVNET_RPC_URL,
+    },
+  ];
+
+  const sender = Wallet.fromMnemonic(MNEMONIC!).address;
+  console.log("deployer", sender);
+
+  for (const config of chainConfigs) {
+    //funds to sender
+    const { stdout } = await exec(
+      `curl -H "Content-Type: application/json" -X POST --data '{
+        "jsonrpc": "2.0",
+        "method": "tenderly_setBalance",
+        "params": [
+          "${sender}",
+          "0x8AC7230489E8000000"
+        ],
+        "id": "${TENDERLY_ACCOUNT_ID}"
+      }' ${config.rpc}`,
+    );
+
+    console.log("set balance: ", stdout);
+    if (!JSON.parse(stdout)?.result) {
+      throw new Error(`failed to tenderly_setBalance, ${sender}, ${config.network}`);
+    }
+
+    const cmd = `DEPLOYMENT_CONTEXT=tenderly-${config.network} forge script scripts/Deploy.s.sol --rpc-url ${config.rpc} --broadcast --slow --mnemonics "${MNEMONIC}" --sender ${sender}  -vvvv && DEPLOYMENT_CONTEXT=tenderly-${config.network} forge script scripts/Deploy.s.sol --sig 'sync()' --rpc-url ${config.rpc} --broadcast --slow --mnemonics "${MNEMONIC}" --sender ${sender}  -v`;
+    // const cmd = `DEPLOYMENT_CONTEXT=tenderly-${config.network} forge script scripts/Deploy.s.sol --rpc-url ${config.rpc} --broadcast --slow --mnemonics "${MNEMONIC}" --sender ${sender}  -vvvvv `;
+    // const cmd = `DEPLOYMENT_CONTEXT=tenderly-${config.network} forge script scripts/Deploy.s.sol --sig 'sync()' --rpc-url ${config.rpc} --broadcast --slow --mnemonics "${MNEMONIC}" --sender ${sender}  -vvvv`;
+
+    _exec(cmd)?.stdout?.pipe(process.stdout);
+  }
+};
+
+deployToDevnets();
