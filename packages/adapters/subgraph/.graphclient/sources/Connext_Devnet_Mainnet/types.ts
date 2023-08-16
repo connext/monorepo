c5462e2217c00dfd636070434faf025017bc4190
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-08-16 11:12:38 +0800

    chore: rebuild

--- packages/adapters/subgraph/.graphclient/sources/Connext_Devnet_Mainnet/types.ts
+++ packages/adapters/subgraph/.graphclient/sources/Connext_Devnet_Mainnet/types.ts
@@ -3,7 +3,7 @@
 import { InContextSdkMethod } from '@graphql-mesh/types';
 import { MeshContext } from '@graphql-mesh/runtime';
 
-export namespace DevnetHubMainnetTypes {
+export namespace ConnextDevnetMainnetTypes {
   export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -1294,7 +1294,7 @@ export type _SubgraphErrorPolicy_ =
   };
 
   export type Context = {
-      ["Devnet_Hub_Mainnet"]: { Query: QuerySdk, Mutation: MutationSdk, Subscription: SubscriptionSdk },
+      ["Connext_Devnet_Mainnet"]: { Query: QuerySdk, Mutation: MutationSdk, Subscription: SubscriptionSdk },
       
     };
 }
