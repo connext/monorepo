f334acaf20f8ba9e6676a44414535118c30bd59c
sanchaymittal <sanchaymittal@gmail.com>
2023-09-25 18:49:13 +0700

    chore: add abi for spoke and hub domain

--- packages/deployments/contracts/src/abi.ts
+++ packages/deployments/contracts/src/abi.ts
@@ -2,8 +2,12 @@ import _ConnextAbi from "../abi/contracts/core/connext/interfaces/IConnext.sol/I
 import _TokenFacetAbi from "../abi/contracts/core/connext/facets/TokenFacet.sol/TokenFacet.json";
 import _BridgeFacetAbi from "../abi/contracts/core/connext/facets/BridgeFacet.sol/BridgeFacet.json";
 import _InboxFacetAbi from "../abi/contracts/core/connext/facets/InboxFacet.sol/InboxFacet.json";
+import _SpokeConnectorAbi from "../abi/contracts/messaging/connectors/SpokeConnector.sol/SpokeConnector.json";
+import _HubConnectorAbi from "../abi/contracts/messaging/connectors/HubConnector.sol/HubConnector.json";
 
 export const ConnextAbi = _ConnextAbi;
 export const TokenFacetAbi = _TokenFacetAbi;
 export const BridgeFacetAbi = _BridgeFacetAbi;
 export const InboxFacetAbi = _InboxFacetAbi;
+export const SpokeConnectorAbi = _SpokeConnectorAbi;
+export const HubConnectorAbi = _HubConnectorAbi;
