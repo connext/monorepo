f833340bf2983de54da35707e202e7b1d374b27f
sanchaymittal <sanchaymittal@gmail.com>
2023-09-25 18:49:40 +0700

    chore: add connectors interface

--- packages/deployments/contracts/src/contracts.ts
+++ packages/deployments/contracts/src/contracts.ts
@@ -1,7 +1,9 @@
 import { utils } from "ethers";
 
-import { ConnextAbi, TokenFacetAbi, BridgeFacetAbi, InboxFacetAbi } from "./abi";
+import { ConnextAbi, TokenFacetAbi, BridgeFacetAbi, InboxFacetAbi, SpokeConnectorAbi, HubConnectorAbi } from "./abi";
 import { ConnextInterface as TConnextInterface } from "./typechain-types/hardhat-diamond-abi/HardhatDiamondABI.sol/Connext";
+import { SpokeConnectorInterface as TSpokeConnectorInterface } from "./typechain-types/contracts/messaging/connectors/SpokeConnector";
+import { HubConnectorInterface as THubConnectorInterface } from "./typechain-types/contracts/messaging/connectors/HubConnector";
 
 const connextAbi = ConnextAbi.concat(
   TokenFacetAbi.filter((k) => k.includes("event"))
@@ -9,3 +11,5 @@ const connextAbi = ConnextAbi.concat(
     .concat(InboxFacetAbi.filter((k) => k.includes("event"))),
 );
 export const ConnextInterface = new utils.Interface(connextAbi) as TConnextInterface;
+export const SpokeConnectorInterface = new utils.Interface(SpokeConnectorAbi) as TSpokeConnectorInterface;
+export const HubConnectorInterface = new utils.Interface(HubConnectorAbi) as THubConnectorInterface;
