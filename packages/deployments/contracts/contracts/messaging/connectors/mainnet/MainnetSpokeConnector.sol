f9c5de457b2f0e40ca181a58ead693d4fa0cc2be
0xng <ng@defi.sucks>
2023-10-09 09:58:37 -0300

    docs: natspec typo

--- packages/deployments/contracts/contracts/messaging/connectors/mainnet/MainnetSpokeConnector.sol
+++ packages/deployments/contracts/contracts/messaging/connectors/mainnet/MainnetSpokeConnector.sol
@@ -72,7 +72,7 @@ contract MainnetSpokeConnector is SpokeConnector, IHubSpokeConnector {
   }
 
   /**
-   * @notice Propose a new aggregate root.
+   * @notice Proposes a new aggregate root.
    * @dev Reverts in the hub domain as there's no need to propose nor finalize.
    * @param _aggregateRoot The aggregate root to propose.
    * @param _rootTimestamp Block.timestamp at which the root was finalized in the root manager contract.
