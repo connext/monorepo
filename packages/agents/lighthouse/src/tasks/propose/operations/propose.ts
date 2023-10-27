fd629b46752389f80eb0b5835ead8b82f3910ea3
preethamr <preethamr@users.noreply.github.com>
2023-10-26 21:50:34 -0700

    fix: update lh prover for initial conditions

--- packages/agents/lighthouse/src/tasks/propose/operations/propose.ts
+++ packages/agents/lighthouse/src/tasks/propose/operations/propose.ts
@@ -6,6 +6,7 @@ import {
   SparseMerkleTree,
   jsonifyError,
   domainToChainId,
+  EMPTY_ROOT,
 } from "@connext/nxtp-utils";
 import { BigNumber } from "ethers";
 
@@ -26,7 +27,6 @@ export type ExtraPropagateParam = {
   _fee: string;
   _encodedData: string;
 };
-const EMPTY_ROOT = "0x27ae5ba08d7291c96c8cbddcc148bf48a6d68c7974b94356f53754ef6171d757";
 
 export const proposeHub = async () => {
   const {
