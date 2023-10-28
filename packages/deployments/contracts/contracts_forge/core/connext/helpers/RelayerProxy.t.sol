d647a7f7fdfbaab959a39a82a0aafa44c524b14a
preethamr <preethamr@users.noreply.github.com>
2023-10-27 22:52:02 -0700

    fix: resove relayerproxy test

--- packages/deployments/contracts/contracts_forge/core/connext/helpers/RelayerProxy.t.sol
+++ packages/deployments/contracts/contracts_forge/core/connext/helpers/RelayerProxy.t.sol
@@ -25,6 +25,7 @@ contract RelayerProxyTest is ForgeHelper {
   address _gelatoRelayer = address(123444412);
   address _feeCollector = address(12335555);
   address _keep3r = address(12335556);
+  uint256 _proposeAggregateRootCooldown = 1000;
 
   RelayerProxy proxy;
 
@@ -46,7 +47,14 @@ contract RelayerProxyTest is ForgeHelper {
     vm.expectEmit(true, true, true, true);
     emit RelayerAdded(_gelatoRelayer);
 
-    proxy = new RelayerProxy(_connext, _spokeConnector, _gelatoRelayer, _feeCollector, _keep3r);
+    proxy = new RelayerProxy(
+      _connext,
+      _spokeConnector,
+      _gelatoRelayer,
+      _feeCollector,
+      _keep3r,
+      _proposeAggregateRootCooldown
+    );
   }
 
   function test_RelayerProxy__deploy_works() public {
