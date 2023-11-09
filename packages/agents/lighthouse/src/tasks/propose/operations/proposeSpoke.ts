ba68a6e5f3cd0d1c07aed68195cd67cc916fdca8
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2023-11-09 22:24:28 +0800

    feat: send tx to relayer proxy

--- packages/agents/lighthouse/src/tasks/propose/operations/proposeSpoke.ts
+++ packages/agents/lighthouse/src/tasks/propose/operations/proposeSpoke.ts
@@ -207,16 +207,16 @@ export const sendRootToHubSpoke = async (_requestContext: RequestContext) => {
   const { requestContext, methodContext } = createLoggingContext(sendRootToHubSpoke.name, _requestContext);
   logger.info("Starting sendRootToHubSpoke operation for hub domain", requestContext, methodContext);
 
-  const rootManagerAddress = config.chains[config.hubDomain].deployments.rootManager;
+  const relayerProxyAddress = config.chains[config.hubDomain].deployments.relayerProxy;
   const hubChainId = domainToChainId(+config.hubDomain);
 
-  const encodedDataForRelayer = contracts.rootManager.encodeFunctionData("sendRootToHubSpoke");
+  const encodedDataForRelayer = contracts.relayerProxyHub.encodeFunctionData("sendRootToHubSpoke");
 
   try {
     const { taskId } = await sendWithRelayerWithBackup(
       hubChainId,
       config.hubDomain,
-      rootManagerAddress,
+      relayerProxyAddress,
       encodedDataForRelayer,
       relayers,
       chainreader,
@@ -229,7 +229,7 @@ export const sendRootToHubSpoke = async (_requestContext: RequestContext) => {
     logger.error("Error at sendWithRelayerWithBackup", requestContext, methodContext, e as NxtpError, {
       hubChainId,
       hubDomain: config.hubDomain,
-      rootManagerAddress,
+      relayerProxyAddress,
       encodedDataForRelayer,
     });
   }
