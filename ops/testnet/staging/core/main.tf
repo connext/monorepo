01b819932dd176f01dd592c6af35f8d5b5ef7e49
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-11-13 09:38:39 +0100

    fix: enable lambda-in-vpc for lh propose

--- ops/testnet/staging/core/main.tf
+++ ops/testnet/staging/core/main.tf
@@ -292,23 +292,7 @@ module "sequencer_web3signer" {
   container_env_vars       = local.sequencer_web3signer_env_vars
 }
 
-module "lighthouse_prover_cron" {
-  source              = "../../../modules/lambda"
-  ecr_repository_name = "nxtp-lighthouse"
-  docker_image_tag    = var.lighthouse_image_tag
-  container_family    = "lighthouse-prover"
-  environment         = var.environment
-  stage               = var.stage
-  container_env_vars = merge(local.lighthouse_env_vars, {
-    LIGHTHOUSE_SERVICE = "prover-pub"
-  })
-  schedule_expression    = "rate(30 minutes)"
-  timeout                = 300
-  memory_size            = 512
-  lambda_in_vpc          = true
-  subnet_ids             = module.network.private_subnets
-  lambda_security_groups = flatten([module.network.allow_all_sg, module.network.ecs_task_sg])
-}
+
 
 module "lighthouse_prover_subscriber" {
   source                   = "../../../modules/service"
@@ -419,16 +403,19 @@ module "lighthouse_web3signer" {
 }
 
 module "lighthouse_propose_cron" {
-  source              = "../../../modules/lambda"
-  ecr_repository_name = "nxtp-lighthouse"
-  docker_image_tag    = var.lighthouse_image_tag
-  container_family    = "lighthouse-propose"
-  environment         = var.environment
-  stage               = var.stage
-  container_env_vars  = merge(local.lighthouse_env_vars, { LIGHTHOUSE_SERVICE = "propose" })
-  schedule_expression = "rate(30 minutes)"
-  memory_size         = 1536
-  timeout             = 900
+  source                 = "../../../modules/lambda"
+  ecr_repository_name    = "nxtp-lighthouse"
+  docker_image_tag       = var.lighthouse_image_tag
+  container_family       = "lighthouse-propose"
+  environment            = var.environment
+  stage                  = var.stage
+  container_env_vars     = merge(local.lighthouse_env_vars, { LIGHTHOUSE_SERVICE = "propose" })
+  schedule_expression    = "rate(30 minutes)"
+  memory_size            = 1536
+  timeout                = 900
+  lambda_in_vpc          = true
+  subnet_ids             = module.network.private_subnets
+  lambda_security_groups = flatten([module.network.allow_all_sg, module.network.ecs_task_sg])
 }
 
 module "relayer" {
