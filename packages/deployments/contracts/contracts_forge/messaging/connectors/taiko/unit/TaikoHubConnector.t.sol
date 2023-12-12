0850bee9326a3cdeea88c6679bc9156599001f6f
0xDiscotech <0xdiscotech@defi.sucks>
2023-12-12 19:56:56 -0300

    chore: use start prank on expect revert unit test

--- packages/deployments/contracts/contracts_forge/messaging/connectors/taiko/unit/TaikoHubConnector.t.sol
+++ packages/deployments/contracts/contracts_forge/messaging/connectors/taiko/unit/TaikoHubConnector.t.sol
@@ -124,7 +124,7 @@ contract Unit_Connector_TaikoHubConnector_ProcessMessage is Base {
 
   function test_revertIfSenderNotAgent(address _sender, bytes memory _data) public {
     vm.assume(_sender != address(_amb));
-    vm.prank(_sender);
+    vm.startPrank(_sender);
     vm.expectRevert(TaikoHubConnector.TaikoHubConnector_SenderNotAllowedAgent.selector);
     taikoHubConnector.forTest_processMessage(_data);
   }
