6743a1c28831bc8beaaf5d85cae21213e3bad7b1
Layne Haber <layne.haber@gmail.com>
2023-12-11 17:34:49 -0700

    config: lh proposer, relayer fee vault as protocol admins

--- packages/deployments/contracts/src/cli/init/config/mainnet/production.ts
+++ packages/deployments/contracts/src/cli/init/config/mainnet/production.ts
@@ -1,6 +1,7 @@
 import { utils } from "ethers";
 
 import { InitConfig } from "../../helpers";
+import { PROTOCOL_ADMINS } from "../../../ownership/helpers";
 
 export const MAINNET_PRODUCTION_INIT_CONFIG: InitConfig = {
   hub: "6648936", // MAINNET
@@ -598,13 +599,13 @@ export const MAINNET_PRODUCTION_INIT_CONFIG: InitConfig = {
   ],
   agents: {
     relayerFeeVaults: {
-      "6648936": "0x4d50a469fc788a3c0CdC8Fd67868877dCb246625",
-      "1869640809": "0x6eCeD04DdC5A7709d5877c963cED0288Fb1c7348",
-      "1886350457": "0x0970Adeb473609F91D03e9Bba85F49C445040cD7",
-      "1634886255": "0x5C711DB90dEc0a5B81C626968DEa4187a7f9C1F2",
-      "6450786": "0x9435Ba7C661a0Fd477deED640491de8c100325A7",
-      "6778479": "0x7616Bc6d0dee5E250BA5b3dDa6cbbB71786FB638",
-      "1818848877": "0x50d22a34183e7cD3164184011998a3D50303f75d",
+      "6648936": PROTOCOL_ADMINS.mainnet["6648936"],
+      "1869640809": PROTOCOL_ADMINS.mainnet["1869640809"],
+      "1886350457": PROTOCOL_ADMINS.mainnet["1886350457"],
+      "1634886255": PROTOCOL_ADMINS.mainnet["1634886255"],
+      "6450786": PROTOCOL_ADMINS.mainnet["6450786"],
+      "6778479": PROTOCOL_ADMINS.mainnet["6778479"],
+      "1818848877": PROTOCOL_ADMINS.mainnet["1818848877"],
     },
     watchers: {
       allowlist: ["0xade09131C6f43fe22C2CbABb759636C43cFc181e", "0x9c77788d761ee0347Ab550883237CeD274a0F248"],
@@ -643,6 +644,11 @@ export const MAINNET_PRODUCTION_INIT_CONFIG: InitConfig = {
         "0x75bA5Af8EFFDCFca32E1e288806d54277D1fde99", // gelato 1balance
         "0x935AaAe0f5b02007c08512F0629a9d37Af2E1A47", // connext relayer
         // "0x99a039d4F0e734aA8CcBE74C0FF9780BccD79f1d", // gelato 1balance zksync era
+      ],
+    },
+    proposers: {
+      allowlist: [
+        "0x163Ac207A8A9b9675bE139256c4d0dc19BBfb93B", // lighthouse
       ],
     },
   },
