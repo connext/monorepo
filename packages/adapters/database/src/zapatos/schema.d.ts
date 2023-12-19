cac51ba5e7755779178e6ca99c004c0fa336fd9f
preethamr <preethamr@users.noreply.github.com>
2023-12-18 17:47:51 -0800

    fix: add snaphots primary key

--- packages/adapters/database/src/zapatos/schema.d.ts
+++ packages/adapters/database/src/zapatos/schema.d.ts
@@ -5603,7 +5603,7 @@ declare module 'zapatos/schema' {
       */
       finalized_timestamp?: number | db.Parameter<number> | null | db.DefaultType | db.SQLFragment | db.SQLFragment<any, number | db.Parameter<number> | null | db.DefaultType | db.SQLFragment>;
     }
-    export type UniqueIndex = 'snapshots_id_key';
+    export type UniqueIndex = 'snapshots_id_key' | 'snapshots_id_pkey';
     export type Column = keyof Selectable;
     export type OnlyCols<T extends readonly Column[]> = Pick<Selectable, T[number]>;
     export type SQLExpression = Table | db.ColumnNames<Updatable | (keyof Updatable)[]> | db.ColumnValues<Updatable> | Whereable | Column | db.ParentColumn | db.GenericSQLExpression;
