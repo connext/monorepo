5e841fd0a125c6d7b6a886933e673c10f6bc949f
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2024-01-06 13:47:12 +0800

    fix: connext relayer minGasPrice (#5476)
    * fix: connext relayer minGasPrice
    
    * chore: lint

--- ops/testnet/prod/core/config.tf
+++ ops/testnet/prod/core/config.tf
@@ -401,7 +401,8 @@ locals {
         providers = ["https://base-goerli.g.alchemy.com/v2/${var.basegoerli_alchemy_key_0}", "https://lb.drpc.org/ogrpc?network=base-goerli&dkey=${var.drpc_key}", "https://goerli.base.org"]
       }
       "2016506996" = {
-        providers = ["https://testrpc.x1.tech", "https://x1testrpc.okx.com/"]
+        providers = ["https://testrpc.x1.tech", "https://x1testrpc.okx.com/"],
+        minGasPrice = "300000000000"
       }
     }
     environment   = var.stage
