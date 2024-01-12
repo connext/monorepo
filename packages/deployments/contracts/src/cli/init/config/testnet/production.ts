1489e2c8dc907baa3079fda8b3664fd696badf6f
just-a-node <eye1717@gmail.com>
2024-01-12 16:28:46 -0700

    feat: redeploy of testnet sDAI

--- packages/deployments/contracts/src/cli/init/config/testnet/production.ts
+++ packages/deployments/contracts/src/cli/init/config/testnet/production.ts
@@ -348,19 +348,19 @@ export const TESTNET_PRODUCTION_INIT_CONFIG: InitConfig = {
       name: "sDAI",
       canonical: {
         domain: "11111",
-        address: "0x110753D17A81fc7B4eA994abaCA3270f9B6Fe2BA",
+        address: "0x272DF088C3bDafeDCa5f66D76B1372A6091eFc64",
         decimals: 18,
       },
       representations: {
         /// Goerli
         "1735353714": {
-          local: "0x110753D17A81fc7B4eA994abaCA3270f9B6Fe2BA",
-          adopted: "0x110753D17A81fc7B4eA994abaCA3270f9B6Fe2BA",
+          local: "0x272DF088C3bDafeDCa5f66D76B1372A6091eFc64",
+          adopted: "0x272DF088C3bDafeDCa5f66D76B1372A6091eFc64",
         },
         /// Mumbai
         "9991": {
-          local: "0x110753D17A81fc7B4eA994abaCA3270f9B6Fe2BA",
-          adopted: "0x110753D17A81fc7B4eA994abaCA3270f9B6Fe2BA",
+          local: "0x272DF088C3bDafeDCa5f66D76B1372A6091eFc64",
+          adopted: "0x272DF088C3bDafeDCa5f66D76B1372A6091eFc64",
         },
       },
     },
