5d45589e12dfa976f93c06f31d1c5f51f9e2737a
Liu <57480598+liu-zhipeng@users.noreply.github.com>
2024-02-28 10:48:58 +0800

    feat: scroll network config

--- packages/deployments/contracts/src/utils.ts
+++ packages/deployments/contracts/src/utils.ts
@@ -62,6 +62,7 @@ export const ProtocolNetworks: Record<string, string> = {
   "324": ProtocolNetwork.MAINNET,
   "5000": ProtocolNetwork.MAINNET,
   "34443": ProtocolNetwork.MAINNET,
+  "534352": ProtocolNetwork.MAINNET,
 };
 
 export const isDevnetName = (_name: string): boolean => {
