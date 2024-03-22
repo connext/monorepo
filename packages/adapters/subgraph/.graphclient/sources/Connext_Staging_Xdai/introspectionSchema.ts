fa05f36ff0db91e23ffae27f809d35fa68c79e4a
Layne Haber <layne.haber@gmail.com>
2024-03-22 12:26:08 -0600

    feat: rebuild graph client

--- packages/adapters/subgraph/.graphclient/sources/Connext_Staging_Xdai/introspectionSchema.ts
+++ packages/adapters/subgraph/.graphclient/sources/Connext_Staging_Xdai/introspectionSchema.ts
@@ -49924,6 +49924,27 @@ const schemaAST = {
             }
           },
           "directives": []
+        },
+        {
+          "kind": "FieldDefinition",
+          "description": {
+            "kind": "StringValue",
+            "value": "The hash of the parent block",
+            "block": true
+          },
+          "name": {
+            "kind": "Name",
+            "value": "parentHash"
+          },
+          "arguments": [],
+          "type": {
+            "kind": "NamedType",
+            "name": {
+              "kind": "Name",
+              "value": "Bytes"
+            }
+          },
+          "directives": []
         }
       ],
       "interfaces": [],
