6ab67dd765a6dd0315439cd2efa006bb80517cea
Layne Haber <layne.haber@gmail.com>
2024-04-09 17:48:22 -0600

    feat: track roots sent from L1 to L2 in subgraph

--- packages/deployments/subgraph/src/amarok-hub-staging/mapping/xlayer.ts
+++ packages/deployments/subgraph/src/amarok-hub-staging/mapping/xlayer.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-const */
-import { NewConnector, MessageProcessed } from "../../../generated/XlayerHubConnector/XlayerHubConnector";
-import { XlayerConnectorMeta, RootMessageProcessed } from "../../../generated/schema";
+import { NewConnector, MessageProcessed, MessageSent } from "../../../generated/XlayerHubConnector/XlayerHubConnector";
+import { XlayerConnectorMeta, RootMessageProcessed, RootMessageSent } from "../../../generated/schema";
 
 const DEFAULT_XLAYER_HUB_CONNECTOR_META_ID = "XLAYER_HUB_CONNECTOR_META_ID";
 
@@ -29,6 +29,29 @@ export function handleXlayerMessageProcessed(event: MessageProcessed): void {
   let message = RootMessageProcessed.load(`${event.params.data.toHexString()}-${meta.spokeDomain.toString()}`);
   if (message == null) {
     message = new RootMessageProcessed(`${event.params.data.toHexString()}-${meta.spokeDomain.toString()}`);
+  }
+
+  message.spokeDomain = meta.spokeDomain;
+  message.hubDomain = meta.hubDomain;
+
+  message.root = event.params.data;
+  message.caller = event.params.caller;
+  message.transactionHash = event.transaction.hash;
+  message.timestamp = event.block.timestamp;
+  message.gasPrice = event.transaction.gasPrice;
+  message.gasLimit = event.transaction.gasLimit;
+  message.blockNumber = event.block.number;
+  message.save();
+}
+
+export function handleXlayerMessageSent(event: MessageSent): void {
+  let meta = XlayerConnectorMeta.load(DEFAULT_XLAYER_HUB_CONNECTOR_META_ID);
+  if (meta == null) {
+    meta = new XlayerConnectorMeta(DEFAULT_XLAYER_HUB_CONNECTOR_META_ID);
+  }
+  let message = RootMessageSent.load(`${event.params.data.toHexString()}-${meta.spokeDomain.toString()}`);
+  if (message == null) {
+    message = new RootMessageSent(`${event.params.data.toHexString()}-${meta.spokeDomain.toString()}`);
   }
 
   message.spokeDomain = meta.spokeDomain;
