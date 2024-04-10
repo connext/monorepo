ad183ff030a0291b0ad1bb3879a735b988033a05
wanglonghong <wanglonghong4467@gmail.com>
2024-04-10 10:45:04 +0800

    Revert "Hotfix inc relayerfee tolerance"

--- ops/mainnet/prod/core/config.tf
+++ ops/mainnet/prod/core/config.tf
@@ -154,7 +154,7 @@ locals {
         url    = "https://${module.relayer_server.service_endpoint}"
       }
     ]
-    relayerFeeTolerance = 99
+    relayerFeeTolerance = 85
     checkOnlyExecuteFee = false
     environment         = var.stage
     database = {
