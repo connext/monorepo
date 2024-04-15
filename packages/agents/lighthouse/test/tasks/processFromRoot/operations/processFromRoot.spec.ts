72b9ca2bee42252b1bc8d06e434b96195cba7b55
Layne Haber <layne.haber@gmail.com>
2024-04-15 16:04:00 -0600

    fix: existing tests

--- packages/agents/lighthouse/test/tasks/processFromRoot/operations/processFromRoot.spec.ts
+++ packages/agents/lighthouse/test/tasks/processFromRoot/operations/processFromRoot.spec.ts
@@ -9,15 +9,19 @@ import {
   mkBytes32,
   RelayerTaskStatus,
   getNtpTimeSeconds,
+  mkAddress,
+  RequestContext,
 } from "@connext/nxtp-utils";
 import { SinonStub, stub } from "sinon";
 
 import * as ProcessFromRootFns from "../../../../src/tasks/processFromRoot/operations/processFromRoot";
+import * as ProcessFromHelperRootFns from "../../../../src/tasks/processFromRoot/helpers";
 import { CouldNotFindRelayer } from "../../../../src/tasks/processFromRoot/errors";
 import * as MockableFns from "../../../../src/mockable";
 import { processFromRootCtxMock, sendWithRelayerWithBackupStub } from "../../../globalTestHook";
 import { ProcessConfigNotAvailable } from "../../../../src/tasks/processFromRoot/errors";
 import { DEFAULT_RELAYER_WAIT_TIME } from "../../../../src/config";
+import { WriteTransaction } from "@connext/nxtp-txservice";
 
 const requestContext = createRequestContext("test");
 describe("Operations: ProcessFromRoot", () => {
@@ -27,7 +31,13 @@ describe("Operations: ProcessFromRoot", () => {
     beforeEach(() => {
       configStub = stub(ProcessFromRootFns, "processorConfigs").value({
         [mock.entity.rootMessage().spokeDomain]: {
-          getArgs: () => Promise.resolve(["tx", 123, [], []]),
+          getWriteTransaction: () =>
+            Promise.resolve({
+              domain: +mock.entity.rootMessage().spokeDomain,
+              to: mkAddress("0xdeadbeef"),
+              value: "0",
+              data: mkBytes32("0x1232131"),
+            } as WriteTransaction),
           hubConnectorPrefix: "Optimism",
         },
       });
@@ -36,13 +46,13 @@ describe("Operations: ProcessFromRoot", () => {
 
     it("should process message from root", async () => {
       const rootMsg = mock.entity.rootMessage();
-      await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      await ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext);
       expect(sendWithRelayerWithBackupStub).to.have.been.calledOnce;
     });
 
     it("should process message from root", async () => {
       const rootMsg = mock.entity.rootMessage();
-      await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      await ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext);
       expect(sendWithRelayerWithBackupStub).to.have.been.calledOnce;
     });
 
@@ -51,16 +61,19 @@ describe("Operations: ProcessFromRoot", () => {
       rootMsg.sentTaskId = mkBytes32("0xdeadbeef");
       rootMsg.relayerType = RelayerType.Connext;
 
-      await expect(ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext)).to.be.rejectedWith(
-        CouldNotFindRelayer,
-      );
+      await expect(
+        ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext),
+      ).to.be.rejectedWith(CouldNotFindRelayer);
     });
 
     it("should not send if successfully sent", async () => {
       const rootMsg = mock.entity.rootMessage();
       rootMsg.sentTaskId = mkBytes32("0xdeadbeef");
       rootMsg.relayerType = RelayerType.Mock;
-      const ret = await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      const ret = await ProcessFromRootFns.processSingleRootMessage(
+        { ...rootMsg, isSpokeClaim: false },
+        requestContext,
+      );
       expect(ret).to.be.eq(undefined);
       expect(sendWithRelayerWithBackupStub).to.not.have.been.called;
     });
@@ -72,7 +85,7 @@ describe("Operations: ProcessFromRoot", () => {
       (processFromRootCtxMock.adapters.relayers[0].instance.getTaskStatus as SinonStub).resolves(
         RelayerTaskStatus.ExecPending,
       );
-      await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      await ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext);
       expect(sendWithRelayerWithBackupStub).to.have.been.calledOnce;
     });
 
@@ -83,7 +96,7 @@ describe("Operations: ProcessFromRoot", () => {
       (processFromRootCtxMock.adapters.relayers[0].instance.getTaskStatus as SinonStub).resolves(
         RelayerTaskStatus.Cancelled,
       );
-      await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      await ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext);
       expect(sendWithRelayerWithBackupStub).to.have.been.calledOnce;
     });
 
@@ -95,7 +108,10 @@ describe("Operations: ProcessFromRoot", () => {
       (processFromRootCtxMock.adapters.relayers[0].instance.getTaskStatus as SinonStub).resolves(
         RelayerTaskStatus.ExecPending,
       );
-      const ret = await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      const ret = await ProcessFromRootFns.processSingleRootMessage(
+        { ...rootMsg, isSpokeClaim: false },
+        requestContext,
+      );
       expect(ret).to.be.eq(undefined);
       expect(sendWithRelayerWithBackupStub).to.callCount(0);
     });
@@ -108,19 +124,20 @@ describe("Operations: ProcessFromRoot", () => {
       (processFromRootCtxMock.adapters.relayers[0].instance.getTaskStatus as SinonStub).resolves(
         RelayerTaskStatus.ExecPending,
       );
-      await ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext);
+      console.log("expecting root message", rootMsg);
+      await ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext);
       expect(sendWithRelayerWithBackupStub).to.have.been.calledOnce;
-      expect(processFromRootCtxMock.adapters.database.saveSentRootMessages).to.have.been.calledOnceWithExactly([
-        rootMsg,
+      expect(processFromRootCtxMock.adapters.database.saveSentRootMessages).to.have.been.calledOnceWith([
+        { ...rootMsg, isSpokeClaim: false, sentTimestamp: getNtpTimeSeconds(), sentTaskId: mkBytes32("0xabcdef123") },
       ]);
     });
 
     it("should error if no config", async () => {
       configStub.value({});
       const rootMsg = mock.entity.rootMessage();
-      await expect(ProcessFromRootFns.processSingleRootMessage(rootMsg, requestContext)).to.be.rejectedWith(
-        ProcessConfigNotAvailable,
-      );
+      await expect(
+        ProcessFromRootFns.processSingleRootMessage({ ...rootMsg, isSpokeClaim: false }, requestContext),
+      ).to.be.rejectedWith(ProcessConfigNotAvailable);
     });
   });
 
@@ -140,6 +157,7 @@ describe("Operations: ProcessFromRoot", () => {
           blockNumber: number;
           processed: boolean;
           count: number;
+          isSpokeClaim: boolean;
         },
         requestContext: BaseRequestContext,
       ],
@@ -156,23 +174,35 @@ describe("Operations: ProcessFromRoot", () => {
 
     it("should only process a single root message for each domain", async () => {
       processSingleRootMessageStub.resolves(mkBytes32());
-      const rootMessages: RootMessage[] = [
-        { ...mock.entity.rootMessage(), timestamp: 1, spokeDomain: "test1" },
-        { ...mock.entity.rootMessage(), timestamp: 2, spokeDomain: "test1" },
-        { ...mock.entity.rootMessage(), timestamp: 1, spokeDomain: "test2" },
-        { ...mock.entity.rootMessage(), timestamp: 2, spokeDomain: "test2" },
-        { ...mock.entity.rootMessage(), timestamp: 1, spokeDomain: "test3" },
-        { ...mock.entity.rootMessage(), timestamp: 2, spokeDomain: "test3" },
+      const rootMessages: (RootMessage & { isSpokeClaim: boolean })[] = [
+        { ...mock.entity.rootMessage(), timestamp: 1, spokeDomain: "test1", isSpokeClaim: false },
+        { ...mock.entity.rootMessage(), timestamp: 2, spokeDomain: "test1", isSpokeClaim: false },
+        { ...mock.entity.rootMessage(), timestamp: 1, spokeDomain: "test2", isSpokeClaim: false },
+        { ...mock.entity.rootMessage(), timestamp: 2, spokeDomain: "test2", isSpokeClaim: false },
+        { ...mock.entity.rootMessage(), timestamp: 1, spokeDomain: "test3", isSpokeClaim: false },
+        { ...mock.entity.rootMessage(), timestamp: 2, spokeDomain: "test3", isSpokeClaim: false },
       ];
 
       (processFromRootCtxMock.adapters.database.getRootMessages as SinonStub).resolves(rootMessages);
 
       await ProcessFromRootFns.processFromRoot();
 
+      expect(processSingleRootMessageStub.callCount).to.eq(3);
       expect(processSingleRootMessageStub).to.be.calledWith(rootMessages[1]);
       expect(processSingleRootMessageStub).to.be.calledWith(rootMessages[3]);
       expect(processSingleRootMessageStub).to.be.calledWith(rootMessages[5]);
-      expect(processSingleRootMessageStub).to.callCount(3);
+    });
+  });
+
+  describe("#getSpokeMessages", () => {
+    let getLatestXLayerSpokeMessage: SinonStub<[number, number, RequestContext], [Promise<any | undefined>]>;
+    beforeEach(() => {
+      getLatestXLayerSpokeMessage = stub(ProcessFromHelperRootFns, "getLatestXLayerSpokeMessage").resolves([]) as any;
+    });
+
+    it("should work", async () => {
+      const messages = await ProcessFromRootFns.getSpokeMessages(requestContext);
+      expect(messages).to.be.deep.eq([]);
     });
   });
 });
