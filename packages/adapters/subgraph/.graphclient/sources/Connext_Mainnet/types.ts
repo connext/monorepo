8e31432399f804b67cbe1cb527652054fc0fd478
preethamr <preethamr@users.noreply.github.com>
2024-04-26 10:38:28 -0700

    fix: build graph client

--- packages/adapters/subgraph/.graphclient/sources/Connext_Mainnet/types.ts
+++ packages/adapters/subgraph/.graphclient/sources/Connext_Mainnet/types.ts
@@ -20,6 +20,7 @@ export type Scalars = {
   BigInt: any;
   mainnet_Bytes: any;
   mainnet_Int8: any;
+  Timestamp: any;
 };
 
 export type mainnet_AggregateRoot = {
@@ -149,6 +150,10 @@ export type mainnet_AggregateRoot_orderBy =
   | 'id'
   | 'root'
   | 'blockNumber';
+
+export type mainnet_Aggregation_interval =
+  | 'hour'
+  | 'day';
 
 export type mainnet_Asset = {
   id: Scalars['ID'];
@@ -3691,6 +3696,8 @@ export type mainnet__Block_ = {
   number: Scalars['Int'];
   /** Integer representation of the timestamp stored in blocks for the chain */
   timestamp?: Maybe<Scalars['Int']>;
+  /** The hash of the parent block */
+  parentHash?: Maybe<Scalars['mainnet_Bytes']>;
 };
 
 /** The type for the top-level _meta field */
